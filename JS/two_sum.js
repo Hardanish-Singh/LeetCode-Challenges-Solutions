c4d9a731bdfa5a570a90c2f4b6af1f91440ee014
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-04-23 20:41:48 -0400

    Update two_sum.js

--- JS/two_sum.js
+++ JS/two_sum.js
@@ -10,19 +10,36 @@
       Because nums[0] + nums[1] = 2 + 7 = 9,
       return [0, 1].
 */
-var twoSum = function (nums, target) {
-  let twoSumHashMap = {
-    key: Number,
-    value: Number,
-  };
+var twoSum = function(nums, target) {
+    let twoPointerArray = [...nums].sort((a, b) => a-b);
+    let leftPosition = 0, rightPosition = 0, lthPosition = 0, rthPosition = 0;
+    let finalArray = new Array();
 
-  for (let i = 0; i < nums.length; i++) {
-    let difference = target - nums[i];
-    let checkDictionaryIndex = twoSumHashMap[difference];
-    if (checkDictionaryIndex != null || checkDictionaryIndex != undefined) {
-      return [checkDictionaryIndex, i];
-    } else {
-      twoSumHashMap[nums[i]] = i;
+    for (let i = 0; i < twoPointerArray.length; i++) {
+      leftPosition = twoPointerArray[i];
+      rightPosition = twoPointerArray[twoPointerArray.length - 1];
+      lthPosition = i;
+      rthPosition = twoPointerArray.length - 1;
+      if (leftPosition === undefined || rightPosition === undefined) break;
+      if (lthPosition === rthPosition) break;
+
+      while(true) {
+        if (leftPosition + rightPosition == target) {
+           i = twoPointerArray.length;
+           break;
+        } else if (leftPosition + rightPosition < target) {
+            lthPosition++;
+            while (twoPointerArray[lthPosition] == leftPosition) lthPosition++;  
+            leftPosition = twoPointerArray[lthPosition];
+        } else if (leftPosition + rightPosition > target) {
+            rthPosition--;
+            while (twoPointerArray[rthPosition] == rightPosition) rthPosition--;
+            rightPosition = twoPointerArray[rthPosition];
+        }
+        if (lthPosition === rthPosition || lthPosition > rthPosition) break;
+      } 
     }
-  }
+
+    finalArray.push(nums.indexOf(leftPosition), nums.lastIndexOf(rightPosition));
+    return finalArray;
 };
