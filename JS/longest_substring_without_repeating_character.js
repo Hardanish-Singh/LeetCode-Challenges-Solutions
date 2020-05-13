8fb29b155f1bcd74e5a9ab48e0442ea06c3d48ce
Hardanish Singh <hardanishsingh1@gmail.com>
2020-05-13 09:21:45 -0400

    Added Longest Substring Without Repeating Character

--- JS/longest_substring_without_repeating_character.js
+++ JS/longest_substring_without_repeating_character.js
@@ -21,10 +21,11 @@ var lengthOfLongestSubstring = function(string) {
     if(string.length===0) return 0; // EMPTY STRING
     if(new Set(string.split("")).size === 1) return 1; // ALL ELEMENTS ARE SAME
     let longestSubstringLength = 1;
+    let count = 1;
     for(let i=0; i<string.length; i++){
         if(string.length-i===longestSubstringLength) break;
-        for(let j=i+1; j<string.length; j++){
-            if((string[i] + string.substring(i+1, j+1)).length <= longestSubstringLength) continue;
+        for(let j=count; j<string.length; j++){
+            count++;
             if(new Set((string[i] + string.substring(i+1, j+1)).split("")).size !== (string[i] + string.substring(i+1, j+1)).length) break;
             if((string[i] + string.substring(i+1, j+1)).length>longestSubstringLength) longestSubstringLength = (string[i] + string.substring(i+1, j+1)).length
         }
@@ -34,6 +35,6 @@ var lengthOfLongestSubstring = function(string) {
 
 /*
 Efficiency
-    Runtime: 304 ms, faster than 20.77% of JavaScript online submissions for Longest Substring Without Repeating Characters.
-    Memory Usage: 41.9 MB, less than 23.89% of JavaScript online submissions for Longest Substring Without Repeating Characters.
+    Runtime: 256 ms, faster than 27.43% of JavaScript online submissions for Longest Substring Without Repeating Characters.
+    Memory Usage:  41.7 MB, less than 27.43% of JavaScript online submissions for Longest Substring Without Repeating Characters.
 */
