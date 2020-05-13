9c66a7ece9130f617631578e298e93efa829a024
Hardanish Singh <hardanishsingh1@gmail.com>
2020-05-12 21:03:33 -0400

    Increased Longest SubString Without Repeating Character Performance

--- JS/longest_substring_without_repeating_character.js
+++ JS/longest_substring_without_repeating_character.js
@@ -18,16 +18,22 @@
                             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
 var lengthOfLongestSubstring = function(string) {
-    if(string.length===0) return 0;
+    if(string.length===0) return 0; // EMPTY STRING
+    if(new Set(string.split("")).size === 1) return 1; // ALL ELEMENTS ARE SAME
     let longestSubstringLength = 1;
     for(let i=0; i<string.length; i++){
+        if(string.length-i===longestSubstringLength) break;
         for(let j=i+1; j<string.length; j++){
-            if(new Set((string[i] + string.substring(i+1, j+1)).split("")).size != (string[i] + string.substring(i+1, j+1)).split("").length) break;
-            if((string[i] + string.substring(i+1, j+1)).split("").length>longestSubstringLength) {
-                longestSubstringLength = (string[i] + string.substring(i+1, j+1)).split("").length;
-            }
+            if((string[i] + string.substring(i+1, j+1)).length <= longestSubstringLength) continue;
+            if(new Set((string[i] + string.substring(i+1, j+1)).split("")).size !== (string[i] + string.substring(i+1, j+1)).length) break;
+            if((string[i] + string.substring(i+1, j+1)).length>longestSubstringLength) longestSubstringLength = (string[i] + string.substring(i+1, j+1)).length
         }
     }
     return longestSubstringLength;
 };
 
+/*
+Efficiency
+    Runtime: 304 ms, faster than 20.77% of JavaScript online submissions for Longest Substring Without Repeating Characters.
+    Memory Usage: 41.9 MB, less than 23.89% of JavaScript online submissions for Longest Substring Without Repeating Characters.
+*/
