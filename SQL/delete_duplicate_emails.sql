36062e9c4b29b9a3648ff2a37945abc85f2f17a0
Hardanish Singh <hardanishsingh1@gmail.com>
2020-07-11 18:45:15 -0400

    Delete Duplicate Emails

--- SQL/delete_duplicate_emails.sql
+++ SQL/delete_duplicate_emails.sql
@@ -1,3 +1,18 @@
+/*
+    Table: Person
+
+                +-------------+---------+
+                | Column Name | Type    |
+                +-------------+---------+
+                | Id          | int     |
+                | Email       | varchar |
+                +-------------+---------+
+                
+                Id is the primary key column for this table.
+    
+    Write a SQL query to delete all duplicate email entries in a table named Person, keeping only unique emails based on its smallest Id.
+    Note: Your output is the whole Person table after executing your sql. Use delete statement.
+*/
 DELETE FROM PERSON  
 WHERE id IN (
                 SELECT 
@@ -15,7 +30,7 @@ WHERE id IN (
                                             PERSON
                                     GROUP BY email
                                     HAVING COUNT(*) > 1
-                            )
+                                   )
                     AND id NOT IN (
                                     SELECT 
                                             MIN(id)
@@ -23,6 +38,6 @@ WHERE id IN (
                                             PERSON
                                     GROUP BY email
                                     HAVING COUNT(*) > 1
-                            )
+                                  )
                 )AS x
             );
