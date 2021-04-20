df13c54f8053394b7dcfc7ce8aa004ad85d7bf92
Hardanish Singh <hardanishsingh1@gmail.com>
2021-04-20 19:32:05 -0400

    Find All Anagrams In A String

---
+++ JS/find_all_anagrams_in_a_string.js
@@ -0,0 +1,96 @@
+/*
+
+        Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+
+        Example 1:
+
+        Input: s = "cbaebabacd", p = "abc"
+        Output: [0,6]
+        
+        Explanation:
+        The substring with start index = 0 is "cba", which is an anagram of "abc".
+        The substring with start index = 6 is "bac", which is an anagram of "abc".
+        
+        Example 2:
+
+        Input: s = "abab", p = "ab"
+        Output: [0,1,2]
+        
+        Explanation:
+        The substring with start index = 0 is "ab", which is an anagram of "ab".
+        The substring with start index = 1 is "ba", which is an anagram of "ab".
+        The substring with start index = 2 is "ab", which is an anagram of "ab".
+        
+        Constraints:
+
+        1 <= s.length, p.length <= 3 * 104
+        s and p consist of lowercase English letters.
+
+*/
+function isAnagram(s1, {...s2}){
+        for(i in s1) {
+                for(j in s2) {
+                        if(i == j && s1[i] == s2[j]) {
+                                delete s2[j];
+                        }
+                }
+        }
+        return Object.keys(s2).length == 0 ? true : false;
+}
+      
+function countSubStringOccurences(start, words, end) {
+        words[start] = words[start] - 1;
+        if(end in words) words[end] = words[end] + 1;
+        else words[end] = 1;
+        return words;
+}
+      
+function countWordOccurences(word) {
+        
+        let isBackward = false;
+        let words = {};
+        let count = 1;
+
+        for( let i=0; i<word.length; i++){
+                isBackward = false;
+                for( let j=0; j<i; j++ ){
+                        if( word[i] == word[j] ) {
+                                isBackward = true;
+                                break;
+                        }
+                }
+                if( isBackward == true ) continue;
+                for( let k=i+1; k<word.length; k++ ){
+                        if( word[i] == word[k] ) count++;
+                }
+                words[word[i]] = count;
+                count = 1;
+        }
+
+        return words;
+}
+      
+var findAnagrams = function(s, p) {
+
+        let substr = "";
+        let anagramSize = p.length;
+        let anagrams = [];
+
+        let words = countWordOccurences(p);
+        let breakwords = {};
+
+
+        for( let i=0; i<s.length; i++ ){
+
+                substr = s.slice(i,anagramSize+i);
+                if( substr.length != p.length || substr.length < p.length) break;
+                else {
+                        if( i==0 ) breakwords = countWordOccurences(substr);
+                        else breakwords = countSubStringOccurences(s[i-1], breakwords, substr[substr.length-1]);
+                        if( isAnagram(breakwords, words) ) anagrams.push(i);
+                }  
+        }
+
+        return anagrams;
+
+};
