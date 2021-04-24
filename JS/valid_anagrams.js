fc581a83582794fe7e73c3b5d56d102d74cb5468
Hardanish Singh <hardanishsingh1@gmail.com>
2021-04-24 11:32:15 -0400

    Valid Anagrams

---
+++ JS/valid_anagrams.js
@@ -0,0 +1,64 @@
+/*
+        Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+        Example 1:
+                Input: s = "anagram", t = "nagaram"
+                Output: true
+        Example 2:
+                Input: s = "rat", t = "car"
+                Output: false
+        
+        Constraints:
+
+                1) 1 <= s.length, t.length <= 5 * 104
+                2) s and t consist of lowercase English letters.
+*/
+
+/*
+        This function checks if two dictionaries are Anagrams or not
+*/
+function Check_Anagram(word1, word2) {
+        for(key1 in word1) {
+                let isFound = false;
+                for(key2 in word2) {
+                        if(key1 == key2 && word1[key1] == word2[key2]) {
+                                delete word2[key2];
+                                isFound = true;
+                                break;
+                        }
+                }
+                if(isFound == false) return false;
+        }
+        return true;
+}
+/*
+        This function counts character occurrences in a word, and returns dictionary
+*/ 
+function countCharacterOccurrences(word) {
+        
+        let words = {};
+        let count = 1;
+
+        for( let i=0; i<word.length; i++) {
+                if(word[i] in words) continue;
+                for( let j=i+1; j<word.length; j++ ) {
+                        if( word[i] == word[j] ) {
+                                count++;
+                        }
+                }
+                words[word[i]] = count;
+                count = 1;
+        }
+
+        return words;
+}
+
+var isAnagram = function(s, t) {
+    
+    if( s.length != t.length ) return false;
+    
+    let word1 = countCharacterOccurrences(s);
+    let word2 = countCharacterOccurrences(t);
+    
+    return Check_Anagram(word1, word2) ? true : false;
+};
