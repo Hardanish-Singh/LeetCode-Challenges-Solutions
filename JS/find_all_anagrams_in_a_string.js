0497e35dd813f5d2e9072a614910fca453f7ad78
Hardanish Singh <hardanishsingh1@gmail.com>
2021-04-26 06:06:42 -0400

    Find All Anagrams In A String

--- JS/find_all_anagrams_in_a_string.js
+++ JS/find_all_anagrams_in_a_string.js
@@ -1,83 +1,88 @@
-/*
-
-        Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
-
-        Example 1:
-                Input: s = "cbaebabacd", p = "abc"
-                Output: [0,6]
-        
-        Explanation:
-                The substring with start index = 0 is "cba", which is an anagram of "abc".
-                The substring with start index = 6 is "bac", which is an anagram of "abc".
-        
-        Example 2:
-                Input: s = "abab", p = "ab"
-                Output: [0,1,2]
-        
-        Explanation:
-                The substring with start index = 0 is "ab", which is an anagram of "ab".
-                The substring with start index = 1 is "ba", which is an anagram of "ab".
-                The substring with start index = 2 is "ab", which is an anagram of "ab".
-        
-        Constraints:
-                1) 1 <= s.length, p.length <= 3 * 104
-                2) s and p consist of lowercase English letters.
-        
-        Companies Where this Question Was Asked Before:
-                1) Amazon
-                2) Facebook
-                3) Microsoft
-                4) Bloomberg
-                5) Snapchat
-                6) Apple
-        
-        Difficulty Level: 
-                Medium
-
-*/
+/****************************************************************
+ *       LEETCODE FIND ALL ANAGRAMS IN A STRING CHALLENGE       *
+ *                                                              *
+ * Problem Statement                                            *
+ * Given two strings s and p, return an array of all the start  *
+ * indices of p's anagrams in s. You may return the answer in   *
+ * any order.                                                   *
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: s = "cbaebabacd", p = "abc"                         *
+ * Output 1: [0,6]                                              *
+ *                                                              *
+ * Input 2: s = "abab", p = "ab"                                *
+ * Output 2: [0,1,2]                                            *
+ *                                                              *
+ * Constraints:                                                 *
+ * 1 <= s.length, p.length <= 3 * 104                           *
+ * s and p consist of lowercase English letters.                *
+ *                                                              *
+ * Companies Where this Question Was Asked Before:              *
+ * Amazon                                                       *
+ * Facebook                                                     *
+ * Microsoft                                                    *
+ * Bloomberg                                                    *
+ * Snapchat                                                     *
+ * Apple                                                        *
+ *                                                              *
+ * Difficulty Level:                                            *
+ * Medium                                                       *
+ *                                                              *
+ ***************************************************************/
 
 /*
         This function checks if two dictionaries are Anagrams or not
 */
-function isAnagram(word1, {...word2}) {
-        for(key1 in word1) {
+function isAnagram( word1, {...word2} ) {
+
+        for( key1 in word1 ) {
                 let isFound = false;
-                for(key2 in word2) {
-                        if(key1 == key2 && word1[key1] == word2[key2]) {
+                for( key2 in word2 ) {
+                        if( key1 == key2 && word1[key1] == word2[key2] ) {
                                 delete word2[key2];
                                 isFound = true;
                                 break;
                         }
                 }
-                if(isFound == false) return false;
+                if( isFound == false ) {
+                        return false;
+                }
         }
+
         return true;
+
 }
 
-function adjustSubStringCounts(start, words, end) {
+function adjustSubStringCounts( start, words, end ) {
+        
         words[start] = words[start] - 1;
-        if(words[start] == 0) {
+        if( words[start] == 0 ) {
                 delete words[start];
         }
-        if(end in words) {
+        
+        if( end in words ) {
                 words[end] = words[end] + 1;
         }
-        else{
+        else {
                 words[end] = 1;
         }
+        
         return words;
+
 }
       
 /*
         This function counts character occurrences in a word, and returns dictionary
 */ 
-function countCharacterOccurrences(word) {
+function countCharacterOccurrences( word ) {
         
         let words = {};
         let count = 1;
 
         for( let i=0; i<word.length; i++) {
-                if(word[i] in words) continue;
+                if(word[i] in words) {
+                        continue;
+                }
                 for( let j=i+1; j<word.length; j++ ) {
                         if( word[i] == word[j] ) {
                                 count++;
@@ -96,7 +101,7 @@ var findAnagrams = function(s, p) {
         let anagramSize = p.length;
         let anagrams = [];
 
-        let words = countCharacterOccurrences(p);
+        let words = countCharacterOccurrences( p );
         let breakwords = {};
 
 
@@ -108,12 +113,12 @@ var findAnagrams = function(s, p) {
                 }
                 else {
                         if( i==0 ) {
-                                breakwords = countCharacterOccurrences(substr);
+                                breakwords = countCharacterOccurrences( substr );
                         }
                         else {
-                                breakwords = adjustSubStringCounts(s[i-1], breakwords, substr[substr.length-1]);
+                                breakwords = adjustSubStringCounts( s[i-1], breakwords, substr[substr.length-1] );
                         }
-                        if( isAnagram(breakwords, words) ) {
+                        if( isAnagram( breakwords, words ) ) {
                                 anagrams.push(i);
                         }
                 }  
