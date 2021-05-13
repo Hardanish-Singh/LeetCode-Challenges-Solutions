a78929a2f7267e685519e0c99ed74d8226528e8d
Hardanish Singh <hardanishsingh1@gmail.com>
2021-05-13 16:28:34 -0400

    Friend Request II Who Has The Most Friends

---
+++ SQL/friend_requests_II_who_has_the_most_friends.sql
@@ -0,0 +1,56 @@
+/*
+SQL Schema
+In social network like Facebook or Twitter, people send friend requests and accept others' requests as well.
+
+ 
+
+Table request_accepted
+
++--------------+-------------+------------+
+| requester_id | accepter_id | accept_date|
+|--------------|-------------|------------|
+| 1            | 2           | 2016_06-03 |
+| 1            | 3           | 2016-06-08 |
+| 2            | 3           | 2016-06-08 |
+| 3            | 4           | 2016-06-09 |
++--------------+-------------+------------+
+This table holds the data of friend acceptance, while requester_id and accepter_id both are the id of a person.
+ 
+
+Write a query to find the the people who has most friends and the most friends number under the following rules:
+
+It is guaranteed there is only 1 people having the most friends.
+The friend request could only been accepted once, which mean there is no multiple records with the same requester_id and accepter_id value.
+For the sample data above, the result is:
+
+Result table:
++------+------+
+| id   | num  |
+|------|------|
+| 3    | 3    |
++------+------+
+The person with id '3' is a friend of people '1', '2' and '4', so he has 3 friends in total, which is the most number than any others.
+*/
+select 
+        *
+from
+(
+    select 
+        id, 
+        count(id) as num
+    from
+    (
+        select
+            requester_id as id
+        from
+            request_accepted
+        union all
+        select
+            accepter_id as id
+        from
+            request_accepted
+    )as x
+    group by x.id
+) as xx
+ORDER BY num DESC
+LIMIT 1
