22f822e49dc1dc8e77c139dcc1a27d143acd1f17
Hardanish Singh <hardanishsingh1@gmail.com>
2021-05-14 20:31:07 -0400

    Move Zeros

---
+++ JS/move_zeros.js
@@ -0,0 +1,24 @@
+
+var moveZeroes = function(nums) {
+    
+        let count = 0;
+
+        for( let i=0; i<nums.length; i++ ){
+                if( nums[i] === 0 ) {
+                        count++;
+                }
+        }
+
+        for( let i=0; i<nums.length; i++ ){
+                if( nums[i] == 0 ) {
+                        nums.splice(i, 1);
+                        i--;
+                } 
+        }
+
+        for( let i=0; i<count; i++ ){
+                nums.push(0);
+        }   
+
+        return nums;
+};
