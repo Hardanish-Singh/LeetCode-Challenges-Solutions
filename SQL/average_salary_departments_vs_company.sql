b5fc628f0557f007b9e7908a1a511facb0b96d7b
Hardanish Singh <hardanishsingh1@gmail.com>
2021-05-31 12:19:20 -0400

    SELECT

---
+++ SQL/average_salary_departments_vs_company.sql
@@ -0,0 +1,28 @@
+SELECT 
+    DATE_FORMAT(pay_date,'%Y-%m') AS pay_month,
+    department_id,
+    CASE 
+        WHEN pay_date_dept_avg > pay_date_avg
+            THEN 
+                'higher'
+        WHEN 
+            pay_date_dept_avg < pay_date_avg
+            THEN
+                'lower'
+        ELSE
+            'same'
+    END AS comparison 
+FROM 
+(
+    SELECT 
+        pay_date,
+        department_id,
+        AVG(amount) OVER(PARTITION BY pay_date) AS pay_date_avg,
+        AVG(amount) OVER(PARTITION BY pay_date, department_id) AS pay_date_dept_avg
+    FROM 
+        salary
+    JOIN employee ON employee.employee_id = salary.employee_id
+    ORDER BY pay_date DESC
+) AS x
+GROUP BY DATE_FORMAT(pay_date,'%Y-%m'),
+department_id
