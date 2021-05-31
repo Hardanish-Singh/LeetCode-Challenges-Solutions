270fcd8c2f051403ad3b1fd6a23ac31422038cf0
Hardanish Singh <hardanishsingh1@gmail.com>
2021-05-31 08:31:03 -0400

    Triangle Judgement

---
+++ SQL/triangle_judgement.sql
@@ -0,0 +1,12 @@
+SELECT
+        *,
+        CASE 
+                WHEN
+                        x + y > z AND x + z > y AND y + z > x
+                                THEN 
+                                        'Yes'
+                ELSE
+                        'No'
+        END AS triangle
+FROM
+    triangle;
