94cedcf55ad93da4a54cd3eafe4a8d1bca810740
Hardanish Singh <hardanishsingh1@gmail.com>
2021-06-01 11:27:11 -0400

    Students Report By Geography

--- SQL/students_report_by_geography.sql
+++ SQL/students_report_by_geography.sql
@@ -1,3 +1,6 @@
+/*
+        SOLUTION 1 USING WINDOW FUNCTION
+*/
 SELECT 
     MAX(america) AS America,
     MAX(asia) AS Asia,
@@ -21,4 +24,32 @@ from
     FROM
         student
 )AS SubQuery
-GROUP BY rownumber
+GROUP BY rownumber;
+
+/*
+        SOLUTION 2 WITHOUT USING WINDOW FUNCTION
+*/
+SELECT
+    MAX(America) AS America,
+    MAX(Asia) AS Asia,
+    MAX(Europe) AS Europe
+FROM
+(
+    SELECT
+        CASE WHEN continent = 'America' THEN name END AS 'America',
+        CASE WHEN continent = 'Asia' THEN name END AS 'Asia',
+        CASE WHEN continent = 'Europe' THEN name END AS 'Europe',
+        CASE
+            WHEN @rowcontinent IS NULL OR @rowcontinent = continent
+                THEN @rownumber := @rownumber + 1
+            WHEN @rowcontinent != continent
+                THEN @rownumber := 1
+        END AS groupping,
+        @rowcontinent := continent
+    FROM
+        student,
+        (SELECT @rownumber := 0) AS rownumber,
+        (SELECT @rowcontinent := NULL) AS rowcontinent
+    ORDER BY continent, name
+) AS SubQuery
+GROUP BY SubQuery.groupping;
