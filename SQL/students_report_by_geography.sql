b90c53a1f8f3528f4cd960b932cc56037bb0623b
Hardanish Singh <hardanishsingh1@gmail.com>
2021-06-01 11:30:35 -0400

    Students Report By Geography

--- SQL/students_report_by_geography.sql
+++ SQL/students_report_by_geography.sql
@@ -1,4 +1,27 @@
 /*
+        SQL Schema
+                A U.S graduate school has students from Asia, Europe and America. The students' location information are stored in table student as below.
+
+                | name   | continent |
+                |--------|-----------|
+                | Jack   | America   |
+                | Pascal | Europe    |
+                | Xi     | Asia      |
+                | Jane   | America   |
+                
+
+                Pivot the continent column in this table so that each name is sorted alphabetically and displayed underneath its corresponding continent. 
+                The output headers should be America, Asia and Europe respectively. It is guaranteed that the student number from America is no less than either Asia or Europe.
+                
+                For the sample input, the output is:
+
+                | America | Asia | Europe |
+                |---------|------|--------|
+                | Jack    | Xi   | Pascal |
+                | Jane    |      |        |
+*/
+
+/*
         SOLUTION 1 USING WINDOW FUNCTION
 */
 SELECT 
