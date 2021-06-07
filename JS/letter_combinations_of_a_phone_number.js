fc7c19e89cfef4a6ede190fc6496db0554f2c415
Hardanish Singh <hardanishsingh1@gmail.com>
2021-06-07 12:23:41 -0400

    Letter Combinations of a Phone Number

--- JS/letter_combinations_of_a_phone_number.js
+++ JS/letter_combinations_of_a_phone_number.js
@@ -1,18 +1,18 @@
 const phoneNumberLetterCombinations = {
-     '2' : ['a', 'b', 'c'],
-     '3' : ['d', 'e', 'f'],
-     '4' : ['g', 'h', 'i'],
-     '5' : ['j', 'k', 'l'],
-     '6' : ['m', 'n', 'o'],
-     '7' : ['p', 'q', 'r', 's'],
-     '8' : ['t', 'u', 'v'],
-     '9' : ['w', 'x', 'y', 'z']
+        '2' : 'abc',
+        '3' : 'def',
+        '4' : 'ghi',
+        '5' : 'jkl',
+        '6' : 'mno',
+        '7' : 'pqrs',
+        '8' : 'tuv',
+        '9' : 'wxyz'
 };
-
+   
 /**
  * @param { string } digits
  * @return { string[] }
-*/
+ */
 
 var letterCombinations = function( digits ) {
         if( digits.length < 1 ) {
@@ -26,11 +26,11 @@ var letterCombinations = function( digits ) {
                 return letterCombinationsOfPhoneNumber;
         }
 };
-
+   
 function makeLetterCombinations( value1, value2 ) {
         let temp = [];
         if(value1.length === 0) {
-                return value2;
+                return value2.split('');
         }
         else {
                 for( let i=0; i<value1.length; i++ ) {
