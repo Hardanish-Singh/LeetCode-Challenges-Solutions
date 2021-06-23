19977670e046bf6a8f0861ac53b0c4f7372dc80d
Hardanish Singh <hardanishsingh1@gmail.com>
2021-06-23 07:59:32 -0400

    Add Two Numbers

--- Java/add_two_numbers.java
+++ Java/add_two_numbers.java
@@ -1,12 +1,28 @@
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+        // Definition for Singly-Linked List
+
+        public class ListNode {
+                
+                int val;
+                ListNode next;
+
+                public ListNode() 
+                { 
+
+                }
+
+                public ListNode( int val )
+                {
+                        this.val = val;
+                }
+                
+                public ListNode( int val, ListNode next )
+                {
+                        this.val = val;
+                        this.next = next;
+                }
+
+        }
 */
 public class Solution {
         
