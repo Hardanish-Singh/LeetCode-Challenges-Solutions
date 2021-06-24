189da7ae314916dcc62c995fe21f3f4e48f0eb99
Hardanish Singh <hardanishsingh1@gmail.com>
2021-06-24 07:15:48 -0400

    Add Two Numbers

--- Java/add_two_numbers.java
+++ Java/add_two_numbers.java
@@ -1,13 +1,13 @@
-/**
+/*
         // Definition for Singly-Linked List
+        public class ListNode
+	{
 
-        public class ListNode {
-                
                 int val;
                 ListNode next;
 
-                public ListNode() 
-                { 
+                public ListNode()
+                {
 
                 }
 
@@ -15,7 +15,7 @@
                 {
                         this.val = val;
                 }
-                
+
                 public ListNode( int val, ListNode next )
                 {
                         this.val = val;
@@ -24,10 +24,10 @@
 
         }
 */
-public class Solution 
+public class Solution
 {
-        
-        public ListNode addTwoNumbers( ListNode l1, ListNode l2 ) 
+
+        public ListNode addTwoNumbers( ListNode l1, ListNode l2 )
         {
                 
                 ListNode result = null;
@@ -36,18 +36,18 @@ public class Solution
                 short vals2 = 0;
                 short carry = 0;
                 short remainder = 0;
-            
-                while( l1 != null || l2 != null ) 
+
+                while( l1 != null || l2 != null )
                 {
                         vals1 = (short) ( ( l1 == null ) ? 0 : l1.val );
                         vals2 = (short) ( ( l2 == null ) ? 0 : l2.val );
-                        if( vals1 + vals2 + carry > 9 ) 
+                        if( vals1 + vals2 + carry > 9 )
                         {
                                 remainder = (short) ( ( vals1 + vals2 + carry ) % 10 );
                                 result = new ListNode( remainder, result );
                                 carry = (short) ( ( vals1 + vals2 + carry ) / 10 );
                         }
-                        else 
+                        else
                         {
                                 result = new ListNode( vals1 + vals2 + carry, result );
                                 carry = 0;
@@ -55,16 +55,16 @@ public class Solution
                         l1 = ( l1 != null ) ? l1.next : null;
                         l2 = ( l2 != null ) ? l2.next : null;
                 }
-                
+
                 result = ( carry > 0 ) ? new ListNode( carry, result ) :  result ;
-                
-                while( result != null ) 
+
+                while( result != null )
                 {
                         reversedList = new ListNode( result.val, reversedList );
                         result = result.next;
                 }
-                
+
                 return reversedList;
-            
+
         }
 }
