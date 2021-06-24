352342d0b3950df24c8ab776f1d09b7f99b6f837
Bibek Saini <itsbibeksaini@gmail.com>
2021-06-24 09:25:17 -0700

    removing spaces

--- Java/Linked_List/add_two_numbers.java
+++ Java/Linked_List/add_two_numbers.java
@@ -16,8 +16,7 @@
                         this.val = val;
                 }
 
-                public ListNode( int val, ListNode next )
-                {
+                public ListNode( int val, ListNode next ){
                         this.val = val;
                         this.next = next;
                 }
