c91159121791efdf281bdfc7e16726820f714dca
Hardanish Singh <hardanishsingh1@gmail.com>
2021-07-04 09:35:14 -0400

    Reverse Linked List

--- Java/Linked_List/Reverse_Linked_List.java
+++ Java/Linked_List/Reverse_Linked_List.java
@@ -1,3 +1,26 @@
+/****************************************************************
+ *             LEETCODE REVERSE LINKED LIST CHALLENGE           *
+ *                                                              *
+ * Problem Statement                                            *
+ * Given the head of a singly linked list, reverse the list,	*
+ * and return the reversed list.      				*
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: head = [1,2,3,4,5]                                  *
+ * Output 1: [5,4,3,2,1]                                        *
+ *                                                              *
+ * Input 2: head = [1,2]                                        *
+ * Output 2: [2,1]                                              *
+ *								*
+ * Input 3: head = []	                                        *
+ * Output 3: []        	                                        *
+ *                                                              *
+ * Constraints							*
+ * 1) The number of nodes in the list is the range [0, 5000].	*
+ * 2) -5000 <= Node.val <= 5000					*
+ *								*
+ ***************************************************************/
+
 /*
         // Definition for Singly-Linked List
         public class ListNode
