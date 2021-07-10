813ae06f3c28450d78693e49c6cb7a71bab62c73
Hardanish Singh <hardanishsingh1@gmail.com>
2021-07-10 12:59:47 -0400

    Valid Parenthesis

--- JS/valid_parenthesis.js
+++ JS/valid_parenthesis.js
@@ -10,32 +10,75 @@
     Example 1:
         Input: "()"
         Output: true
-    
+
     Example 2:
         Input: "()[]{}"
         Output: true
-    
+
     Example 3:
         Input: "(]"
         Output: false
-    
+
     Example 4:
         Input: "([)]"
         Output: false
-    
+
     Example 5:
         Input: "{[]}"
         Output: true
 */
-var isValid = function(string) {
-    let splittedString = string.split("");
-    for(let i=0; i<splittedString.length; i++){
-        if(i>=splittedString.length/2) return false;
-        if(splittedString[i] === '[' && splittedString[i+1] === ']' || splittedString[i] === '{' && splittedString[i+1] === '}' || splittedString[i] === '(' && splittedString[i+1] === ')') {
-            splittedString.splice(i, 2);
-             i = -1;
+/**
+ * @param { string } s
+ * @return { boolean }
+ */
+var isValid = function( s )
+{
+
+        let openingBrackets = [];
+
+        for( let i=0; i<s.length; i++ )
+        {
+                if( s[i] === '(' || s[i] === '{' || s[i] === '[' )
+                {
+                        openingBrackets.push( s[i] );
+                }
+                else
+                {
+                        if( s[i] === ')' )
+                        {
+                                if( !(openingBrackets[openingBrackets.length-1] == '(') )
+                                {
+                                      return false;
+                                }
+                                else
+                                {
+                                    openingBrackets.pop();
+                                }
+                        }
+                        else if( s[i] === '}' )
+                        {
+                                if( !(openingBrackets[openingBrackets.length-1] == '{') )
+                                {
+                                      return false;
+                                }
+                                else
+                                {
+                                    openingBrackets.pop();
+                                }
+                        }
+                        else if( s[i] === ']' )
+                        {
+                                if( !(openingBrackets[openingBrackets.length-1] == '[') )
+                                {
+                                      return false;
+                                }
+                                else
+                                {
+                                    openingBrackets.pop();
+                                }
+                        }
+                }
         }
-    }
-    if(splittedString.length === 0) return true;
-    else return false;
+
+        return openingBrackets.length > 0 ? false : true;
 };
