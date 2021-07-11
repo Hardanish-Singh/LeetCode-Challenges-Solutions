adceae89e018b8c8ca8010fa9e41a4f24d319890
Hardanish Singh <hardanishsingh1@gmail.com>
2021-07-11 16:06:53 -0400

    Two Sum

--- JS/Two_Sum_II_Input_Array_Is_Sorted_Easy.js
+++ JS/Two_Sum_II_Input_Array_Is_Sorted_Easy.js
@@ -1,3 +1,10 @@
+/*
+	Given an array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+
+	Return the indices of the two numbers (1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1] <= numbers.length.
+
+	The tests are generated such that there is exactly one solution. You may not use the same element twice.
+*/
 /**
  * @param {number[]} nums
  * @param {number} target
