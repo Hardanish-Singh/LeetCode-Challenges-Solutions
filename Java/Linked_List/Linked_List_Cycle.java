111c4bf01d02f1bdf538a1d4f082c8184a8fcf2d
Hardanish Singh <hardanishsingh1@gmail.com>
2021-07-23 07:41:07 -0400

    Linked List Cycle

--- Java/Linked_List/Linked_List_Cycle.java
+++ Java/Linked_List/Linked_List_Cycle.java
@@ -1,19 +1,39 @@
+/*
+        // Definition for Singly-Linked List
+        public class ListNode
+	{
+                int val;
+                ListNode next;
+                public ListNode()
+                {
+                }
+                public ListNode( int val )
+                {
+                        this.val = val;
+                }
+                public ListNode( int val, ListNode next )
+                {
+                        this.val = val;
+                        this.next = next;
+                }
+        }
+*/
 public class Solution
 {
-    public boolean hasCycle(ListNode head)
-    {
-        while( head != null )
-        {
-                if( head.val == -100002 )
-                {
-                        return true;
-                }
-                else
-                {
-                        head.val = -100002;
-                }
-                head = head.next;
-        }
-        return false;
-    }
+	public boolean hasCycle(ListNode head)
+	{
+		while( head != null )
+		{
+			if( head.val == -100002 )
+			{
+				return true;
+			}
+			else
+			{
+			        head.val = -100002;
+			}
+			head = head.next;
+		}
+		return false;
+	}
 }
