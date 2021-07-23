32b691206ed45da7bc79a7d556bd0f626a899785
Hardanish Singh <hardanishsingh1@gmail.com>
2021-07-23 07:56:02 -0400

    Linked List Cycle

--- Java/Linked_List/Linked_List_Cycle.java
+++ Java/Linked_List/Linked_List_Cycle.java
@@ -10,10 +10,28 @@
 			3 ---> 2 ---> 0 ---> 4
 			       ↑	     ↓
 			       ↑ ← ← ← ← ← ← ↓
-       Input: head = [3,2,0,-4], pos = 1
-       Output: true
-       Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+	Input: head = [3,2,0,-4], pos = 1
+        Output: true
+        Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+	Example 2:
+			1 ---> 2
+			↑      ↓
+			↑ ←  ← ↓
+
+	Input: head = [1,2], pos = 0
+	Output: true
+	Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+	Example 3:
+			1
+
+	Input: head = [1], pos = -1
+	Output: false
+	Explanation: There is no cycle in the linked list.
 */
+
 /*
         // Definition for Singly-Linked List
         public class ListNode
