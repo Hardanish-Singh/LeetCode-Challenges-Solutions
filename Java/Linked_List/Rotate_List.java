9cc8965a97e595beb5ab45bf4f2af3de57b3b803
Hardanish Singh <hardanishsingh1@gmail.com>
2021-08-04 08:40:53 -0400

    Rotate List

--- Java/Linked_List/Rotate_List.java
+++ Java/Linked_List/Rotate_List.java
@@ -23,11 +23,13 @@ class Solution
 {
         public ListNode rotateRight( ListNode head, int k ) 
         {
+                // VALIDATION/SANITY CHECK
                 if( head == null )
                 {
                         return null;
                 }
                 
+                // STORE THE REFERENCE OF HEAD NODE
                 ListNode h = head;
                 int count = 1;
                 while( head.next != null )
@@ -35,8 +37,11 @@ class Solution
                         count++;
                         head = head.next;
                 }
-                k = k%count;
+                // UPDATE k
+                k = k % count;
+                // RESTORE HEAD NODE REFERENCE BACK
                 head = h;
+
                 for( int i=0; i<k; i++ ) 
                 {
                         ListNode current = head;
