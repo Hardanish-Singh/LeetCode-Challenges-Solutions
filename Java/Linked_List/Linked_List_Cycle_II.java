0a4633e9b63dcad8228ef2278a52e6efdefcb0ec
Hardanish Singh <hardanishsingh1@gmail.com>
2021-08-06 09:28:58 -0400

    /**

---
+++ Java/Linked_List/Linked_List_Cycle_II.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+            while( head != null )
+		{
+			if( head.val == -100002 )
+			{
+				return head;
+			}
+			else
+			{
+			        head.val = -100002;
+			}
+			head = head.next;
+		}
+		return null;
+    }
+}
