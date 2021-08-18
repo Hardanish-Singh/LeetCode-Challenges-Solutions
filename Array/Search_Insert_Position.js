ac6694cb9c2b8131645cf8fb3d238b807bcb22e4
Hardanish Singh <hardanishsingh1@gmail.com>
2021-08-18 12:24:29 -0400

    Search Insert Position

--- Array/Search_Insert_Position.js
+++ Array/Search_Insert_Position.js
@@ -35,7 +35,7 @@ var searchInsert = function( nums, target ) {
         if( target > nums[nums.length-1]  ) {
                 return nums.length;
         }
-        for(let i=0; i<nums.length; i++ ) {
+        for( let i=0; i<nums.length; i++ ) {
                 if( target === nums[i] ) {
                         return i;
                 }
