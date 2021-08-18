139dc78697f1ecfa9ed442eb032b319b1804937b
Hardanish Singh <hardanishsingh1@gmail.com>
2021-08-18 12:23:44 -0400

    Maximum SubArray

--- Array/Search_Insert_Position.js
+++ Array/Search_Insert_Position.js
@@ -2,8 +2,6 @@
         Given a sorted array of distinct integers and a target value, return the index if the target is found. 
         If not, return the index where it would be if it were inserted in order.
         
-        You must write an algorithm with O(log n) runtime complexity.
-        
         Example 1:
                 Input: nums = [1,3,5,6], target = 5
                 Output: 2
@@ -21,10 +19,10 @@
                 Output: 0
 
         Constraints:
-                1) 1 <= nums.length <= 104
-                2) -104 <= nums[i] <= 104
+                1) 1 <= nums.length <= 10^4
+                2) -10^4 <= nums[i] <= 10^4
                 3) nums contains distinct values sorted in ascending order.
-                4) -104 <= target <= 104
+                4) -10^4 <= target <= 10^4
 */
 
 /*
