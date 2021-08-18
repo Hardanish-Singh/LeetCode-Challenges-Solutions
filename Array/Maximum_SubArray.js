9c415a50edae146d988050deaf77e4634b421888
Hardanish Singh <hardanishsingh1@gmail.com>
2021-08-18 10:28:53 -0400

    Maximum SubArray

--- Array/Maximum_SubArray.js
+++ Array/Maximum_SubArray.js
@@ -28,3 +28,22 @@ var maxSubArray = function( nums ) {
         }
         return sum;
 };
+
+/*
+        SOLUTION 2: DYNAMIC PROGRAMMING, KADANE'S ALGORITHM
+                        Time Complexity: O(N), where N is the length of nums.
+                        Space Complexity: O(1)
+*/
+
+/*
+ * @param { number[] } nums
+ * @return { number }
+*/
+var maxSubArray = function( nums ) {
+        let max = nums[0];
+        for( let i=1; i<nums.length; i++ ){
+                nums[i] = Math.max( nums[i], nums[i] + nums[i-1] );
+                max = Math.max( max, nums[i] );
+        } 
+        return max;
+};
