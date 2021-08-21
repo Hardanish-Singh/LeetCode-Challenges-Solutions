6e98a80c121e128f5bc0c96784dfa210435dabb5
Hardanish Singh <hardanishsingh1@gmail.com>
2021-08-21 09:23:05 -0400

    Maximum Absolute Sum Of Any SubArray

--- Array/Medium/Maximum_Absolute_Sum_of_Any_Subarray.js
+++ Array/Medium/Maximum_Absolute_Sum_of_Any_Subarray.js
@@ -1,17 +1,15 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
- var maxAbsoluteSum = function(nums) {
+/*
+ * @param { number[] } nums
+ * @return { number }
+*/
+var maxAbsoluteSum = function(nums) {
         let max = nums[0];
         let maxSubarray = nums[0];
-        for( let i=1; i<nums.length; i++ ) {
-                maxSubarray = Math.max( nums[i], nums[i] + maxSubarray );
-                max = Math.max( max, maxSubarray );
-        } 
         let min = nums[0];
         let minSubarray = nums[0];
         for( let i=1; i<nums.length; i++ ) {
+                maxSubarray = Math.max( nums[i], nums[i] + maxSubarray );
+                max = Math.max( max, maxSubarray );
                 minSubarray = Math.min( nums[i], nums[i] + minSubarray );
                 min = Math.min( min, minSubarray );
         }
