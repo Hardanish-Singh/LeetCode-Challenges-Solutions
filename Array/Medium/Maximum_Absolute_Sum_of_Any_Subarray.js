558715402baadf9ea684bf4e739d986c5d868137
Hardanish Singh <hardanishsingh1@gmail.com>
2021-08-21 20:50:13 -0400

    Maximum Absolute Sum Of Any SubArray

--- Array/Medium/Maximum_Absolute_Sum_of_Any_Subarray.js
+++ Array/Medium/Maximum_Absolute_Sum_of_Any_Subarray.js
@@ -1,4 +1,27 @@
 /*
+        You are given an integer array nums.
+        Return the maximum absolute sum of any (possibly empty) subarray of nums.
+        Note that abs(x) is defined as follows:
+                If x is a negative integer, then abs(x) = -x.
+                If x is a non-negative integer, then abs(x) = x.
+
+        Example 1:
+                Input: nums = [1,-3,2,3,-4]
+                Output: 5
+                Explanation: The subarray [2,3] has absolute sum = abs(2+3) = abs(5) = 5.
+
+        Example 2:
+                Input: nums = [2,-5,1,-4,3,-2]
+                Output: 8
+                Explanation: The subarray [-5,1,-4] has absolute sum = abs(-5+1-4) = abs(-8) = 8.
+ 
+        Constraints:
+
+                1) 1 <= nums.length <= 10^5
+                2) -10^4 <= nums[i] <= 10^4
+*/
+
+/*
  * @param { number[] } nums
  * @return { number }
 */
