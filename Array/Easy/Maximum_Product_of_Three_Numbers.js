81fb11e0085942141273ebcee9a370c9519872d7
Hardanish Singh <hardanishsingh1@gmail.com>
2021-08-22 09:48:00 -0400

    Maximum Product of Three Numbers

--- Array/Easy/Maximum_Product_of_Three_Numbers.js
+++ Array/Easy/Maximum_Product_of_Three_Numbers.js
@@ -4,5 +4,13 @@
 */
 var maximumProduct = function(nums) {
         nums.sort( (a, b) => a - b );
-        return Math.max( (nums[0]*nums[1]*nums[nums.length-1]) , (nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3]));
+        /*
+                It could also be possible that two negative numbers lying at the left extreme end could also contribute to lead to a larger product if the third number in the triplet being considered is the largest positive number in the nums array.
+        */
+        let firstHalf = nums[0] * nums[1] * nums[nums.length-1];
+        /*
+                Another solution could be to sort the given numsnums array(in ascending order) and find out the product of the last three numbers.
+        */
+        let lastHalf = nums[nums.length-1] * nums[nums.length-2] * nums[nums.length-3];
+        return Math.max( firstHalf , lastHalf );
 };
