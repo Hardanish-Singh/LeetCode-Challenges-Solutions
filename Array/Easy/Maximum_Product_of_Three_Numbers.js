e9302bff16724590d0fae0cb3a1d4a198740af90
Hardanish Singh <hardanishsingh1@gmail.com>
2021-08-22 19:06:44 -0400

    Maximum Product of Three Numbers

--- Array/Easy/Maximum_Product_of_Three_Numbers.js
+++ Array/Easy/Maximum_Product_of_Three_Numbers.js
@@ -1,7 +1,26 @@
 /*
+        Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+        Example 1:
+                Input: nums = [1,2,3]
+                Output: 6
+        Example 2:
+                Input: nums = [1,2,3,4]
+                Output: 24
+        Example 3:
+                Input: nums = [-1,-2,-3]
+                Output: -6
+
+        Constraints:
+                1) 3 <= nums.length <= 10^4
+                2) -1000 <= nums[i] <= 1000
+*/
+
+/*
  * @param { number[] } nums
  * @return { number }
 */
+
 var maximumProduct = function(nums) {
         nums.sort( (a, b) => a - b );
         /*
