d0d3f453c58a5d664936a62914b74732353a9abe
Hardanish Singh <hardanishsingh1@gmail.com>
2021-08-27 13:41:04 -0400

    Add Strings

--- String/Easy/Add_Strings.js
+++ String/Easy/Add_Strings.js
@@ -9,20 +9,18 @@ var addStrings = function( num1, num2 ) {
                 return addStrings( num2, num1 );
         }
         let count = num2.length;
-        while( count++ != num1.length ) {
+        while( count++ !== num1.length ) {
                 num2 = "0" + num2;
         }
-        let carry = 0;
-        let sum;
+        let carry = sum = 0;
         let answer = '';
-        for( let i=num1.length-1; i>=0; i-- ){
-                sum = String( +num1[i] + ( num2[i] ? +num2[i] : 0 ) + carry);
+        for( let i = num1.length-1; i>=0; i-- ){
+                sum = String( +num1[i] + ( num2[i] ? +num2[i] : 0 ) + carry );
+                answer = String( +sum[sum.length-1] ) + answer;
                 if( sum.length > 1 ) {
-                        answer = String( +sum[1] ) + answer;
                         carry = +sum[0];
                 } 
                 else {
-                        answer = String( +sum[0] ) + answer;
                         carry = 0;
                 }
         }
