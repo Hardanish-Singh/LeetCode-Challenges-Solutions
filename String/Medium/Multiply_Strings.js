6521c5c70977d54e4ee31c51f7428ebb966ff3d3
Hardanish Singh <hardanishsingh1@gmail.com>
2021-08-27 12:27:05 -0400

    Multiply Strings

---
+++ String/Medium/Multiply_Strings.js
@@ -0,0 +1,27 @@
+/*
+        Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+        Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+        Example 1:
+                Input: num1 = "2", num2 = "3"
+                Output: "6"
+        Example 2:
+                Input: num1 = "123", num2 = "456"
+                Output: "56088"
+
+        Constraints:
+                1) 1 <= num1.length, num2.length <= 200
+                2) num1 and num2 consist of digits only.
+                3) Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+
+*/
+
+/*
+ * @param { string } num1
+ * @param { string } num2
+ * @return { string }
+*/
+
+var multiply = function( num1, num2 ) {
+        
+};
