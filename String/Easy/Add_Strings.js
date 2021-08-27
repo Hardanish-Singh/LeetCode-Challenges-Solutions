1f615ac064edef9d7839743ee4aa4e08b2d78ab8
Hardanish Singh <hardanishsingh1@gmail.com>
2021-08-27 13:35:02 -0400

    Add Strings

--- String/Easy/Add_Strings.js
+++ String/Easy/Add_Strings.js
@@ -8,9 +8,8 @@ var addStrings = function( num1, num2 ) {
         if( num2.length > num1.length ) {
                 return addStrings( num2, num1 );
         }
-        let count = 0;
-        let count2 = num1.length - num2.length;
-        while( count != count2 ){
+        let count = num2.length;
+        while( count != num1.length ){
                 num2 = "0" + num2;
                 count++;
         }
