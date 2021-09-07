e3f5c98a4079f5749800795ff003a66d3d684406
Hardanish Singh <hardanishsingh1@gmail.com>
2021-09-07 10:53:34 -0400

    Subtract Strings

--- String/Medium/Subtract_Strings.js
+++ String/Medium/Subtract_Strings.js
@@ -1,3 +1,36 @@
+/*
+
+        describe('Some simple subtraction', function() {
+                it('simple', function() {
+                        Test.assertEquals(subtract("2", "3"), "-1");
+                        Test.assertEquals(subtract("30", "69"), "-39");
+                        Test.assertEquals(subtract("6", "2"), "4");
+                        Test.assertEquals(subtract("85", "11"), "74");
+                });
+        });
+
+        describe('Some corner case', function() {
+                it('corner cases', function() {
+                        Test.assertEquals(subtract("2" ,"0"), "2");
+                        Test.assertEquals(subtract("0", "30"), "-30");
+                        Test.assertEquals(subtract("0000001", "3"), "-2");
+                        Test.assertEquals(subtract("1009", "03"), "1006");
+                        Test.assertEquals(subtract("234", "242"), "-8");
+                        Test.assertEquals(subtract("294", "334"), "-40");
+                });
+        });
+
+        describe('Some big subtraction', function() {
+                it('big boys', function() {
+                        Test.assertEquals(subtract("98765", "56894"), "41871");
+                        Test.assertEquals(subtract("1020303004875647366210", "2774537626200857473632627613"), "-2774536605897852597985261403");
+                        Test.assertEquals(subtract("58608473622772837728372827", "7586374672263726736374"), "58600887248100574001636453");
+                        Test.assertEquals(subtract("9007199254740991", "9007199254740991"), "0");
+                });
+        });
+
+*/
+
 function subtract( num1, num2 ) {
         
         // REMOVE / TRUNCATE LEADING ZERIOS BY REGULAR EXPRESSIONSS
@@ -29,6 +62,6 @@ function subtract( num1, num2 ) {
         }
           
         answer = answer.replace(/^0+/, '');
-        
+
         return answer === '' ? '0' : minus + answer;
 }
