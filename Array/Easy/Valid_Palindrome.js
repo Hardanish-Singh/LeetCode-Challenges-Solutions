5abee8396cc663e369e67a41bb3d86cf254893a8
Hardanish Singh <hardanishsingh1@gmail.com>
2021-09-12 10:27:52 -0400

    Valid Palindrome

--- Array/Easy/Valid_Palindrome.js
+++ Array/Easy/Valid_Palindrome.js
@@ -1,5 +1,23 @@
 /*
- * @param { string } s
+        Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+        Example 1:
+                Input: s = "A man, a plan, a canal: Panama"
+                Output: true
+                Explanation: "amanaplanacanalpanama" is a palindrome.
+        
+        Example 2:
+                Input: s = "race a car"
+                Output: false
+                Explanation: "raceacar" is not a palindrome.
+
+        Constraints:
+                1) 1 <= s.length <= 2 * 105
+                2) s consists only of printable ASCII characters.
+*/
+
+/*
+ * @param { string } str
  * @return { boolean }
 */
  var isPalindrome = function( str ) {
