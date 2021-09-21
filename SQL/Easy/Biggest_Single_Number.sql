dfaed02ee1e9300b3e0f61ef962255ac3074e23d
Hardanish Singh <hardanishsingh1@gmail.com>
2021-09-21 09:47:45 -0400

    Biggest Single Number

---
+++ SQL/Easy/Biggest_Single_Number.sql
@@ -0,0 +1,29 @@
+-- SOLUTION 1
+SELECT 
+        MAX(num) AS num 
+FROM
+(
+        SELECT
+                COUNT(num) AS count_num,
+                num
+        FROM
+                my_numbers
+        GROUP BY num
+)AS x 
+WHERE x.count_num = 1
+
+-- SOLUTION 2
+SELECT
+        IFNULL(
+                (
+                        SELECT 
+                                num 
+                        FROM 
+                                my_numbers
+                        GROUP BY num
+                        HAVING COUNT(num) = 1
+                        ORDER BY num DESC
+                        LIMIT 1
+                ),
+                NULL
+              ) AS num
