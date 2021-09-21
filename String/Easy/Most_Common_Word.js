fdf6cb4b403a68fc85c6c9204bf85423c6c4baa9
Hardanish Singh <hardanishsingh1@gmail.com>
2021-09-21 09:03:58 -0400

    Most Common Word

--- String/Easy/Most_Common_Word.js
+++ String/Easy/Most_Common_Word.js
@@ -3,8 +3,6 @@
  * @param {string[]} banned
  * @return {string}
 */
-
-//paragraph consists of English letters, space ' ', or one of the symbols: " ! ? ' , ; . "
 var mostCommonWord = function( paragraph, banned ) {
         paragraph = paragraph.toLowerCase();
         let new_paragraph = [];
@@ -14,9 +12,7 @@ var mostCommonWord = function( paragraph, banned ) {
                         temp += paragraph[i];
                 }
                 else {
-                        if( temp.length > 0 ) {
-                                new_paragraph.push(temp);
-                        }
+                        new_paragraph.push(temp);
                         temp = '';
                 }
         }
