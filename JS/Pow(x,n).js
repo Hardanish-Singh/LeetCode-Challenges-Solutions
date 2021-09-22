fdb0c42c7bbec1a314a4121f36b2f8bf5f53e5f0
Hardanish Singh <hardanishsingh1@gmail.com>
2021-09-22 19:00:20 -0400

    Pow(x,n)

---
+++ JS/Pow(x,n).js
@@ -0,0 +1,30 @@
+/*
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+*/
+
+
+ var myPow = function(x, n) {
+        // let power = 1;
+        // if( n < 0 ) {
+        //         x = 1/x;
+        //         n = -n;
+        // }
+        // for( let i=1; i<=n; i++ ) {
+        //         power *= x;
+        // }
+        // return power;
+        
+        // let ans = 1;
+        // let current_product = x;
+        // for (let i = Math.abs(n); i > 0; i = parseInt(i/2) ) {
+        //   if (i%2 == 1) {
+        //     ans = ans * current_product;
+        //   }
+        //   current_product = current_product * current_product;
+        // }
+        // return  n < 0 ? 1/ans:ans;
+
+        
+};
