1ce254f97914235f80cb412441f470fe019be213
Hardanish Singh <hardanishsingh1@gmail.com>
2021-09-28 10:35:20 -0400

    Rotate Image

--- Array/Medium/Rotate_Image.js
+++ Array/Medium/Rotate_Image.js
@@ -35,3 +35,33 @@ var rotate = function( matrix ) {
 
         return matrix;
 };
+
+/*
+        SOLUTION 2
+*/
+var rotate2 = function( matrix ) {
+        for( let i=0; i<matrix.length; i++ ){
+                for( let j=0; j<matrix[i].length; j++ ){
+                        if( j > i ) {
+                                let temp = matrix[i][j];
+                                matrix[i][j] = matrix[j][i];
+                                matrix[j][i] = temp;
+                        }
+                }
+        }
+        for( let i=0; i<matrix.length; i++ ){
+                let leftPointer = i;
+                let rightPointer = 0;
+                let count = matrix[i].length - 1;
+                let c = 0;
+                while( c != parseInt(matrix[i].length/2) ) {
+                        let temp = matrix[leftPointer][rightPointer];
+                        matrix[leftPointer][rightPointer] = matrix[leftPointer][count];
+                        matrix[leftPointer][count] = temp;
+                        count--;
+                        rightPointer++;
+                        c++;
+                }
+        }
+        return matrix;
+}
