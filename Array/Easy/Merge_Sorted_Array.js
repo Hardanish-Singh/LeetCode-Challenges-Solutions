c38fabd76bed4dd0073da45b15056047b581a689
Hardanish Singh <hardanishsingh1@gmail.com>
2021-09-30 10:27:08 -0400

    Merge Sorted Array

--- Array/Easy/Merge_Sorted_Array.js
+++ Array/Easy/Merge_Sorted_Array.js
@@ -6,6 +6,7 @@
  * @return { void } Do not return anything, modify nums1 in-place instead.
 */
 
+//SOLUTION 1
 var merge = function( nums1, m, nums2, n ) {
         let sorted_array = [];
 
@@ -21,6 +22,38 @@ var merge = function( nums1, m, nums2, n ) {
         for( let i=0; i<sorted_array.length; i++ ){
                 nums1[i] = sorted_array[i];
         }
+        
+        return nums1;
+};
+
+//SOLUTION 2
+var merge = function( nums1, m, nums2, n ) {
+        
+        for( let i=m; i>=0; i-- ) {
+                if( nums1[i] === 0 ) {
+                        nums1.splice( i, 1 );
+                        i++;
+                }
+                else {
+                        break;
+                }
+        }
+        
+        for( let i=n; i>=0; i-- ) {
+                if( nums2[i] === 0 ) {
+                        nums2.splice( i, 1 );
+                        i++;
+                }
+                else {
+                        break;
+                }
+        }
+        
+        for( let i=0; i<n; i++ ) {
+                nums1.push( nums2[i] );
+        }
+        
+        nums1.sort( ( num1, num2 ) => num1 - num2 );
         
         return nums1;
 };
