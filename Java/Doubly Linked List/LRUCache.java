d011e43ac67d2990b3d3c86b49a547634d2fe3d3
Hardanish Singh <hardanishsingh1@gmail.com>
2021-10-07 07:48:07 -0400

    LRU Cache

--- Java/Doubly Linked List/LRUCache.java
+++ Java/Doubly Linked List/LRUCache.java
@@ -1,21 +1,19 @@
 /*
-        Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
-
+        Design a Data Structure that follows the constraints of a LEAST RECENTLY USED (LRU) CACHE.
         Implement the LRUCache class:
-
-        LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
-        int get(int key) Return the value of the key if the key exists, otherwise return -1.
-        void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
-        The functions get and put must each run in O(1) average time complexity.
+                1) LRUCache( int capacity ) Initialize the LRU Cache with positive size capacity.
+                2) int get( int key ) Return the value of the key if the key exists, otherwise return -1.
+                3) void put( int key, int value ) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+                4) The functions get and put must each run in O(1) average time complexity.
 
         Example 1:
-                Input
-                ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
-                [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
-        Output
-                [null, null, null, 1, null, -1, null, -1, 3, 4]
+                Input:  
+                        [ "LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get" ]
+                        [ [2], [1,1], [2,2], [1], [3,3], [2], [4,4], [1], [3], [4] ]
+                Output: 
+                        [null, null, null, 1, null, -1, null, -1, 3, 4]
 
-        Explanation
+        Explanation:
                 LRUCache lRUCache = new LRUCache(2);
                 lRUCache.put(1, 1); // cache is {1=1}
                 lRUCache.put(2, 2); // cache is {1=1, 2=2}
