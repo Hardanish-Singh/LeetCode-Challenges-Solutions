99ec00cf69708a1ea3664ef00bba849fd5c91ce3
Hardanish Singh <hardanishsingh1@gmail.com>
2021-10-07 08:16:58 -0400

    LRU Cache

--- Java/Doubly Linked List/LRUCache.java
+++ Java/Doubly Linked List/LRUCache.java
@@ -14,16 +14,16 @@
                         [ null, null, null, 1, null, -1, null, -1, 3, 4 ]
 
         Explanation:
-                LRUCache lRUCache = new LRUCache(2);
-                lRUCache.put(1, 1); // cache is {1=1}
-                lRUCache.put(2, 2); // cache is {1=1, 2=2}
-                lRUCache.get(1);    // return 1
-                lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-                lRUCache.get(2);    // returns -1 (not found)
-                lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-                lRUCache.get(1);    // return -1 (not found)
-                lRUCache.get(3);    // return 3
-                lRUCache.get(4);    // return 4
+                LRUCache obj = new LRUCache(2);
+                obj.put(1, 1); // cache is { 1=1 }
+                obj.put(2, 2); // cache is { 2=2, 1=1 }
+                obj.get(1);    // return 1 & cache is { 1=1, 2=2 }
+                obj.put(3, 3); // LRU key was 2, evicts key 2, cache is { 3=3, 1=1 }
+                obj.get(2);    // returns -1 ( not found )
+                obj.put(4, 4); // LRU key was 1, evicts key 1, cache is { 4=4, 3=3 }
+                obj.get(1);    // return -1 (not found)
+                obj.get(3);    // return 3 & cache is { 3=3, 4=4 }
+                obj.get(4);    // return 4 & cache is { 4=4, 3=3 }
 */
 
 import java.util.HashMap;
