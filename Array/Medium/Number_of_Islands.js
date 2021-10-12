39d37fb0ba2d12114844197a3878f6ddfa98c454
Hardanish Singh <hardanishsingh1@gmail.com>
2021-10-12 17:01:19 -0400

    Number of Islands

--- Array/Medium/Number_of_Islands.js
+++ Array/Medium/Number_of_Islands.js
@@ -1,3 +1,36 @@
+/*
+        Given an m x n 2D binary grid, which represents a map of '1's ( land ) and '0's ( water ), return the number of islands.
+
+        An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically directions and not diagonal.
+        You may assume all four edges of the grid are all surrounded by water.
+
+        Example 1:
+
+        Input: grid = [
+                        ["1","1","1","1","0"],
+                        ["1","1","0","1","0"],
+                        ["1","1","0","0","0"],
+                        ["0","0","0","0","0"]
+                ]
+        Output: 1
+        
+        Example 2:
+
+        Input: grid = [
+                        ["1","1","0","0","0"],
+                        ["1","1","0","0","0"],
+                        ["0","0","1","0","0"],
+                        ["0","0","0","1","1"]
+                ]
+        Output: 3
+
+        Constraints:
+                1) m == grid.length
+                2) n == grid[i].length
+                3) 1 <= m, n <= 300
+                4) grid[i][j] is '0' or '1'.
+*/
+
 const is_visited = ( grid, i, j ) => grid[i][j] === '1' ? ( grid[i][j] = '2', (i + "," + j) ) : null;
 
 const move_left  = ( grid, i, j ) => --j >= 0 ? is_visited( grid, i, j ) : null;
