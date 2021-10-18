d4214ab8f29a51408827ed2de8ae4ccce8b59ae7
Hardanish Singh <hardanishsingh1@gmail.com>
2021-10-18 14:58:15 -0400

    Duplicate Zeros

--- Array/Easy/Duplicate_Zeros.js
+++ Array/Easy/Duplicate_Zeros.js
@@ -1,8 +1,8 @@
 /*
  * @param {number[]} arr
  * @return {void} Do not return anything, modify arr in-place instead.
- */
- var duplicateZeros = function( arr ) {
+*/
+var duplicateZeros = function( arr ) {
         for( let i=0; i<arr.length-1; i++ ){
                 if( arr[i] == 0 ) {
                         for( let j=arr.length-1; j>i+1; j-- ){
