3c20484464be00da288d70e8c15b6098ee1476e4
Hardanish Singh <hardanishsingh1@gmail.com>
2021-10-17 20:52:52 -0400

    Unique Path

--- Array/Medium/Unique_Path.js
+++ Array/Medium/Unique_Path.js
@@ -72,8 +72,8 @@ function uniquePaths( m, n ) {
         }
         i = 1;
         j = 1;
-        for( let i = 0; i<m; i++ ){
-                for( let j=0; j<n; j++ ){
+        for( let i = 1; i<m; i++ ){
+                for( let j=1; j<n; j++ ){
                         if( matrix[i][j] !== 1 ) {
                                 matrix[i][j] = matrix[i-1][j]+matrix[i][j-1];
                         }
