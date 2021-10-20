b572899de2d1981dd94cec83e8d5838c23681cdc
Hardanish Singh <hardanishsingh1@gmail.com>
2021-10-20 15:05:00 -0400

    Check If N and Its Double Exist

--- Array/Easy/Check_If_N_and_Its_Double_Exist.js
+++ Array/Easy/Check_If_N_and_Its_Double_Exist.js
@@ -36,15 +36,15 @@
 const binarySearch = (array, target) => {
         let startIndex = 0;
         let endIndex = array.length - 1;
-        while(startIndex <= endIndex) {
-                let middleIndex = Math.floor((startIndex + endIndex) / 2);
-                if(target === array[middleIndex]) {
+        while( startIndex <= endIndex ) {
+                let middleIndex = Math.floor( (startIndex + endIndex) / 2 );
+                if( target === array[middleIndex] ) {
                         return middleIndex;
                 }
-                if(target > array[middleIndex]) {
+                if( target > array[middleIndex] ) {
                         startIndex = middleIndex + 1;
                 }
-                if(target < array[middleIndex]) {
+                if( target < array[middleIndex] ) {
                         endIndex = middleIndex - 1;
                 }
         }
