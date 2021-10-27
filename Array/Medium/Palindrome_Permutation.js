2a60df81050ca39d0c9661934f3c1b8031da63ef
Hardanish Singh <hardanishsingh1@gmail.com>
2021-10-27 12:40:07 -0400

    Palindrome Permutations

--- Array/Medium/Palindrome_Permutation.js
+++ Array/Medium/Palindrome_Permutation.js
@@ -1,4 +1,23 @@
 /*
+        Given a string s, return true if a permutation of the string could form a palindrome.
+
+        Example 1:
+                Input: s = "code"
+                Output: false
+        Example 2:
+                Input: s = "aab"
+                Output: true
+        Example 3:
+                Input: s = "carerac"
+                Output: true
+
+        Constraints:
+
+                1) 1 <= s.length <= 5000
+                2) s consists of only lowercase English letters.
+*/
+
+/*
  * @param {string} s
  * @return {boolean}
 */
