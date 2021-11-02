f46a6b582153a586827029add779fbc51a7f09c6
Hardanish Singh <hardanishsingh1@gmail.com>
2021-11-02 07:59:11 -0400

    Spiral Matrix II

--- Array/Medium/Spiral_Matrix_II.js
+++ Array/Medium/Spiral_Matrix_II.js
@@ -3,10 +3,18 @@
         
         Example 1:
                 Input: n = 3
-                Output: [[1,2,3],[8,9,4],[7,6,5]]
+                Output: 
+                        [
+                                [1, 2, 3],
+                                [8, 9, 4],
+                                [7, 6 ,5]
+                        ]
         Example 2:
                 Input: n = 1
-                Output: [[1]]
+                Output: 
+                        [
+                                [ 1 ]
+                        ]
 
         Constraints:
                 1) 1 <= n <= 20
