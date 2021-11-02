a361409941dda16c1dc073f995a527f5e8450ad5
Hardanish Singh <hardanishsingh@Hardanishs-MacBook-Pro.local>
2021-11-01 22:22:45 -0400

    Spiral Matrix

--- Array/Medium/Spiral_Matrix.js
+++ Array/Medium/Spiral_Matrix.js
@@ -1,4 +1,33 @@
 /*
+        Given an m x n matrix, 
+                return all elements of the matrix in spiral order 
+                or 
+                return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+        
+        Input: matrix = 
+                        [ 
+                                [1, 2, 3],
+                                [4, 5, 6],
+                                [7, 8, 9]
+                        ]
+        Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
+
+        Input: matrix = 
+                        [
+                                [1,  2,  3,  4],
+                                [5,  6,  7,  8],
+                                [9, 10, 11, 12]
+                        ]
+        Output: [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+
+        Constraints:
+                1) m == matrix.length
+                2) n == matrix[i].length
+                3) 1 <= m, n <= 10
+                4) -100 <= matrix[i][j] <= 100
+*/
+
+/*
  * @param {number[][]} matrix
  * @return {number[]}
 */
