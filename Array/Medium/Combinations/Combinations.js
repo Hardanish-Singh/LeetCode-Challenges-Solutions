60e14b54fe4cbb4de47aef027b5a920ec38fc2ed
Hardanish Singh <hardanishsingh1@gmail.com>
2021-11-05 16:21:54 -0400

    Combinations

--- Array/Medium/Combinations/Combinations.js
+++ Array/Medium/Combinations/Combinations.js
@@ -1,3 +1,30 @@
+/*
+        Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
+        You may return the answer in any order.
+
+        Example 1:
+                Input: n = 4, k = 2
+                Output:
+                        [
+                                [2,4],
+                                [3,4],
+                                [2,3],
+                                [1,2],
+                                [1,3],
+                                [1,4],
+                        ]
+        Example 2:
+                Input: n = 1, k = 1
+                Output: 
+                        [
+                                [1]
+                        ]
+        
+        Constraints:
+                1) 1 <= n <= 20
+                2) 1 <= k <= n
+*/
+
 var combine = function( n, k ) {
         let str = "";
         for( let i=0; i<n; i++ ) {
@@ -62,5 +89,3 @@ var combine = function( n, k ) {
         }
         return combinations;
 };
-
-console.log( combine(20, 16) );
