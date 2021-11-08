b0db621fb6fccb8a8744bd7fe732453d15a9213a
Hardanish Singh <hardanishsingh1@gmail.com>
2021-11-08 07:10:42 -0500

    Jump Game II

--- Array/Medium/Jump_Game/Jump_Game_II.ts
+++ Array/Medium/Jump_Game/Jump_Game_II.ts
@@ -1,3 +1,21 @@
+/*
+        Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+        Each element in the array represents your maximum jump length at that position.
+        Your goal is to reach the last index in the minimum number of jumps.
+        You can assume that you can always reach the last index.
+
+        Example 1:
+                Input: nums = [2,3,1,1,4]
+                Output: 2
+                Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+        Example 2:
+                Input: nums = [2,3,0,1,4]
+                Output: 2
+        Constraints:
+                1) 1 <= nums.length <= 10^4
+                2) 0 <= nums[i] <= 1000
+*/
+
 function getMaxValue( arr: number[], i: number ): number[] {
         let max: number = -1;
         let index: number = -1;
