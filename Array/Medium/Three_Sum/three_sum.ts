c1a12832e7b35e9b4f637a29b6271c376305e413
Hardanish Singh <hardanishsingh1@gmail.com>
2021-12-01 18:30:03 -0500

    Three Sum

---
+++ Array/Medium/Three_Sum/three_sum.ts
@@ -0,0 +1,39 @@
+/*
+ * @param { number[] } nums
+ * @return { number[][] }
+*/
+
+function threeSum( nums: Array<number> ): Array<Array<number>> {
+	nums.sort( (a, b) => a - b );
+	let triplets: Array<Array<number>> = [];
+        let target: number = 0;
+	for( let i: number = 0; i<nums.length; i++ ) {
+                if( nums[i] == nums[i-1] ) {
+                        continue;
+                }
+		let leftIndex: number = i+1;
+		let rightIndex: number = nums.length-1;
+		
+		while( leftIndex < rightIndex ){
+                        if( nums[leftIndex] === nums[leftIndex-1] && leftIndex != i + 1 ) {
+                                leftIndex++;
+                        }
+                        else if( nums[rightIndex] === nums[rightIndex+1] ) {
+                                rightIndex--;
+                        }
+			else if( nums[leftIndex] + nums[rightIndex] + nums[i] < target) {
+				leftIndex++;
+			}
+			else if( nums[leftIndex] + nums[rightIndex] + nums[i] > target) {
+				rightIndex--;
+			}
+			else if( nums[leftIndex] + nums[rightIndex] + nums[i] === target) {
+				triplets.push( [ nums[i], nums[leftIndex], nums[rightIndex] ] );
+				leftIndex++;
+				rightIndex--;
+			}
+		}	
+		
+	}
+	return triplets;
+}
