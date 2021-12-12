3c9fa64e49a6a5642264b51b1cafa1e705f8bda1
Hardanish Singh <hardanishsingh1@gmail.com>
2021-12-12 18:43:14 -0500

    Remove Linked List Elements

--- Java/Linked_List/Remove_Linked_List_Elements.java
+++ Java/Linked_List/Remove_Linked_List_Elements.java
@@ -1,4 +1,26 @@
 /*
+        Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+        
+        Example 1: 
+                        1 ---> 2 ---> 6 ---> 3 ---> 4 ---> 5 ---> 6
+                                        ↓
+                        1 ---> 2 ---> 3 ---> 4 ---> 5
+        
+                Input: head = [1, 2, 6, 3, 4, 5, 6], val = 6
+                Output: [1, 2, 3, 4, 5]
+                
+        Example 2:
+                
+                Input: head = [], val = 1
+                Output: []
+        
+        Example 3:
+                      
+                Input: head = [7, 7, 7, 7], val = 7
+                Output: []
+*/
+
+/*
         // Definition for Singly-Linked List
         public class ListNode
 	{
