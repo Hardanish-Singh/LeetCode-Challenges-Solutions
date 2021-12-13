e6ddc15be452382af6a0aa523c8f158e2f87a5a0
Hardanish Singh <hardanishsingh1@gmail.com>
2021-12-13 09:53:24 -0500

    Delete the Middle Node of a Linked List

---
+++ Java/Linked_List/Delete_the_Middle_Node_of_a_Linked_List.java
@@ -0,0 +1,58 @@
+/*
+        // Definition for Singly-Linked List
+        public class ListNode
+	{
+                int val;
+                ListNode next;
+                public ListNode()
+                {
+                }
+                public ListNode( int val )
+                {
+                        this.val = val;
+                }
+                public ListNode( int val, ListNode next )
+                {
+                        this.val = val;
+                        this.next = next;
+                }
+        }
+*/
+
+class Solution 
+{
+        public ListNode deleteMiddle( ListNode head ) 
+        {
+                ListNode current = head;
+                ListNode previous = null;
+                int count = 1;
+                while( current.next != null )
+                {
+                        count++;
+                        current = current.next;
+                }
+                int middle = ( int ) Math.ceil( count / 2 );
+                current = head;
+                count = 0;
+                while( true )
+                {
+                        if( middle == count )
+                        {
+                                if( previous == null )
+                                {
+                                        return null;
+                                }
+                                previous.next = current.next;
+                                break;
+                        }
+                        if( current.next == null )
+                        {
+                                break;  
+                        }
+                        previous = current;
+                        current = current.next;
+                        count++;
+                }
+                return head;
+        }
+}
