1258708d78b55807950c6b05bb87eafed82738c0
Hardanish Singh <hardanishsingh1@gmail.com>
2021-12-14 13:55:51 -0500

    Generate Parenthesis

--- Array/Medium/Generate_Parentheses.js
+++ Array/Medium/Generate_Parentheses.js
@@ -1,4 +1,27 @@
 /*
+        Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+        Example 1:
+                Input: n = 3
+                Output: [ "((()))", "(()())", "(())()", "()(())", "()()()" ]
+
+        Example 2:
+                Input: n = 1
+                Output: [ "()" ]
+
+        Constraints:
+                1) 1 <= n <= 8
+
+        Coding Tips:
+                1) if n = 3, then we can have 3 open & 3 closing parenthesis
+                2) closing bracket cannot come before open bracket
+        Rules:
+                1) Only add open parenthesis if open < n
+                2) Only add closing parenthesis if open > close
+                3) We get a valid combination if and only if, openParenthesisCount == closeParenthesisCount && closeParenthesisCount == n && openParenthesisCount == n
+*/
+
+/*
  * @param { number } n
  * @return { string[] }
 */
