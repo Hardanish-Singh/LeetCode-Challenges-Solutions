1d608dc85a12b2d9348222c4f94dea57f7910f25
Hardanish Singh <hardanishsingh1@gmail.com>
2021-12-24 19:21:45 -0500

    Number of Spaces Cleaning Robot Cleaned

--- Array/Medium/Number_of_Spaces_Cleaning_Robot_Cleaned.js
+++ Array/Medium/Number_of_Spaces_Cleaning_Robot_Cleaned.js
@@ -10,7 +10,11 @@ var numberOfCleanRooms = function( room ) {
         let i = 0, j = 0, noOfCellsRobotCleaned = 0;
 
         while( true ) {
-                
+                /* 
+                        WHEN THE ROBOT REACHES A SPACE/POSITION 
+                        THAT IT HAS ALREADY CLEANED & IS FACING THE SAME DIRECTION AS BEFORE, 
+                        WE CAN STOP THE SIMULATION.
+                */
                 if( visitedPositions[i + "," + j + "," + currentDirection] ) {
                         break;
                 }
@@ -22,46 +26,46 @@ var numberOfCleanRooms = function( room ) {
                 }
                 room[i][j] = 2;
 
-                //MOVE RIGHT
+                // MOVE RIGHT
                 if( currentDirection == "right" ) {
                         j = j + 1;
-                        //OUT OF GRID OR ROTATE 90 deg CLOCKWISE
+                        // OUT OF GRID OR ROTATE 90° CLOCKWISE
                         if( j == room[i].length || room[i][j] == 1 ) {
                                 j = j - 1;
-                                //ROTATE 90 deg CLOCKWISE
+                                // ROTATE 90° CLOCKWISE
                                 currentDirection = direction[currentDirection];
                         }
                 }
 
-                //MOVE DOWN
+                // MOVE DOWN
                 else if( currentDirection == "down" ) {
                         i = i + 1;
-                        //OUT OF GRID OR ROTATE 90 deg CLOCKWISE
+                        // OUT OF GRID OR ROTATE 90° CLOCKWISE
                         if( i == room.length || room[i][j] == 1 ) {
                                 i = i - 1;
-                                //ROTATE 90 deg CLOCKWISE
+                                // ROTATE 90° CLOCKWISE
                                 currentDirection = direction[currentDirection];
                         }
                 }
 
-                //MOVE LEFT
+                // MOVE LEFT
                 else if( currentDirection == "left" ) {
                         j = j - 1;
-                        //OUT OF GRID OR ROTATE 90 deg CLOCKWISE
+                        // OUT OF GRID OR ROTATE 90° CLOCKWISE
                         if( j < 0 ||  room[i][j] == 1 ) {
                                 j = j + 1;
-                                //ROTATE 90 deg CLOCKWISE
+                                // ROTATE 90° CLOCKWISE
                                 currentDirection = direction[currentDirection];
                         }
                 }
 
-                //MOVE TOP
+                // MOVE TOP
                 else if( currentDirection == "top" ) {
                         i = i - 1;
-                        //OUT OF GRID OR ROTATE 90 deg CLOCKWISE
+                        // OUT OF GRID OR ROTATE 90° CLOCKWISE
                         if( i < 0 || room[i][j] == 1 ) {
                                 i = i + 1;
-                                //ROTATE 90 deg CLOCKWISE
+                                // ROTATE 90° CLOCKWISE
                                 currentDirection = direction[currentDirection];
                         }
                 }
