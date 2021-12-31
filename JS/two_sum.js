b62a2bbaa67d4b1c16acc07ac20d9316f8ffd2d3
Hardanish Singh <hardanishsingh1@gmail.com>
2021-12-31 08:30:47 -0500

    Two Sum

--- JS/two_sum.js
+++ JS/two_sum.js
@@ -1,10 +1,24 @@
 /*
-      Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-      You may assume that each input would have exactly one solution, and you may not use the same element twice.
-      Example:
-	        Given nums = [2, 7, 11, 15], target = 9,
-	        Because nums[0] + nums[1] = 2 + 7 = 9,
-	        return [0, 1].
+        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order.
+
+        Example 1:
+                Input: nums = [2,7,11,15], target = 9
+                Output: [0,1]
+                Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+        Example 2:
+                Input: nums = [3,2,4], target = 6
+                Output: [1,2]
+        Example 3:
+                Input: nums = [3,3], target = 6
+                Output: [0,1]
+        
+        Constraints:
+                1) 2 <= nums.length <= 10^4
+                2) -10^9 <= nums[i] <= 10^9
+                3) -10^9 <= target <= 10^9
+                4) Only one valid answer exists.
 */
 
 /*
