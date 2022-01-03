5152d3b7e5ee5c9805bb854dc47489e1ededc9d7
Hardanish Singh <hardanishsingh1@gmail.com>
2022-01-03 18:22:01 -0500

    Two Sum Less Than K

--- Array/Easy/Two_Sum/Two_Sum_Less_Than_K.js
+++ Array/Easy/Two_Sum/Two_Sum_Less_Than_K.js
@@ -1,3 +1,28 @@
+/*
+        Given an array nums of integers and integer k, return the maximum sum such that there exists i < j with nums[i] + nums[j] = sum and sum < k. 
+        If no i, j exist satisfying this equation, return -1.
+        
+        Example 1:
+                Input: nums = [34,23,1,24,75,33,54,8], k = 60
+                Output: 58
+                Explanation: We can use 34 and 24 to sum 58 which is less than 60.
+        Example 2:
+                Input: nums = [10,20,30], k = 15
+                Output: -1
+                Explanation: In this case it is not possible to get a pair sum less that 15.
+        
+        Constraints:
+                1) 1 <= nums.length <= 100
+                2) 1 <= nums[i] <= 1000
+                3) 1 <= k <= 2000
+*/
+
+/*
+	SOLUTION 1( MOST OPTIMAL )
+		Time Complexity: O( nlog(n) )
+		Space Complexity: O( 1 )
+*/
+
 /**
  * @param { number[] } nums
  * @param { number } target
