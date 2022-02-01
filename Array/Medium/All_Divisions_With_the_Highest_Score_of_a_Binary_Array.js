5cf91abe9a3ab1d556de795b85feca18d2f619f3
Hardanish Singh <hardanishsingh1@gmail.com>
2022-02-01 07:31:57 -0500

    All Divisions With the Highest Score of a Binary Array

--- Array/Medium/All_Divisions_With_the_Highest_Score_of_a_Binary_Array.js
+++ Array/Medium/All_Divisions_With_the_Highest_Score_of_a_Binary_Array.js
@@ -1,3 +1,13 @@
+/*
+        You are given a 0-indexed binary array nums of length n. 
+        nums can be divided at index i (where 0 <= i <= n) into two arrays (possibly empty) nums(left) and nums(right):
+                1) nums(left) has all the elements of nums between index 0 and i - 1 (inclusive), while nums(right) has all the elements of nums between index i and n - 1 (inclusive).
+                2) If i == 0, nums(left) is empty, while nums(right) has all the elements of nums
+                3) If i == n, nums(left) has all the elements of nums, while nums(right) is empty.
+        
+        The division score of an index i is the sum of the number of 0's in nums(left) and the number of 1's in nums(right).
+        Return all distinct indices that have the highest possible division score. You may return the answer in any order.
+*/
 var maxScoreIndices = function(nums) {
         let hash_table = {
 
