e1965f3d42bf0b742a3bf6dd9eeb49f11d5a0b4f
Hardanish Singh <hardanishsingh1@gmail.com>
2022-02-20 07:59:36 -0500

    Merge Intervals

--- Array/Medium/Merge_Intervals.js
+++ Array/Medium/Merge_Intervals.js
@@ -1,5 +1,6 @@
 /*
-        Given an array of intervals where intervals[i] = [start(i), end(i)], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+        Given an array of intervals where intervals[i] = [start(i), end(i)], merge all overlapping intervals, 
+        and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
         Example 1:
                 Input: intervals = [ [1,3], [2,6], [8,10], [15,18] ]
