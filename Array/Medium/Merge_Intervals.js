a85daa3af5848cc51a00d6be84f800af2816f226
Hardanish Singh <hardanishsingh1@gmail.com>
2022-02-20 07:56:01 -0500

    Merge Intervals

--- Array/Medium/Merge_Intervals.js
+++ Array/Medium/Merge_Intervals.js
@@ -45,14 +45,12 @@
 
 var merge = function(intervals) {
         intervals.sort( (a,b) => a[0] - b[0] );
-        for( let i=0; i<intervals.length; i++ ){
-                for( let j=i+1; j< intervals.length; j++ ){
-                        if( intervals[j][0] >= intervals[i][0] && intervals[j][0] <= intervals[i][1] ) {
-                                intervals[i] = [intervals[i][0], Math.max(intervals[i][1], intervals[j][1])];
-                                intervals.splice(j,1);
-                                i--;
-                                break;
-                        }
+        for( let i = 0; i < intervals.length; i++ ) {
+                let j = i + 1;
+                if( intervals[j][0] >= intervals[i][0] && intervals[j][0] <= intervals[i][1] ) {
+                        intervals[i] = [intervals[i][0], Math.max(intervals[i][1], intervals[j][1])];
+                        intervals.splice(j,1);
+                        i--;
                 }
         }
         return intervals;
