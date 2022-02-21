335d24a69fcb0055f25802657c1de7ce9ca3daf9
Hardanish Singh <hardanishsingh1@gmail.com>
2022-02-21 10:32:34 -0500

    Insert Intervals

---
+++ Array/Medium/Insert_Interval.js
@@ -0,0 +1,27 @@
+/*
+ * @param { number[][] } intervals
+ * @param { number[] } newInterval
+ * @return { number[][] }
+*/
+
+var merge = function( intervals ) {
+        intervals.sort( (a,b) => a[0] - b[0] );
+        for( let i = 0; i < intervals.length - 1; i++ ) {
+                let j = i + 1;
+                if( 
+                        intervals[j][0] >= intervals[i][0] 
+                                && 
+                        intervals[j][0] <= intervals[i][1] 
+                ) {
+                        intervals[i] = [ intervals[i][0], Math.max( intervals[i][1], intervals[j][1] ) ];
+                        intervals.splice(j, 1);
+                        i--;
+                }
+        }
+        return intervals;
+};
+
+var insert = function( intervals, newInterval ) {
+        intervals = [...intervals, newInterval];
+        return merge(intervals);
+};
