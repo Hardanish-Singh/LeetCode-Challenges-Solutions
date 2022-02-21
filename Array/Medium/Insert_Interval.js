80b74929c7637137fac21101e60d292516e71013
Hardanish Singh <hardanishsingh1@gmail.com>
2022-02-21 10:35:36 -0500

    Insert Intervals

--- Array/Medium/Insert_Interval.js
+++ Array/Medium/Insert_Interval.js
@@ -1,4 +1,22 @@
 /*
+        You are given an array of non-overlapping intervals intervals where intervals[i] = [start(i), end(i)] represent the start and the end of the i^th interval and intervals is sorted in ascending order by start(i). 
+        You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+        
+        Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+        Return intervals after the insertion.
+
+        Example 1:
+                Input: intervals = [ [1,3], [6,9] ], newInterval = [2,5]
+                Output: [ [1,5], [6,9] ]
+        Example 2:
+
+        Input: intervals = [ [1,2], [3,5], [6,7], [8,10], [12,16] ], newInterval = [4,8]
+        Output: [ [1,2], [3,10], [12,16] ]
+        Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+*/
+
+/*
  * @param { number[][] } intervals
  * @param { number[] } newInterval
  * @return { number[][] }
