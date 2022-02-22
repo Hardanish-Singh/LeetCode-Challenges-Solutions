bc53a64a9298c1fa8d68bc2d647b8eb50b2cdbd7
Hardanish Singh <hardanishsingh1@gmail.com>
2022-02-22 15:11:22 -0500

    Set Matrix Zeros

--- Array/Medium/Set_Matrix_Zeroes.js
+++ Array/Medium/Set_Matrix_Zeroes.js
@@ -4,26 +4,25 @@
 */
 
 var setZeroes = function( matrix ) {
-        
         for( let i = 0; i < matrix.length; i++ ) {
                 for( let j = 0; j < matrix[i].length; j++ ) {
                         if( matrix[i][j] == 0 ) {
                                 matrix[i][j] = 'X';
-                                let I = 0;
-                                let J = 0;
+                                let row = 0;
+                                let column = 0;
                                 // Change the entire row to 0
-                                while( J <= matrix[i].length - 1 ) {
-                                        if( matrix[i][J] != 0 ) {
-                                                matrix[i][J] = 'X';
+                                while( column <= matrix[i].length - 1 ) {
+                                        if( matrix[i][column] != 0 ) {
+                                                matrix[i][column] = 'X';
                                         }
-                                        J++;
+                                        column++;
                                 }
                                 // Change the entire column to 0
-                                while( I <= matrix.length - 1 ) {
-                                        if( matrix[I][j] != 0 ) {
-                                                matrix[I][j] = 'X';
+                                while( row <= matrix.length - 1 ) {
+                                        if( matrix[row][j] != 0 ) {
+                                                matrix[row][j] = 'X';
                                         }
-                                        I++;
+                                        row++;
                                 }
                         }
                 }
