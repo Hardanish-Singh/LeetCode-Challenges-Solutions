5a9a6b6ccae7467bb51374d9a82dd4ef6b91aef5
Hardanish Singh <hardanishsingh1@gmail.com>
2022-02-22 15:14:45 -0500

    Set Matrix Zeros

--- Array/Medium/Set_Matrix_Zeroes.js
+++ Array/Medium/Set_Matrix_Zeroes.js
@@ -1,4 +1,38 @@
 /*
+        Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+        You must do it in place.
+
+        Example 1:
+                Input: matrix = [
+                                        [1, 1, 1],
+                                        [1, 0, 1],
+                                        [1, 1, 1]
+                                ]
+                Output: 
+                                [
+                                        [1, 0, 1],
+                                        [0, 0, 0],
+                                        [1, 0, 1]
+                                ]
+        Example 2:
+                Input: matrix = [
+                                        [0, 1, 2, 0],
+                                        [3, 4, 5, 2],
+                                        [1, 3, 1, 5]
+                                ]
+                Output:         [
+                                        [0, 0, 0, 0],
+                                        [0, 4, 5, 0],
+                                        [0, 3, 1, 0]
+                                ]
+        Constraints:
+                1) m == matrix.length
+                2) n == matrix[0].length
+                3) 1 <= m, n <= 200
+                4) -2^31 <= matrix[i][j] <= 2^31 - 1
+*/
+
+/*
  * @param { number[][] } matrix
  * @return { void } Do not return anything, modify matrix in-place instead.
 */
@@ -27,7 +61,7 @@ var setZeroes = function( matrix ) {
                         }
                 }
         }
-        
+
         for( let i = 0; i < matrix.length; i++ ) {
                 for( let j = 0; j < matrix[i].length; j++ ) {
                         if( matrix[i][j] === 'X' ) {
