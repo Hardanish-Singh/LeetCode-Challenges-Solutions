f079fefc3a236b67f6643be6290697d3806dfd4c
Hardanish Singh <hardanishsingh1@gmail.com>
2022-02-23 05:46:53 -0500

    Sum of Digits of String After Convert

---
+++ String/Easy/Sum_of_Digits_of_String_After_Convert.js
@@ -0,0 +1,23 @@
+/*
+ * @param { string } s
+ * @param { number } k
+ * @return { number }
+*/
+
+var getLucky = function( s, k ) {
+        let string = '';
+        for( let i = 0; i < s.length; i++ ) {
+                string += s.charCodeAt(i) - 96;
+        }
+        
+        let sum = 0;
+        for( let i = 0; i < k; i++ ) {
+                sum = 0;
+                for( let j = 0; j < string.length; j++ ) {
+                        sum += Number( string[j] );
+                }
+                string = String( sum );
+        }
+        
+        return sum;
+};
