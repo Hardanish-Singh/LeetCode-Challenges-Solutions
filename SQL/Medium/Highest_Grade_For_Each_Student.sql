46cf224d1694eb36f347ae058e5b57cdb572da61
Hardanish Singh <hardanishsingh1@gmail.com>
2022-02-24 08:27:47 -0500

    Highest Grade For Each Student

--- SQL/Medium/Highest_Grade_For_Each_Student.sql
+++ SQL/Medium/Highest_Grade_For_Each_Student.sql
@@ -1,3 +1,44 @@
+/*
+        Table: Enrollments
+
+        +---------------+---------+
+        | Column Name   | Type    |
+        +---------------+---------+
+        | student_id    | int     |
+        | course_id     | int     |
+        | grade         | int     |
+        +---------------+---------+
+        (student_id, course_id) is the primary key of this table.
+
+        Write a SQL query to find the highest grade with its corresponding course for each student.
+        In case of a tie, you should find the course with the smallest course_id.
+        Return the result table ordered by student_id in ascending order.
+        The query result format is in the following example.
+
+        Example 1:
+                Input: 
+                        Enrollments table:
+                        +------------+-------------------+
+                        | student_id | course_id | grade |
+                        +------------+-----------+-------+
+                        | 2          | 2         | 95    |
+                        | 2          | 3         | 95    |
+                        | 1          | 1         | 90    |
+                        | 1          | 2         | 99    |
+                        | 3          | 1         | 80    |
+                        | 3          | 2         | 75    |
+                        | 3          | 3         | 82    |
+                        +------------+-----------+-------+
+                Output: 
+                        +------------+-------------------+
+                        | student_id | course_id | grade |
+                        +------------+-----------+-------+
+                        | 1          | 2         | 99    |
+                        | 2          | 2         | 95    |
+                        | 3          | 3         | 82    |
+                        +------------+-----------+-------+
+*/
+
 SELECT 
         SubQuery.student_id,
         MIN( Enrollments.course_id ) AS course_id,
