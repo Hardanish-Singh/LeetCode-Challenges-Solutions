d51fee65258b115f33638284256c961a10abb297
Hardanish Singh <hardanishsingh1@gmail.com>
2022-03-10 09:16:13 -0500

    Max Consecutive Ones II

---
+++ Array/Medium/Max_Consecutive_Ones_II.js
@@ -0,0 +1,21 @@
+var findMaxConsecutiveOnes = function(nums) {
+        var count = 0;
+        var maxConsecutiveOnes = 0;
+        for( let i = 0; i < nums.length; i++ ) {
+                if( nums[i] ) {
+                        count++; 
+                } else {
+                        count++;
+                        let j = i + 1;
+                        while( nums[j++] ) {
+                                count++;
+                        }
+                        maxConsecutiveOnes = Math.max( count, maxConsecutiveOnes );
+                        count = 0;
+                }
+        }
+        if( count > 0 ) {
+                maxConsecutiveOnes = Math.max( count, maxConsecutiveOnes );
+        }
+        return maxConsecutiveOnes;
+};
