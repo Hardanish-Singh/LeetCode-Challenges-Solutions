45d0929b579c36264288f1b510378538d74aa4f4
Hardanish Singh <hardanishsingh1@gmail.com>
2022-03-10 09:18:04 -0500

    Max Consecutive Ones II

--- Array/Medium/Max_Consecutive_Ones_II.js
+++ Array/Medium/Max_Consecutive_Ones_II.js
@@ -1,4 +1,21 @@
 /*
+        Given a binary array nums, return the maximum number of consecutive 1's in the array if you can flip at most one 0.
+
+        Example 1:
+                Input: nums = [1, 0, 1, 1, 0]
+                Output: 4
+                Explanation: Flip the first zero will get the maximum number of consecutive 1's. After flipping, the maximum number of consecutive 1's is 4.
+
+        Example 2:
+                Input: nums = [1, 0, 1, 1 ,0, 1]
+                Output: 4
+
+        Constraints:
+                1) 1 <= nums.length <= 10^5
+                2) nums[i] is either 0 or 1.
+*/
+
+/*
  * @param { number[] } nums
  * @return { number }
 */
