292baca0f715d40799c6edeb09f07a487179f974
Hardanish Singh <hardanishsingh1@gmail.com>
2022-03-17 08:17:03 -0400

    Squares of a Sorted Array

--- Array/Easy/Squares_of_a_Sorted_Array.js
+++ Array/Easy/Squares_of_a_Sorted_Array.js
@@ -2,13 +2,13 @@
         Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 
         Example 1:
-                Input: nums = [-4,-1,0,3,10]
-                Output: [0,1,9,16,100]
-                Explanation: After squaring, the array becomes [16,1,0,9,100].
-                After sorting, it becomes [0,1,9,16,100].
+                Input: nums = [-4, -1, 0, 3, 10]
+                Output: [0, 1, 9, 16, 100]
+                Explanation: After squaring, the array becomes [16, 1, 0, 9, 100].
+                After sorting, it becomes [0, 1, 9, 16, 100].
         Example 2:
-                Input: nums = [-7,-3,2,3,11]
-                Output: [4,9,9,49,121]
+                Input: nums = [-7, -3, 2, 3, 11]
+                Output: [4, 9, 9, 49, 121]
 
         Constraints:
                 1) 1 <= nums.length <= 10^4
@@ -21,6 +21,7 @@
  * @param { number[] } nums
  * @return { number[] }
 */
+
 var sortedSquares = function( nums ) {
         for( let i=0; i<nums.length; i++ ) {
                 nums[i] = Math.pow( nums[i],2 );
