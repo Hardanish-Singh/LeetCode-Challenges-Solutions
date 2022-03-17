58d89efb9ad5f356832647feea3f72b13e767e37
Hardanish Singh <hardanishsingh1@gmail.com>
2022-03-17 08:10:09 -0400

    Find Numbers with Even Number of Digits

--- Array/Easy/Find_Numbers_with_Even_Number_of_Digits.js
+++ Array/Easy/Find_Numbers_with_Even_Number_of_Digits.js
@@ -2,7 +2,7 @@
         Given an array nums of integers, return how many of them contain an even number of digits.
 
         Example 1:
-                Input: nums = [12,345,2,6,7896]
+                Input: nums = [12, 345, 2, 6, 7896]
                 Output: 2
                 Explanation: 
                         12 contains 2 digits (even number of digits). 
@@ -12,7 +12,7 @@
                         7896 contains 4 digits (even number of digits). 
                         Therefore only 12 and 7896 contain an even number of digits.
         Example 2:
-                Input: nums = [555,901,482,1771]
+                Input: nums = [555, 901, 482, 1771]
                 Output: 1 
                 Explanation: 
                         Only 1771 contains an even number of digits.
@@ -23,12 +23,13 @@
 */
 
 /*
- * @param {number[]} nums
- * @return {number}
+ * @param { number[] } nums
+ * @return { number }
 */
+
 var findNumbers = function( nums ) {
-        let count = 0;
-        for( let i=0; i<nums.length; i++ ) {
+        var count = 0;
+        for( let i = 0; i < nums.length; i++ ) {
                 if( String( nums[i] ).length % 2 === 0 ) {
                         count++;
                 }
