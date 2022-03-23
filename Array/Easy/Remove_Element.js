8c9ec01be700e05bb99e8837f687452320527a57
Hardanish Singh <hardanishsingh1@gmail.com>
2022-03-23 08:05:16 -0400

    Remove Element

--- Array/Easy/Remove_Element.js
+++ Array/Easy/Remove_Element.js
@@ -27,7 +27,7 @@
 
 var removeElement = function( nums, val ) {
         for( let i = nums.length - 1; i >= 0; i-- ) {
-                if(nums[i]===val) {
+                if( nums[i] === val ) {
                         nums.splice(i, 1);
                 }
         }
