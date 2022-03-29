da1db7d4c9f0059b83c30360f474d43cd7ba2aba
Hardanish Singh <hardanishsingh1@gmail.com>
2022-03-29 16:21:59 -0400

    Find Substring With Given Hash Value

--- String/Hard/Find_Substring_With_Given_Hash_Value.py
+++ String/Hard/Find_Substring_With_Given_Hash_Value.py
@@ -8,38 +8,45 @@ class Solution(object):
         :type hashValue: int
         :rtype: str
         """
-        calculateOnce = False;
-        sum = 0;
-        modulus  = 0
-        result = ''
+        hash = 0;
+        product = 0
+        powerData = 1
         addCharacter = ''
         addCharacterValue = 0
         deleteCharacter = ''
         deleteCharacterValue = 0
         i = len( s ) - 1
-        powerData = 1
+        result = ''
         dictionary = {}
         for item in range(97, 123):
                 dictionary[chr(item)] = item - 96
+                
         for c in reversed(s):
                 if( i < k - 1 ):
                         break
                 substring = s[i - k + 1 : i + 1]
-                if( calculateOnce ):
+                if( i != len( s ) - 1 ):
                         addCharacter = substring[0]
                         deleteCharacter = s[ i + 1 ]
-                if( not calculateOnce ):
+                if( i == len( s ) - 1 ):
                         for j in range(0, len(substring)):
-                                sum += dictionary[ substring[j] ] * powerData
-                                powerData *= power
-                        powerData //= power
-                        calculateOnce = True
+                                product = ( ( dictionary[ substring[j] ] % modulo ) * ( powerData % modulo ) ) % modulo
+                                hash = ( ( hash % modulo ) + ( product % modulo ) ) % modulo
+                                powerData = ( ( powerData % modulo ) * ( power % modulo ) ) % modulo
                 else:
-                        deleteCharacterValue = dictionary[ deleteCharacter ] * powerData
+                        deleteCharacterValue = dictionary[ deleteCharacter ]
+                        deleteCharacterValue = ( ( deleteCharacterValue % modulo ) * ( powerData % modulo ) ) % modulo
+                        
+                        # Multipy hash by power
+                        hash = ( ( hash % modulo ) * ( power % modulo ) ) % modulo
+                        
                         addCharacterValue = dictionary[ addCharacter ]
-                        sum = ( ( sum - deleteCharacterValue ) * power ) + addCharacterValue
-                modulus = sum % modulo
-                if( modulus == hashValue):
+                        # Add addCharacterValue to hash
+                        hash = ( ( hash % modulo ) + ( addCharacterValue % modulo ) ) % modulo
+                        
+                        hash = ( ( hash % modulo ) - deleteCharacterValue + modulo ) % modulo
+                if( hash == hashValue ):
                         result = substring
                 i -= 1
+        
         return result
