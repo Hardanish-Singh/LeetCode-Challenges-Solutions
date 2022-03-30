145bcd33f332f4a5946e7d8954056e60c382c45a
Hardanish Singh <hardanishsingh1@gmail.com>
2022-03-30 09:31:24 -0400

    Longest Palindromic Substring

--- String/Medium/Longest_Palindromic_Substring.js
+++ String/Medium/Longest_Palindromic_Substring.js
@@ -1,4 +1,21 @@
 /*
+        Given a string s, return the longest palindromic substring in s.
+
+        Example 1:
+                Input: s = "babad"
+                Output: "bab"
+                Explanation: "aba" is also a valid answer.
+        
+        Example 2:
+                Input: s = "cbbd"
+                Output: "bb"
+
+        Constraints:
+                1) 1 <= s.length <= 1000
+                2) s consist of only digits and English letters.
+*/
+
+/*
  * @param { string } s
  * @return { string }
 */
