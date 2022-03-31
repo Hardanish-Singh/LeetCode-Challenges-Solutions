ff9650c5a283b513de6c063059aed9ebfbce9c4b
Hardanish Singh <hardanishsingh1@gmail.com>
2022-03-31 09:25:42 -0400

    Add Binary

--- String/Easy/Add_Binary.js
+++ String/Easy/Add_Binary.js
@@ -4,27 +4,27 @@
  * @return { string }
 */
 
-var addStrings = function( num1, num2 ) {
-        if( num2.length > num1.length ) {
-                return addStrings( num2, num1 );
+var addBinary = function(a, b) {
+        if( b.length > a.length ) {
+                return addBinary( b, a );
         }
         let carry = 0;
         let sum = 0;
         let answer = '';
-        let j = num2.length-1;
-        let temp = num2[j];
-        for( let i = num1.length-1; i>=0; i-- ){
-                temp = num2[j];
+        let j = b.length-1;
+        let temp = b[j];
+        for( let i = a.length-1; i>=0; i-- ){
+                temp = b[j];
                 if( temp == undefined ) {
                         temp = '0';
                 }
-                if( num1[i] === '1' && temp === '1' ) {
+                if( a[i] === '1' && temp === '1' ) {
                         carry === 1 ? sum = String( 1 ) : sum = String( 0 );
                         carry = 1;
                 } else if( 
-                                ( num1[i] === '1' && temp === '0' ) 
+                                ( a[i] === '1' && temp === '0' ) 
                                         ||
-                                ( num1[i] === '0' && temp === '1' )
+                                ( a[i] === '0' && temp === '1' )
                 ) {
                         carry === 1 ? ( sum = String( 0 ), carry = 1 ) : ( sum = String( 1 ), carry = 0 );
                 } else {
@@ -36,8 +36,4 @@ var addStrings = function( num1, num2 ) {
         }
         carry > 0 ? answer = carry + answer : null;
         return answer.length > 1 && answer.startsWith("0") ? answer.slice(1) : answer;
-};
-
-var addBinary = function(a, b) {
-        return addStrings( a, b );
 };
