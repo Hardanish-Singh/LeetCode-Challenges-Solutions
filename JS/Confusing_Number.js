9199de3803482d5f14adfe42de9c049a0f854c89
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-01 18:25:36 -0400

    Confusing Number

---
+++ JS/Confusing_Number.js
@@ -0,0 +1,37 @@
+/*
+ * @param {number} n
+ * @return {boolean}
+*/
+
+var confusingNumber = function( n ) {
+        var rotation = {
+                0 : '0',
+                1 : '1',
+                6 : '9',
+                8 : '8',
+                9 : '6'
+        };
+        var invalids = {
+                2 : true,
+                3 : true,
+                4 : true,
+                5 : true,
+                7 : true
+        };
+        
+        var s1 = String( n );
+        var s2 = "";
+        
+        for( let i = s1.length - 1; i >= 0; i-- ) {
+                if( invalids[ s1[ i ] ] ) {
+                        return false;
+                }
+                else {
+                        s2 += rotation[ s1[i] ];
+                }
+        }
+        
+        s2 = s2.replace(/^0+/, '');
+        return s1 !== s2 && s2.length > 0 ? true : false;
+
+};
