580f1f08420d786aa3da6d5019fd611d6dfe227f
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-02 21:25:44 -0400

    Strobogrammatic Number

---
+++ String/Easy/Strobogrammatic_Number.js
@@ -0,0 +1,35 @@
+/*
+ * @param { string } num
+ * @return { boolean }
+*/
+
+var isStrobogrammatic = function(n) {
+        var rotation = {
+                0 : '0',
+                1 : '1',
+                6 : '9',
+                8 : '8',
+                9 : '6'
+        };
+        var invalids = {
+                2 : true,
+                3 : true,
+                4 : true,
+                5 : true,
+                7 : true
+        };
+        
+        var s1 = String( n );
+        var s2 = "";
+        
+        for( let i = s1.length - 1; i >= 0; i-- ) {
+                if( invalids[ s1[ i ] ] ) {
+                        return false;
+                }
+                else {
+                        s2 += rotation[ s1[i] ];
+                }
+        }
+        
+        return s1 === s2 ? true : false;
+};
