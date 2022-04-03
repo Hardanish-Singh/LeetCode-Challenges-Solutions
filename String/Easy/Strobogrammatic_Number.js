aacda3b54262a9b243c90b075a5f0e08c8acfc69
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-02 21:27:04 -0400

    Strobogrammatic Number

--- String/Easy/Strobogrammatic_Number.js
+++ String/Easy/Strobogrammatic_Number.js
@@ -1,4 +1,26 @@
 /*
+        Given a string num which represents an integer, return true if num is a strobogrammatic number.
+        A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+
+        Example 1:
+                Input: num = "69"
+                Output: true
+        
+        Example 2:
+                Input: num = "88"
+                Output: true
+        
+        Example 3:
+                Input: num = "962"
+                Output: false
+
+        Constraints:
+                1) 1 <= num.length <= 50
+                2) num consists of only digits.
+                3) num does not contain any leading zeros except for zero itself.
+*/
+
+/*
  * @param { string } num
  * @return { boolean }
 */
