c6577b9df42dc2733ca29b4c7c328c35ef41f788
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-05 07:49:32 -0400

    Search in Rotated Sorted Array

---
+++ Array/Medium/Search_in_Rotated_Sorted_Array.js
@@ -0,0 +1,32 @@
+/*
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+*/
+
+var search = function(array, target) {
+        
+        let startIndex = 0;
+        let endIndex = array.length - 1;
+        while( startIndex <= endIndex ) {
+                let middleIndex = Math.floor( (startIndex + endIndex) / 2 );
+                if( target === array[middleIndex] ) {
+                        return middleIndex;
+                }
+                if( array[startIndex] <= array[middleIndex] ) {
+                        if( target < array[middleIndex] && target >= array[startIndex] ) {
+                                endIndex = middleIndex - 1;
+                        } else {
+                                startIndex = middleIndex + 1
+                        }
+                }
+                else {
+                        if( target > array[middleIndex] && target <= array[endIndex] ) {
+                                startIndex = middleIndex + 1;
+                        } else {
+                                endIndex = middleIndex - 1;
+                        }
+                }
+        }
+        return -1;
+};
