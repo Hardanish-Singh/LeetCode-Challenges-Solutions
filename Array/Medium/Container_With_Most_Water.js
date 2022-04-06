c3a48ffa7d170d0a1d26f1bc90a04c970b5b7e7e
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-06 08:33:32 -0400

    Container With Most Water

--- Array/Medium/Container_With_Most_Water.js
+++ Array/Medium/Container_With_Most_Water.js
@@ -6,7 +6,7 @@
         Notice that you may not slant the container.
 
         Example 1:
-                Input: height = [1,8,6,2,5,4,8,3,7]
+                Input: height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
                 Output: 49
                 Explanation: The above vertical lines are represented by array [1, 8, 6, 2, 5, 4, 8, 3, 7]. 
                 In this case, the max area of water (blue section) the container can contain is 49.
