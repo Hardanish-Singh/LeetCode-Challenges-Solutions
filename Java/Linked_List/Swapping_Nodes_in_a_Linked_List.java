725a8f93254d92c3482972e15fe421839de6fc44
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-06 08:38:35 -0400

    Swapping Nodes in a Linked List

--- Java/Linked_List/Swapping_Nodes_in_a_Linked_List.java
+++ Java/Linked_List/Swapping_Nodes_in_a_Linked_List.java
@@ -1,6 +1,10 @@
 /*
         You are given the head of a linked list, and an integer k.
         Return the head of the linked list after swapping the values of the k^th node from the beginning and the k^th node from the end (the list is 1-indexed).
+        
+        Example 1:
+                Input: head = [1, 2, 3, 4, 5], k = 2
+                Output: [1, 4, 3, 2, 5]
 */
 
 /*
