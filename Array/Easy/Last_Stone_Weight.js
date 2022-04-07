0324904ab92d8639a2f4916e814fab71c7ba2392
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-07 08:57:21 -0400

    Last Stone Weight

--- Array/Easy/Last_Stone_Weight.js
+++ Array/Easy/Last_Stone_Weight.js
@@ -7,6 +7,19 @@
         At the end of the game, there is at most one stone left.
 
         Return the smallest possible weight of the left stone. If there are no stones left, return 0.
+
+        Example 1:
+                Input: stones = [2, 7, 4, 1, 8, 1]
+                Output: 1
+                Explanation: 
+                We combine 7 and 8 to get 1 so the array converts to [2, 4, 1, 1, 1] then,
+                we combine 2 and 4 to get 2 so the array converts to [2, 1, 1, 1] then,
+                we combine 2 and 1 to get 1 so the array converts to [1, 1, 1] then,
+                we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
+        
+        Example 2:
+                Input: stones = [1]
+                Output: 1
 */
 
 /*
