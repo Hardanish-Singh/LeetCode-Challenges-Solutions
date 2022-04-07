20ace55128381ada9757e04c7297ef67037ac5b0
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-07 08:56:11 -0400

    Last Stone Weight

--- Array/Easy/Last_Stone_Weight.js
+++ Array/Easy/Last_Stone_Weight.js
@@ -1,4 +1,15 @@
 /*
+        You are given an array of integers stones where stones[i] is the weight of the i^th stone.
+        We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. 
+        The result of this smash is:
+                1) If x == y, both stones are destroyed, and
+                2) If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+        At the end of the game, there is at most one stone left.
+
+        Return the smallest possible weight of the left stone. If there are no stones left, return 0.
+*/
+
+/*
  * @param { number[] } stones
  * @return { number }
 */
