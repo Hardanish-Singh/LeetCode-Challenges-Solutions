b5b9422eff21e43aa51f4f8cced6b20834a6a34f
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-11 22:19:47 -0400

    3Sum With Multiplicity

--- Array/Medium/Three_Sum/3Sum_With_Multiplicity.js
+++ Array/Medium/Three_Sum/3Sum_With_Multiplicity.js
@@ -33,7 +33,7 @@
  * @return { number }
 */
 
-// SOLUTION 1 ( USING THREE POINTER ) 
+// SOLUTION 1 ( USING THREE POINTER )
 var threeSumMulti = function( nums, target ) {
         nums.sort( (a, b) => a - b );
         let count = 0;
