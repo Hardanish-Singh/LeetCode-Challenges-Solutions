bf5ffceb7bce7ad53c7e2461f0445188f9e28c40
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-21 06:25:47 -0400

    Contains Duplicate II

--- Array/Easy/Contains_Duplicate_II.js
+++ Array/Easy/Contains_Duplicate_II.js
@@ -1,4 +1,25 @@
 /*
+        Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+        Example 1:
+                Input: nums = [1, 2, 3, 1], k = 3
+                Output: true
+
+        Example 2:
+                Input: nums = [1, 0, 1, 1], k = 1
+                Output: true
+
+        Example 3:
+                Input: nums = [1, 2, 3, 1, 2, 3], k = 2
+                Output: false
+ 
+        Constraints:
+                1) 1 <= nums.length <= 10^5
+                2) -10^9 <= nums[i] <= 10^9
+                3) 0 <= k <= 10^5
+*/
+
+/*
  * @param { number[] } nums
  * @param { number } k
  * @return { boolean }
