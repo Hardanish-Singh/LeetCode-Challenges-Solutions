e23ffe5a05e2844a496ce153a631234568fb7a21
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-21 06:29:01 -0400

    Contains Duplicate III

--- Array/Medium/Contains_Duplicate_III.js
+++ Array/Medium/Contains_Duplicate_III.js
@@ -1,4 +1,20 @@
 /*
+        Given an integer array nums and two integers k and t, return true if there are two distinct indices i and j in the array such that abs(nums[i] - nums[j]) <= t and abs(i - j) <= k.
+
+        Example 1:
+                Input: nums = [1, 2, 3, 1], k = 3, t = 0
+                Output: true
+        
+        Example 2:
+                Input: nums = [1, 0, 1, 1], k = 1, t = 2
+                Output: true
+        
+        Example 3:
+                Input: nums = [1, 5, 9, 1, 5, 9], k = 2, t = 3
+                Output: false
+*/
+
+/*
  * @param { number[] } nums
  * @param { number } k
  * @param { number } t
