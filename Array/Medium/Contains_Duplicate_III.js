01c8a1283add03856a3753f137c9e562d8905b1f
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-21 06:27:50 -0400

    Contains Duplicate III

--- Array/Medium/Contains_Duplicate_III.js
+++ Array/Medium/Contains_Duplicate_III.js
@@ -5,7 +5,7 @@
  * @return { boolean }
 */
 
-var containsNearbyAlmostDuplicate = function(nums, k, t) {
+var containsNearbyAlmostDuplicate = function( nums, k, t ) {
         for( let i = 0; i < nums.length - 1; i++ ) {
                 for( let j = i + 1; j < nums.length; j++ ) {
                         if( Math.abs( nums[ i ] - nums[ j ] ) <= t && Math.abs( i - j ) <= k ) {
