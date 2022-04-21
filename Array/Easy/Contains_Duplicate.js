eee7e58872d9034da9c46d4e989d73aea5fecee0
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-21 06:17:35 -0400

    Contains Duplicate

--- Array/Easy/Contains_Duplicate.js
+++ Array/Easy/Contains_Duplicate.js
@@ -2,20 +2,20 @@
         Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
         Example 1:
-                Input: nums = [1,2,3,1]
+                Input: nums = [1, 2, 3, 1]
                 Output: true
 
         Example 2:
-                Input: nums = [1,2,3,4]
+                Input: nums = [1, 2, 3, 4]
                 Output: false
                 
         Example 3:
-                Input: nums = [1,1,1,3,3,4,3,2,4,2]
+                Input: nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
                 Output: true
  
         Constraints:
-                1) 1 <= nums.length <= 105
-                2) -109 <= nums[i] <= 109
+                1) 1 <= nums.length <= 10^5
+                2) -10^9 <= nums[i] <= 10^9
 */
 
 /*
