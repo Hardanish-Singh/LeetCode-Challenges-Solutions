66cca9de9659e7c5a8d89fc8f22c86ba4bb54bbb
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-30 13:05:39 -0400

    Number of Islands

--- Array/Medium/Number_of_Islands.js
+++ Array/Medium/Number_of_Islands.js
@@ -87,12 +87,7 @@ const move_right = ( grid, i, j ) => {
         return null;
 }
 
-const move_down = ( grid, i, j ) => {
-        ++i;
-        if( i <= grid.length - 1 ) {
-                return is_visited( grid, i, j );  
-        }
-}
+const move_down = ( grid, i, j ) => i <= grid.length - 1 ? is_visited( grid, i, j ) : null;
 
 const move_in_all_four_directions = ( grid, i, j, queue ) => {
         let position = "";
@@ -114,7 +109,7 @@ const move_in_all_four_directions = ( grid, i, j, queue ) => {
                 queue.push(position);
         }
         // MOVE DOWN & ADD COORDINATES TO QUEUE
-        position = move_down( grid, i, j );
+        position = move_down( grid, ++i, j );
         if( position ) {
                 queue.push(position);
         }
