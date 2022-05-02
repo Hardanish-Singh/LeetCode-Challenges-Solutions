08f1be394e4098e9e9192e0bb6cc25c33ac83e58
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-02 09:29:22 -0400

    Min Stack

--- Array/Easy/Min_Stack.java
+++ Array/Easy/Min_Stack.java
@@ -1,3 +1,13 @@
+/*
+        Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+        Implement the MinStack class:
+                1) MinStack() initializes the stack object.
+                2) void push(int val) pushes the element val onto the stack.
+                3) void pop() removes the element on the top of the stack.
+                4) int top() gets the top element of the stack.
+                5) int getMin() retrieves the minimum element in the stack.
+*/
+
 import java.util.*;
 
 class MinStack {
