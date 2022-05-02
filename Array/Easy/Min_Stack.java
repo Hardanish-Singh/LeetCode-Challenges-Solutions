58fd3cdbe5ed62df136b315e0419af6cfce3f16a
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-02 09:33:35 -0400

    Min Stack

--- Array/Easy/Min_Stack.java
+++ Array/Easy/Min_Stack.java
@@ -63,11 +63,11 @@ class MinStack {
         }
 }
 
-/**
+/*
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
  * obj.push(val);
  * obj.pop();
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
- */
+*/
