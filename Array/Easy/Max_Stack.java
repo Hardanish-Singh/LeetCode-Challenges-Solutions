f3e8cb492b925437464fc2df5ffcdeec8357739a
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-02 09:36:20 -0400

    Max Stack

--- Array/Easy/Max_Stack.java
+++ Array/Easy/Max_Stack.java
@@ -1,3 +1,14 @@
+/*
+        Design a max stack data structure that supports the stack operations and supports finding the stack's maximum element.
+        Implement the MaxStack class:
+                1) MaxStack() Initializes the stack object.
+                2) void push(int x) Pushes element x onto the stack.
+                3) int pop() Removes the element on top of the stack and returns it.
+                4) int top() Gets the element on the top of the stack without removing it.
+                5) int peekMax() Retrieves the maximum element in the stack without removing it.
+                6) int popMax() Retrieves the maximum element in the stack and removes it. If there is more than one maximum element, only remove the top-most one.
+*/
+
 import java.util.*;
 
 class MaxStack 
