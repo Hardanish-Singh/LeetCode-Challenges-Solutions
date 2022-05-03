673dca024e620e336ed533011f7d8a9363cea132
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-03 07:12:55 -0400

    Letter Combinations of a Phone Number

--- JS/letter_combinations_of_a_phone_number.js
+++ JS/letter_combinations_of_a_phone_number.js
@@ -5,17 +5,18 @@
 
         Example 1:
                 Input: digits = "23"
-                Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+                Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
         Example 2:
                 Input: digits = ""
                 Output: []
         Example 3:
                 Input: digits = "2"
-                Output: ["a","b","c"]
+                Output: ["a", "b", "c"]
         Constraints:
                 0 <= digits.length <= 4
                 digits[i] is a digit in the range ['2', '9'].
 */
+
 const phoneNumberLetterCombinations = {
         '0' : '0',
         '1' : '1',
