d9bae7d184315cb7167b43335e8e62dd88ea590c
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-08 15:04:15 -0400

    Rotate Array

--- Array/Medium/Rotate_Array.js
+++ Array/Medium/Rotate_Array.js
@@ -5,9 +5,9 @@
                 Input: nums = [1, 2, 3, 4, 5, 6, 7], k = 3
                 Output: [5, 6, 7, 1, 2, 3, 4]
                 Explanation:
-                        rotate 1 steps to the right: [7,1,2,3,4,5,6]
-                        rotate 2 steps to the right: [6,7,1,2,3,4,5]
-                        rotate 3 steps to the right: [5,6,7,1,2,3,4]
+                        rotate 1 steps to the right: [7, 1, 2, 3, 4, 5, 6]
+                        rotate 2 steps to the right: [6, 7, 1, 2, 3, 4, 5]
+                        rotate 3 steps to the right: [5, 6, 7, 1, 2, 3, 4]
         Example 2:
                 Input: nums = [-1,-100,3,99], k = 2
                 Output: [3,99,-1,-100]
