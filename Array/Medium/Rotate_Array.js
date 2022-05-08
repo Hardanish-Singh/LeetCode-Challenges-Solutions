fea8edb8cdf5df48cccbb0d7c5f1a44feb7c32c5
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-08 15:03:56 -0400

    Rotate Array

--- Array/Medium/Rotate_Array.js
+++ Array/Medium/Rotate_Array.js
@@ -2,8 +2,8 @@
         Given an array, rotate the array to the right by k steps, where k is non-negative.    
 
         Example 1:
-                Input: nums = [1,2,3,4,5,6,7], k = 3
-                Output: [5,6,7,1,2,3,4]
+                Input: nums = [1, 2, 3, 4, 5, 6, 7], k = 3
+                Output: [5, 6, 7, 1, 2, 3, 4]
                 Explanation:
                         rotate 1 steps to the right: [7,1,2,3,4,5,6]
                         rotate 2 steps to the right: [6,7,1,2,3,4,5]
