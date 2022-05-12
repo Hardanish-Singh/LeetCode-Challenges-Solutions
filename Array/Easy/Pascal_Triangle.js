3402bd6c90d812b676b520c7b33c256bd39e8e3d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-12 08:37:15 -0400

    Pascal's Triangle

--- Array/Easy/Pascal_Triangle.js
+++ Array/Easy/Pascal_Triangle.js
@@ -5,10 +5,18 @@
 
         Example 1:
                 Input: numRows = 5
-                Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+                Output: [
+                                [1],
+                                [1,1],
+                                [1,2,1],
+                                [1,3,3,1],
+                                [1,4,6,4,1]
+                        ]
         Example 2:
                 Input: numRows = 1
-                Output: [[1]]
+                Output: [
+                                [1]
+                        ]
 */
 
 /**
