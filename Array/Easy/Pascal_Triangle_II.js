ca91540fa1f8dfaa3249fcb751ada3182eb83226
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-12 08:59:37 -0400

    Pascal's Triangle II

--- Array/Easy/Pascal_Triangle_II.js
+++ Array/Easy/Pascal_Triangle_II.js
@@ -1,3 +1,9 @@
+/*
+        Given an integer rowIndex, return the rowIndex^th (0-indexed) row of the Pascal's triangle.
+
+        In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+*/
+
 /**
  * @param { number } rowIndex
  * @return { number[] }
