adcf1ead95851652b5b468bb02510502431acf7d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-12 09:01:20 -0400

    Pascal's Triangle II

--- Array/Easy/Pascal_Triangle_II.js
+++ Array/Easy/Pascal_Triangle_II.js
@@ -6,9 +6,11 @@
         Example 1:
                 Input: rowIndex = 3
                 Output: [1, 3, 3, 1]
+        
         Example 2:
                 Input: rowIndex = 0
                 Output: [1]
+        
         Example 3:
                 Input: rowIndex = 1
                 Output: [1, 1]
