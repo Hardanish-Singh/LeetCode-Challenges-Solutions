0f1448fe0dc3926094b6ce5b26138816391a8f75
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-18 16:26:27 -0400

    Fizz Buzz

--- JS/Fizz_Buzz.js
+++ JS/Fizz_Buzz.js
@@ -17,6 +17,9 @@
         Example 3:
                 Input: n = 15
                 Output: ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
+        
+        Constraints:
+                1) 1 <= n <= 10^4
 */
 
 /**
