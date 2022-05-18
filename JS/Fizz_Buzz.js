068ff5b8c69fe837dadb23eaf3c3a9b783751904
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-18 16:25:09 -0400

    Fizz Buzz

--- JS/Fizz_Buzz.js
+++ JS/Fizz_Buzz.js
@@ -1,3 +1,22 @@
+/*
+        Given an integer n, return a string array answer (1-indexed) where:
+                1) answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+                2) answer[i] == "Fizz" if i is divisible by 3.
+                3) answer[i] == "Buzz" if i is divisible by 5.
+                4) answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+        Example 1:
+                Input: n = 3
+                Output: ["1","2","Fizz"]
+        Example 2:
+                Input: n = 5
+                Output: ["1","2","Fizz","4","Buzz"]
+        Example 3:
+                Input: n = 15
+                Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+*/
+
 /**
  * @param { number } n
  * @return { string[] }
