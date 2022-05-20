fad0e7954b0a1d2e36eb62db1b89d9bc5325bafd
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-19 20:29:11 -0400

    Next Greater Element I

--- Array/Easy/Next_Greater_Element_I.js
+++ Array/Easy/Next_Greater_Element_I.js
@@ -1,6 +1,9 @@
 /*
         The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
         You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+        For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. 
+        If there is no next greater element, then the answer for this query is -1.
 */
 
 /*
