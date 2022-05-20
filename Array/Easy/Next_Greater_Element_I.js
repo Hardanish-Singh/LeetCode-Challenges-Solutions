acb3ae17c473280f1b7831354ac493790141b24c
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-19 20:30:04 -0400

    Next Greater Element I

--- Array/Easy/Next_Greater_Element_I.js
+++ Array/Easy/Next_Greater_Element_I.js
@@ -8,7 +8,8 @@
         Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
 
         Example 1:
-                Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+                Input: 
+                        nums1 = [4, 1, 2], nums2 = [1, 3, 4, 2]
                 Output: [-1,3,-1]
                 Explanation: The next greater element for each value of nums1 is as follows:
                 - 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
