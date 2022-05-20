3adf541f653667906f4e328fe291837206e8092b
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-19 20:28:52 -0400

    Next Greater Element I

--- Array/Easy/Next_Greater_Element_I.js
+++ Array/Easy/Next_Greater_Element_I.js
@@ -1,4 +1,9 @@
 /*
+        The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+        You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+*/
+
+/*
  * @param { number[] } nums1
  * @param { number[] } nums2
  * @return { number[] }
