83b3121f3203265ff8fa9e7b60186d86c9a0db26
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-24 11:20:24 -0400

    Sum of Subarray Ranges

--- Array/Medium/Sum_of_Subarray_Ranges.js
+++ Array/Medium/Sum_of_Subarray_Ranges.js
@@ -6,14 +6,15 @@
         Example 1:
                 Input: nums = [1, 2, 3]
                 Output: 4
-                Explanation: The 6 subarrays of nums are the following:
-                [1], range = largest - smallest = 1 - 1 = 0 
-                [2], range = 2 - 2 = 0
-                [3], range = 3 - 3 = 0
-                [1,2], range = 2 - 1 = 1
-                [2,3], range = 3 - 2 = 1
-                [1,2,3], range = 3 - 1 = 2
-                So the sum of all ranges is 0 + 0 + 0 + 1 + 1 + 2 = 4.
+                Explanation: 
+                        The 6 subarrays of nums are the following:
+                                [1], range = largest - smallest = 1 - 1 = 0 
+                                [2], range = 2 - 2 = 0
+                                [3], range = 3 - 3 = 0
+                                [1,2], range = 2 - 1 = 1
+                                [2,3], range = 3 - 2 = 1
+                                [1,2,3], range = 3 - 1 = 2
+                                So the sum of all ranges is 0 + 0 + 0 + 1 + 1 + 2 = 4.
 */
 
 /**
