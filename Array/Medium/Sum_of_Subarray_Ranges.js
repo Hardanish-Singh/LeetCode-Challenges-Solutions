07668b60ca89db16f7ae6d1e13d8a4b98b5bb800
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-24 11:20:10 -0400

    Sum of Subarray Ranges

--- Array/Medium/Sum_of_Subarray_Ranges.js
+++ Array/Medium/Sum_of_Subarray_Ranges.js
@@ -1,3 +1,21 @@
+/*
+        You are given an integer array nums. The range of a subarray of nums is the difference between the largest and smallest element in the subarray.
+        Return the sum of all subarray ranges of nums.
+        A subarray is a contiguous non-empty sequence of elements within an array.
+
+        Example 1:
+                Input: nums = [1, 2, 3]
+                Output: 4
+                Explanation: The 6 subarrays of nums are the following:
+                [1], range = largest - smallest = 1 - 1 = 0 
+                [2], range = 2 - 2 = 0
+                [3], range = 3 - 3 = 0
+                [1,2], range = 2 - 1 = 1
+                [2,3], range = 3 - 2 = 1
+                [1,2,3], range = 3 - 1 = 2
+                So the sum of all ranges is 0 + 0 + 0 + 1 + 1 + 2 = 4.
+*/
+
 /**
  * @param { number[] } nums
  * @return { number }
