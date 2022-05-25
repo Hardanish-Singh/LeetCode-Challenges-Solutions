367892f5dded3bba633e784d6097715ed770279f
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-25 07:50:50 -0400

    Find First and Last Position of Element in Sorted Array

--- Array/Medium/Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
+++ Array/Medium/Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
@@ -3,16 +3,16 @@
         If target is not found in the array, return [-1, -1].
 
         Example 1:
-                Input: nums = [5,7,7,8,8,10], target = 8
-                Output: [3,4]
+                Input: nums = [5, 7, 7, 8, 8, 10], target = 8
+                Output: [3, 4]
         
         Example 2:
-                Input: nums = [5,7,7,8,8,10], target = 6
-                Output: [-1,-1]
+                Input: nums = [5, 7, 7, 8, 8, 10], target = 6
+                Output: [-1, -1]
 
         Example 3:
                 Input: nums = [], target = 0
-                Output: [-1,-1]
+                Output: [-1, -1]
 */
 
 /**
