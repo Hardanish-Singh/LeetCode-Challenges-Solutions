a81d29cbd4440b3d790eb458fa9b9dbb9529c46a
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-25 07:48:41 -0400

    Find First and Last Position of Element in Sorted Array

---
+++ Array/Medium/Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var searchRange = function(nums, target) {
+        return[ nums.indexOf( target ), nums.lastIndexOf( target ) ];
+};
