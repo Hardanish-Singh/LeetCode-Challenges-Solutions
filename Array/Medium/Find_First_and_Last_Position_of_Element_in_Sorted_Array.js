d0dab7f78f67919e53fe11b9ac95e3f3ba59eb0f
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-25 07:49:43 -0400

    Find First and Last Position of Element in Sorted Array

--- Array/Medium/Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
+++ Array/Medium/Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
@@ -1,9 +1,15 @@
+/*
+        Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+        If target is not found in the array, return [-1, -1].
+        You must write an algorithm with O(log n) runtime complexity.
+*/
+
 /**
  * @param { number[] } nums
  * @param { number } target
  * @return { number[] }
- */
+*/
 
-var searchRange = function(nums, target) {
+var searchRange = function( nums, target ) {
         return[ nums.indexOf( target ), nums.lastIndexOf( target ) ];
 };
