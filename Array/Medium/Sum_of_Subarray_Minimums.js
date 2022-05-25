6aa4562be2c24b4d68052f86c8a27f709a076c4d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-25 07:43:02 -0400

    Sum of Subarray Minimums

---
+++ Array/Medium/Sum_of_Subarray_Minimums.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+ var sumSubarrayMins = function(array) {
+        var sum = 0;
+            for( let i = 0; i < array.length; i++ ) {
+                    let min = array[i];
+                    sum += min;
+                    for( let j = i + 1; j < array.length; j++ ) {
+                            min = Math.min( min, array[j] );
+                            sum += min;
+                    }
+            }
+            return sum % 1000000007;
+    };
