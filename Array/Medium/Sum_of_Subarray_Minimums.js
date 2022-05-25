0202f3f00481ae7dea2044c411e160f39a09a677
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-25 07:44:28 -0400

    Sum of Subarray Minimums

--- Array/Medium/Sum_of_Subarray_Minimums.js
+++ Array/Medium/Sum_of_Subarray_Minimums.js
@@ -1,3 +1,8 @@
+/*
+        Given an array of integers arr, find the sum of min(b), where b ranges over every (contiguous) subarray of arr. 
+        Since the answer may be large, return the answer modulo 10^9 + 7.
+*/
+
 /**
  * @param { number[] } arr
  * @return { number }
