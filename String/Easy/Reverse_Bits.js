8342bb9f154a82e9d8d1c2ca840a962eef1bb834
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-26 08:04:32 -0400

    Reverse Bits

--- String/Easy/Reverse_Bits.js
+++ String/Easy/Reverse_Bits.js
@@ -1,3 +1,14 @@
+/*
+        Reverse bits of a given 32 bits unsigned integer.
+
+        Note:
+                1) Note that in some languages, such as Java, there is no unsigned integer type. 
+                   In this case, both input and output will be given as a signed integer type. 
+                   They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+                2) In Java, the compiler represents the signed integers using 2's complement notation. 
+                   Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.
+*/
+
 /**
  * @param { number } n 
  * @return { number } 
