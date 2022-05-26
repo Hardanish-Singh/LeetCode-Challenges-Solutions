bba7c26227ec155b80a83c600dfeb736fb111c27
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-26 08:09:03 -0400

    Happy Number

--- JS/Happy_Number.js
+++ JS/Happy_Number.js
@@ -5,21 +5,21 @@
 
 var isHappy = function( n ) {
         let c = 0;
-    while( true ) {
-            c++;
-            if( c > 7 ) break;
-            if( n == 1 ) {
-                    return true;
-            }
-            let str = String( n );
-            
-            let splitData = str.split("");
-            let sum = 0;
-            for( let i = 0; i < splitData.length; i++ ) {
-                    sum += Number( splitData[ i ] ) * Number( splitData[ i ] );
-            }
-            
-            n = sum;
-    }
-    return false;  
+        while( true ) {
+                c++;
+                if( c > 7 ) {
+                        break;
+                }
+                if( n == 1 ) {
+                        return true;
+                }
+                let str = String( n );
+                let splitData = str.split("");
+                let sum = 0;
+                for( let i = 0; i < splitData.length; i++ ) {
+                        sum += Number( splitData[ i ] ) * Number( splitData[ i ] );
+                }
+                n = sum;
+        }
+        return false;  
 };
