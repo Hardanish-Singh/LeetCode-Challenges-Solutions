596e264436709c4aea8a46af0268e928e8dafaf6
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-26 08:05:22 -0400

    Reverse Bits

--- String/Easy/Reverse_Bits.js
+++ String/Easy/Reverse_Bits.js
@@ -7,6 +7,19 @@
                    They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
                 2) In Java, the compiler represents the signed integers using 2's complement notation. 
                    Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.
+
+        Example 1:
+                Input: n = 00000010100101000001111010011100
+                Output:    964176192 (00111001011110000010100101000000)
+                Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+        
+        Example 2:
+                Input: n = 11111111111111111111111111111101
+                Output:   3221225471 (10111111111111111111111111111111)
+                Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+        
+        Constraints:
+                1) The input must be a binary string of length 32
 */
 
 /**
