249bb14f5a0eb2e41adc232f8a96fccc2fc883fe
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-29 08:12:16 -0400

    Climbing Stairs

---
+++ JS/Climbing_Stairs.js
@@ -0,0 +1,7 @@
+var climbStairs = function(n) {
+        let stairs = [0, 1, 2];        
+        for( let i = 3; i <= n; i++ ) {
+                stairs[ i ] = stairs[ i - 1 ] + stairs[ i - 2 ]; 
+        }
+        return stairs[n];
+}
