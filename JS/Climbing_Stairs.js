444e468bd98b2e11d8c52a27d0df72ea2ae1d352
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-30 07:25:56 -0400

    Climbing Stairs

--- JS/Climbing_Stairs.js
+++ JS/Climbing_Stairs.js
@@ -1,6 +1,6 @@
 /*
         You are climbing a staircase. It takes n steps to reach the top.
-        Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+        Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top ?
 
         Example 1:
                 Input: n = 2
@@ -16,6 +16,9 @@
                 1. 1 step + 1 step + 1 step
                 2. 1 step + 2 steps
                 3. 2 steps + 1 step
+        
+        Constraints:
+                1) 1 <= n <= 45
 */
 
 
