b261a490a5d7cc348db0cdf4fb5d0a0efd342f0d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-30 07:22:20 -0400

    Climbing Stairs

--- JS/Climbing_Stairs.js
+++ JS/Climbing_Stairs.js
@@ -1,4 +1,23 @@
+var climbStairs = function(n) {
+        let stairs = new Set();
+        stairs.add( '1'.repeat( n ) );
+        const iterator = stairs.values();
+        let stair = iterator.next().value;
+        let count = 0;
 
+        while( stair !== undefined ) {
+        let temp = stair;
+        let t1 = temp.slice( 0, stair.lastIndexOf( 2 ) + 1 );
+        for( let i = stair.lastIndexOf( 2 ) + 1; i < stair.length - 1; i++ ) {
+                stairs.add( t1 + 2 +  temp.slice( i + 2 ) );
+                t1 += temp[i];
+        }
+        stairs.delete( stair );
+        count++;
+        stair = iterator.next().value;
+        }
+        return count;
+};
 
 // SOLUTION 2
 var climbStairs = function( n ) {
