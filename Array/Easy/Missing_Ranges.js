fbe8afb01901e1c2bdb369ddf7cd21f61926b835
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-04 08:37:28 -0400

    Missing Ranges

--- Array/Easy/Missing_Ranges.js
+++ Array/Easy/Missing_Ranges.js
@@ -1,3 +1,10 @@
+/*
+        You are given an inclusive range [lower, upper] and a sorted unique integer array nums, where all elements are in the inclusive range.
+        A number x is considered missing if x is in the range [lower, upper] and x is not in nums.
+        Return the smallest sorted list of ranges that cover every missing number exactly. That is, no element of nums is in any of the ranges, and each missing number is in one of the ranges.
+        Each range [a,b] in the list should be output as:
+*/
+
 /**
  * @param { number[] } nums
  * @param { number } lower
