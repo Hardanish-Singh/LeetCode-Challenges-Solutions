c41734e1869ba9d270a937cea10ec5be3de5913f
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-09 07:01:38 -0400

    Inorder Tree Traversal

---
+++ Tree/Inorder_Traversal.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+def inorderRecursiveTraversal( root, answer ):
+        if root is None:
+                return
+        
+        inorderRecursiveTraversal( root.left, answer )
+        answer.append( root.val )
+        inorderRecursiveTraversal( root.right, answer )
+        
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        
+        answer = []
+        inorderRecursiveTraversal( root, answer )
+        return answer
