d9191eb4fcf64ee4664479c2ec637907cb92acad
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-09 07:01:09 -0400

    Preorder Tree Traversal

---
+++ Tree/Preorder_Traversal.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+def preorderRecursiveTraversal( root, answer ):
+                if root is None:
+                        return
+                
+                answer.append( root.val )
+                
+                preorderRecursiveTraversal( root.left, answer)
+                preorderRecursiveTraversal( root.right, answer )
+                
+class Solution(object):
+        def preorderTraversal(self, root):
+                """
+                :type root: TreeNode
+                :rtype: List[int]
+                """
+                answer = []
+                preorderRecursiveTraversal( root, answer )
+                return answer
