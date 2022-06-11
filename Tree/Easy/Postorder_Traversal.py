0416873ea3c14b9fe86a31d23193e145d6cb9885
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-11 08:51:28 -0400

    Postorder Tree Traversal

--- Tree/Easy/Postorder_Traversal.py
+++ Tree/Easy/Postorder_Traversal.py
@@ -34,9 +34,9 @@ class Solution(object):
         def postorderTraversal(self, root):
                 postOrderList, stack = [], [root]
                 while stack:
-                        node = stack.pop()
-                        if node:
-                                postOrderList.append(node.val)
-                                stack.append(node.left)
-                                stack.append(node.right)
+                        currentNode = stack.pop()
+                        if currentNode:
+                                postOrderList.append(currentNode.val)
+                                stack.append(currentNode.left)
+                                stack.append(currentNode.right)
                 return postOrderList[::-1]
