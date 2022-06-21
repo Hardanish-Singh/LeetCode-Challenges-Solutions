e82a1419b2dc0ce60e9181ab9349171dce744b2c
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-21 16:33:13 -0400

    Postorder Tree Traversal

--- Tree/Easy/Postorder_Traversal.py
+++ Tree/Easy/Postorder_Traversal.py
@@ -1,8 +1,8 @@
 # Leetcode: https://leetcode.com/problems/binary-tree-postorder-traversal/
 
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
+# class TreeNode( object ):
+#     def __init__( self, val = 0, left = None, right = None ):
 #         self.val = val
 #         self.left = left
 #         self.right = right
