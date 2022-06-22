2858f700609e6e9b3101c5f560ebe26aabdd0e3a
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-22 11:18:22 -0400

    Valid Binary Search Tree

--- Tree/Medium/Validate_Binary_Search_Tree.py
+++ Tree/Medium/Validate_Binary_Search_Tree.py
@@ -1,3 +1,12 @@
+# Leetcode: https://leetcode.com/problems/validate-binary-search-tree/
+
+# Definition for a binary tree node.
+# class TreeNode( object ):
+#     def __init__( self, val = 0, left = None, right = None ):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
 """
         SOLUTION 1: RECURSIVE
 """
