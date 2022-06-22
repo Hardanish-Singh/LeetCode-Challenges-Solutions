c55d571c894de7a15363ef8c32efcc6aae3872c0
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-22 09:46:59 -0400

    Lowest Common Ancestor of a Binary Tree

--- Tree/Medium/Lowest_Common_Ancestor_of_a_Binary_Tree.py
+++ Tree/Medium/Lowest_Common_Ancestor_of_a_Binary_Tree.py
@@ -33,3 +33,55 @@ class Solution( object ):
                         return left
                 else:
                         return right
+
+"""
+        SOLUTION 2: ITERATIVE
+"""
+
+class Solution( object ):
+        def lowestCommonAncestor( self, root, p, q ):
+                """
+                :type root: TreeNode
+                :type p: TreeNode
+                :type q: TreeNode
+                :rtype: TreeNode
+                """
+                parentNodeReference = { root.val: float('inf') }
+                test = { root: None }
+                stack = [ root ]
+                
+                while len( stack ) > 0:
+                        currentNode = stack.pop()
+                        if currentNode.left:
+                                stack.append( currentNode.left )
+                                parentNodeReference[ currentNode.left.val ] = currentNode.val
+                                test[ currentNode.left ] = currentNode
+                        if currentNode.right:
+                                stack.append( currentNode.right )
+                                parentNodeReference[ currentNode.right.val ] = currentNode.val
+                                test[ currentNode.right ] = currentNode
+                
+                pList = [ p.val ]
+                qList = [ q.val ]
+                
+                key = p.val
+                while( key in parentNodeReference ):
+                        pList.append( parentNodeReference[ key ] )
+                        key = parentNodeReference[ key ]
+                
+                key = q.val
+                while( key in parentNodeReference ):
+                        qList.append( parentNodeReference[ key ] )
+                        key = parentNodeReference[ key ]
+                
+                commonElement = None
+                for i in pList:
+                        if i in qList:
+                                commonElement = i;
+                                break
+                
+                for node in test:
+                        if node.val == commonElement:
+                                return node
+                
+                return None
