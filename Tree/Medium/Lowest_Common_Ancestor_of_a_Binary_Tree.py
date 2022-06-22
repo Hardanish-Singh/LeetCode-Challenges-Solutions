361c5291b790b4b1f9543fb9ebb9f9e132a880c6
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-22 06:29:24 -0400

    Lowest Common Ancestor of a Binary Tree

--- Tree/Medium/Lowest_Common_Ancestor_of_a_Binary_Tree.py
+++ Tree/Medium/Lowest_Common_Ancestor_of_a_Binary_Tree.py
@@ -17,7 +17,6 @@ class Solution( object ):
                 """
                 if root is None:
                         return None
-
                 if root.val == p.val or root.val == q.val:
                         return root
 
