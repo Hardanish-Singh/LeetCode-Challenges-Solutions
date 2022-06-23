2d46b12e095c814c0359f7e6d434b6c048dc57b9
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-23 08:54:05 -0400

    Insert Into Binary Search Tree

--- Tree/Medium/Delete_Node_From_A_Binary_Search_Tree.py
+++ Tree/Medium/Delete_Node_From_A_Binary_Search_Tree.py
@@ -1,13 +1,13 @@
 # Definition for a binary tree node.
 # class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
+#     def __init__( self, val = 0, left = None, right = None ):
 #         self.val = val
 #         self.left = left
 #         self.right = right
 
 
-class Solution(object):
-        def deleteNode(self, root, key):
+class Solution( object ):
+        def deleteNode( self, root, key ):
                 """
                 :type root: TreeNode
                 :type key: int
@@ -15,4 +15,20 @@ class Solution(object):
                 """
                 if root is None:
                         return None
-                
+                if root.val == key:
+                        return moveNodePointers( root )
+                currentNode = root
+
+                while currentNode:
+                        if val > currentNode.val:
+                                if root.right is not None and root.right.val == key:
+                                        root.right = moveNodePointers( root.right )
+                                else:
+                                        root = root.right
+                        else:
+                                if root.left is not None and root.left.val == key:
+                                        root.left = moveNodePointers( root.left )
+                                else:
+                                        root = root.left
+
+                return root
