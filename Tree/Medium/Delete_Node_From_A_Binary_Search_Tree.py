a6b0669fc960b954e73c9875619a5405d0c2b91c
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-23 09:23:37 -0400

    Delete Node From a Binary Search Tree

--- Tree/Medium/Delete_Node_From_A_Binary_Search_Tree.py
+++ Tree/Medium/Delete_Node_From_A_Binary_Search_Tree.py
@@ -1,3 +1,5 @@
+# Leetcode: https://leetcode.com/problems/delete-node-in-a-bst/
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__( self, val = 0, left = None, right = None ):
