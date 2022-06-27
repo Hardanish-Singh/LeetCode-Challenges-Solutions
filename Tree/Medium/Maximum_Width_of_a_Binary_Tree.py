28b7d46100a3aba61f64162de72d8c3820bcc742
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-27 15:52:13 -0400

    Maximum Width of a Binary Tree

--- Tree/Medium/Maximum_Width_of_a_Binary_Tree.py
+++ Tree/Medium/Maximum_Width_of_a_Binary_Tree.py
@@ -1,9 +1,12 @@
+# Leetcode: https://leetcode.com/problems/maximum-width-of-binary-tree/
+
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
+# class TreeNode( object ):
+#     def __init__( self, val = 0, left = None, right = None ):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution(object):
         def widthOfBinaryTree(self, root):
                 """
