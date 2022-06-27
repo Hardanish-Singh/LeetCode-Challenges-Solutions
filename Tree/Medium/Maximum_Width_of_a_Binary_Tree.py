7521f5829ef39875371be305ca7c3a63a0cbe89b
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-27 15:44:37 -0400

    Maximum Width of a Binary Tree

--- Tree/Medium/Maximum_Width_of_a_Binary_Tree.py
+++ Tree/Medium/Maximum_Width_of_a_Binary_Tree.py
@@ -10,4 +10,22 @@ class Solution(object):
                 :type root: TreeNode
                 :rtype: int
                 """
-                pass
+                if root is None:
+                        return 0
+                width = 1
+                queue = [ [ root, 1 ] ]
+                
+                while len( queue ) > 0:
+                        temp = []
+                        for item in queue:
+                                node, position = item
+                                if node.left:
+                                        queue.append( [ node.left, 2 * position ] )
+                                if node.right:
+                                        queue.append( [ node.right, ( 2 * position ) + 1 ] )
+                        
+                        if len( temp ) > 0:
+                                width = max( width, ( ( temp[-1][1] - temp[0][1] ) + 1 ) )
+                        queue = temp
+                
+                return width
