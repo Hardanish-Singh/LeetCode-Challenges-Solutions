7b8a438a454fb868e02bc44920138e92095cf436
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-27 07:53:37 -0400

    Minimum Depth of a Binary Tree

--- Tree/Easy/Minimum_Depth_of_Binary_Tree.py
+++ Tree/Easy/Minimum_Depth_of_Binary_Tree.py
@@ -2,7 +2,7 @@
 
 # Definition for a binary tree node.
 # class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
+#     def __init__( self, val = 0, left = None, right = None ):
 #         self.val = val
 #         self.left = left
 #         self.right = right
