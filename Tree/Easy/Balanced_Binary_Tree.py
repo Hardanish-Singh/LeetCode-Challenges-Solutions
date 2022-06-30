7d45700d1d335a365371befa992de73234699744
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-30 09:19:19 -0400

    Balanced Binary Tree

--- Tree/Easy/Balanced_Binary_Tree.py
+++ Tree/Easy/Balanced_Binary_Tree.py
@@ -1,3 +1,5 @@
+# Leetcode: https://leetcode.com/problems/balanced-binary-tree/
+
 # Definition for a binary tree node.
 # class TreeNode( object ):
 #     def __init__( self, val = 0, left = None, right = None ):
@@ -5,8 +7,8 @@
 #         self.left = left
 #         self.right = right
 
-class Solution(object):
-        def isBalanced(self, root):
+class Solution( object ):
+        def isBalanced( self, root ):
                 """
                 :type root: TreeNode
                 :rtype: bool
