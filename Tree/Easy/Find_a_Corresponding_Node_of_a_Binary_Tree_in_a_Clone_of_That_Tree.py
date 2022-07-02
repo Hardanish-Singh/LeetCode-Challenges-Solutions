9c1d364d352c37991f024a5da06ebc3548572979
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-02 18:02:21 -0400

    Find a Corresponding Node of a Binary Tree in a Clone of That Tree

--- Tree/Easy/Find_a_Corresponding_Node_of_a_Binary_Tree_in_a_Clone_of_That_Tree.py
+++ Tree/Easy/Find_a_Corresponding_Node_of_a_Binary_Tree_in_a_Clone_of_That_Tree.py
@@ -1,8 +1,8 @@
 # Leetcode: https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/
 
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
+# class TreeNode( object ):
+#     def __init__( self, x ):
 #         self.val = x
 #         self.left = None
 #         self.right = None
