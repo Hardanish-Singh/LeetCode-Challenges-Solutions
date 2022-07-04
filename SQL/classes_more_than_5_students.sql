e5bdd1068c14bc2263eac18cbe183b86133b4df0
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-04 11:58:43 -0400

    Classes More Than 5 Students

--- SQL/classes_more_than_5_students.sql
+++ SQL/classes_more_than_5_students.sql
@@ -48,3 +48,22 @@ FROM
     courses
 GROUP BY class
 HAVING COUNT( * ) >= 5
+
+-- SOLUTION 3
+
+SELECT
+    DISTINCT
+        c2.class
+FROM
+    courses as c1
+JOIN
+(
+    SELECT
+        class,
+        COUNT( * ) AS class_count
+    FROM
+        courses
+    GROUP BY class
+) AS c2 ON c2.class = c1.class
+
+WHERE c2.class_count >= 5;
