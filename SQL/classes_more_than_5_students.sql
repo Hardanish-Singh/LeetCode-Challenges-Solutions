db3f7addb8c3a2c10e44e5132127485074e4eb4f
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-04 12:02:50 -0400

    Classes More Than 5 Students

--- SQL/classes_more_than_5_students.sql
+++ SQL/classes_more_than_5_students.sql
@@ -1,34 +1,50 @@
 /*
-        There is a table courses with columns: student and class
+        Leetcode: https://leetcode.com/problems/classes-more-than-5-students/
 
-        Please list out all classes which have more than or equal to 5 students.
+        Table: Courses
 
-        For example, the table:
+        +-------------+---------+
+        | Column Name | Type    |
+        +-------------+---------+
+        | student     | varchar |
+        | class       | varchar |
+        +-------------+---------+
+        (student, class) is the primary key column for this table.
+        Each row of this table indicates the name of a student and the class in which they are enrolled.
+    
+        Write an SQL query to report all the classes that have at least five students.
+        Return the result table in any order.
+        The query result format is in the following example.
 
-        +---------+------------+
-        | student | class      |
-        +---------+------------+
-        | A       | Math       |
-        | B       | English    |
-        | C       | Math       |
-        | D       | Biology    |
-        | E       | Math       |
-        | F       | Computer   |
-        | G       | Math       |
-        | H       | Math       |
-        | I       | Math       |
-        +---------+------------+
-        
-        Output:
+        Example 1:
+            Input: 
+                Courses table:
+                +---------+----------+
+                | student | class    |
+                +---------+----------+
+                | A       | Math     |
+                | B       | English  |
+                | C       | Math     |
+                | D       | Biology  |
+                | E       | Math     |
+                | F       | Computer |
+                | G       | Math     |
+                | H       | Math     |
+                | I       | Math     |
+                +---------+----------+
+            
+            Output: 
+                +---------+
+                | class   |
+                +---------+
+                | Math    |
+                +---------+
 
-        +---------+
-        | class   |
-        +---------+
-        | Math    |
-        +---------+
-        
-
-        Note: The students should not be counted duplicate in each course.
+            Explanation: 
+                - Math has 6 students, so we include it.
+                - English has 1 student, so we do not include it.
+                - Biology has 1 student, so we do not include it.
+                - Computer has 1 student, so we do not include it.
 */
 
 -- SOLUTION 1
