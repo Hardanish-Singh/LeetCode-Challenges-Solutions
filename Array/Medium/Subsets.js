3afe22ce269d4927e5127e9f00c4354938cf465d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-09 10:43:19 -0400

    Subsets

--- Array/Medium/Subsets.js
+++ Array/Medium/Subsets.js
@@ -4,10 +4,10 @@
 
         Example 1:
                 Input: nums = [1,2,3]
-                Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+                Output: [[], [1], [2], [1,2], [3], [1,3], [2,3], [1,2,3]]
         Example 2:
                 Input: nums = [0]
-                Output: [[],[0]]
+                Output: [[], [0]]
 */
 
 var subsets = function( array ) {
