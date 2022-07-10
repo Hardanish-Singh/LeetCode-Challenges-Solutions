4e8fb2139e6e9eef055cc0ae6882b8cf8612b55e
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-10 08:21:20 -0400

    Leaf Similar Tree

--- Tree/Easy/Leaf_Similar_Trees.py
+++ Tree/Easy/Leaf_Similar_Trees.py
@@ -1,11 +1,12 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
+# class TreeNode( object ):
+#     def __init__( self, val = 0, left = None, right = None ):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-        def leafSimilar(self, root1, root2):
+
+class Solution( object ):
+        def leafSimilar( self, root1, root2 ):
                 """
                 :type root1: TreeNode
                 :type root2: TreeNode
