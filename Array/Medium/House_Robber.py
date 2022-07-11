c1b4fec4de5b14d9d9d7b05ebfdc34364b6cdf2d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-11 09:12:23 -0400

    House Robber

---
+++ Array/Medium/House_Robber.py
@@ -0,0 +1,16 @@
+class Solution(object):
+        def rob(self, nums):
+                """
+                :type nums: List[int]
+                :rtype: int
+                """
+                if len( nums ) == 1: 
+                        return nums[ 0 ]
+                
+                dp = nums
+                dp[ 1 ] = max( nums[0], nums[1] )
+
+                for i in range( 2, len( nums ) ):
+                        dp[ i ] = max( dp[ i - 1 ], nums[ i ] + dp[ i - 2 ] )
+
+                return dp[ -1 ]
