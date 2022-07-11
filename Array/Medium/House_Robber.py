b3afc55c780e4928aeec6cf73f4437890b231032
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-11 09:15:12 -0400

    House Robber

--- Array/Medium/House_Robber.py
+++ Array/Medium/House_Robber.py
@@ -1,7 +1,10 @@
 """
+        Leetcode: https://leetcode.com/problems/house-robber/
+        
         You are a professional robber planning to rob houses along a street. 
         Each house has a certain amount of money stashed, 
-        the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+        the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected 
+        and it will automatically contact the police if two adjacent houses were broken into on the same night.
         Given an integer array nums representing the amount of money of each house, 
         return the maximum amount of money you can rob tonight without alerting the police.
 
@@ -16,6 +19,10 @@
                 Output: 12
                 Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
                 Total amount you can rob = 2 + 9 + 1 = 12.
+        
+        Constraints:
+                1) 1 <= nums.length <= 100
+                2) 0 <= nums[i] <= 400
 """
 
 class Solution(object):
