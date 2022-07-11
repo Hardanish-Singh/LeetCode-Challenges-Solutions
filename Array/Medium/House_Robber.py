af836cb5088b1ea0257342b7ad62af454f60ee4c
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-11 17:09:16 -0400

    House Robber

--- Array/Medium/House_Robber.py
+++ Array/Medium/House_Robber.py
@@ -37,9 +37,9 @@ class Solution( object ):
                 if len( nums ) == 1: 
                         return nums[ 0 ]
                 
-                dp = [0] * len( nums )
-                dp[0] = nums[ 0 ]
-                dp[ 1 ] = max( nums[0], nums[1] )
+                dp = [ 0 ] * len( nums )
+                dp[ 0 ] = nums[ 0 ]
+                dp[ 1 ] = max( nums[ 0 ], nums[ 1 ] )
 
                 for i in range( 2, len( nums ) ):
                         dp[ i ] = max( dp[ i - 1 ], nums[ i ] + dp[ i - 2 ] )
