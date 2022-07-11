1d5c58c8fe25a7ce736ea35d1f61b195916acb50
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-11 17:07:05 -0400

    House Robber II

--- Array/Medium/House_Robber_II.py
+++ Array/Medium/House_Robber_II.py
@@ -14,13 +14,13 @@ class Solution( object ):
                         # This problem is exactly the House Robber part I problem with a variation that here there can be two possibilites-
                         # 1) Rob last house and skip the first house  
                         # 2) Rob first house and skip last house
-                        return max( nums[1:], nums[:-1] )
+                        return max( self.robHouses( nums[1:] ), self.robHouses( nums[:-1] ) )
         
         def robHouses( self, nums ):
                 if len( nums ) == 0:
                         return 0
-                elif len( nums ) == 1:
-                        return 1:
+                elif len( nums ) == 1
+                        return nums[0]
                 else:
                         dp = [0] * len( nums )
                         dp[0] = nums[0]
