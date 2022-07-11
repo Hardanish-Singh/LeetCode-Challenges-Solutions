46de100acc899f3fba7535a5709f21a49b42038f
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-11 16:59:36 -0400

    House Robber II

--- Array/Medium/House_Robber_II.py
+++ Array/Medium/House_Robber_II.py
@@ -4,3 +4,8 @@ class Solution( object ):
                 :type nums: List[int]
                 :rtype: int
                 """
+                if len( nums ) == 0:
+                        return 0
+                
+                if len( nums ) == 1:
+                        return nums[ 0 ]
