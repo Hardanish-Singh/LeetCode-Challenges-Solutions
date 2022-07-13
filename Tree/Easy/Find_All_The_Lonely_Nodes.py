3419d84ef6c7704a9b77180dd73f3d97a25ddf1d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-13 15:33:33 -0400

    Find All The Lonely Nodes

--- Tree/Easy/Find_All_The_Lonely_Nodes.py
+++ Tree/Easy/Find_All_The_Lonely_Nodes.py
@@ -1,18 +1,21 @@
+# Leetcode: https://leetcode.com/problems/find-all-the-lonely-nodes/
+
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
+# class TreeNode( object ):
+#     def __init__( self, val = 0, left = None, right = None ):
 #         self.val = val
 #         self.left = left
 #         self.right = right
 
-class Solution(object):
-        def getLonelyNodes(self, root):
+class Solution( object ):
+        def getLonelyNodes( self, root ):
                 """
                 :type root: TreeNode
                 :rtype: List[int]
                 """
                 lonelyNode = []
                 queue = [ root ]
+
                 while len( queue ) > 0:
                         currentNode = queue.pop(0)
                         if currentNode.left and currentNode.right is None:
@@ -23,4 +26,5 @@ class Solution(object):
                                 queue.append( currentNode.left )
                         if currentNode.right:
                                 queue.append( currentNode.right )
+
                 return lonelyNode
