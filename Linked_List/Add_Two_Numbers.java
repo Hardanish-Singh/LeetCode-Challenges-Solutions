00afb1c960699bdcff4164c93f17a8bd52eefc9c
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-16 09:39:49 -0400

    Add Two Numbers

--- Linked_List/Add_Two_Numbers.java
+++ Linked_List/Add_Two_Numbers.java
@@ -21,7 +21,7 @@
         }
 */
 
-public class Solution
+class Solution
 {
 
         public ListNode addTwoNumbers( ListNode l1, ListNode l2 )
@@ -30,20 +30,20 @@ public class Solution
 		ListNode head = null;
 		ListNode tail = null;
 		ListNode node = null;
-		short vals1 = 0;
-                short vals2 = 0;
-                short carry = 0;
-                short remainder = 0;
+		int vals1 = 0;
+                int vals2 = 0;
+                int carry = 0;
+                int remainder = 0;
 
                 while( l1 != null || l2 != null )
                 {
-                        vals1 = (short) ( ( l1 == null ) ? 0 : l1.val );
-                        vals2 = (short) ( ( l2 == null ) ? 0 : l2.val );
+                        vals1 = l1 == null ? 0 : l1.val;
+                        vals2 = l2 == null ? 0 : l2.val;
                         if( vals1 + vals2 + carry > 9 )
                         {
-                                remainder = (short) ( ( vals1 + vals2 + carry ) % 10 );
+                                remainder = ( vals1 + vals2 + carry ) % 10;
 				node = new ListNode( remainder );
-                                carry = (short) ( ( vals1 + vals2 + carry ) / 10 );
+                                carry = ( vals1 + vals2 + carry ) / 10;
                         }
                         else
                         {
@@ -59,8 +59,8 @@ public class Solution
 			    tail.next = node;
 			    tail = node;
 			}
-                        l1 = ( l1 != null ) ? l1.next : null;
-                        l2 = ( l2 != null ) ? l2.next : null;
+                        l1 = l1 != null ? l1.next : null;
+                        l2 = l2 != null ? l2.next : null;
                 }
 
                 if( carry > 0 )
