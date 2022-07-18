237cb6ce449449ba83164f3c3ac1b8dcdaca2367
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-18 13:43:12 -0400

    Middle of the Linked List

--- Linked_List/Middle_of_the_Linked_List.java
+++ Linked_List/Middle_of_the_Linked_List.java
@@ -1,3 +1,26 @@
+// Leetcode: https://leetcode.com/problems/middle-of-the-linked-list/
+
+/*
+        // Definition for Singly-Linked List
+        public class ListNode
+	{
+                int val;
+                ListNode next;
+                public ListNode()
+                {
+                }
+                public ListNode( int val )
+                {
+                        this.val = val;
+                }
+                public ListNode( int val, ListNode next )
+                {
+                        this.val = val;
+                        this.next = next;
+                }
+        }
+*/
+
 class Solution 
 {
         public ListNode middleNode(ListNode head) 
