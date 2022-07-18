f5e26abc73968087788353bf452b4ca6aa1af4fc
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-18 08:01:29 -0400

    Add Two Numbers II

--- Linked_List/Add_Two_Numbers_II.java
+++ Linked_List/Add_Two_Numbers_II.java
@@ -1,13 +1,25 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// Leetcode: https://leetcode.com/problems/add-two-numbers-ii/
+
+/*
+        // Definition for Singly-Linked List
+        public class ListNode
+	{
+                int val;
+                ListNode next;
+                public ListNode()
+                {
+                }
+                public ListNode( int val )
+                {
+                        this.val = val;
+                }
+                public ListNode( int val, ListNode next )
+                {
+                        this.val = val;
+                        this.next = next;
+                }
+        }
+*/
 
 class Solution
 {
