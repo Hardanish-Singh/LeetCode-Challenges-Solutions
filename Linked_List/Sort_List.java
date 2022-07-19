9399fa7b765ab82070757817aa9b70438988e9d9
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-19 15:19:35 -0400

    Sort List

--- Linked_List/Sort_List.java
+++ Linked_List/Sort_List.java
@@ -1,5 +1,26 @@
 // Leetcode: https://leetcode.com/problems/sort-list/
 
+/*
+        // Definition for Singly-Linked List
+        public class ListNode
+	{
+                int val;
+                ListNode next;
+                public ListNode()
+                {
+                }
+                public ListNode( int val )
+                {
+                        this.val = val;
+                }
+                public ListNode( int val, ListNode next )
+                {
+                        this.val = val;
+                        this.next = next;
+                }
+        }
+*/
+
 import java.util.Arrays;
 
 // SOLUTION 1: BUBBLE SORT
