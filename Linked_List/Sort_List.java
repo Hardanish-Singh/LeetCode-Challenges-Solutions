6f3f818ae116f36805d771331eee463cdd51b9ae
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-19 06:47:03 -0400

    Sort List

---
+++ Linked_List/Sort_List.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+class Solution {
+    public ListNode sortList(ListNode head) {
+                ListNode current = head;
+                ListNode index = null;
+                int temp;
+        
+                if (head == null) 
+                {
+                        return null;
+                }
+                else 
+                {
+                        while (current != null) 
+                        {
+                                index = current.next;
+                                while (index != null) 
+                                {
+                                        if (current.val > index.val) 
+                                        {
+                                                temp = current.val;
+                                                current.val = index.val;
+                                                index.val = temp;
+                                        }
+                                        index = index.next;
+                                }
+                                current = current.next;
+                        }
+                        return head;
+                }
+    }
+}
