cb3cd65a12bb28976dcb3ac25b4e59dd40800810
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-21 13:01:31 -0400

    Circular Array Loop

--- Array/Medium/Circular_Array_Loop.js
+++ Array/Medium/Circular_Array_Loop.js
@@ -3,6 +3,17 @@
 /*
         You are playing a game involving a circular array of non-zero integers nums. 
         Each nums[i] denotes the number of indices forward/backward you must move if you are located at index i:
+                1) If nums[i] is positive, move nums[i] steps forward,
+                2) If nums[i] is negative, move nums[i] steps backward.
+        Since the array is circular, you may assume that moving forward from the last element puts you on the first element, 
+        and moving backwards from the first element puts you on the last element.
+
+        A cycle in the array consists of a sequence of indices seq of length k where:
+                1) Following the movement rules above results in the repeating index sequence seq[0] -> seq[1] -> ... -> seq[k - 1] -> seq[0] -> ...
+                2) Every nums[seq[j]] is either all positive or all negative.
+                3) k > 1
+        
+        Return true if there is a cycle in nums, or false otherwise
 */
 
 // SOLUTION 1
