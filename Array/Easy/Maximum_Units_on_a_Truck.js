9286a948a2987d130fc3435176417f36fd86a40c
Hardanish Singh <hardanishsingh1@gmail.com>
2022-08-03 11:43:17 -0400

    Maximum Units on a Truck

--- Array/Easy/Maximum_Units_on_a_Truck.js
+++ Array/Easy/Maximum_Units_on_a_Truck.js
@@ -1,5 +1,15 @@
 // Leetcode: https://leetcode.com/problems/maximum-units-on-a-truck/
 
+/*
+        You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes, where boxTypes[i] = [numberOfBoxes, numberOfUnitsPerBox]:
+                1) numberOfBoxes is the number of boxes of type i.
+                2) numberOfUnitsPerBox is the number of units in each box of the type i.
+        
+        You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck. 
+        You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+        
+        Return the maximum total number of units that can be put on the truck.
+*/
 
 // SOLUTION 1
 
