2b55aa8595787519f8c59a35dc113b3608c640b2
Hardanish Singh <hardanishsingh1@gmail.com>
2022-08-08 07:39:30 -0400

    Number of Arithmetic Triplets

--- Array/Easy/Number_of_Arithmetic_Triplets.ts
+++ Array/Easy/Number_of_Arithmetic_Triplets.ts
@@ -11,7 +11,14 @@
                 Output: 2        
                 Explanation:
                         (1, 2, 4) is an arithmetic triplet because both 7 - 4 == 3 and 4 - 1 == 3.
-                        (2, 4, 5) is an arithmetic triplet because both 10 - 7 == 3 and 7 - 4 == 3. 
+                        (2, 4, 5) is an arithmetic triplet because both 10 - 7 == 3 and 7 - 4 == 3.
+        
+        Example 2:
+                Input: nums = [4, 5, 6, 7, 8, 9], diff = 2
+                Output: 2
+                Explanation:
+                        (0, 2, 4) is an arithmetic triplet because both 8 - 6 == 2 and 6 - 4 == 2.
+                        (1, 3, 5) is an arithmetic triplet because both 9 - 7 == 2 and 7 - 5 == 2.
 */
 
 function arithmeticTriplets( nums: number[], diff: number ): number {
