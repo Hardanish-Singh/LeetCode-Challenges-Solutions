3ae527691054215a9148c7cef22c33f103014fc0
Hardanish Singh <hardanishsingh1@gmail.com>
2022-08-08 07:10:38 -0400

    Number of Arithmetic Triplets

--- Array/Easy/Number_of_Arithmetic_Triplets.ts
+++ Array/Easy/Number_of_Arithmetic_Triplets.ts
@@ -1,19 +1,8 @@
 function arithmeticTriplets( nums: number[], diff: number ): number {
         let numberOfArithmeticTriplets: number = 0;
-        let hash_table: {
-                [ key: string ]: boolean
-        } = { 
-
-        };
         
         for( let i: number = 0; i < nums.length; i++ ) {
-                if( !hash_table[ nums[ i ] ] ) {
-                        hash_table[ nums[ i ] ] = true;
-                }
-        }
-        
-        for( let i: number = 0; i < nums.length; i++ ) {
-                if( hash_table[ nums[ i ] + diff ] && hash_table[ nums[ i ] + diff + diff ] ) {
+                if( nums.includes( nums[ i ] + diff ) && nums.includes( nums[ i ] + diff + diff ) ) {
                         numberOfArithmeticTriplets++;
                 }
         }
