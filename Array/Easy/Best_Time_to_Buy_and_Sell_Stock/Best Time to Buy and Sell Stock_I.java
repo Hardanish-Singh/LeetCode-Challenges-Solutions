9ecbe87bd7d3ba892babf01517be16563ef6d285
Hardanish Singh <hardanishsingh1@gmail.com>
2022-08-20 15:41:26 -0400

    Best Time to Buy and Sell Stock

--- Array/Easy/Best_Time_to_Buy_and_Sell_Stock/Best Time to Buy and Sell Stock_I.java
+++ Array/Easy/Best_Time_to_Buy_and_Sell_Stock/Best Time to Buy and Sell Stock_I.java
@@ -4,7 +4,7 @@
         Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
         Example 1:
-                Input: prices = [7,1,5,3,6,4]
+                Input: prices = [7, 1, 5, 3, 6, 4]
                 Output: 5
                 Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
                 Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
