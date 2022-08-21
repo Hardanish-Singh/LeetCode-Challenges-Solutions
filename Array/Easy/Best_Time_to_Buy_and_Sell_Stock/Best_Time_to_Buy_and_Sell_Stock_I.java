0f4da2705d24b3fb4c34847385dcc234a995cfd6
Hardanish Singh <hardanishsingh1@gmail.com>
2022-08-21 09:26:18 -0400

    Best Time to Buy and Sell Stock

--- Array/Easy/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock_I.java
+++ Array/Easy/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock_I.java
@@ -1,3 +1,5 @@
+// Leetcode: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
 /*
         You are given an array prices where prices[i] is the price of a given stock on the i^th day.
         You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
