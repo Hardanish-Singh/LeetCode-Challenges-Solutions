f533143f56205ec71703be4c21f42d5800743b38
Hardanish Singh <hardanishsingh1@gmail.com>
2022-08-21 11:39:32 -0400

    Best Time to Buy and Sell Stock II

--- Array/Medium/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock_II.js
+++ Array/Medium/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock_II.js
@@ -6,6 +6,14 @@
         However, you can buy it then immediately sell it on the same day.
 
         Find and return the maximum profit you can achieve.
+
+        Example 1:
+                Input: prices = [7, 1, 5, 3, 6, 4]
+                Output: 7
+                Explanation: 
+                        Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5 - 1 = 4.
+                        Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6 - 3 = 3.
+                        Total profit is 4 + 3 = 7.
 */
 
 /**
