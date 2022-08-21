e1f49920ce969d2ad8fe0bd87f60c500da1776ea
Hardanish Singh <hardanishsingh1@gmail.com>
2022-08-21 11:38:39 -0400

    Best Time to Buy and Sell Stock II

--- Array/Medium/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock_II.js
+++ Array/Medium/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock_II.js
@@ -1,5 +1,13 @@
 // Leetcode: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 
+/*
+        You are given an integer array prices where prices[i] is the price of a given stock on the i^th day.
+        On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. 
+        However, you can buy it then immediately sell it on the same day.
+
+        Find and return the maximum profit you can achieve.
+*/
+
 /**
  * @param { number[] } prices
  * @return { number }
