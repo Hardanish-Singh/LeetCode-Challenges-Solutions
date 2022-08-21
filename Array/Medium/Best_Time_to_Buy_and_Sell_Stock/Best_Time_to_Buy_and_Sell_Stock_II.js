e136343f56caa15d49d021f95931a4b804ede55e
Hardanish Singh <hardanishsingh1@gmail.com>
2022-08-21 11:37:50 -0400

    Best Time to Buy and Sell Stock II

--- Array/Medium/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock_II.js
+++ Array/Medium/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock_II.js
@@ -1,6 +1,8 @@
+// Leetcode: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
 /**
- * @param {number[]} prices
- * @return {number}
+ * @param { number[] } prices
+ * @return { number }
 */
 
 var maxProfit = function( prices ) {
