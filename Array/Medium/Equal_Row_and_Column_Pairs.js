cd341ee29af3cd52f4d85a985be8a52b0bc43c16
Hardanish Singh <hardanishsingh1@gmail.com>
2022-08-21 15:52:08 -0400

    Equal Row and Column Pairs

--- Array/Medium/Equal_Row_and_Column_Pairs.js
+++ Array/Medium/Equal_Row_and_Column_Pairs.js
@@ -6,7 +6,7 @@
 */
 
 var equalPairs = function( grid ) {
-        let matrix = JSON.parse(JSON.stringify(grid));
+        let matrix = JSON.parse( JSON.stringify( grid ) );
          // Take Matrix Transpose
         for( let i=0; i<matrix.length; i++ ) {
                 for( let j=0; j<matrix[i].length; j++ ) {
