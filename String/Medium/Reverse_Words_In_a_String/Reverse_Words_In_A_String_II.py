890ac5206404f02b50dfcdcd4063121e7e8549cc
Hardanish Singh <hardanishsingh1@gmail.com>
2022-08-22 11:13:15 -0400

    Reverse Words In A String II

---
+++ String/Medium/Reverse_Words_In_a_String/Reverse_Words_In_A_String_II.py
@@ -0,0 +1,21 @@
+class Solution(object):
+        def reverse(self, s, left, right):
+                while left < right:
+                        s[left], s[right] = s[right], s[left]
+                        left, right = left + 1, right - 1
+        
+        def reverseWords(self, str):
+                """
+                :type s: List[str]
+                :rtype: None Do not return anything, modify s in-place instead.
+                """
+
+                str.reverse()
+                
+                left = 0
+                for i in range( len( str ) ):
+                        if str[i] == " ":
+                                self.reverse( str, left, i - 1 )
+                                left = i + 1
+                        elif i == len( str ) - 1:
+                                self.reverse( str, left, i )
