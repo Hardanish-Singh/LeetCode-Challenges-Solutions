83ec88b5377393ab267c94655e4be58985b6b197
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-01 10:24:47 -0400

    Two Sum II Input Array Is Sorted

--- Array/Easy/Two_Sum/Two_Sum_II_Input_Array_Is_Sorted.js
+++ Array/Easy/Two_Sum/Two_Sum_II_Input_Array_Is_Sorted.js
@@ -36,7 +36,7 @@
 
 var twoSum = function( nums, target ) {
         for( let i = 0; i < nums.length; i++ ) {
-                for( let j = i+1; j < nums.length; j++ ) {
+                for( let j = i + 1; j < nums.length; j++ ) {
                         if( nums[i] + nums[j] === target ) {
                                 return [ i+1, j+1 ];
                         }
