6b8db6d7d2d127ba3de273bfaeafc8ee7ef09d51
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-01 10:31:06 -0400

    Two Sum

--- Array/Easy/Two_Sum/Two_Sum.js
+++ Array/Easy/Two_Sum/Two_Sum.js
@@ -1,31 +1,6 @@
 // Leetcode: https://leetcode.com/problems/two-sum/
 
 /*
-        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-        You may assume that each input would have exactly one solution, and you may not use the same element twice.
-        You can return the answer in any order.
-
-        Example 1:
-                Input: nums = [2, 7, 11, 15], target = 9
-                Output: [0, 1]
-                Output: Because nums[0] + nums[1] == 9, we return [0, 1].
-
-        Example 2:
-                Input: nums = [3, 2, 4], target = 6
-                Output: [1, 2]
-
-        Example 3:
-                Input: nums = [3, 3], target = 6
-                Output: [0, 1]
-
-        Constraints:
-                1) 2 <= nums.length <= 10^4
-                2) -10^9 <= nums[i] <= 10^9
-                3) -10^9 <= target <= 10^9
-                4) Only one valid answer exists.
-*/
-
-/*
         SOLUTION 1: BRUTE FORCE
                 Time Complexity: O( n ^ 2 )
                 Space Complexity: O( n )
