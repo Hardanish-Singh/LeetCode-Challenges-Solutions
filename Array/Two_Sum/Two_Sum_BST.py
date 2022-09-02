e7a25a366cf8124a09b4fed260552fbe4fd3342b
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-02 10:07:42 -0400

    Two Sum BST

---
+++ Array/Two_Sum/Two_Sum_BST.py
@@ -0,0 +1,49 @@
+# Leetcode: https://leetcode.com/problems/two-sum-bsts/
+
+# Definition for a binary tree node.
+# class TreeNode( object ):
+#     def __init__( self, val = 0, left = None, right = None ):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution( object ):
+        def preorderRecursiveTraversal( self, root, preOrderList ):
+                if root is None:
+                        return None
+                preOrderList.append( root.val )
+                self.preorderRecursiveTraversal( root.left, preOrderList )
+                self.preorderRecursiveTraversal( root.right, preOrderList )
+                
+        def twoSumBSTs( self, root1, root2, target ):
+                """
+                :type root1: TreeNode
+                :type root2: TreeNode
+                :type target: int
+                :rtype: bool
+                """
+                if root1 is None or root2 is None:
+                        return None
+
+                preOrderList1 = []
+                self.preorderRecursiveTraversal( root1, preOrderList1 )
+                
+                hash_table = { }
+                
+                for i in range( 0, len( preOrderList1 ) ):
+                        if preOrderList1[i] in hash_table:
+                                continue
+                        else:
+                                hash_table[ preOrderList1[i] ] = True
+                
+                preOrderList2 = []
+                self.preorderRecursiveTraversal( root2, preOrderList2 )
+                
+                for i in range( 0, len( preOrderList2 ) ):
+                        difference = target - preOrderList2[ i ]
+                        if difference in hash_table:
+                                return True
+                        else:
+                                hash_table[ preOrderList2[ i ] ] = i
+                
+                return False
