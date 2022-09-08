0dfc2176c08e87dfd7c387c7a44b0dd9fbc318ac
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-08 18:18:41 -0400

    Three Sum

--- Array/Medium/Three_Sum/three_sum.ts
+++ Array/Medium/Three_Sum/three_sum.ts
@@ -7,37 +7,36 @@
 
 function threeSum( nums: Array<number> ): Array<Array<number>> {
 	nums.sort( (a, b) => a - b );
-	let triplets: Array<Array<number>> = [];
+	let hash_table: {
+		[key: string]: boolean
+	} = {};
         let target: number = 0;
-	let leftIndexes = { };
 	for( let i: number = 0; i<nums.length; i++ ) {
                 if( nums[i] == nums[i-1] ) {
                         continue;
                 }
-		leftIndexes[i] = true;
 		let leftIndex: number = i+1;
 		let rightIndex: number = nums.length-1;
 		
 		while( leftIndex < rightIndex ) {
-                        if( nums[leftIndex] === nums[leftIndex-1] && !( leftIndexes[ leftIndex - 1 ] ) ) {
-                                leftIndex++;
-                        }
-                        else if( nums[rightIndex] === nums[rightIndex+1] ) {
-                                rightIndex--;
-                        }
-			else if( nums[leftIndex] + nums[rightIndex] + nums[i] < target) {
+			if( nums[leftIndex] + nums[rightIndex] + nums[i] < target) {
 				leftIndex++;
 			}
 			else if( nums[leftIndex] + nums[rightIndex] + nums[i] > target) {
 				rightIndex--;
 			}
 			else if( nums[leftIndex] + nums[rightIndex] + nums[i] === target) {
-				triplets.push( [ nums[i], nums[leftIndex], nums[rightIndex] ] );
+				hash_table[ nums[i] + "," + nums[leftIndex] + "," + nums[rightIndex] ] = true;
 				leftIndex++;
 				rightIndex--;
 			}
 		}	
 		
 	}
+	
+	let triplets: Array<Array<number>> = [];
+        for(const key of Object.keys(hash_table)) {
+		triplets.push(key.split(",").map(Number));     
+        }
 	return triplets;
 }
