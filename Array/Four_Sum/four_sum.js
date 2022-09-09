73a0210f0439dc2b58bfb4dbdcc467d4c8fcab7b
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-09 10:02:28 -0400

    Four Sum

--- Array/Four_Sum/four_sum.js
+++ Array/Four_Sum/four_sum.js
@@ -1,19 +1,6 @@
-/*
-        Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? 
-        Find all unique quadruplets in the array which gives the sum of target.
-        
-        Note: The solution set must not contain duplicate quadruplets.
-        Example:
-                Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
-                A solution set is:
-                        [
-                                [-1,  0, 0, 1],
-                                [-2, -1, 1, 2],
-                                [-2,  0, 0, 2]
-                        ]
-*/
+// Leetcode: https://leetcode.com/problems/4sum/
 
-/*
+/**
  * @param { number[] } nums
  * @param { number } target
  * @return { number[][] }
@@ -22,26 +9,28 @@
 var fourSum = function( nums, target ) {
         nums.sort( (a, b) => a - b );
         let hash_table = {};
-        for( let u = 0; u < nums.length; u++ ) {
-                for( let i = u + 1; i < nums.length; i++ ) {
-                        let leftIndex = i + 1;
-                        let rightIndex = nums.length - 1;
 
-                        while( leftIndex < rightIndex ) {
-                                if( nums[u] + nums[leftIndex] + nums[rightIndex] + nums[i] < target) {
-                                        leftIndex++;
+        for( let i = 0; i < nums.length; i++ ) {
+                for( let j = i + 1; j < nums.length; j++ ) {
+                        let leftPointer = j + 1;
+                        let rightPointer = nums.length - 1;
+
+                        while( leftPointer < rightPointer ) {
+                                if( nums[i] + nums[j] + nums[leftPointer] + nums[rightPointer] < target ) {
+                                        leftPointer++;
                                 }
-                                else if( nums[u] + nums[leftIndex] + nums[rightIndex] + nums[i] > target) {
-                                        rightIndex--;
+                                else if( nums[i] + nums[j] + nums[leftPointer] + nums[rightPointer] > target ) {
+                                        rightPointer--;
                                 }
-                                else if( nums[u] + nums[leftIndex] + nums[rightIndex] + nums[i] === target) {
-                                        hash_table[ nums[i] + "," + nums[u] + "," + nums[leftIndex] + "," + nums[rightIndex] ] = true;
-                                        leftIndex++;
-                                        rightIndex--;
+                                else if( nums[i] + nums[j] + nums[leftPointer] + nums[rightPointer] === target ) {
+                                        hash_table[ nums[i] + "," + nums[j] + "," + nums[leftPointer] + "," + nums[rightPointer] ] = true;
+                                        leftPointer++;
+                                        rightPointer--;
                                 }
                         }
                 }
         }
+
         let quadruplets = [];
         for( const key of Object.keys( hash_table ) ) {
 		quadruplets.push( key.split(",").map( Number ) );     
