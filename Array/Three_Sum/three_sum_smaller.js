805467f69f6f7b6ee9dc6f394a61b12947891910
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-09 09:43:24 -0400

    Three Sum Smaller

---
+++ Array/Three_Sum/three_sum_smaller.js
@@ -0,0 +1,43 @@
+// Leetcode: https://leetcode.com/problems/3sum-smaller/
+
+/**
+ * @param { number[] } nums
+ * @param { number } target
+ * @return { number }
+*/
+
+var threeSumSmaller = function(nums, target) {
+        nums.sort( (a, b) => a - b );
+        let sum = 0;
+	
+	for( let i = 0; i < nums.length-1; i++ ) {
+                let leftIndex = i + 1;
+		let rightIndex = nums.length - 1;
+		
+		while( leftIndex < rightIndex ) {
+                        if( nums[leftIndex] + nums[rightIndex] + nums[i] < target ) {
+                                sum++;
+                                
+                                // FIX LEFT POINTER & MOVE RIGHT POINTER
+                                let start = leftIndex;
+                                let end = rightIndex - 1;
+                                while( start < end ) {
+                                        if( nums[ i ] + nums[ start ] + nums[ end ] < target ) {
+                                                sum++;
+                                        } 
+                                        else{
+                                                break;
+                                        }
+                                        end--;
+                                }
+                                
+				leftIndex++;
+			}
+			else{
+				rightIndex--;
+			}
+		}	
+		
+	}
+	return sum;
+};
