e6385ec2a2afdeb35c46740a560e69d68e7c0dcb
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-10 15:26:58 -0400

    Array Partition

--- Array/Easy/Array_Partition.js
+++ Array/Easy/Array_Partition.js
@@ -8,8 +8,8 @@
 var arrayPairSum = function( nums ) {
         nums.sort( (a, b) => a - b );
         let sum = 0;
-        for( let i = 0; i < nums.length; i += 2 ){
-                sum += nums[i]
+        for( let i = 0; i < nums.length; i += 2 ) {
+                sum += nums[i];
         }
         return sum;
 };
