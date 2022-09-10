dde81bb516f1059aa1627ddbab9edf536a15da17
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-10 13:39:40 -0400

    Majority Element

--- Array/Easy/Majority_Element.js
+++ Array/Easy/Majority_Element.js
@@ -1,21 +1,4 @@
-/*
-        Given an array nums of size n, return the majority element.
-        The majority element is the element that appears more than ⌊n / 2⌋ times. 
-        You may assume that the majority element always exists in the array.
-
-        Example 1:
-                Input: nums = [3,2,3]
-                Output: 3
-        
-        Example 2:
-                Input: nums = [2,2,1,1,1,2,2]
-                Output: 2
-
-        Constraints:
-                1) n == nums.length
-                2) 1 <= n <= 5 * 10^4
-                3) -2^31 <= nums[i] <= 2^31 - 1
-*/
+// Leetcode: https://leetcode.com/problems/majority-element/
 
 /*
  * @param { number[] } nums
@@ -23,15 +6,16 @@
 */
 
 var majorityElement = function( nums ) {
-        let majority = Math.floor( nums.length / 2 );
+        let majority = parseInt( nums.length / 2 );
         let numbers = {
                 
         };
         let count = 0;
         let index = 0;
+
         for( let i = 0; i < nums.length; i++ ) {
                 count = 0;
-                if( numbers[nums[i]] ) {
+                if( nums[i] in numbers ) {
                         continue;
                 }
                 for( let j = i + 1; j < nums.length; j++ ){
@@ -44,5 +28,6 @@ var majorityElement = function( nums ) {
                         index = i;
                 }
         }
+
         return nums[index];
 };
