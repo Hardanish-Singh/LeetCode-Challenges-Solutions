8d3b1bba2d87bd4abf6d25f16c975c35898ad221
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-10 13:45:52 -0400

    Majority Element II

--- Array/Majority_Element/Majority_Element_II.js
+++ Array/Majority_Element/Majority_Element_II.js
@@ -1,22 +1,4 @@
-/*
-        Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
-
-        Example 1:
-                Input: nums = [3,2,3]
-                Output: [3]
-
-        Example 2:
-                Input: nums = [1]
-                Output: [1]
-        
-        Example 3:
-                Input: nums = [1,2]
-                Output: [1,2]
-
-        Constraints:
-                1) 1 <= nums.length <= 5 * 10^4
-                2) -10^9 <= nums[i] <= 10^9
-*/
+// Leetcode: https://leetcode.com/problems/majority-element-ii/
 
 /*
  * @param { number[] } nums
