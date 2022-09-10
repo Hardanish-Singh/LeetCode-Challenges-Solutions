df8dea1452e3b489488ba2847537badc0220dac3
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-10 15:26:25 -0400

    Array Partition

--- Array/Easy/Array_Partition.js
+++ Array/Easy/Array_Partition.js
@@ -1,8 +1,8 @@
 // Leetcode: https://leetcode.com/problems/array-partition/
 
-/*
- * @param {number[]} nums
- * @return {number}
+/**
+ * @param { number[] } nums
+ * @return { number }
 */
 
 var arrayPairSum = function( nums ) {
