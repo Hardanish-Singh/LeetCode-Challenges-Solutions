7982aaee345fd376ea95ddc81aa594b9a70d876b
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-11 21:48:32 -0400

    Contains Duplicate

--- Array/Medium/Contains_Duplicate_III.js
+++ Array/Medium/Contains_Duplicate_III.js
@@ -1,24 +1,4 @@
-/*
-        Given an integer array nums and two integers k and t, return true if there are two distinct indices i and j in the array such that abs(nums[i] - nums[j]) <= t and abs(i - j) <= k.
-
-        Example 1:
-                Input: nums = [1, 2, 3, 1], k = 3, t = 0
-                Output: true
-        
-        Example 2:
-                Input: nums = [1, 0, 1, 1], k = 1, t = 2
-                Output: true
-        
-        Example 3:
-                Input: nums = [1, 5, 9, 1, 5, 9], k = 2, t = 3
-                Output: false
-        
-        Constraints:
-                1) 1 <= nums.length <= 2 * 10^4
-                2) -2^31 <= nums[i] <= 2^31 - 1
-                3) 0 <= k <= 10^4
-                4) 0 <= t <= 2^31 - 1
-*/
+// Leetcode: https://leetcode.com/problems/contains-duplicate-iii/
 
 /**
  * @param { number[] } nums
