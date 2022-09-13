ad9f63325840c7882971c989842c36d8d9eb5d40
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-12 20:09:48 -0400

    Best Time to Buy and Sell Stocks With Cooldown

--- Array/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
+++ Array/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
@@ -1,28 +1,25 @@
 // Leetcode: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+
 class Solution {
-    public int maxProfit(int[] prices) {
-        if (prices == null || prices.length <= 1) {
-             return 0;
-         }
-        int k = 1;
-        int[] buy = new int[k + 1];
-        int[] sell = new int[k + 1];
-        Arrays.fill(buy, Integer.MAX_VALUE);
-        
-        int i = 1;
-        int cooldown = sell[ i - 1 ];
-            
-        // int cooldown = 0;
-            
-        for (int price : prices) {
-                for( i = 1; i <= k; i++ ) {
-                        buy[ i ] = Math.min( buy[ i ], price - cooldown );
-                        cooldown = sell[i];
-                        sell[ i ] = Math.max( sell[ i ], price - buy[ i ] );
+        public int maxProfit(int[] prices) {
+
+                int k = 1;
+                int[] buy = new int[k + 1];
+                int[] sell = new int[k + 1];
+                Arrays.fill(buy, Integer.MAX_VALUE);
+
+                int i = 1;
+                int cooldown = sell[ i - 1 ];
+
+                for (int price : prices) {
+                        for( i = 1; i <= k; i++ ) {
+                                buy[ i ] = Math.min( buy[ i ], price - cooldown );
+                                cooldown = sell[i];
+                                sell[ i ] = Math.max( sell[ i ], price - buy[ i ] );
+                        }
                 }
+
+                return sell[ k ];
         }
-        
-        return sell[ k ];
-    }
 }
 
