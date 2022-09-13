14e1525cd0db2d408b0600dd6d3b13ecc137052d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-12 20:14:41 -0400

    Best Time to Buy and Sell Stocks With Transaction Fee

---
+++ Array/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.java
@@ -0,0 +1,22 @@
+// Leetcode: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+
+class Solution {
+        public int maxProfit(int[] prices) {
+
+                int k = 1;
+                int[] buy = new int[k + 1];
+                int[] sell = new int[k + 1];
+                Arrays.fill(buy, Integer.MAX_VALUE);
+
+                for (int price : prices) {
+                        for( int i = 1; i <= k; i++ ) {
+                                buy[ i ] = Math.min( buy[ i ], price - sell[i-1] );
+                                sell[ i ] = Math.max( sell[ i ], price - buy[ i ] );
+                                sell[i-1] = sell[i];
+                        }
+                }
+
+                return sell[ k ];
+        }
+}
+
