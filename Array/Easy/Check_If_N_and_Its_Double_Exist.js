f83815f32e3c8de7ce0f1914f9e68cfafecb0d04
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-15 11:29:46 -0400

    Check if N and its double exist

--- Array/Easy/Check_If_N_and_Its_Double_Exist.js
+++ Array/Easy/Check_If_N_and_Its_Double_Exist.js
@@ -1,44 +1,5 @@
 // Leetcode: https://leetcode.com/problems/check-if-n-and-its-double-exist/
 
-/**
- * @param { number[] } arr
- * @return { boolean }
-*/
-
-/*
-        SOLUTION 1 USING BINARY SEARCH
-*/
-const binarySearch = (array, target) => {
-        let startIndex = 0;
-        let endIndex = array.length - 1;
-        while( startIndex <= endIndex ) {
-                let middleIndex = Math.floor( (startIndex + endIndex) / 2 );
-                if( target === array[middleIndex] ) {
-                        return middleIndex;
-                }
-                if( target > array[middleIndex] ) {
-                        startIndex = middleIndex + 1;
-                }
-                if( target < array[middleIndex] ) {
-                        endIndex = middleIndex - 1;
-                }
-        }
-        return null;
-}
-
-var checkIfExist = function( arr ) {
-        arr.sort( (i,j) => i-j );
-        for( let i=0; i<arr.length-1; i++ ) {
-                let temp1 = arr.slice(0,i);
-                let temp2 = arr.slice(i+1);
-                let index = binarySearch( [...temp1, ...temp2], arr[i]*2 );
-                if( typeof(index) === 'number' ) {
-                        return true;
-                }
-        }
-        return false;
-};
-
 /*
         SOLUTION 2 USING HASHMAP/DICTIONARY
 */
