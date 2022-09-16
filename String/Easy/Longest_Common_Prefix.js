738cf5443bcaa3fabc8687aea25e2398ae039f9a
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-16 13:01:15 -0400

    Longest Common Prefix

--- String/Easy/Longest_Common_Prefix.js
+++ String/Easy/Longest_Common_Prefix.js
@@ -1,23 +1,6 @@
-/*
-        Write a function to find the longest common prefix string amongst an array of strings.
-        If there is no common prefix, return an empty string "".
+// Leetcode: https://leetcode.com/problems/longest-common-prefix/
 
-        Example 1:
-                Input: strs = ["flower", "flow", "flight"]
-                Output: "fl"
-        
-        Example 2:
-                Input: strs = ["dog", "racecar", "car"]
-                Output: ""
-                Explanation: There is no common prefix among the input strings.
-
-        Constraints:
-                1) 1 <= strs.length <= 200
-                2) 0 <= strs[i].length <= 200
-                3) strs[i] consists of only lower-case English letters.
-*/
-
-/*
+/**
  * @param { string[] } strs
  * @return { string }
 */
