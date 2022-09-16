604f63483c3747d48ccb48eb37129153b0dfca7d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-16 13:06:50 -0400

    Longest Common Prefix

--- String/Easy/Longest_Common_Prefix.js
+++ String/Easy/Longest_Common_Prefix.js
@@ -33,3 +33,36 @@ var longestCommonPrefix = function( strs ) {
  * @param { string[] } strs
  * @return { string }
 */
+
+var longestCommonPrefix = function(strs) {
+        strs.sort( (a, b) => a.length - b.length );
+        if( strs.length === 1 ) {
+                return strs[0];
+        }
+        if( strs[0] === "" ){
+                return "";
+        }
+        
+        let flag = true;
+        let index = 0;
+        let word = strs[0][index];
+        
+        while( flag ) {
+                for( let i = 1; i < strs.length; i++ ) {
+                        if( strs[i].indexOf(word) !== 0 ) {
+                                word = word.slice(0, word.length - 1);
+                                flag = false;
+                                break;
+                        }
+                }
+                if( flag ) {
+                        let character = strs[0][++index];
+                        if( character == null ) {
+                                break;
+                        } else {
+                                word += character;
+                        }
+                }
+        }
+        return word;
+};
