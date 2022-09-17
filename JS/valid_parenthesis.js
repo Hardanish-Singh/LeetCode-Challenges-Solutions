5f435295d3e0b9070f014cc0f7148a8962dbaa3d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-17 08:44:39 -0400

    Valid Parenthesis

--- JS/valid_parenthesis.js
+++ JS/valid_parenthesis.js
@@ -1,84 +1,28 @@
-/*
-    Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-
-    An input string is valid if:
-
-    1) Open brackets must be closed by the same type of brackets.
-    2) Open brackets must be closed in the correct order.
-    Note that an empty string is also considered valid.
-
-    Example 1:
-        Input: "()"
-        Output: true
-
-    Example 2:
-        Input: "()[]{}"
-        Output: true
-
-    Example 3:
-        Input: "(]"
-        Output: false
-
-    Example 4:
-        Input: "([)]"
-        Output: false
-
-    Example 5:
-        Input: "{[]}"
-        Output: true
-*/
-/**
- * @param { string } s
- * @return { boolean }
- */
-var isValid = function( s )
-{
-
-        let openingBrackets = [];
-
-        for( let i=0; i<s.length; i++ )
-        {
-                if( s[i] === '(' || s[i] === '{' || s[i] === '[' )
-                {
-                        openingBrackets.push( s[i] );
-                }
-                else
-                {
-                        if( s[i] === ')' )
-                        {
-                                if( !(openingBrackets[openingBrackets.length-1] == '(') )
-                                {
-                                      return false;
-                                }
-                                else
-                                {
-                                    openingBrackets.pop();
-                                }
-                        }
-                        else if( s[i] === '}' )
-                        {
-                                if( !(openingBrackets[openingBrackets.length-1] == '{') )
-                                {
-                                      return false;
-                                }
-                                else
-                                {
-                                    openingBrackets.pop();
-                                }
-                        }
-                        else if( s[i] === ']' )
-                        {
-                                if( !(openingBrackets[openingBrackets.length-1] == '[') )
-                                {
-                                      return false;
-                                }
-                                else
-                                {
-                                    openingBrackets.pop();
-                                }
-                        }
-                }
+var isValid = function( s ) {
+    var stack = s.split("");
+    var validBrackets = {
+        ')' : '(',
+        ']' : '[',
+        '}' : '{'
+    };
+    var i = 0;
+    
+    while( true ) {
+        if( stack.length === 0 ) {
+            return true;
         }
-
-        return openingBrackets.length > 0 ? false : true;
+        if( i == stack.length ) {
+            return false;
+        }
+        if( validBrackets.hasOwnProperty( stack[i] ) && validBrackets[ stack[i] ] === stack[i-1] ) {
+            stack.splice(--i, 2);
+        }
+        else if( validBrackets.hasOwnProperty( stack[i] ) && validBrackets[ stack[i] ] !== stack[i-1] ) {
+            return false;
+        }
+        else {
+            i++;
+        }
+    }
+    
 };
