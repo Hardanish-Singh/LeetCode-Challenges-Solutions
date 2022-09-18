cf5680429f34db75fc52cd4995ab79b5937e887f
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-18 18:29:00 -0400

    Longest Valid Parenthesis

--- Array/Valid_Invalid_Parenthesis/Longest_Valid_Parentheses.js
+++ Array/Valid_Invalid_Parenthesis/Longest_Valid_Parentheses.js
@@ -25,7 +25,7 @@ var mergeIntervals = function( intervals ) {
 var longestValidParentheses = function( s ) {
 
         let stack = [];
-        let temp = [];
+        let brackets = [];
 
         for( let i = 0; i < s.length; i++ ) {
                 if( s[i] === '(' ) {
@@ -34,28 +34,28 @@ var longestValidParentheses = function( s ) {
                 else {
                         let item = stack.pop();
                         if( item != null ) {
-                                temp.push( [item, i] );
+                                brackets.push( [item, i] );
                         }
                 }
         }
         
         // MERGE NESTED BRACKETS/INTERVALS & OVERLAPPING INTERVALS
-        temp = mergeIntervals(temp);
+        brackets = mergeIntervals(brackets);
         // CONSTRUCT CONSECUTIVE BRACKETS 
-        for( let i = 0; i < temp.length - 1; i++ ) {
-                if( temp[i + 1][0] - temp[i][1] === 1 ) {
-                        temp[i + 1][0] = temp[i][1];
+        for( let i = 0; i < brackets.length - 1; i++ ) {
+                if( brackets[i + 1][0] - brackets[i][1] === 1 ) {
+                        brackets[i + 1][0] = brackets[i][1];
                 }
         }
         // MERGE OVERLAPPING INTERVALS
-        temp = mergeIntervals(temp);
-        if( temp.length == 0 ) {
+        brackets = mergeIntervals(brackets);
+        if( brackets.length == 0 ) {
                 return 0;
         }
         
         let result = [];
-        for( let i = 0; i < temp.length; i++ ) {
-                result.push( temp[i][1] - temp[i][0] + 1 );
+        for( let i = 0; i < brackets.length; i++ ) {
+                result.push( brackets[i][1] - brackets[i][0] + 1 );
         }
         return Math.max.apply( null, result );
 };
