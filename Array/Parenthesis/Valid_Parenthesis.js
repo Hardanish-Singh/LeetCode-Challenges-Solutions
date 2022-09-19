0dcab87824c7445b9c01da3364af87e2a8759fbe
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-19 09:11:51 -0400

    Valid Parenthesis

--- Array/Parenthesis/Valid_Parenthesis.js
+++ Array/Parenthesis/Valid_Parenthesis.js
@@ -1,1 +1,36 @@
 // Leetcode: https://leetcode.com/problems/valid-parentheses/
+
+/**
+ * @param { string } s
+ * @return { boolean }
+*/
+
+var isValid = function( s ) {
+        let stack = [];
+        let openingBrackets = {
+                '(' : true,
+                '[' : true,
+                '{' : true
+        };
+        let validBrackets = {
+                ')' : '(',
+                ']' : '[',
+                '}' : '{'
+        };
+    
+        for( let i = 0; i < s.length; i++ ) {
+                if( s[i] in openingBrackets ) {
+                        stack.push( s[i] );
+                } else {
+                        if( stack.length > 0 && stack[ stack.length - 1 ] === validBrackets[ s[i] ] ) {
+                                stack.pop();
+                        } 
+                        else {
+                                return false;
+                        }
+                }
+        }
+    
+        return stack.length > 0 ? false : true;
+        
+};
