77df3233f53f49b6e258b0fb3089b29ba482a298
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-19 15:00:57 -0400

    Remove Invalid Parenthesis

--- Array/Parenthesis/Remove_Invalid_Parentheses.ts
+++ Array/Parenthesis/Remove_Invalid_Parentheses.ts
@@ -1,8 +1,8 @@
 // Leetcode: https://leetcode.com/problems/remove-invalid-parentheses/
 
 type Paranthesis = {
-        [key: string]: boolean
-}
+        [key: string]: boolean;
+};
 
 /**
  * @param { string } str
@@ -10,22 +10,32 @@ type Paranthesis = {
  * @param { Set<string> } hash_set
  * @param { Paranthesis } isVisited
  * @return { number }
-*/
+ */
 
-function generateValidParenthesis( str: string, minimumNumberOfBracketsToBeRemoved: number, hash_set: Set<string>, isVisited: Paranthesis ): void {
-        if( minimumNumberOfBracketsToBeRemoved === 0 ) {
+function generateValidParenthesis(
+        str: string,
+        minimumNumberOfBracketsToBeRemoved: number,
+        hash_set: Set<string>,
+        isVisited: Paranthesis
+): void {
+        if (minimumNumberOfBracketsToBeRemoved === 0) {
                 // CHECK IF THE STRING IS VALID
-                if( getMinimumNumberOfBracketsToBeRemoved( str ) === 0 ) {
-                        hash_set.add( str );
+                if (getMinimumNumberOfBracketsToBeRemoved(str) === 0) {
+                        hash_set.add(str);
                 }
                 return;
         }
-        for( let i: number = 0; i < str.length; i++ ) {
+        for (let i: number = 0; i < str.length; i++) {
                 let left: string = str.slice(0, i);
                 let right: string = str.slice(i + 1);
-                if( !isVisited[left + right] ) {
+                if (!isVisited[left + right]) {
                         isVisited[left + right] = true;
-                        generateValidParenthesis( left + right, minimumNumberOfBracketsToBeRemoved - 1, hash_set, isVisited );
+                        generateValidParenthesis(
+                                left + right,
+                                minimumNumberOfBracketsToBeRemoved - 1,
+                                hash_set,
+                                isVisited
+                        );
                 }
         }
 }
@@ -33,20 +43,20 @@ function generateValidParenthesis( str: string, minimumNumberOfBracketsToBeRemov
 /**
  * @param { string } str
  * @return { number }
-*/
+ */
 
-function getMinimumNumberOfBracketsToBeRemoved( str: string ): number{
+function getMinimumNumberOfBracketsToBeRemoved(str: string): number {
         let stack: Array<string> = [];
-        for( let i: number = 0; i < str.length; i++ ) {
+        for (let i: number = 0; i < str.length; i++) {
                 let bracket: string = str[i];
-                if( bracket === '(' ) {
-                        stack.push( bracket );
-                } else if( bracket === ')' ) {
-                        if( stack.length === 0 ) {
+                if (bracket === "(") {
+                        stack.push(bracket);
+                } else if (bracket === ")") {
+                        if (stack.length === 0) {
                                 stack.push(bracket);
-                        } else if( stack[ stack.length - 1 ] === ')' ) {
+                        } else if (stack[stack.length - 1] === ")") {
                                 stack.push(bracket);
-                        } else if( stack[ stack.length - 1 ] === '(' ) {
+                        } else if (stack[stack.length - 1] === "(") {
                                 stack.pop();
                         }
                 }
@@ -54,16 +64,15 @@ function getMinimumNumberOfBracketsToBeRemoved( str: string ): number{
         return stack.length;
 }
 
-
 /**
  * @param { string } s
  * @return { string[] }
-*/
+ */
 
-var removeInvalidParentheses = function( s: string ): Array<string> {
-        let minimumNumberOfBracketsToBeRemoved: number = getMinimumNumberOfBracketsToBeRemoved( s );
+var removeInvalidParentheses = function (s: string): Array<string> {
+        let minimumNumberOfBracketsToBeRemoved: number = getMinimumNumberOfBracketsToBeRemoved(s);
         let hash_set: Set<string> = new Set<string>();
         let isVisited: Paranthesis = {};
-        generateValidParenthesis( s, minimumNumberOfBracketsToBeRemoved, hash_set, isVisited );
-        return Array.from( hash_set );
+        generateValidParenthesis(s, minimumNumberOfBracketsToBeRemoved, hash_set, isVisited);
+        return Array.from(hash_set);
 };
