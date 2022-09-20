b7807164ec1f6d71f38796e6b1684f169b64e9ab
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-20 11:36:18 -0400

    Score of Parenthesis

---
+++ Array/Parenthesis/Score_Of_Parenthesis.ts
@@ -0,0 +1,17 @@
+// Leetcode: https://leetcode.com/problems/score-of-parentheses/
+
+function scoreOfParentheses(str: string): number {
+        const stack: Array<number> = [];
+
+        for (let i: number = 0; i < str.length; i++) {
+                let bracket: string = str[i];
+                if (bracket === "(") {
+                        stack.push(0);
+                } else {
+                        let item = stack.pop();
+                        let score = 2 * item || 1;
+                        stack.length > 0 ? (stack[stack.length - 1] += score) : (stack[0] = score);
+                }
+        }
+        return stack.pop();
+}
