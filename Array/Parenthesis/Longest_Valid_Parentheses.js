001583d6c893fccaec6d5cfa6698ff3b1acb6440
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-21 09:38:33 -0400

    Longest Valid Parenthesis

--- Array/Parenthesis/Longest_Valid_Parentheses.js
+++ Array/Parenthesis/Longest_Valid_Parentheses.js
@@ -3,21 +3,17 @@
 /**
  * @param { number[][] } intervals
  * @return { number[][] }
-*/
+ */
 
-var mergeIntervals = function( intervals ) {
-        intervals.sort( (a,b) => a[0] - b[0] );
-        for( let i = 0; i < intervals.length - 1; i++ ) {
+var mergeIntervals = function (intervals) {
+        intervals.sort((a, b) => a[0] - b[0]);
+        for (let i = 0; i < intervals.length - 1; i++) {
                 let j = i + 1;
-                if( intervals[i + 1][0] - intervals[i][1] === 1 ) {
+                if (intervals[i + 1][0] - intervals[i][1] === 1) {
                         intervals[i + 1][0] = intervals[i][1];
                 }
-                if( 
-                        intervals[j][0] >= intervals[i][0]
-                                &&
-                        intervals[j][0] <= intervals[i][1]
-                ) {
-                        intervals[i] = [ intervals[i][0], Math.max( intervals[i][1], intervals[j][1] ) ];
+                if (intervals[j][0] >= intervals[i][0] && intervals[j][0] <= intervals[i][1]) {
+                        intervals[i] = [intervals[i][0], Math.max(intervals[i][1], intervals[j][1])];
                         intervals.splice(j, 1);
                         i--;
                 }
@@ -28,32 +24,30 @@ var mergeIntervals = function( intervals ) {
 /**
  * @param { string } s
  * @return { number }
-*/
+ */
 
-var longestValidParentheses = function( s ) {
-
+var longestValidParentheses = function (s) {
         let stack = [];
         let brackets = [];
 
-        for( let i = 0; i < s.length; i++ ) {
-                if( s[i] === '(' ) {
-                        stack.push( i ); 
-                }
-                else {
-                        if( stack.length > 0 ) {
-                                brackets.push( [stack.pop(), i] );
+        for (let i = 0; i < s.length; i++) {
+                if (s[i] === "(") {
+                        stack.push(i);
+                } else {
+                        if (stack.length > 0) {
+                                brackets.push([stack.pop(), i]);
                         }
                 }
         }
         // MERGE OVERLAPPING INTERVALS
         brackets = mergeIntervals(brackets);
-        if( brackets.length == 0 ) {
+        if (brackets.length == 0) {
                 return 0;
         }
-        
+
         let max = 0;
-        for( let i = 0; i < brackets.length; i++ ) {
-                max = Math.max( max, brackets[i][1] - brackets[i][0] + 1 );
+        for (let i = 0; i < brackets.length; i++) {
+                max = Math.max(max, brackets[i][1] - brackets[i][0] + 1);
         }
         return max;
 };
