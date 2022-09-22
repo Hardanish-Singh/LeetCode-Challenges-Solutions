bd14b0f9965b2dc12582c8045d4448e346a32d90
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-22 10:43:17 -0400

    Generate Parenthesis

--- Array/Parenthesis/Generate_Parentheses.js
+++ Array/Parenthesis/Generate_Parentheses.js
@@ -18,29 +18,21 @@
 var generateParenthesis = function (n) {
         let parenthesis = [["(", 1, 0]];
         while (true) {
-                let [, open, close] = parenthesis[0];
+                let [bracket, open, close] = parenthesis.shift();
                 // RULE 3
                 if (open == close && close == n && open == n) {
+                        parenthesis.push([bracket, open, close]);
                         break;
                 }
-                let bracket = parenthesis.shift();
-                let temp = [];
                 let str = "";
-                // RULE 1
                 if (open < n) {
-                        str = bracket[0] + "(";
-                        [, o, c] = bracket;
-                        temp.push(str, ++o, c);
-                        parenthesis.push(temp);
-                        temp = [];
+                        str = bracket + "(";
+                        parenthesis.push([str, 1 + open, close]);
                 }
                 // RULE 2
                 if (open > close) {
-                        str = bracket[0] + ")";
-                        [, o, c] = bracket;
-                        temp.push(str, o, ++c);
-                        parenthesis.push(temp);
-                        temp = [];
+                        str = bracket + ")";
+                        parenthesis.push([str, open, 1 + close]);
                 }
         }
         return parenthesis.map((result) => result[0]);
