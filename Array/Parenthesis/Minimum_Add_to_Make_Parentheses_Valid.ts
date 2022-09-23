5a2963e5519fbc252639503e8ef0095e958f2d83
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-23 11:29:43 -0400

    Minimum Add to Make Parenthesis Valid

---
+++ Array/Parenthesis/Minimum_Add_to_Make_Parentheses_Valid.ts
@@ -0,0 +1,18 @@
+// Leetcode: https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+
+function minAddToMakeValid(str: string): number {
+        let stack: Array<string> = [];
+        for (let i: number = 0; i < str.length; i++) {
+                let bracket: string = str[i];
+                if (bracket === "(") {
+                        stack.push(bracket);
+                } else {
+                        if (stack[stack.length - 1] === "(") {
+                                stack.pop();
+                        } else {
+                                stack.push(bracket);
+                        }
+                }
+        }
+        return stack.length;
+}
