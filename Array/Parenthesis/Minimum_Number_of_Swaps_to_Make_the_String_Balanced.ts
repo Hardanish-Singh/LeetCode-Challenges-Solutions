ebfa8b7f43d511a4ae2b606fd22a3fc2adce1c0c
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-24 10:38:17 -0400

    Minimum Number of Swaps to Make the String Balanced

--- Array/Parenthesis/Minimum_Number_of_Swaps_to_Make_the_String_Balanced.ts
+++ Array/Parenthesis/Minimum_Number_of_Swaps_to_Make_the_String_Balanced.ts
@@ -1,10 +1,5 @@
 // Leetcode: https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/
 
-/*
-        In the case of are even number of pairs. Simplying using integer division by 2 returns the the number of switches needed to matched the unmatched pair.
-        In the case of odd numbers, even after switching the half of the unmatched pairs, you will be left with one pair which needs an additional swapping. Hence, you either add + 1 or use a ceiling function to round the number up.
-*/
-
 function minSwaps(str: string): number {
         let stack: Array<string> = [];
         for (let i: number = 0; i < str.length; i++) {
@@ -20,5 +15,11 @@ function minSwaps(str: string): number {
                 }
         }
         let length = stack.length / 2; // Because the string consists of exactly n / 2 opening brackets '[' and n / 2 closing brackets ']'.
+        /*
+                If even number of pairs: Simplying using integer division by 2 returns the the number of switches needed to matched the unmatched pair.
+                If odd numbers of pairs: Even after switching the half of the unmatched pairs, you will be left with one pair which needs an additional swapping. 
+                                         Hence, you either add + 1 or use a ceiling function to round the number up.
+        */
         return length % 2 === 0 ? length / 2 : Math.floor(length / 2) + 1;
+        // return length % 2 === 0 ? length / 2 : Math.ceil(length / 2);
 }
