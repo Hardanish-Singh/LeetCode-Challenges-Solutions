f8fcd32924a630e217c4d1aac8bff043d355fcb3
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-24 09:55:36 -0400

    Minimum Remove to Make Valid Parenthesis

--- Array/Parenthesis/Minimum_Remove_to_Make_Valid_Parentheses.ts
+++ Array/Parenthesis/Minimum_Remove_to_Make_Valid_Parentheses.ts
@@ -1,6 +1,7 @@
 // Leetcode: https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
 
-function minRemoveToMakeValid(str: string): string {
+// SOLUTION 1( USING STACK )
+function minRemoveToMakeValid1(str: string): string {
         let stack: Array<number> = [];
         for (let i: number = 0; i < str.length; i++) {
                 let bracket: string = str[i];
@@ -22,30 +23,30 @@ function minRemoveToMakeValid(str: string): string {
                 }
         }
         return result;
+}
 
-        //         let temp: string = '';
-        //         let open: number = 0;
-
-        //         for( let i: number = 0; i < str.length; i++ ) {
-        //                 if( str[i] === '(' ) {
-        //                         open++;
-        //                 } else if( str[i] === ')' ) {
-        //                         if( open === 0 ) {
-        //                                 continue;
-        //                         }
-        //                         open--;
-        //                 }
-
-        //                 temp += str[i];
-        //         }
-
-        //         let result: string = '';
-        //         for( let i = temp.length - 1; i >= 0; i-- ) {
-        //                 if( temp[i] === '(' && open > 0 ) {
-        //                         open--;
-        //                         continue;
-        //                 }
-        //                 result += temp[i];
-        //         }
-        //         return result.split('').reverse().join('');
+// SOLUTION 2
+function minRemoveToMakeValid2(str: string): string {
+        let temp: string = "";
+        let open: number = 0;
+        for (let i: number = 0; i < str.length; i++) {
+                if (str[i] === "(") {
+                        open++;
+                } else if (str[i] === ")") {
+                        if (open === 0) {
+                                continue;
+                        }
+                        open--;
+                }
+                temp += str[i];
+        }
+        let result: string = "";
+        for (let i = temp.length - 1; i >= 0; i--) {
+                if (temp[i] === "(" && open > 0) {
+                        open--;
+                        continue;
+                }
+                result += temp[i];
+        }
+        return result.split("").reverse().join("");
 }
