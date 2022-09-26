3d3c5c84873cf35ff06a3413a825acf3cfb181e6
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-26 09:00:37 -0400

    Valid Parenthesis String

--- Array/Parenthesis/Valid_Parenthesis_String.ts
+++ Array/Parenthesis/Valid_Parenthesis_String.ts
@@ -1,17 +1,30 @@
-function checkValidString(s: string): boolean {
+// Leetcode: https://leetcode.com/problems/valid-parenthesis-string/submissions/
+
+/**
+ * @param { string } str
+ * @returns { boolean }
+ */
+
+function checkValidString(str: string): boolean {
         let openCount = 0;
         let closedCount = 0;
-        for (let i: number = 0; i < s.length; i++) {
-                if (s[i] == "*" || s[i] == "(") openCount++;
-                else openCount--;
+        for (let i: number = 0; i < str.length; i++) {
+                if (str[i] == "*" || str[i] == "(") {
+                        openCount++;
+                } else {
+                        openCount--;
+                }
                 if (openCount < 0) {
                         return false;
                 }
         }
 
-        for (let i: number = s.length - 1; i >= 0; i--) {
-                if (s[i] == "*" || s[i] == ")") closedCount++;
-                else closedCount--;
+        for (let i: number = str.length - 1; i >= 0; i--) {
+                if (str[i] == "*" || str[i] == ")") {
+                        closedCount++;
+                } else {
+                        closedCount--;
+                }
                 if (closedCount < 0) {
                         return false;
                 }
