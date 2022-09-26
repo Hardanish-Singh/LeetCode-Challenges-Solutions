cbf1fb4bd9416c35b64437736d2f0d1f7c0b478d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-26 10:26:40 -0400

    Minimum Insertions to Balance a Parentheses String

---
+++ Array/Parenthesis/Minimum_Insertions_to_Balance_a_Parentheses_String.ts
@@ -0,0 +1,45 @@
+/**
+ * @param { string } str
+ * @returns { number }
+ */
+
+function minInsertions(str: string): number {
+        let stack: Array<string> = [];
+        let open: number = 0;
+        let close: number = 0;
+        for (let i: number = 0; i < str.length; i++) {
+                if (str[i] === "(") {
+                        stack.push(str[i]);
+                } else if (str[i] === ")" && str[i + 1] === ")") {
+                        // There is '(' in stack, so we pop.
+                        if (stack.length > 0) {
+                                stack.pop();
+                        }
+                        // No '(' in stack, thus we increment opening bracker counter
+                        else {
+                                open++;
+                        }
+                        i++;
+                } else {
+                        /*
+                                There is '(' in stack, so we pop & increment closing bracket counter
+                                for a filled stack -> ')' bracket and atleast 1 '(' in stack would need 1 ')' to make '())' 
+                        */
+                        if (stack.length > 0) {
+                                close++;
+                                stack.pop();
+                        }
+                        // for an empty stack -> ')' bracket needs an '(' and a ')' to make '( ))'
+                        else {
+                                open++;
+                                close++;
+                        }
+                }
+        }
+        /*
+                Every unmatched '(', needs two ')'
+                Hence, we multiple by 2
+        */
+        let total = stack.length * 2;
+        return total + open + close;
+}
