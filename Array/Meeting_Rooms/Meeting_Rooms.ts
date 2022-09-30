59be5b401d112e866239f192fe0693bd905fad06
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-30 09:06:22 -0400

    Meeting Rooms

--- Array/Meeting_Rooms/Meeting_Rooms.ts
+++ Array/Meeting_Rooms/Meeting_Rooms.ts
@@ -9,9 +9,10 @@ function canAttendMeetings(intervals: number[][]): boolean {
         intervals.sort((a, b) => a[0] - b[0]);
         for (let i: number = 0; i < intervals.length - 1; i++) {
                 let j: number = i + 1;
-                if (intervals[j][0] === intervals[i][0] && intervals[j][1] === intervals[i][1]) {
-                        return false;
-                } else if (intervals[j][0] > intervals[i][0] && intervals[j][0] < intervals[i][1]) {
+                if (
+                        (intervals[j][0] === intervals[i][0] && intervals[j][1] === intervals[i][1]) ||
+                        (intervals[j][0] > intervals[i][0] && intervals[j][0] < intervals[i][1])
+                ) {
                         return false;
                 }
         }
