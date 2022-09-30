1d6246c37d0cc5802118d9166e48de9d44cab8dd
Hardanish Singh <hardanishsingh1@gmail.com>
2022-09-30 08:53:06 -0400

    Insert Interval

--- Array/Medium/Insert_Interval.ts
+++ Array/Medium/Insert_Interval.ts
@@ -2,14 +2,13 @@
 
 /**
  * @param { number[][] } intervals
- * @param { number[] } newInterval
  * @return { number[][] }
  */
 
-var merge = function (intervals) {
+var merge = function (intervals: Array<Array<number>>): Array<Array<number>> {
         intervals.sort((a, b) => a[0] - b[0]);
-        for (let i = 0; i < intervals.length - 1; i++) {
-                let j = i + 1;
+        for (let i: number = 0; i < intervals.length - 1; i++) {
+                let j: number = i + 1;
                 if (intervals[j][0] >= intervals[i][0] && intervals[j][0] <= intervals[i][1]) {
                         intervals[i] = [intervals[i][0], Math.max(intervals[i][1], intervals[j][1])];
                         intervals.splice(j, 1);
@@ -19,7 +18,12 @@ var merge = function (intervals) {
         return intervals;
 };
 
-var insert = function (intervals, newInterval) {
+/**
+ * @param { number[][] } intervals
+ * @param { number[] } newInterval
+ * @return { number[][] }
+ */
+var insert = function (intervals: Array<Array<number>>, newInterval: Array<number>): Array<Array<number>> {
         intervals = [...intervals, newInterval];
         return merge(intervals);
 };
