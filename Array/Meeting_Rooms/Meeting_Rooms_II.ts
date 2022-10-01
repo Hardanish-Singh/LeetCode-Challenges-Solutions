12a125be75c721b1374aa0823b4bb3b02457613d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-10-01 10:28:56 -0400

    Meeting Rooms II

--- Array/Meeting_Rooms/Meeting_Rooms_II.ts
+++ Array/Meeting_Rooms/Meeting_Rooms_II.ts
@@ -5,6 +5,17 @@
         But before adding rooms, we check to see if any previous meeting ends, which is why we check start with the end. 
         When the start is bigger than end, it means at this time one of the previous meeting ends, and it can take and reuse that room. 
         Then the next meeting need to compare with the second end because the first end's room is already taken. 
+
+        for example, we have meetings that span along time as follows:
+
+        |_____|
+              |______|
+        |_________|
+                |_______|
+        
+        Then, the start time array and end time array after sorting appear like follows:
+        ||    ||
+             |   |   |  |
 */
 
 /**
