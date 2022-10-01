7b06002bee1f5f00b32aa5dfdd649bc5829b221f
Hardanish Singh <hardanishsingh1@gmail.com>
2022-10-01 12:06:53 -0400

    Unique Paths

--- Array/Medium/Unique_Path.js
+++ Array/Medium/Unique_Path.js
@@ -1,64 +1,20 @@
-/*
+// Leetcode: https://leetcode.com/problems/unique-paths/
+
+/**
  * @param { number } m
  * @param { number } n
  * @return { number }
-*/
+ */
 
-/*
-        SOLUTION 1 (BRUTE FORCE)
-*/
-const move_right = ( matrix, i, j ) => ++j <= matrix[i].length-1 ? ( i + "," + j ) : null;
-
-const move_down  = ( matrix, i, j ) => ++i <= matrix.length-1 ? ( i + "," + j ) : null;
-
-function move_in_right_down_directions( matrix, i, j, queue ) {
-        let position = "";
-        // MOVE RIGHT & ADD COORDINATES TO QUEUE
-        position = move_right( matrix, i, j );
-        position ? queue.push( position ) : null;
-        // MOVE DOWN & ADD COORDINATES TO QUEUE
-        position = move_down( matrix, i, j );
-        position ? queue.push( position ) : null;
-}
-
-function perform_enqueue_dequeue_operation( queue, matrix ) {
-        let count = 0;
-        while( queue.length != 0 ) {
-                [i, j] = queue.shift().split(",");
-                if( i == matrix.length - 1 && j == matrix[0].length - 1 ) {
-                        count++;
-                }
-                move_in_right_down_directions( matrix, i, j, queue );
+function uniquePaths(m, n) {
+        const matrix = Array(m);
+        for (let i = 0; i < m; i++) {
+                matrix[i] = Array(n).fill(1);
         }
-        return count;
-}
-
-var uniquePaths = function( m, n ) {
-        const matrix = Array( m );
-        for( let i = 0; i < m; i++ ) {
-                matrix[i] = Array( n ).fill('0');
-        }
-        let i = 0;
-        let j = 0;
-        let queue = [];
-        move_in_right_down_directions( matrix, i, j, queue );
-        let count = perform_enqueue_dequeue_operation( queue, matrix );
-        return count;
-};
-
-/*
-        SOLUTION 2 
-*/
-
-function uniquePaths( m, n ) {
-        const matrix = Array( m );
-        for( let i = 0; i < m; i++ ) {
-                matrix[i] = Array( n ).fill(1);
-        }
-        for( let i = 1; i<m; i++ ){
-                for( let j=1; j<n; j++ ){
-                        matrix[i][j] = matrix[i-1][j]+matrix[i][j-1];
+        for (let i = 1; i < m; i++) {
+                for (let j = 1; j < n; j++) {
+                        matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
                 }
         }
-        return matrix[m-1][n-1];
+        return matrix[m - 1][n - 1];
 }
