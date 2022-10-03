974abdde2cc23ae4487785ee4782293b035beb65
Hardanish Singh <hardanishsingh1@gmail.com>
2022-10-03 10:57:57 -0400

    Unique Path II

--- Array/Unique_Paths/Unique_Path_II.js
+++ Array/Unique_Paths/Unique_Path_II.js
@@ -39,7 +39,7 @@ var uniquePathsWithObstacles = function (obstacleGrid) {
         for (let i = 1; i < row; i++) {
                 for (let j = 1; j < column; j++) {
                         if (obstacleGrid[i][j] == 0) {
-                                if (obstacleGrid[i - 1][j] == 0 && obstacleGrid[i][j - 1] == 0) {
+                                if (obstacleGrid[i - 1][j] === 0 && obstacleGrid[i][j - 1] === 0) {
                                         obstacleGrid[i][j] = 0;
                                 } else {
                                         obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
