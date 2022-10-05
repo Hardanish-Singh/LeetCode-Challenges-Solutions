7a211e30a8cb1f1362da8a615fbbed4297e92818
Hardanish Singh <hardanishsingh1@gmail.com>
2022-10-04 20:11:32 -0400

    Word Search

--- Array/Medium/Word_Search/Word_Search.js
+++ Array/Medium/Word_Search/Word_Search.js
@@ -1,44 +1,44 @@
-function is_visited ( ...args ) {
-        let [ grid, i, j, count, word ] = args;
-        if( grid[i][j] === word[count] && grid[i][j] != '2' ) {
+function is_visited(...args) {
+        let [grid, i, j, count, word] = args;
+        if (grid[i][j] === word[count] && grid[i][j] != "2") {
                 return i + "," + j;
         }
 }
 
-function move_left( ...args ) {
-        let [ grid, i, j, count, word ] = args;
+function move_left(...args) {
+        let [grid, i, j, count, word] = args;
         --j;
-        if( j >= 0  ) {
-                return is_visited( grid, i, j, count, word );
+        if (j >= 0) {
+                return is_visited(grid, i, j, count, word);
         }
 }
 
-function move_top( ...args ) {
-        let [ grid, i, j, count, word ] = args;
+function move_top(...args) {
+        let [grid, i, j, count, word] = args;
         --i;
-        if( i >= 0 ) {
-                return is_visited( grid, i, j, count, word );  
+        if (i >= 0) {
+                return is_visited(grid, i, j, count, word);
         }
 }
 
-function move_right( ...args ) {
-        let [ grid, i, j, count, word ] = args;
+function move_right(...args) {
+        let [grid, i, j, count, word] = args;
         ++j;
-        if( j <= grid[i].length - 1 ) {
-                return is_visited( grid, i, j, count, word );
+        if (j <= grid[i].length - 1) {
+                return is_visited(grid, i, j, count, word);
         }
 }
 
-function move_down( ...args ) {
-        let [ grid, i, j, count, word ] = args;
+function move_down(...args) {
+        let [grid, i, j, count, word] = args;
         ++i;
-        if( i <= grid.length - 1 ) {
-                return is_visited( grid, i, j, count, word );  
+        if (i <= grid.length - 1) {
+                return is_visited(grid, i, j, count, word);
         }
 }
 
-function move_in_all_four_directions ( ...args ) {
-        let [ grid, i, j, stack, count, word, gridTraversal ] = args;
+function move_in_all_four_directions(...args) {
+        let [grid, i, j, stack, count, word, gridTraversal] = args;
         let leftPosition = "";
         let topPosition = "";
         let rightPosition = "";
@@ -49,119 +49,103 @@ function move_in_all_four_directions ( ...args ) {
         let bottomCounter = 0;
 
         // MARK THE COORDINATE AS VISITED
-        grid[i][j] = '2';
-        
+        grid[i][j] = "2";
+
         // MOVE LEFT & ADD COORDINATES TO STACK
-        leftPosition = move_left( grid, i, j, count, word );
-        if( leftPosition ) {
-                leftCounter = ( Number( count ) + 1 );
-                stack.push( leftPosition + "," + leftCounter );
+        leftPosition = move_left(grid, i, j, count, word);
+        if (leftPosition) {
+                leftCounter = Number(count) + 1;
+                stack.push(leftPosition + "," + leftCounter);
         }
         // MOVE TOP & ADD COORDINATES TO STACK
-        topPosition = move_top( grid, i, j, count, word );
-        if( topPosition ) {
-                topCounter = ( Number( count ) + 1 );
-                stack.push( topPosition + "," + topCounter );
+        topPosition = move_top(grid, i, j, count, word);
+        if (topPosition) {
+                topCounter = Number(count) + 1;
+                stack.push(topPosition + "," + topCounter);
         }
         // MOVE RIGHT & ADD COORDINATES TO STACK
-        rightPosition = move_right( grid, i, j, count, word );
-        if( rightPosition ) {
-                rightCounter = ( Number( count ) + 1 );
-                stack.push( rightPosition + "," + rightCounter );
+        rightPosition = move_right(grid, i, j, count, word);
+        if (rightPosition) {
+                rightCounter = Number(count) + 1;
+                stack.push(rightPosition + "," + rightCounter);
         }
         // MOVE DOWN & ADD COORDINATES TO STACK
-        bottomPosition = move_down( grid, i, j, count, word );
-        if( bottomPosition ) {
-                bottomCounter = ( Number( count ) + 1 );
-                stack.push( bottomPosition + "," + bottomCounter );
+        bottomPosition = move_down(grid, i, j, count, word);
+        if (bottomPosition) {
+                bottomCounter = Number(count) + 1;
+                stack.push(bottomPosition + "," + bottomCounter);
         }
 
-        if( 
-                ! leftPosition 
-                        && 
-                ! topPosition 
-                        && 
-                ! rightPosition
-                        && 
-                ! bottomPosition
-                        &&
-                gridTraversal.length > 0
-        ) {
+        if (!leftPosition && !topPosition && !rightPosition && !bottomPosition && gridTraversal.length > 0) {
                 grid = gridTraversal.pop();
         }
 
-        if( leftCounter == topCounter && leftCounter > 0 && topCounter > 0 ) {
-                gridTraversal.push( JSON.parse( JSON.stringify( grid ) ) );
+        if (leftCounter == topCounter && leftCounter > 0 && topCounter > 0) {
+                gridTraversal.push(JSON.parse(JSON.stringify(grid)));
         }
 
-        if( leftCounter == rightCounter && leftCounter > 0 && rightCounter > 0 ) {
-                gridTraversal.push( JSON.parse( JSON.stringify( grid ) ) );
+        if (leftCounter == rightCounter && leftCounter > 0 && rightCounter > 0) {
+                gridTraversal.push(JSON.parse(JSON.stringify(grid)));
         }
 
-        if( leftCounter == bottomCounter && leftCounter > 0 && bottomCounter > 0 ) {
-                gridTraversal.push( JSON.parse( JSON.stringify( grid ) ) );
+        if (leftCounter == bottomCounter && leftCounter > 0 && bottomCounter > 0) {
+                gridTraversal.push(JSON.parse(JSON.stringify(grid)));
         }
 
-        if( topCounter == rightCounter && topCounter > 0 && rightCounter > 0 ) {
-                gridTraversal.push( JSON.parse( JSON.stringify( grid ) ) );
+        if (topCounter == rightCounter && topCounter > 0 && rightCounter > 0) {
+                gridTraversal.push(JSON.parse(JSON.stringify(grid)));
         }
 
-        if( topCounter == bottomCounter && topCounter > 0 && bottomCounter > 0 ) {
-                gridTraversal.push( JSON.parse( JSON.stringify( grid ) ) );
+        if (topCounter == bottomCounter && topCounter > 0 && bottomCounter > 0) {
+                gridTraversal.push(JSON.parse(JSON.stringify(grid)));
         }
 
-        if( rightCounter == bottomCounter && rightCounter > 0 && bottomCounter > 0 ) {
-                gridTraversal.push( JSON.parse( JSON.stringify( grid ) ) );
+        if (rightCounter == bottomCounter && rightCounter > 0 && bottomCounter > 0) {
+                gridTraversal.push(JSON.parse(JSON.stringify(grid)));
         }
 
         return grid;
 }
 
-
-function perform_push_pop_operation( ...args ) {
+function perform_push_pop_operation(...args) {
         let [stack, grid, word, gridTraversal] = args;
-        while( stack.length != 0 ) {
+        while (stack.length != 0) {
                 // DFS METHOD
                 [i, j, count] = stack.pop().split(",").map(Number);
-                if( count == word.length ) {
-                        grid[i][j] = '2';
+                if (count == word.length) {
+                        grid[i][j] = "2";
                         return true;
                 }
-                grid = move_in_all_four_directions( grid, i, j, stack, count, word, gridTraversal );
+                grid = move_in_all_four_directions(grid, i, j, stack, count, word, gridTraversal);
         }
 }
 
-var exist = function( ...args ) {
-        let [ grid, word ] = args;
+var exist = function (...args) {
+        let [grid, word] = args;
         // DEEP COPY ARRAY
-        let originalGrid = JSON.parse( JSON.stringify( grid ) );
+        let originalGrid = JSON.parse(JSON.stringify(grid));
         let gridTraversal = [];
 
-        for( let i = 0; i < grid.length; i++ ) {
-                for( let j = 0; j < grid[i].length; j++ ) {
+        for (let i = 0; i < grid.length; i++) {
+                for (let j = 0; j < grid[i].length; j++) {
                         // RESTORE ORIGINIAL GRID
-                        grid = JSON.parse( JSON.stringify( originalGrid ) );
-                        if( grid[i][j] === word[0] ) {
+                        grid = JSON.parse(JSON.stringify(originalGrid));
+                        if (grid[i][j] === word[0]) {
                                 let stack = [];
                                 grid = move_in_all_four_directions(
-                                                                grid,
-                                                                i,
-                                                                j,
-                                                                stack,
-                                                                defaultCount = 1,
-                                                                word,
-                                                                gridTraversal
-                                                        );
-                                if( grid[i][j] == '2' && word.length == 1 ) {
+                                        grid,
+                                        i,
+                                        j,
+                                        stack,
+                                        (defaultCount = 1),
+                                        word,
+                                        gridTraversal
+                                );
+                                if (grid[i][j] == "2" && word.length == 1) {
                                         return true;
                                 }
-                                let isFound = perform_push_pop_operation(
-                                                                                stack,
-                                                                                grid,
-                                                                                word,
-                                                                                gridTraversal
-                                                                        );
-                                if( isFound ) {
+                                let isFound = perform_push_pop_operation(stack, grid, word, gridTraversal);
+                                if (isFound) {
                                         return true;
                                 }
                         }
