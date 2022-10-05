5f4ee844b21a037e87c193c99f98c75cffb9b3de
Hardanish Singh <hardanishsingh1@gmail.com>
2022-10-05 08:02:42 -0400

    Word Search

--- Array/Medium/Word_Search/Word_Search.js
+++ Array/Medium/Word_Search/Word_Search.js
@@ -54,25 +54,25 @@ function move_in_all_four_directions(...args) {
         // MOVE LEFT & ADD COORDINATES TO STACK
         leftPosition = move_left(grid, i, j, count, word);
         if (leftPosition) {
-                leftCounter = Number(count) + 1;
+                leftCounter = count + 1;
                 stack.push(leftPosition + "," + leftCounter);
         }
         // MOVE TOP & ADD COORDINATES TO STACK
         topPosition = move_top(grid, i, j, count, word);
         if (topPosition) {
-                topCounter = Number(count) + 1;
+                topCounter = count + 1;
                 stack.push(topPosition + "," + topCounter);
         }
         // MOVE RIGHT & ADD COORDINATES TO STACK
         rightPosition = move_right(grid, i, j, count, word);
         if (rightPosition) {
-                rightCounter = Number(count) + 1;
+                rightCounter = count + 1;
                 stack.push(rightPosition + "," + rightCounter);
         }
         // MOVE DOWN & ADD COORDINATES TO STACK
         bottomPosition = move_down(grid, i, j, count, word);
         if (bottomPosition) {
-                bottomCounter = Number(count) + 1;
+                bottomCounter = count + 1;
                 stack.push(bottomPosition + "," + bottomCounter);
         }
 
