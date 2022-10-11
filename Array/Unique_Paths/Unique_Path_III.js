815073a68a002ec3d8ec05d868e1e9c845288d9d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-10-11 12:41:54 -0400

    Unique Paths III

--- Array/Unique_Paths/Unique_Path_III.js
+++ Array/Unique_Paths/Unique_Path_III.js
@@ -1,3 +1,27 @@
+function move_left(...args) {
+        let [grid, i, j] = args;
+        j = j - 1;
+        return j >= 0 ? i + "," + j : null;
+}
+
+function move_top(...args) {
+        let [grid, i, j] = args;
+        i = i - 1;
+        return i >= 0 ? i + "," + j : null;
+}
+
+function move_right(...args) {
+        let [grid, i, j] = args;
+        j = j + 1;
+        return j <= grid[i].length - 1 ? i + "," + j : null;
+}
+
+function move_down(...args) {
+        let [grid, i, j] = args;
+        i = i + 1;
+        return i <= grid.length - 1 ? i + "," + j : null;
+}
+
 var uniquePathsIII = function (grid) {
         let zeros = 0;
         let starting_row = null;
@@ -13,4 +37,83 @@ var uniquePathsIII = function (grid) {
                         }
                 }
         }
+
+        let stack = [[starting_row, staring_column, [`${starting_row},${staring_column}`], 0]];
+        let result = 0;
+
+        while (stack.length > 0) {
+                let [row, column, path, count] = stack.pop();
+                let leftPosition = move_left(grid, row, column);
+                if (leftPosition != null) {
+                        let [ni, nj] = leftPosition.split(",");
+                        if (grid[ni][nj] !== -1) {
+                                if (grid[ni][nj] === 2) {
+                                        if (count === zeros) {
+                                                result++;
+                                        }
+                                } else {
+                                        if (!path.includes(`${ni},${nj}`)) {
+                                                let temp = JSON.parse(JSON.stringify(path));
+                                                temp.push(`${ni},${nj}`);
+                                                stack.push([+ni, +nj, temp, count + 1]);
+                                        }
+                                }
+                        }
+                }
+
+                let topPosition = move_top(grid, row, column);
+                if (topPosition != null) {
+                        let [ni, nj] = topPosition.split(",");
+                        if (grid[ni][nj] !== -1) {
+                                if (grid[ni][nj] === 2) {
+                                        if (count === zeros) {
+                                                result++;
+                                        }
+                                } else {
+                                        if (!path.includes(`${ni},${nj}`)) {
+                                                let temp = JSON.parse(JSON.stringify(path));
+                                                temp.push(`${ni},${nj}`);
+                                                stack.push([+ni, +nj, temp, count + 1]);
+                                        }
+                                }
+                        }
+                }
+
+                let bottomPosition = move_down(grid, row, column);
+                if (bottomPosition != null) {
+                        let [ni, nj] = bottomPosition.split(",");
+                        if (grid[ni][nj] !== -1) {
+                                if (grid[ni][nj] === 2) {
+                                        if (count === zeros) {
+                                                result++;
+                                        }
+                                } else {
+                                        if (!path.includes(`${ni},${nj}`)) {
+                                                let temp = JSON.parse(JSON.stringify(path));
+                                                temp.push(`${ni},${nj}`);
+                                                stack.push([+ni, +nj, temp, count + 1]);
+                                        }
+                                }
+                        }
+                }
+
+                let rightPosition = move_right(grid, row, column);
+                if (rightPosition != null) {
+                        let [ni, nj] = rightPosition.split(",");
+                        if (grid[ni][nj] !== -1) {
+                                if (grid[ni][nj] === 2) {
+                                        if (count === zeros) {
+                                                result++;
+                                        }
+                                } else {
+                                        if (!path.includes(`${ni},${nj}`)) {
+                                                let temp = JSON.parse(JSON.stringify(path));
+                                                temp.push(`${ni},${nj}`);
+                                                stack.push([+ni, +nj, temp, count + 1]);
+                                        }
+                                }
+                        }
+                }
+        }
+        return result;
 };
