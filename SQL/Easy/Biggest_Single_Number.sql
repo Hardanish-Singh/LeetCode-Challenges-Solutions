7bd1a24da25163825f9525975e8e6ccfa62b33d2
Hardanish Singh <hardanishsingh1@gmail.com>
2022-10-20 09:55:47 -0400

    Biggest Single Number

--- SQL/Easy/Biggest_Single_Number.sql
+++ SQL/Easy/Biggest_Single_Number.sql
@@ -1,26 +1,5 @@
-/*
-        Table my_numbers contains many numbers in column num including duplicated ones.
-        Can you write a SQL query to find the biggest number, which only appears once.
+-- Leetcode: https://leetcode.com/problems/biggest-single-number/
 
-                +---+
-                |num|
-                +---+
-                | 8 |
-                | 8 |
-                | 3 |
-                | 3 |
-                | 1 |
-                | 4 |
-                | 5 |
-                | 6 | 
-        For the sample data above, your query should return the following result:
-                +---+
-                |num|
-                +---+
-                | 6 |
-        Note:
-                If there is no such number, just output null.
-*/
 -- SOLUTION 1
 SELECT 
         MAX(num) AS num 
