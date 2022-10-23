6cf916ef26b682f257395e32f02c0139a11f548d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-10-23 09:31:34 -0400

    Customers Who Bought Products A and B but Not C

--- SQL/Medium/Customers_Who_Bought_Products_A_and_B_but_Not_C.sql
+++ SQL/Medium/Customers_Who_Bought_Products_A_and_B_but_Not_C.sql
@@ -1,1 +1,39 @@
 -- Leetcode: https://leetcode.com/problems/customers-who-bought-products-a-and-b-but-not-c/
+
+SELECT
+        *
+FROM
+        Customers
+WHERE
+        customer_id IN 
+                        (
+                                SELECT
+                                        customer_id
+                                FROM
+                                        Orders
+                                WHERE
+                                        product_name = 'A'
+                        )
+AND
+        customer_id IN
+                        (
+                                SELECT
+                                        customer_id
+                                FROM
+                                        Orders
+                                WHERE
+                                        product_name = 'B'
+                        )
+AND
+        customer_id NOT IN 
+                        (
+                                SELECT
+                                        customer_id
+                                FROM
+                                        Orders
+                                WHERE
+                                        product_name = 'C'
+                        )
+        
+
+ORDER BY customer_id;
