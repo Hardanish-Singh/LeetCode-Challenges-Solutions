9f8c13efc7e99b9b35080983c25b156b8be6e9ab
Hardanish Singh <hardanishsingh1@gmail.com>
2022-10-27 17:57:45 -0400

    Product Sales Analysis IV

--- SQL/Easy/Product_Sales_Analysis_IV.sql
+++ SQL/Easy/Product_Sales_Analysis_IV.sql
@@ -5,15 +5,13 @@ SELECT
         product_id  
 FROM 
 (
-
         SELECT
                 user_id,
                 product_id,
                 data,
                 dense_rank() OVER ( partition by user_id order by data desc )  AS ranks
         FROM
-        (
-                
+        (    
                 SELECT
                         x.user_id,
                         x.product_id,
@@ -30,9 +28,7 @@ FROM
                 ) AS x
                 JOIN Product ON Product.product_id = x.product_id
                 ORDER BY x.user_id, x.product_id
-
         ) AS xx
-
 ) AS xxx
 WHERE xxx.ranks = 1;
         
