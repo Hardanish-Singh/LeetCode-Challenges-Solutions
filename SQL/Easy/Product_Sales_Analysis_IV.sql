0fc3c807bbf59d67b9e44e651d5867c64dba390d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-10-27 17:56:01 -0400

    Product Sales Analysis IV

--- SQL/Easy/Product_Sales_Analysis_IV.sql
+++ SQL/Easy/Product_Sales_Analysis_IV.sql
@@ -14,22 +14,22 @@ FROM
         FROM
         (
                 
-        SELECT
-                x.user_id,
-                x.product_id,
-                x.quantity * Product.price AS data
-        FROM 
-        (
                 SELECT
-                        product_id,
-                        user_id,
-                        SUM(quantity) AS quantity
-                FROM
-                        Sales
-                GROUP BY user_id, product_id
-        ) AS x
-        JOIN Product ON Product.product_id = x.product_id
-        ORDER BY x.user_id, x.product_id
+                        x.user_id,
+                        x.product_id,
+                        x.quantity * Product.price AS data
+                FROM 
+                (
+                        SELECT
+                                product_id,
+                                user_id,
+                                SUM(quantity) AS quantity
+                        FROM
+                                Sales
+                        GROUP BY user_id, product_id
+                ) AS x
+                JOIN Product ON Product.product_id = x.product_id
+                ORDER BY x.user_id, x.product_id
 
         ) AS xx
 
