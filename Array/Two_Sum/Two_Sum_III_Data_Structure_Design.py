ba71512c0a4b413a317716d3f80686291cb9f1a0
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-04 10:05:59 -0400

    Two Sum III Data Structure Design

--- Array/Two_Sum/Two_Sum_III_Data_Structure_Design.py
+++ Array/Two_Sum/Two_Sum_III_Data_Structure_Design.py
@@ -1,21 +1,22 @@
 # Leetcode: https://leetcode.com/problems/two-sum-iii-data-structure-design/
 
+# Leetcode: https://leetcode.com/problems/two-sum-iii-data-structure-design/
+
 class TwoSum:
         def __init__(self):
-                self.hash_table = { }
+                self.numbers = []
 
         def add(self, number: int) -> None:
-                if number in self.hash_table:
-                        self.hash_table[ number ] += 1
-                else:
-                        self.hash_table[ number ] = 1
+                self.numbers.append(number)
 
         def find(self, target: int) -> bool:
-                dictionary = { }
-                for key, value in self.hash_table.items():
-                        difference = target - key
-                        if ( difference in self.hash_table and value == 2 ) or ( difference in dictionary ):
+                hash_table = { }
+                
+                for i in range( 0, len( self.numbers ) ):
+                        difference = target - self.numbers[ i ]
+                        if difference in hash_table:
                                 return True
                         else:
-                                dictionary[ key ] = 1
+                                hash_table[ self.numbers[ i ] ] = True
+                
                 return False
