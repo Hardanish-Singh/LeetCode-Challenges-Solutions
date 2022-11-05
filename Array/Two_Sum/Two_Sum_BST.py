e350529aed8fb058f7d5ee33f2b6bd927fb1238f
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-05 10:51:17 -0400

    Two Sum BST

--- Array/Two_Sum/Two_Sum_BST.py
+++ Array/Two_Sum/Two_Sum_BST.py
@@ -5,7 +5,7 @@
 #     def __init__( self, val = 0, left = None, right = None ):
 #         self.val = val
 #         self.left = left
-#         self.right = right
+#         self.right = right 
 
 class Solution:
         def preorderRecursiveTraversal( self, root: Optional[TreeNode], preOrderList: List[int] ) -> List[int]:
