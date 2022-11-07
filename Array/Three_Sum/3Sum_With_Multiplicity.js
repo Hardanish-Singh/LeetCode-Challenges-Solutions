5062de8b37a1801d7f8bbb744f96880a1ae081f9
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-07 14:29:02 -0500

    Three Sum With Multiplicity

--- Array/Three_Sum/3Sum_With_Multiplicity.js
+++ Array/Three_Sum/3Sum_With_Multiplicity.js
@@ -4,40 +4,38 @@
  * @param { number[] } arr
  * @param { number } target
  * @return { number }
-*/
+ */
 
 // SOLUTION 1 ( USING THREE POINTER )
-var threeSumMulti = function( nums, target ) {
-        nums.sort( (a, b) => a - b );
+var threeSumMulti = function (nums, target) {
+        nums.sort((a, b) => a - b);
         let count = 0;
-        
-	for( let i = 0; i < nums.length; i++ ) {
-		let leftPointer = i + 1;
-		let rightPointer = nums.length - 1;
-		
-		while( leftPointer < rightPointer ) {
-                        if( nums[leftPointer] + nums[rightPointer] + nums[i] < target ) {
-				leftPointer++;
-			}
-			else if( nums[leftPointer] + nums[rightPointer] + nums[i] > target ) {
-				rightPointer--;
-			}
-			else if( nums[leftPointer] + nums[rightPointer] + nums[i] === target ) {
-                                if( nums[leftPointer] === nums[rightPointer] ) {
+
+        for (let i = 0; i < nums.length; i++) {
+                let leftPointer = i + 1;
+                let rightPointer = nums.length - 1;
+
+                while (leftPointer < rightPointer) {
+                        if (nums[leftPointer] + nums[rightPointer] + nums[i] < target) {
+                                leftPointer++;
+                        } else if (nums[leftPointer] + nums[rightPointer] + nums[i] > target) {
+                                rightPointer--;
+                        } else if (nums[leftPointer] + nums[rightPointer] + nums[i] === target) {
+                                if (nums[leftPointer] === nums[rightPointer]) {
                                         let n = rightPointer - leftPointer;
-                                        count += Math.floor( ( n * (n + 1) ) / 2 ) ;
+                                        count += Math.floor((n * (n + 1)) / 2);
                                         break;
                                 }
-                                
+
                                 count++;
 
                                 // FIX LEFT POINTER & MOVE RIGHT POINTER
                                 let start = leftPointer;
                                 let end = rightPointer - 1;
-                                while( start < end ) {
-                                        if( nums[ i ] + nums[ start ] + nums[ end ] === target ) {
+                                while (start < end) {
+                                        if (nums[i] + nums[start] + nums[end] === target) {
                                                 count++;
-                                        } else if( nums[ i ] + nums[ start ] + nums[ end ] < target ) {
+                                        } else if (nums[i] + nums[start] + nums[end] < target) {
                                                 break;
                                         }
                                         end--;
@@ -46,20 +44,20 @@ var threeSumMulti = function( nums, target ) {
                                 // FIX RIGHT POINTER & MOVE LEFT POINTER
                                 start = leftPointer + 1;
                                 end = rightPointer;
-                                while( start < end ) {
-                                        if( nums[ i ] + nums[ start ] + nums[ end ] === target ) {
+                                while (start < end) {
+                                        if (nums[i] + nums[start] + nums[end] === target) {
                                                 count++;
-                                        } else if( nums[ i ] + nums[ start ] + nums[ end ] > target ) {
+                                        } else if (nums[i] + nums[start] + nums[end] > target) {
                                                 break;
                                         }
                                         start++;
                                 }
-                                
-				leftPointer++;
-				rightPointer--;
-			}
-		}
-	}
+
+                                leftPointer++;
+                                rightPointer--;
+                        }
+                }
+        }
         let mod = 1000000007;
-	return count % mod;
+        return count % mod;
 };
