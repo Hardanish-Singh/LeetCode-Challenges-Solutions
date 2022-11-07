c2471fc371bde2034e0ab656d02bb04eafaa1d6c
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-07 14:27:41 -0500

    Three Sum Closest

--- Array/Three_Sum/three_sum_closest.ts
+++ Array/Three_Sum/three_sum_closest.ts
@@ -1,35 +1,40 @@
 // Leetcode: https://leetcode.com/problems/3sum-closest/
 
 /**
+ * @param { number[] } closestSumArray
+ * @param { number } target
+ * @return { number }
+ */
+
+function findClosestElementToTarget(closestSumArray: Array<number>, target: number): number {
+        if (closestSumArray.length == 0) {
+                return 0;
+        }
+        let closest: number = closestSumArray[0];
+        for (let item of closestSumArray) {
+                if (Math.abs(item - target) < Math.abs(closest - target)) {
+                        closest = item;
+                }
+        }
+        return closest;
+}
+
+/**
  * @param { number[] } nums
  * @param { number } target
  * @return { number }
  */
 
-const findClosestElementToTarget = (closestSumArray, num) => {
-        if (closestSumArray.length == 0) {
-                return 0;
-        }
-
-        let closest = closestSumArray[0];
-        for (let item of closestSumArray) {
-                if (Math.abs(item - num) < Math.abs(closest - num)) {
-                        closest = item;
-                }
-        }
-        return closest;
-};
-
-function threeSumClosest(nums, target) {
+function threeSumClosest(nums: Array<number>, target: number): number {
         nums.sort((a, b) => a - b);
-        let closestSumArray = [];
+        let closestSumArray: Array<number> = [];
 
-        for (let i = 0; i < nums.length - 1; i++) {
-                let leftPointer = i + 1;
-                let rightPointer = nums.length - 1;
+        for (let i: number = 0; i < nums.length - 1; i++) {
+                let leftPointer: number = i + 1;
+                let rightPointer: number = nums.length - 1;
 
                 while (leftPointer < rightPointer) {
-                        let sum = nums[i] + nums[leftPointer] + nums[rightPointer];
+                        let sum: number = nums[i] + nums[leftPointer] + nums[rightPointer];
                         closestSumArray.push(sum);
                         if (nums[leftPointer] + nums[rightPointer] + nums[i] < target) {
                                 leftPointer++;
