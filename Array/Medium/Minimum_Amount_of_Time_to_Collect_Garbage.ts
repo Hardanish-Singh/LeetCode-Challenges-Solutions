0b5faee346d4a964fd8b955f84529a4e07498038
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-07 09:12:12 -0500

    Minimum Amount of Time to Collect Garbage

--- Array/Medium/Minimum_Amount_of_Time_to_Collect_Garbage.ts
+++ Array/Medium/Minimum_Amount_of_Time_to_Collect_Garbage.ts
@@ -2,18 +2,18 @@ type garbageCount = {
         [key: string]: number;
 };
 
-var calculateCost = function (garbage: Array<string>, travel: Array<number>, map: garbageCount, key: string) {
+var calculateCost = function (garbage: Array<string>, travel: Array<number>, map: garbageCount, key: string): number {
         if (!map.hasOwnProperty(key)) {
                 return 0;
         }
-        let cost = 0;
-        for (let i = 0; i < garbage.length; i++) {
+        let cost: number = 0;
+        for (let i: number = 0; i < garbage.length; i++) {
                 if (map[key] <= 0) {
                         return cost;
                 }
                 cost += travel[i - 1] || 0;
-                let garbageType = garbage[i].split("");
-                for (let j = 0; j < garbageType.length; j++) {
+                let garbageType: Array<string> = garbage[i].split("");
+                for (let j: number = 0; j < garbageType.length; j++) {
                         if (garbageType[j] === key) {
                                 cost++;
                                 map[garbageType[j]] -= 1;
