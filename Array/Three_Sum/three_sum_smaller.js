5c50196756269b8e40d09c626fe45a338d5f4857
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-07 09:26:10 -0500

    Three Sum Smaller

--- Array/Three_Sum/three_sum_smaller.js
+++ Array/Three_Sum/three_sum_smaller.js
@@ -4,39 +4,38 @@
  * @param { number[] } nums
  * @param { number } target
  * @return { number }
-*/
+ */
 
-var threeSumSmaller = function( nums, target ) {
-        nums.sort( (a, b) => a - b );
+var threeSumSmaller = function (nums, target) {
+        nums.sort((a, b) => a - b);
         let sum = 0;
-	
-	for( let i = 0; i < nums.length-1; i++ ) {
+
+        for (let i = 0; i < nums.length - 1; i++) {
                 let leftPointer = i + 1;
-		let rightPointer = nums.length - 1;
-		
-		while( leftPointer < rightPointer ) {
-                        if( nums[leftPointer] + nums[rightPointer] + nums[i] < target ) {
+                let rightPointer = nums.length - 1;
+
+                while (leftPointer < rightPointer) {
+                        if (nums[leftPointer] + nums[rightPointer] + nums[i] < target) {
                                 sum++;
-                                // NOW WE NEED TO FIND HOW MANY PAIRS WITH leftPointer index THAT SATISFY THIS CONDITION?
-                                // FIX LEFT POINTER & MOVE RIGHT POINTER
+                                /*
+                                        NOW WE NEED TO FIND HOW MANY PAIRS WITH leftPointer index THAT SATISFY THIS CONDITION?
+                                        FIX LEFT POINTER & MOVE RIGHT POINTER
+                                */
                                 let start = leftPointer;
                                 let end = rightPointer - 1;
-                                while( start < end ) {
-                                        if( nums[ i ] + nums[ start ] + nums[ end ] < target ) {
+                                while (start < end) {
+                                        if (nums[i] + nums[start] + nums[end] < target) {
                                                 sum++;
-                                        } 
-                                        else {
+                                        } else {
                                                 break;
                                         }
                                         end--;
                                 }
-				leftPointer++;
-			}
-			else {
-				rightPointer--;
-			}
-		}	
-		
-	}
-	return sum;
+                                leftPointer++;
+                        } else {
+                                rightPointer--;
+                        }
+                }
+        }
+        return sum;
 };
