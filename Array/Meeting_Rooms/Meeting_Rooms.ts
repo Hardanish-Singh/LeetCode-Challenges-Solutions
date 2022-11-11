5e19a7549a6f025fc2043596fa6ba6a5fb4fca2a
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-11 18:43:33 -0500

    Meeting Rooms I

--- Array/Meeting_Rooms/Meeting_Rooms.ts
+++ Array/Meeting_Rooms/Meeting_Rooms.ts
@@ -9,6 +9,7 @@ function canAttendMeetings(intervals: number[][]): boolean {
         intervals.sort((a, b) => a[0] - b[0]);
         for (let i: number = 0; i < intervals.length - 1; i++) {
                 let j: number = i + 1;
+                // A half-closed interval [a, b) is the interval between a and b including a and not including b
                 if (intervals[j][0] >= intervals[i][0] && intervals[j][0] < intervals[i][1]) {
                         return false;
                 }
