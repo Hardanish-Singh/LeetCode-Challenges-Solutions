c1557b205b07393bfb5b2680b62abff6964cbfe5
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-12 11:09:19 -0500

    Meeting Rooms III

--- Array/Meeting_Rooms/Meeting_Rooms_III.js
+++ Array/Meeting_Rooms/Meeting_Rooms_III.js
@@ -3,6 +3,7 @@
  * @param {number[][]} meetings
  * @return {number}
  */
+
 var mostBooked = function (n, meetings) {
         let meetingCount = [];
         let endTime = [];
@@ -17,28 +18,24 @@ var mostBooked = function (n, meetings) {
         for (let i = 0; i < meetings.length; i++) {
                 let startTime = meetings[i][0];
                 let end = meetings[i][1];
-                let earliestRoom = -1;
-                let earliestTime = Number.MAX_VALUE;
                 let freeRoomFound = false;
                 for (let j = 0; j < n; j++) {
                         // When the start is bigger than end, it means at this time one of the previous meeting ends, and it can take and reuse that room.
-                        if (startTime >= endTime[j]) {
-                                //This is the room with the smallest index which is free on or before startTime time of the current meeting
-                                meetingCount[j]++;
-                                endTime[j] = end; //This room will be avialable at 'end' time of the current meeting.
+                        if (startTime >= endTime[j] || endTime[j] == -1) {
+                                meetingCount[j]++; // This is the room with the smallest index which is free on or before startTime time of the current meeting
+                                endTime[j] = end; // This room will be avialable at 'end' time of the current meeting.
                                 freeRoomFound = true;
                                 break;
-                        } else if (earliestTime > endTime[j]) {
-                                //Let's keep track of the "room with smallest index and being available at the earliest"
-                                earliestTime = endTime[j];
-                                earliestRoom = j;
                         }
                 }
-                if (freeRoomFound === false) {
-                        //If we couldn't find any meeting room then we will wait for the "room with smallest index and being available at the earliest"
-                        meetingCount[earliestRoom]++;
-                        endTime[earliestRoom] += end - startTime; //Time to vacant for the room will be increased by the duration of current meeting.
+                // If we couldn't find any meeting room that is free
+                if (!freeRoomFound) {
+                        let minEndTimeIndex = endTime.indexOf(Math.min.apply(null, endTime)); // Find index of the room with minimum endTime
+                        let duration = end - startTime; // calculate duration of current meeting
+                        endTime[minEndTimeIndex] += duration; // Time to vacant the room will be increased by the duration of current meeting.
+                        meetingCount[minEndTimeIndex]++;
                 }
         }
+        // Now return the room number with the maximum frequency that occured at first in order.
         return meetingCount.indexOf(Math.max.apply(null, meetingCount));
 };
