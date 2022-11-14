74249b8363f16ace1875a507e7f16e2cb447ea02
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-14 06:36:30 -0500

    Meeting Rooms I

--- Array/Meeting_Rooms/Meeting_Rooms.ts
+++ Array/Meeting_Rooms/Meeting_Rooms.ts
@@ -1,16 +1,16 @@
 // Leetcode: https://leetcode.com/problems/meeting-rooms/
 
 /**
- * @param { number[][] } intervals
+ * @param { number[][] } meetings
  * @return { boolean }
  */
 
-function canAttendMeetings(intervals: number[][]): boolean {
-        intervals.sort((a, b) => a[0] - b[0]);
-        for (let i: number = 0; i < intervals.length - 1; i++) {
+function canAttendMeetings(meetings: number[][]): boolean {
+        meetings.sort((a, b) => a[0] - b[0]);
+        for (let i: number = 0; i < meetings.length - 1; i++) {
                 let j: number = i + 1;
                 // A half-closed interval [a, b) is the interval between a and b including a and not including b
-                if (intervals[j][0] >= intervals[i][0] && intervals[j][0] < intervals[i][1]) {
+                if (meetings[j][0] >= meetings[i][0] && meetings[j][0] < meetings[i][1]) {
                         return false;
                 }
         }
