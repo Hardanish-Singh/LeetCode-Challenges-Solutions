30d8493d470a875ebc0b5e9098baec1ec8c348a1
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-14 07:33:36 -0500

    Find Minimum in Rotated Sorted Array

---
+++ Array/Find_Minimum_in_Rotated_Sorted_Array/Find_Minimum_in_Rotated_Sorted_Array.js
@@ -0,0 +1,10 @@
+// Leetcode: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var findMin = function (nums) {
+        return Math.min.apply(null, nums);
+};
