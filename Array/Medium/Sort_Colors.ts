eedb675b8c64a48be017aa506cc412a625c05f47
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-16 09:08:00 -0500

    Sort Colors

--- Array/Medium/Sort_Colors.ts
+++ Array/Medium/Sort_Colors.ts
@@ -9,12 +9,22 @@ function sortColors(nums: number[]): void {
         let blue = 0;
 
         for (let i = 0; i < nums.length; i++) {
-                if (nums[i] == 0) red++;
-                else if (nums[i] == 1) white++;
-                else if (nums[i] == 2) blue++;
+                if (nums[i] == 0) {
+                        red++;
+                } else if (nums[i] == 1) {
+                        white++;
+                } else if (nums[i] == 2) {
+                        blue++;
+                }
         }
 
-        for (let i = 0; i < red; i++) nums[i] = 0;
-        for (let i = 0; i < white; i++) nums[red + i] = 1;
-        for (let i = 0; i < blue; i++) nums[red + white + i] = 2;
+        for (let i = 0; i < red; i++) {
+                nums[i] = 0;
+        }
+        for (let i = 0; i < white; i++) {
+                nums[red + i] = 1;
+        }
+        for (let i = 0; i < blue; i++) {
+                nums[red + white + i] = 2;
+        }
 }
