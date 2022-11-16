4d4729ef6f48089324a31c1dff46f1cf1dc9eb43
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-16 09:07:30 -0500

    Sort Colors

---
+++ Array/Medium/Sort_Colors.ts
@@ -0,0 +1,20 @@
+// Leetcode: https://leetcode.com/problems/sort-colors/
+
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums: number[]): void {
+        let red = 0;
+        let white = 0;
+        let blue = 0;
+
+        for (let i = 0; i < nums.length; i++) {
+                if (nums[i] == 0) red++;
+                else if (nums[i] == 1) white++;
+                else if (nums[i] == 2) blue++;
+        }
+
+        for (let i = 0; i < red; i++) nums[i] = 0;
+        for (let i = 0; i < white; i++) nums[red + i] = 1;
+        for (let i = 0; i < blue; i++) nums[red + white + i] = 2;
+}
