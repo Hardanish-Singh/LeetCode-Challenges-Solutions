004a88747683a892bd8e8de3ff3c5b4310b7c3f1
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-22 09:42:32 -0500

    String Compression I

--- String/String_Compression/String_Compression_I.ts
+++ String/String_Compression/String_Compression_I.ts
@@ -1,14 +1,8 @@
-/**
- *
- * @param { string[] }  chars
- * @returns { number }
- */
-
-function compress(chars: Array<string>): number {
-        chars.push("+-");
+function compress(chars: string[]): number {
+        chars.push("π");
         let i = 0;
 
-        while (chars[i] != "+-") {
+        while (chars[i] != "π") {
                 let j = i;
                 let count = 1;
                 while (chars[j] == chars[j + 1]) {
@@ -19,13 +13,9 @@ function compress(chars: Array<string>): number {
                 count > 1 && chars.push(...String(count).split(""));
                 i = j + 1;
         }
-        let index = chars.indexOf("+-");
-        if (chars[index + 1] == null) {
-                chars.splice(index, 1);
-        } else {
-                for (let i = index; i >= 0; i--) {
-                        chars.splice(i, 1);
-                }
+        let index = chars.indexOf("π");
+        for (let i = index; i >= 0; i--) {
+                chars.splice(i, 1);
         }
         return chars.length;
 }
