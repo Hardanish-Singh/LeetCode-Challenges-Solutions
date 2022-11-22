199a2972516802449c1fc21435cdf31a4fc5ec17
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-22 09:24:17 -0500

    String Compression I

--- JS/string_compression.ts
+++ JS/string_compression.ts
@@ -1,48 +1,27 @@
-// Leetcode: https://leetcode.com/problems/string-compression/
+function compress(chars: string[]): number {
+        chars.push("+-");
+        let i = 0;
 
-var compress = function (chars: Array<String>): number {
-        for (let i: number = 0; i < chars.length; i++) {
-                let charCount: number = 1;
-                for (let j: number = i + 1; j < chars.length; j++) {
-                        if (chars[i] === chars[j]) {
-                                charCount++;
-                        } else {
-                                break;
-                        }
+        while (chars[i] != "+-") {
+                let j = i;
+                let count = 1;
+                while (chars[j] == chars[j + 1]) {
+                        count++;
+                        j++;
                 }
-                if (charCount === 1) {
-                        continue;
+                chars.push(chars[i]);
+                if (count > 1) {
+                        chars.push(...String(count).split(""));
                 }
-                let index: number = -1;
-                if (charCount > 9) {
-                        index = RemoveDuplicateCharactersAndAddGroupCount(chars, index, i);
-                        for (let k: number = 0; k < String(charCount).length; k++) {
-                                chars.splice(index, 0, String(charCount)[k]);
-                                i = index;
-                                index++;
-                        }
-                } else {
-                        index = RemoveDuplicateCharactersAndAddGroupCount(chars, index, i);
-                        chars.splice(index, 0, String(charCount));
-                        i = index;
+                i = j + 1;
+        }
+        let index = chars.indexOf("+-");
+        if (chars[index + 1] == null) {
+                chars.splice(index, 1);
+        } else {
+                for (let i = index; i >= 0; i--) {
+                        chars.splice(i, 1);
                 }
         }
         return chars.length;
-};
-
-function RemoveDuplicateCharactersAndAddGroupCount(...args: Array<any>): number {
-        let [chars, index, i] = args;
-        for (let k: number = i; k < chars.length; k++) {
-                if (chars[k] !== chars[k + 1]) {
-                        index = k + 1;
-                        break;
-                }
-        }
-        let splicingIndex: number = Math.abs(i + 1 - (index - 1)) + 1;
-        if (splicingIndex === 0) {
-                splicingIndex = 1;
-        }
-        chars.splice(i + 1, splicingIndex);
-        index = i + 1;
-        return index;
 }
