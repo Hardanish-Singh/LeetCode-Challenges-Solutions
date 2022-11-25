d8233369517a41f5284b79182a69cc315b3c29a7
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-25 16:14:01 -0500

    Permutations I

--- Array/Medium/Permutations.js
+++ Array/Medium/Permutations.js
@@ -1,46 +1,12 @@
-/*
-        Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
-
-        Example 1:
-                Input: nums = [1, 2, 3]
-                Output: 
-                        [ 
-                                [1, 2, 3],
-                                [1, 3, 2],
-                                [2, 1, 3],
-                                [2, 3, 1],
-                                [3, 1, 2],
-                                [3, 2, 1]
-                        ]
-        Example 2:
-                Input: nums = [0,1]
-                Output: 
-                        [
-                                [0, 1],
-                                [1, 0]
-                        ]
-        Example 3:
-                Input: nums = [1]
-                Output: 
-                        [
-                                [1]
-                        ]
-
-        Constraints:
-                1) 1 <= nums.length <= 6
-                2) -10 <= nums[i] <= 10
-                3) All the integers of nums are unique.
-*/
-
-/*
+/**
  * @param {number[]} nums
  * @return {number[][]}
-*/
+ */
 
-function is_complete( array1, array2 ) {
+function is_complete(array1, array2) {
         let is_complete = true;
-        for( let k=0; k<array1.length; k++ ) {
-                if( array1[k].length !== array2.length ) {
+        for (let k = 0; k < array1.length; k++) {
+                if (array1[k].length !== array2.length) {
                         is_complete = false;
                         break;
                 }
@@ -48,28 +14,28 @@ function is_complete( array1, array2 ) {
         return is_complete ? false : true;
 }
 
-var permute = function( nums ) {
-        if( nums.length === 0 ) {
+var permute = function (nums) {
+        if (nums.length === 0) {
                 return [];
         }
-        if( nums.length === 1 ) {
+        if (nums.length === 1) {
                 return [nums];
         }
         let permutations = [];
         let temp = [];
         let temp_permutations = [];
-        for( let i=0; i<nums.length; i++ ) {
+        for (let i = 0; i < nums.length; i++) {
                 temp_permutations.push([nums[i]]);
-                temp = [...nums.slice(0,i), ...nums.slice(i+1)];
-                while( is_complete( temp_permutations, nums ) ) {
+                temp = [...nums.slice(0, i), ...nums.slice(i + 1)];
+                while (is_complete(temp_permutations, nums)) {
                         let elementArray = temp_permutations.shift();
-                        for( let j=0; j<temp.length; j++ ) {
-                                if( ! ( elementArray.includes( temp[j] ) ) ) {
-                                        let t = [ ...elementArray, temp[j] ];
-                                        if( t.length === nums.length ){
-                                                permutations.push( t );
+                        for (let j = 0; j < temp.length; j++) {
+                                if (!elementArray.includes(temp[j])) {
+                                        let t = [...elementArray, temp[j]];
+                                        if (t.length === nums.length) {
+                                                permutations.push(t);
                                         }
-                                        temp_permutations.push( [ ...elementArray, temp[j] ] );
+                                        temp_permutations.push([...elementArray, temp[j]]);
                                 }
                         }
                 }
