87b81aaef026e75aefeef0b42e95a794868d1f21
Hardanish Singh <hardanishsingh1@gmail.com>
2022-11-26 09:41:44 -0500

    Permutations I

---
+++ Array/Permutations/Permutations.py
@@ -0,0 +1,18 @@
+class Solution:
+        def permute(self, nums: List[int]) -> List[List[int]]:
+                nums = sorted(nums)
+                stack = []
+                result = []
+                
+                for num in nums:
+                        stack.append([num])
+
+                while stack: 
+                        item = stack.pop(0)
+                        if len(item) == len(nums):
+                                result.append(item)
+                        else:
+                                for num in nums:
+                                        if num not in item:
+                                                stack.append(item + [num])
+                return result
