3f3e81e0c007814518b14aab632f96ac5d1bbfc7
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-04 11:22:02 -0500

    Permutations II

---
+++ Array/Permutations/Permutations_II.py
@@ -0,0 +1,19 @@
+# Leetcode: https://leetcode.com/problems/permutations/
+
+class Solution:
+        def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+                nums = sorted(nums)
+                stack, result = [], []
+                
+                for i in range(0, len(nums)):
+                        stack.append([ [nums[i]], [i] ])
+
+                while stack: 
+                        item, indexes = stack.pop(0)
+                        if len(item) == len(nums) and item not in result:
+                                result.append(item)
+                        else:
+                                for i in range(0, len(nums)):
+                                        if i not in indexes:
+                                                stack.append([ item + [nums[i]], indexes + [i] ])
+                return result
