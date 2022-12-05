e4f06b9763bcf59a9fb702976c2ce29702b569a5
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-05 14:38:24 -0500

    Combinations

---
+++ Array/Medium/Combinations/Combinations.py
@@ -0,0 +1,21 @@
+class Solution:
+        def combine(self, n: int, k: int) -> List[List[int]]:
+                stack, result = [], []
+                
+                for i in range(1, n + 1):
+                        stack.append( [i] )
+                
+                while stack:
+                        item = stack.pop()
+                        
+                        index = 0
+                        if item:
+                                index = item[-1]      
+                        
+                        if len(item) == k:
+                                result.append( item )
+                        
+                        for i in range( index+1, n+1 ):
+                                stack.append( item + [i] )
+                
+                return result
