c42346cba89ab914097018be61ddd4b61da54183
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-05 14:58:00 -0500

    Combination Sum I

---
+++ Array/Combinations/ Combination_Sum.py
@@ -0,0 +1,22 @@
+class Solution(object):
+        def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+                candidates = sorted(candidates)
+                stack = []
+                result = []
+                
+                for candidate in candidates:
+                        stack.append([candidate])
+
+                while stack: 
+                        item = stack.pop(0)
+                        remainder = target - sum(item)
+                        if remainder == 0:
+                                result.append(item)
+                        else:
+                                for candidate in candidates:
+                                        if remainder >= candidate:
+                                                temp = sorted(item + [candidate])
+                                                if temp not in stack:
+                                                        stack.append(temp)
+
+                return result
