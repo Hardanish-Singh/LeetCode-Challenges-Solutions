ddeb0337960281de20dfa293457e8348108e74d7
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-05 19:15:08 -0500

    Combination Sum II

--- Array/Combinations/Combination_Sum_II.py
+++ Array/Combinations/Combination_Sum_II.py
@@ -2,13 +2,13 @@ class Solution:
         def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
                 candidates.sort()
                 stack = [[0, []]]
-                res = []
+                result = []
 
                 while stack:
                         index, combo = stack.pop()
                         comboSum = sum(combo)
-                        if comboSum == target and combo not in res:
-                                res.append(combo)
+                        if comboSum == target and combo not in result:
+                                result.append(combo)
                         else:
                                 for i in range(index, len(candidates)):
                                         if i != index and candidates[i] == candidates[i-1]:
@@ -18,4 +18,4 @@ class Solution:
                                         elif comboSum + candidates[i] <= target:
                                                 newCombo = combo + [candidates[i]]
                                                 stack.append([i+1, newCombo])
-                return res
+                return result
