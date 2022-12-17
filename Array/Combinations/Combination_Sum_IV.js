9430e515ba9b1a58a5e378f9f25c28693d823356
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-16 19:46:19 -0500

    Combination Sum IV

--- Array/Combinations/Combination_Sum_IV.js
+++ Array/Combinations/Combination_Sum_IV.js
@@ -1,29 +1,29 @@
-var combinationSum4 = function (nums, targetElement) {
+var combinationSum4 = function (nums, target) {
         let count = 0;
 
         nums.sort((a, b) => a - b);
         for (let i = 0; i < nums.length; i++) {
-                if (nums[i] > targetElement || nums[i] < 0) {
+                if (nums[i] > target || nums[i] < 0) {
                         nums.splice(i, 1);
                         i--;
                 }
         }
 
-        let permute = function (target) {
-                if (target < 0 || target > targetElement) {
+        let permute = function (sum = 0) {
+                if (sum < 0 || sum > target) {
                         return count;
                 }
 
-                if (target === 0) {
+                if (sum === target) {
                         count++;
                         return count;
                 }
 
                 for (let num of nums) {
-                        permute(target - num);
+                        permute(sum + num);
                 }
         };
 
-        permute(targetElement);
+        permute();
         return count;
 };
