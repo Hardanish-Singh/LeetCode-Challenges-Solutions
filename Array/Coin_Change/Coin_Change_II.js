6bcfb1ebf255c7a261f28d512a4102733b9eb484
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-18 12:13:35 -0500

    Coin Change II

--- Array/Coin_Change/Coin_Change_II.js
+++ Array/Coin_Change/Coin_Change_II.js
@@ -4,6 +4,7 @@
  * @return { number }
  */
 
+// SOLUTION 1: RECURSION (TLE)
 var change = function (amount, coins) {
         let count = 0;
         coins.sort((a, b) => a - b);
@@ -24,4 +25,42 @@ var change = function (amount, coins) {
 
         combinationSum();
         return count;
+};
+
+/**
+ * @param { number } amount
+ * @param { number[] } coins
+ * @return { number }
+ */
+
+// SOLUTION 2: RECURSION WITH MEMOIZATION
+
+var change = function (amount, coins) {
+        let cache = {};
+        coins.sort((a, b) => a - b);
+
+        function combinationSum(sum = 0, idx = 0) {
+                let count = 0;
+                if (sum < 0 || sum > amount) {
+                        return count;
+                }
+
+                const key = `${sum},${idx}`;
+                if (key in cache) {
+                        count += cache[key];
+                        return count;
+                }
+
+                if (sum == amount) {
+                        return ++count;
+                }
+
+                for (let i = idx; i < coins.length; i++) {
+                        count += combinationSum(sum + coins[i], i);
+                }
+                cache[key] = count;
+                return cache[key];
+        }
+
+        return combinationSum();
 };
