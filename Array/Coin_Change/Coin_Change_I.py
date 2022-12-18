8b9a139bb5597662b6127cdf8dc42456cf6b033b
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-18 15:36:01 -0500

    Coin Change I

--- Array/Coin_Change/Coin_Change_I.py
+++ Array/Coin_Change/Coin_Change_I.py
@@ -1,17 +1,17 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        stack = [[amount, 0]]
+        stack = [[amount, []]]
         seen = set([amount])
         
         while stack:
             amountInHand, num_coins = stack.pop(0)
             if amountInHand == 0:
-                    return num_coins
+                    return len(num_coins)
             else:
                 for coin in coins:
                     remainder = amountInHand - coin
                     if remainder >= 0 and remainder not in seen:
-                        stack.append([remainder, num_coins + 1])
+                        stack.append([remainder, num_coins + [coin]])
                         seen.add(remainder)
                     
         return -1
