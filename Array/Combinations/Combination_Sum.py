6f6cef4478db8b9a20912c6d0769c19e03d35d27
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-20 09:12:49 -0500

    Combination Sum

--- Array/Combinations/Combination_Sum.py
+++ Array/Combinations/Combination_Sum.py
@@ -3,13 +3,13 @@
 class Solution(object):
         def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
                 candidates = sorted(candidates)
-                stack, result = [], []
+                queue, result = [], []
                 
                 for candidate in candidates:
-                        stack.append( [candidate] )
+                        queue.append( [candidate] )
 
-                while stack: 
-                        item = stack.pop(0)
+                while queue: 
+                        item = queue.pop(0)
                         remainder = target - sum(item)
                         if remainder == 0:
                                 result.append(item)
@@ -17,7 +17,7 @@ class Solution(object):
                                 for candidate in candidates:
                                         if remainder >= candidate:
                                                 temp = sorted(item + [candidate])
-                                                if temp not in stack:
-                                                        stack.append(temp)
+                                                if temp not in queue:
+                                                        queue.append(temp)
 
                 return result
