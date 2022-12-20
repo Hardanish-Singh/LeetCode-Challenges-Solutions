b52ce40e535e0f410e7736f4035c8a856d424957
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-20 09:11:11 -0500

    Combination Sum III

--- Array/Combinations/Combination_Sum_III.py
+++ Array/Combinations/Combination_Sum_III.py
@@ -3,17 +3,17 @@
 class Solution:
         def combinationSum3(self, k: int, n: int) -> List[List[int]]:
                 combinations = []
-                queue = [[[], 1]]
+                stack = [[[], 1]]
 
-                while queue:
-                        current_combination, integer = queue.pop()
-                        combination_sum = sum(current_combination)
-                        if combination_sum == n and len(current_combination) == k: 
-                                combinations.append(current_combination)
+                while stack:
+                        currentCombination, integer = stack.pop()
+                        combinationSum = sum(currentCombination)
+                        if combinationSum == n and len(currentCombination) == k: 
+                                combinations.append(currentCombination)
                         else:
                                 for i in range(integer, 10):
-                                        if combination_sum + i > n: 
+                                        if combinationSum + i > n: 
                                                 break
-                                        queue.append([current_combination + [i], i + 1])
+                                        stack.append([currentCombination + [i], i + 1])
                 
                 return combinations
