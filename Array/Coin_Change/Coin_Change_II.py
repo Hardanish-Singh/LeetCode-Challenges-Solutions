73ed59961c1371680ccf3caa4369c9f46decc168
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-22 14:32:42 -0500

    Coin Change II

--- Array/Coin_Change/Coin_Change_II.py
+++ Array/Coin_Change/Coin_Change_II.py
@@ -6,23 +6,21 @@ class Solution:
                         return 0  
                 
                 cache = {}
-                coins.sort() 
-                
-                #currentsum, index, instruction
-                stack = [[0, 0, 'ENTER']]
+                coins.sort()
+                stack = [[0, 0, 'PROCESSING']]
                 
                 while stack:
                         currentsum, index, instruction = stack.pop()
                         newsum = currentsum + coins[index]
                         if currentsum == amount:
                                 cache[(currentsum, index)] = 1
-                        elif instruction == 'ENTER':
-                                stack.append([currentsum, index, 'EXIT'])
+                        elif instruction == 'PROCESSING':
+                                stack.append([currentsum, index, 'PROCESSED'])
                                 if (index+1) < len(coins) and (currentsum, index+1) not in cache.keys():
-                                        stack.append([currentsum, index+1, 'ENTER'])
+                                        stack.append([currentsum, index+1, 'PROCESSING'])
                                 if newsum <= amount and (newsum, index) not in cache.keys():
-                                        stack.append([newsum, index, 'ENTER'])
-                        elif instruction == 'EXIT':
+                                        stack.append([newsum, index, 'PROCESSING'])
+                        elif instruction == 'PROCESSED':
                                 cache[(currentsum, index)] = ( cache.get((newsum, index)) or 0 ) + ( cache.get((currentsum, index+1)) or 0 )
                 
                 return cache[(0,0)]
