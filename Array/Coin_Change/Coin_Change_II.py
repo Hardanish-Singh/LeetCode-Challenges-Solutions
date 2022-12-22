3904957ef3cd5ad1ef72b5ce8e40ebd792e46fea
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-22 09:04:24 -0500

    Coin Change II

--- Array/Coin_Change/Coin_Change_II.py
+++ Array/Coin_Change/Coin_Change_II.py
@@ -34,39 +34,4 @@ class Solution:
                                 right = cache.get((currentsum, index+1)) or 0
                                 cache[(currentsum, index)] = left + right
                 
-                return cache[(0,0)]def change(self, amount: int, coins: List[int]) -> int:
-                if amount<=0:
-                        return 1
-                if len(coins) == 0:
-                        return 0  
-                
-                cache = {}
-                coins.sort()
-                
-                #currentsum, index, instruction
-                stack = [[0, 0, 'ENTER']]
-                
-                while stack:
-                        currentsum, index, instruction = stack.pop()
-                        if currentsum == amount:
-                                cache[(currentsum, index)] = 1
-                        elif instruction == 'ENTER':
-                                stack.append([currentsum, index, 'EXIT'])
-                        
-                                if (index+1) < len(coins):
-                                        if (currentsum, index+1) not in cache.keys():
-                                                stack.append([currentsum, index+1, 'ENTER'])
-                        
-                                newsum = currentsum + coins[index]
-                                if newsum <= amount:
-                                        if (newsum, index) not in cache.keys():
-                                                stack.append([newsum, index, 'ENTER'])
-                                
-
-                        elif instruction == 'EXIT':
-                                newsum = currentsum + coins[index]
-                                left = cache.get((newsum, index)) or 0
-                                right = cache.get((currentsum, index+1)) or 0
-                                cache[(currentsum, index)] = left + right
-                
                 return cache[(0,0)]
