e1260336be1f9e554a7d764129d2779c32095c18
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-22 11:17:09 -0500

    Coin Change I

--- Array/Coin_Change/Coin_Change_II.py
+++ Array/Coin_Change/Coin_Change_II.py
@@ -13,6 +13,7 @@ class Solution:
                 
                 while stack:
                         currentsum, index, instruction = stack.pop()
+                        newsum = currentsum + coins[index]
                         if currentsum == amount:
                                 cache[(currentsum, index)] = 1
                         elif instruction == 'ENTER':
@@ -20,11 +21,9 @@ class Solution:
                                 if (index+1) < len(coins) and (currentsum, index+1) not in cache.keys():
                                         stack.append([currentsum, index+1, 'ENTER'])
                         
-                                newsum = currentsum + coins[index]
                                 if newsum <= amount and (newsum, index) not in cache.keys():
                                         stack.append([newsum, index, 'ENTER'])
                         elif instruction == 'EXIT':
-                                newsum = currentsum + coins[index]
                                 left = cache.get((newsum, index)) or 0
                                 right = cache.get((currentsum, index+1)) or 0
                                 cache[(currentsum, index)] = left + right
