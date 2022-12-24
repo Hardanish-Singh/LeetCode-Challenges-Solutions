cc2b0c99edcae3e961e2e23c9498248ba22aab62
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-24 15:13:36 -0500

    Min Cost Climbing Stairs

---
+++ Array/Climbing_Stairs/Min_Cost_Climbing_Stairs.py
@@ -0,0 +1,19 @@
+from typing import Dict, List
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        def minSteps( stepIndex: int, cache: Dict[int, int] = {} ) -> int:
+            if stepIndex in cache.keys():
+                return cache[stepIndex]
+            if stepIndex >= len(cost):
+                return 0
+            
+            minCost = min( 
+                            cost[stepIndex] + minSteps( stepIndex + 1, cache ),
+                            cost[stepIndex] + minSteps( stepIndex + 2, cache ),
+                         )
+            cache[stepIndex] = minCost
+            return minCost
+
+        return min( minSteps( 0 ), minSteps( 1 ) )
