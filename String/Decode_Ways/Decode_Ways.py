7a3f7e05f15b076a2f10abd3eb7c8ac2b1b30bac
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-26 12:23:36 -0500

    Decode Ways

--- String/Decode_Ways/Decode_Ways.py
+++ String/Decode_Ways/Decode_Ways.py
@@ -11,7 +11,10 @@
                 f(n) = f(n-1) if valid + f(n-2) if valid
 
                 f(n-1) can be added if current digit is not 0. It is guaranteed that all characters are digits, so this is the only condition we need.
-                f(n-2) First, previous digit needs to be either 1 or 2. Second, if the previous digit is 2, the current digit must be betwen [0, 6].
+                f(n-2) 
+                        First, previous digit needs to be either 1 or 2.
+                        Second, if the previous digit is 1, then current digit must be between [0,9] 
+                        Second, if the previous digit is 2, then current digit must be betwen [0, 6].
 """
 
 class Solution:
@@ -23,8 +26,8 @@ class Solution:
                         decodeFN1 = f1 if ch != '0' else 0
 
                         # f(n-2) validity
-                        decodeFN2 = f0 if string[i-1] == '1' or (string[i-1] == '2' and '0' <= ch <= '6') else 0
-                        # decodeFN2 = f0 if (string[i-1] == '1' and '0' <= ch <= '9') or (string[i-1] == '2' and '0' <= ch <= '6') else 0
+                        decodeFN2 = f0 if (string[i-1] == '1' and '0' <= ch <= '9') or (string[i-1] == '2' and '0' <= ch <= '6') else 0
+                        # decodeFN2 = f0 if string[i-1] == '1' or (string[i-1] == '2' and '0' <= ch <= '6') else 0
 
                         f0, f1 = f1, (decodeFN2 + decodeFN1)
 
