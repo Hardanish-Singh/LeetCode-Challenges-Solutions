fb6139eb7ee738ab3b294d20a3301da88cdae6e5
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-28 13:32:46 -0500

    String Compression II

--- String/String_Compression/String_Compression_II.js
+++ String/String_Compression/String_Compression_II.js
@@ -1,3 +1,5 @@
+// Leetcode: https://leetcode.com/problems/string-compression-ii/
+
 /**
  * @param {string} s
  * @param {number} k
@@ -20,16 +22,18 @@ var getLengthOfOptimalCompression = function (s, k) {
                         return memo[key];
                 }
 
+                let count = 0;
                 if (s[i] === lastChar) {
                         const incrementor = [1, 9, 99].includes(lastCharCount) ? 1 : 0;
-                        memo[key] = incrementor + backtrack(i + 1, lastChar, lastCharCount + 1, k);
+                        count = incrementor + backtrack(i + 1, lastChar, lastCharCount + 1, k);
                 } else {
-                        memo[key] = Math.min(
+                        count = Math.min(
                                 1 + backtrack(i + 1, s[i], 1, k), //keep char
                                 backtrack(i + 1, lastChar, lastCharCount, k - 1) //delete char
                         );
                 }
-                return memo[key];
+                memo[key] = count;
+                return count;
         };
         return backtrack(0, "", 0, k);
 };
