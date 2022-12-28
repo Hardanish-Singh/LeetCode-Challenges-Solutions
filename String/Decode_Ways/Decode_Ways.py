56f3ac2a360107d3c66d8bdbc3773f182a75cf35
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-28 09:20:17 -0500

    Decode Ways

--- String/Decode_Ways/Decode_Ways.py
+++ String/Decode_Ways/Decode_Ways.py
@@ -13,8 +13,8 @@
                 f(n-1): 
                         can be added if current digit is not 0. It is guaranteed that all characters are digits, so this is the only condition we need.
                 f(n-2): 
-                        First, previous digit needs to be either 1 or 2.
-                        Second, if the previous digit is 1, then current digit must be between [0,9] 
+                        Previous digit needs to be either 1 or 2.
+                        First, if the previous digit is 1, then current digit must be between [0,9] 
                         Second, if the previous digit is 2, then current digit must be betwen [0, 6].
 """
 
@@ -25,10 +25,12 @@ class Solution:
                 for i, ch in enumerate(string):
                         # f(n-1) validity
                         decodeFN1 = f1 if ch != '0' else 0
-
                         # f(n-2) validity
-                        decodeFN2 = f0 if (string[i-1] == '1' and '0' <= ch <= '9') or (string[i-1] == '2' and '0' <= ch <= '6') else 0
-                        # decodeFN2 = f0 if string[i-1] == '1' or (string[i-1] == '2' and '0' <= ch <= '6') else 0
+                        decodeFN2 = 0
+                        if i > 0 and string[i-1] == '1' and '0' <= ch <= '9':
+                                decodeFN2 += f0
+                        if i > 0 and string[i-1] == '2' and '0' <= ch <= '6':
+                                decodeFN2 += f0
 
                         f0, f1 = f1, (decodeFN2 + decodeFN1)
 
