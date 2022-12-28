32df253ebf715c1e0131c2865992eba1c2b89161
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-28 12:45:05 -0500

    Add Digits

--- JS/Add_Digits.js
+++ JS/Add_Digits.js
@@ -1,37 +1,19 @@
-/*
-        Given an integer num, repeatedly add all its digits until the result has only one digit, and return it. 
+// Leetcode: https://leetcode.com/problems/add-digits/
 
-        Example 1:
-                Input: num = 38
-                Output: 2
-                Explanation: 
-                        The process is
-                                38 --> 3 + 8 --> 11
-                                11 --> 1 + 1 --> 2 
-                                Since 2 has only one digit, return it.
-        
-        Example 2:
-                Input: num = 0
-                Output: 0
-
-        Constraints:
-                1) 0 <= num <= 2^31 - 1
-*/
-
-/*
+/**
  * @param { number } num
  * @return { number }
-*/
+ */
 
-var addDigits = function(num) {
+var addDigits = function (num) {
         let sum = num;
-        num = String( num );
-        while( num.length > 1 ) {
+        num = String(num);
+        while (num.length > 1) {
                 sum = 0;
-                for( let i = 0; i < num.length; i++ ) {
-                        sum += Number( num[i] );
+                for (let i = 0; i < num.length; i++) {
+                        sum += Number(num[i]);
                 }
-                num = String( sum );
+                num = String(sum);
         }
         return sum;
 };
