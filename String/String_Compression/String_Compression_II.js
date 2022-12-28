261299bb663e1081a7bb8286e1810c239980efa7
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-28 13:35:10 -0500

    String Compression II

--- String/String_Compression/String_Compression_II.js
+++ String/String_Compression/String_Compression_II.js
@@ -9,7 +9,7 @@
 var getLengthOfOptimalCompression = function (s, k) {
         const memo = new Map();
 
-        const backtrack = (i, lastChar, lastCharCount, k) => {
+        const findMinimumLength = (i, lastChar, lastCharCount, k) => {
                 if (k < 0) {
                         return Number.POSITIVE_INFINITY;
                 }
@@ -25,15 +25,15 @@ var getLengthOfOptimalCompression = function (s, k) {
                 let count = 0;
                 if (s[i] === lastChar) {
                         const incrementor = [1, 9, 99].includes(lastCharCount) ? 1 : 0;
-                        count = incrementor + backtrack(i + 1, lastChar, lastCharCount + 1, k);
+                        count = incrementor + findMinimumLength(i + 1, lastChar, lastCharCount + 1, k);
                 } else {
                         count = Math.min(
-                                1 + backtrack(i + 1, s[i], 1, k), //keep char
-                                backtrack(i + 1, lastChar, lastCharCount, k - 1) //delete char
+                                1 + findMinimumLength(i + 1, s[i], 1, k), //keep char
+                                findMinimumLength(i + 1, lastChar, lastCharCount, k - 1) //delete char
                         );
                 }
                 memo[key] = count;
                 return count;
         };
-        return backtrack(0, "", 0, k);
+        return findMinimumLength(0, "", 0, k);
 };
