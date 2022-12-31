05431c76b58db7e6490f567be5c37102eef30ee1
Hardanish Singh <hardanishsingh1@gmail.com>
2022-12-31 11:31:28 -0500

    Sum of Digits in the Minimum Number

---
+++ Miscellaneous/Sum_of_Digits_in_the_Minimum_Number.ts
@@ -0,0 +1,19 @@
+// Leetcode: https://leetcode.com/problems/sum-of-digits-in-the-minimum-number/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var sumOfDigits = function (nums: number[]): number {
+        return Math.min
+                .apply(null, nums)
+                .toString()
+                .split("")
+                .map(Number)
+                .reduce((a: number, b: number) => a + b, 0) %
+                2 ===
+                0
+                ? 1
+                : 0;
+};
