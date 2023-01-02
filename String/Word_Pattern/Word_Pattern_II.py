8f86f0338902d8ef034dafda74bb09e6ce480592
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-02 13:57:25 -0500

    Word Pattern II

--- String/Word_Pattern/Word_Pattern_II.py
+++ String/Word_Pattern/Word_Pattern_II.py
@@ -1,18 +1,18 @@
 class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
-        map = { }
-        str = s.split(" ")
-        
-        if len(str) != len(pattern):
-            return False
-        
-        for i in range(0, len(pattern)):
-            if map.get(pattern[i]) and map.get(pattern[i]) != str[i]:
-                return False
-            else:
-                map[pattern[i]] = str[i]
-        
-        return len(map.values()) == len(set(map.values()))
+        def wordPattern(self, pattern: str, s: str) -> bool:
+                map = { }
+                str = s.split(" ")
+
+                if len(str) != len(pattern):
+                        return False
+
+                for i in range(0, len(pattern)):
+                        if map.get(pattern[i]) and map.get(pattern[i]) != str[i]:
+                                return False
+                        else:
+                                map[pattern[i]] = str[i]
+
+                return len(map.values()) == len(set(map.values()))
 
     def wordPatternMatch(self, pattern: str, s: str) -> bool:
         lp, ls = len(pattern), len(s)
