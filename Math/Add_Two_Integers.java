5071945b1d2f2c4d61807f3407633d63bb246ec8
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-04 07:52:48 -0500

    Add Two Integers

---
+++ Math/Add_Two_Integers.java
@@ -0,0 +1,9 @@
+// Leetcode: https://leetcode.com/problems/add-two-integers/
+
+class Solution 
+{
+        public int sum(int num1, int num2) 
+        {
+                return num1 + num2;
+        }
+}
