bfe5ad54a3d20fc0ad9f106655507efb9428ca8d
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-04 12:53:55 -0500

    Number of Good Pairs

---
+++ Math/Number_of_Good_Pairs.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution 
+{
+        public int numIdenticalPairs(int[] nums) 
+        {
+                Arrays.sort(nums);
+                int goodPairs = 0;
+                for( int i = 0; i < nums.length; i++ ) 
+                {
+                        for(int j = i+1; j < nums.length; j++)
+                        {
+                                if( nums[i] == nums[j] ) 
+                                {
+                                        goodPairs++;
+                                }
+                        }
+                }
+                return goodPairs;
+        }
+}
