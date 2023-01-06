3291d40fdcbf4e93f00fb0ed624aedcd2424cf97
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-06 17:35:55 -0500

    Find N Unique Integers Sum up to Zero

---
+++ Math/Easy/Find_N_Unique_Integers_Sum_up_to_Zero.js
@@ -0,0 +1,17 @@
+// Leetcode: https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+
+/**
+ * @param { number } n
+ * @return { number[] }
+ */
+
+var sumZero = function (n) {
+        const arr = [];
+        for (let i = 1; i <= parseInt(n / 2); i++) {
+                arr.push(i, -i);
+        }
+        if (n % 2 != 0) {
+                arr.push(0);
+        }
+        return arr;
+};
