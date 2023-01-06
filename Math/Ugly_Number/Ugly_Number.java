fdec979b1ae628ad22e6db5d78c0e336cea9db76
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-05 20:00:45 -0500

    Ugly Number I

--- Math/Ugly_Number/Ugly_Number.java
+++ Math/Ugly_Number/Ugly_Number.java
@@ -1,12 +1,14 @@
+// Leetcode: https://leetcode.com/problems/ugly-number/
+
 class Solution {
         public boolean isUgly(int n) {
                 if(n < 1) {
                         return false;
                 }
-                int[] prime = {2, 3, 5};
-                for (int factor : prime) {
+                int[] primeFactors = new int[] {2, 3, 5};
+                for (int factor : primeFactors) {
                         while (n % factor == 0) {
-                        n /= factor;
+                                n /= factor;
                         }
                 }
                 return n == 1;
