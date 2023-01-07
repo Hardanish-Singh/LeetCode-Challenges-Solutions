16767ef756d04547c5a88bd57c0d3e7c06679ea8
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-07 12:45:35 -0500

    Subtract the Product and Sum of Digits of an Integer

--- Math/Easy/Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.java
+++ Math/Easy/Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.java
@@ -3,19 +3,17 @@ class Solution
         public int subtractProductAndSum(int n) 
         {
                 int num = n;
-
                 int sum = 0;
                 int prod = 1;
 
                 while(num > 0)
                 {
                         int remainder = num % 10; 
-
                         prod *= remainder;  
                         sum += remainder;
-
                         num /= 10;
                 }
+
                 return prod - sum; 
         }
 }
