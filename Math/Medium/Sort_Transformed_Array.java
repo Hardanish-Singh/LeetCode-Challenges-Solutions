dbf48e5f7083deff13222d7a515970c23b96c4f9
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-07 12:53:25 -0500

    Sort Transformed Array

---
+++ Math/Medium/Sort_Transformed_Array.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+        public int[] sortTransformedArray(int[] nums, int a, int b, int c) 
+        {
+                int[] transformedArray = new int[nums.length];
+                int index = 0;
+                for( int num : nums )
+                {
+                        transformedArray[index++] = a * (int)Math.pow(num, 2) + b * num + c;
+                }
+                Arrays.sort(transformedArray);
+                return transformedArray;
+        }
+}
