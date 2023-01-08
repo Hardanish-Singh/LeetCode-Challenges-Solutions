beedf877ba2932148016329691d984cf371d3b44
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-08 09:57:40 -0500

    Count Number of Distinct Integers After Reverse Operations

---
+++ Math/Medium/Count_Number_of_Distinct_Integers_After_Reverse_Operations.java
@@ -0,0 +1,28 @@
+class Solution 
+{
+        public int countDistinctIntegers(int[] nums) 
+        {
+                Set<Integer>st=new HashSet<>();
+
+                for(int i=0;i<nums.length;i++)
+                {
+                        st.add(nums[i]);
+                        st.add(reverse(nums[i]));
+                }
+
+                return st.size();
+        }
+
+        public static int reverse(int num) 
+        {
+                String strNum = Integer.toString(num);
+                StringBuilder sb = new StringBuilder();
+
+                for(int i = strNum.length() - 1; i >= 0; i--)
+                {
+                        sb.append(strNum.charAt(i));
+                }
+
+                return Integer.parseInt(sb.toString());
+        }
+}
