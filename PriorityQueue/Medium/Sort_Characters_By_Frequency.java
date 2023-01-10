5e5091c3039c9df6fb7dac8c0b7b216e47bccc70
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-10 12:31:06 -0500

    Sort Characters By Frequency

---
+++ PriorityQueue/Medium/Sort_Characters_By_Frequency.java
@@ -0,0 +1,38 @@
+class Solution 
+{
+    public String frequencySort(String s) 
+    {
+        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+        for( int i = 0; i < s.length(); i++ )
+        {
+            char key = s.charAt(i);
+            if(map.containsKey(key))
+            {
+                map.put(key, map.get(key) + 1);
+            }
+            else
+            {
+                map.put(key, 1);
+            }
+        }
+
+        PriorityQueue<Character> pq = new PriorityQueue((a,b) -> (map.get(b) - map.get(a)));
+        for (Character c : map.keySet()) 
+        {
+            pq.add(c);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while(!pq.isEmpty())
+        {
+            char ch = pq.poll();
+            int count = map.get(ch);
+            while(count-- > 0)
+            {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
