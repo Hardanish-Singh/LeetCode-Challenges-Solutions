2222da56b58ced6fb59ae3fef7c3d29d51a178f0
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-10 12:31:36 -0500

    Sort Characters By Frequency

--- PriorityQueue/Medium/Sort_Characters_By_Frequency.java
+++ PriorityQueue/Medium/Sort_Characters_By_Frequency.java
@@ -1,38 +1,38 @@
 class Solution 
 {
-    public String frequencySort(String s) 
-    {
-        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
-        for( int i = 0; i < s.length(); i++ )
+        public String frequencySort(String s) 
         {
-            char key = s.charAt(i);
-            if(map.containsKey(key))
-            {
-                map.put(key, map.get(key) + 1);
-            }
-            else
-            {
-                map.put(key, 1);
-            }
-        }
+                HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+                for( int i = 0; i < s.length(); i++ )
+                {
+                        char key = s.charAt(i);
+                        if(map.containsKey(key))
+                        {
+                                map.put(key, map.get(key) + 1);
+                        }
+                        else
+                        {
+                                map.put(key, 1);
+                        }
+                }
 
-        PriorityQueue<Character> pq = new PriorityQueue((a,b) -> (map.get(b) - map.get(a)));
-        for (Character c : map.keySet()) 
-        {
-            pq.add(c);
-        }
+                PriorityQueue<Character> pq = new PriorityQueue((a,b) -> (map.get(b) - map.get(a)));
+                for (Character c : map.keySet()) 
+                {
+                        pq.add(c);
+                }
 
-        StringBuilder sb = new StringBuilder();
+                StringBuilder sb = new StringBuilder();
 
-        while(!pq.isEmpty())
-        {
-            char ch = pq.poll();
-            int count = map.get(ch);
-            while(count-- > 0)
-            {
-                sb.append(ch);
-            }
+                while(!pq.isEmpty())
+                {
+                        char ch = pq.poll();
+                        int count = map.get(ch);
+                        while(count-- > 0)
+                        {
+                                sb.append(ch);
+                        }
+                }
+                return sb.toString();
         }
-        return sb.toString();
-    }
 }
