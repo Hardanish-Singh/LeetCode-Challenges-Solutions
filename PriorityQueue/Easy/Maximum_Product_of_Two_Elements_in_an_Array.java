11c17125c94ba9e27d9284d91c54d69c4074c982
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-10 08:18:22 -0500

    Maximum Product of Two Elements in an Array

---
+++ PriorityQueue/Easy/Maximum_Product_of_Two_Elements_in_an_Array.java
@@ -0,0 +1,16 @@
+class Solution 
+{
+        public int maxProduct(int[] nums) 
+        {
+                PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+                for(int i=0; i<nums.length; i++)
+                {
+                        pq.add(nums[i]);
+                }
+
+                int val1 = pq.poll();
+                int val2 = pq.poll();
+
+                return (val1 - 1) * (val2 - 1);
+        }
+}
