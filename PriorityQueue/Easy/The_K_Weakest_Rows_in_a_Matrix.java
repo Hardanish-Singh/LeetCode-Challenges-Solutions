21688b8e3b811cc3c271731e7b6f72fae77b25b2
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-12 07:47:54 -0500

    The K Weakest Rows in a Matrix

--- PriorityQueue/Easy/The_K_Weakest_Rows_in_a_Matrix.java
+++ PriorityQueue/Easy/The_K_Weakest_Rows_in_a_Matrix.java
@@ -1,1 +1,40 @@
 // Leetcode: https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+
+class Solution 
+{
+    public int[] kWeakestRows(int[][] mat, int k) 
+    {
+        HashMap<Integer, Integer> map= new HashMap<Integer, Integer>();
+        for(int i=0; i<mat.length; i++)
+        {
+            int count=0;
+            for(int j=0; j<mat[0].length; j++)
+            {
+                if(mat[i][j]==1)
+                    count++;
+            }
+            map.put(i, count);
+        }
+        
+        PriorityQueue<Map.Entry<Integer, Integer>> pq= new PriorityQueue<>((a,b)->{
+            if(a.getValue() == b.getValue())
+                return a.getKey() - b.getKey();
+            else 
+                return a.getValue() - b.getValue(); 
+        });
+        for(Map.Entry<Integer, Integer> key: map.entrySet())
+        {
+            pq.add(key);
+        }
+
+        int[] result = new int[k];
+        int index = 0;
+        while(!pq.isEmpty() && k > 0)
+        {
+            result[index++] = pq.poll().getKey();
+            k--;
+        }
+        return result;
+    }
+    
+}
