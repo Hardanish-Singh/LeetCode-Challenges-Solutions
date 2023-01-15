76bfe3f8058821bfc5a6e7b00b76a2ed3cb3dbe7
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-15 09:45:41 -0500

    Linked List Cycle

---
+++ Linked_List/Easy/Linked_List_Cycle.java
@@ -0,0 +1,42 @@
+// Leetcode: https://leetcode.com/problems/linked-list-cycle/
+
+/*
+        // Definition for Singly-Linked List
+        public class ListNode
+	{
+                int val;
+                ListNode next;
+                public ListNode()
+                {
+                }
+                public ListNode( int val )
+                {
+                        this.val = val;
+                }
+                public ListNode( int val, ListNode next )
+                {
+                        this.val = val;
+                        this.next = next;
+                }
+        }
+*/
+
+class Solution
+{
+	public boolean hasCycle(ListNode head)
+	{
+		while( head != null )
+		{
+			if( head.val == -100002 )
+			{
+				return true;
+			}
+			else
+			{
+				head.val = -100002;
+			}
+			head = head.next;
+		}
+		return false;
+	}
+}
