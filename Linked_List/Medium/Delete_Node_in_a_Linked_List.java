0a8887334fa5cef78ce5ddd7c96de13a9e3cd5dd
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-15 09:34:21 -0500

    Delete Node in a Linked List

---
+++ Linked_List/Medium/Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,30 @@
+// Leetcode: https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+/*
+        // Definition for Singly-Linked List
+        public class ListNode
+	{
+                int val;
+                ListNode next;
+                public ListNode()
+                {
+                }
+                public ListNode( int val )
+                {
+                        this.val = val;
+                }
+                public ListNode( int val, ListNode next )
+                {
+                        this.val = val;
+                        this.next = next;
+                }
+        }
+*/
+class Solution 
+{
+        public void deleteNode(ListNode node) 
+        {
+                node.val = node.next.val;
+                node.next = node.next.next;
+        }
+}
