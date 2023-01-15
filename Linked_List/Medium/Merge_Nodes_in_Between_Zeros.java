b8b99765990ca759e649020a1af01d6e737245d3
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-15 09:49:37 -0500

    Merge Nodes in Between Zeros

--- Linked_List/Medium/Merge_Nodes_in_Between_Zeros.java
+++ Linked_List/Medium/Merge_Nodes_in_Between_Zeros.java
@@ -1,31 +1,4 @@
-/*
-        You are given the head of a linked list, which contains a series of integers separated by 0's. 
-        The beginning and end of the linked list will have Node.val == 0.
-        
-        For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. 
-        The modified list should not contain any 0's.
-        
-        Return the head of the modified linked list.
-
-        Example 1:
-                Input:
-                        0 ---> 3 ---> 1 ---> 0 ---> 4 ---> 5 ---> 2 ---> 0
-                Output:
-                        4 ---> 11
-        
-        Example 2:
-                Input:
-                        0 ---> 1 ---> 0 ---> 3 ---> 0 ---> 2 ---> 2 ---> 0
-                Output:
-                        1 ---> 3 ---> 4
-        
-        Constraints:
-                1) The number of nodes in the list is in the range [3, 2 * 10^5].
-                2) 0 <= Node.val <= 1000
-                3) There are no two consecutive nodes with Node.val == 0.
-                4) The beginning and end of the linked list have Node.val == 0.
-
-*/
+// Leetcode: https://leetcode.com/problems/merge-nodes-in-between-zeros/
 
 /*
         // Definition for Singly-Linked List
