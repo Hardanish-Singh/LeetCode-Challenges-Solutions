77fc0dbbf4d2c82e31972e58a5bd9d1f81628d53
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-15 09:15:38 -0500

    Largest Number After Digit Swaps by Parity

--- PriorityQueue/Easy/Largest_Number_After_Digit_Swaps_by_Parity.java
+++ PriorityQueue/Easy/Largest_Number_After_Digit_Swaps_by_Parity.java
@@ -4,8 +4,8 @@ class Solution
 {
         public int largestInteger(int num) 
         {
-                PriorityQueue<Integer> oddQueue = new PriorityQueue<>((a,b) -> b - a);
-                PriorityQueue<Integer> evenQueue = new PriorityQueue<>((a,b) -> b - a);
+                PriorityQueue<Integer> oddQueue = new PriorityQueue<Integer>((a,b) -> b - a);
+                PriorityQueue<Integer> evenQueue = new PriorityQueue<Integer>((a,b) -> b - a);
                 String s = Integer.toString(num);
 
                 for(int i = 0; i < s.length(); i++)
