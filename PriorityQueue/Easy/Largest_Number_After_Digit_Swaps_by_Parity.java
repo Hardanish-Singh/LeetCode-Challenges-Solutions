ca0bc64123b3a22be6fab496f2dfa032a121de58
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-15 09:15:14 -0500

    Largest Number After Digit Swaps by Parity

--- PriorityQueue/Easy/Largest_Number_After_Digit_Swaps_by_Parity.java
+++ PriorityQueue/Easy/Largest_Number_After_Digit_Swaps_by_Parity.java
@@ -2,41 +2,39 @@
 
 class Solution 
 {
-    public int largestInteger(int num) 
-    {
-
-        PriorityQueue<Integer> oddQueue = new PriorityQueue<>((a,b) -> b - a);
-        PriorityQueue<Integer> evenQueue = new PriorityQueue<>((a,b) -> b - a);
-
-        String s = Integer.toString(num);
-
-        for(int i = 0; i < s.length(); i++)
+        public int largestInteger(int num) 
         {
-            int n = Character.getNumericValue( s.charAt(i) );
-            if(n % 2 == 0)
-            {
-                evenQueue.add(n);
-            }
-            else
-            {
-                oddQueue.add(n);
-            }
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s.length(); i++)
-        {
-            int n = Character.getNumericValue( s.charAt(i) );
-            if(n % 2 == 0)
-            {
-                sb.append(evenQueue.poll());
-            }
-            else
-            {
-                sb.append(oddQueue.poll());
-            }
-        }
+                PriorityQueue<Integer> oddQueue = new PriorityQueue<>((a,b) -> b - a);
+                PriorityQueue<Integer> evenQueue = new PriorityQueue<>((a,b) -> b - a);
+                String s = Integer.toString(num);
 
-        return new Integer(sb.toString());
-    }
+                for(int i = 0; i < s.length(); i++)
+                {
+                        int n = Character.getNumericValue( s.charAt(i) );
+                        if(n % 2 == 0)
+                        {
+                                evenQueue.add(n);
+                        }
+                        else
+                        {
+                                oddQueue.add(n);
+                        }
+                }
+
+                StringBuilder sb = new StringBuilder();
+                for(int i = 0; i < s.length(); i++)
+                {
+                        int n = Character.getNumericValue( s.charAt(i) );
+                        if(n % 2 == 0)
+                        {
+                                sb.append(evenQueue.poll());
+                        }
+                        else
+                        {
+                                sb.append(oddQueue.poll());
+                        }
+                }
+
+                return Integer.parseInt(sb.toString());
+        }
 }
