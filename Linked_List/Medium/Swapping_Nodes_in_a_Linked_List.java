487ca5c8c635945552c00a18693fc47d2369c686
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-17 08:27:27 -0500

    Swapping Nodes In a Linked List

--- Linked_List/Medium/Swapping_Nodes_in_a_Linked_List.java
+++ Linked_List/Medium/Swapping_Nodes_in_a_Linked_List.java
@@ -1,24 +1,4 @@
-/*
-        You are given the head of a linked list, and an integer k.
-        Return the head of the linked list after swapping the values of the k^th node from the beginning and the k^th node from the end.
-        
-        Example 1:
-                1 ---> 2 ---> 3 ---> 4 ---> 5
-                              ↓
-                1 ---> 4 ---> 3 ---> 2 ---> 5
-
-                Input: head = [1, 2, 3, 4, 5], k = 2
-                Output: [1, 4, 3, 2, 5]
-        
-        Example 2:
-                Input: head = [7, 9, 6, 6, 7, 8, 3, 0, 9, 5], k = 5
-                Output: [7, 9, 6, 6, 8, 7,3, 0, 9, 5]
-        
-        Constraints:
-                1) The number of nodes in the list is n.
-                2) 1 <= k <= n <= 10^5
-                3) 0 <= Node.val <= 100
-*/
+// Leetcode: https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
 
 /*
         // Definition for Singly-Linked List
