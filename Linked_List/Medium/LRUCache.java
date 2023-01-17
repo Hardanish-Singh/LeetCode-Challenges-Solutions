6fd5ebdbb7502ee78efd614367371eb57004f2eb
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-17 08:20:55 -0500

    LRU Cache

--- Linked_List/Medium/LRUCache.java
+++ Linked_List/Medium/LRUCache.java
@@ -1,31 +1,4 @@
-/*
-        Design a Data Structure that follows the constraints of a LEAST RECENTLY USED (LRU) CACHE.
-        
-        Implement the LRUCache class:
-                1) LRUCache( int capacity ) Initialize the LRU Cache with positive size capacity.
-                2) int get( int key ) Return the value of the key if the key exists, otherwise return -1.
-                3) void put( int key, int value ) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
-                4) The functions get and put must each run in O(1) average time complexity.
-
-        Example 1:
-                Input:  
-                        [ "LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get" ]
-                        [ [2], [1,1], [2,2], [1], [3,3], [2], [4,4], [1], [3], [4] ]
-                Output: 
-                        [ null, null, null, 1, null, -1, null, -1, 3, 4 ]
-
-        Explanation:
-                LRUCache obj = new LRUCache(2);
-                obj.put(1, 1); // cache is { 1=1 }
-                obj.put(2, 2); // cache is { 2=2, 1=1 }
-                obj.get(1);    // return 1 & cache is { 1=1, 2=2 }
-                obj.put(3, 3); // LRU key was 2, evicts key 2, cache is { 3=3, 1=1 }
-                obj.get(2);    // returns -1 ( not found )
-                obj.put(4, 4); // LRU key was 1, evicts key 1, cache is { 4=4, 3=3 }
-                obj.get(1);    // return -1 (not found)
-                obj.get(3);    // return 3 & cache is { 3=3, 4=4 }
-                obj.get(4);    // return 4 & cache is { 4=4, 3=3 }
-*/
+// Leetcode: https://leetcode.com/problems/lru-cache/
 
 /*
          THIS SOLUTION USES HASHMAP & DOUBLY LINKED LIST FOR ITS IMPLEMENTATION
@@ -167,10 +140,3 @@ class LRUCache
         }
 
 }
-
-/*
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
-*/
