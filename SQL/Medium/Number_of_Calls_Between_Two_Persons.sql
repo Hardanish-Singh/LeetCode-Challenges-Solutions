306be1d156cf51e5b1b3f217de0ef745dd76dbc0
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-19 07:36:11 -0500

    Number of Calls Between Two Persons

--- SQL/Medium/Number_of_Calls_Between_Two_Persons.sql
+++ SQL/Medium/Number_of_Calls_Between_Two_Persons.sql
@@ -2,31 +2,31 @@
 
 with users as
 (
-select
-    from_id as person1
-    ,to_id as person2
-    ,duration as duration
-from
-    calls
-union all
-select
-    to_id as person1
-    ,from_id as person2
-    ,duration as duration
-from
-    calls
+        select
+                from_id as person1,
+                to_id as person2,
+                duration as duration
+        from
+                calls
+        union all
+        select
+                to_id as person1,
+                from_id as person2,
+                duration as duration
+        from
+                calls
 )
 
 select 
-    person1
-    ,person2
-    ,count(*) as call_count
-    ,sum(duration) as total_duration
+        person1,
+        person2,
+        count(*) as call_count,
+        sum(duration) as total_duration
 from
-    users
+        users
 where
-    person1 < person2
+        person1 < person2
 group by 
-    person1
-    ,person2
+        person1,
+        person2
 ;
