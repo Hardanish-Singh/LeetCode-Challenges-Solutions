0166d40ea7d48980ba05631abea546cfa25fba38
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-19 07:35:06 -0500

    Number of Calls Between Two Persons

--- SQL/Medium/Number_of_Calls_Between_Two_Persons.sql
+++ SQL/Medium/Number_of_Calls_Between_Two_Persons.sql
@@ -1,1 +1,32 @@
 -- Leetcode: https://leetcode.com/problems/number-of-calls-between-two-persons/
+
+with users as
+(
+select
+    from_id as person1
+    ,to_id as person2
+    ,duration as duration
+from
+    calls
+union all
+select
+    to_id as person1
+    ,from_id as person2
+    ,duration as duration
+from
+    calls
+)
+
+select 
+    person1
+    ,person2
+    ,count(*) as call_count
+    ,sum(duration) as total_duration
+from
+    users
+where
+    person1 < person2
+group by 
+    person1
+    ,person2
+;
