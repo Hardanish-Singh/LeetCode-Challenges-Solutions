ae8ae645c34fc4c0a037220a4bd02c3a15f81e91
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-22 09:03:26 -0500

    Find K Closest Elements

--- PriorityQueue/Medium/Find_K_Closest_Elements.java
+++ PriorityQueue/Medium/Find_K_Closest_Elements.java
@@ -1,8 +1,8 @@
 // Leetcode: https://leetcode.com/problems/find-k-closest-elements/description/
 
-class Solution 
+class Solution
 {
-        public List<Integer> findClosestElements(int[] arr, int k, int x) 
+        public List<Integer> findClosestElements(int[] arr, int k, int x)
         {
                 PriorityQueue<Integer> pq = new PriorityQueue<Integer>( (Integer a , Integer b) -> {
                         if(Math.abs(a-x) < Math.abs(b-x) || (Math.abs(a-x) == Math.abs(b-x) && a < b))
@@ -16,7 +16,7 @@ class Solution
                 });
                 
                 
-                for(int i = 0; i < arr.length; i++ )
+                for(int i = 0; i < arr.length; i++)
                 {
                         pq.add(arr[i]);
                 }
