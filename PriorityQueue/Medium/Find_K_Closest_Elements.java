10dc9bea623335b18b870f85d9b9e169f573fee6
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-21 21:23:54 -0500

    Find K Closest Elements

--- PriorityQueue/Medium/Find_K_Closest_Elements.java
+++ PriorityQueue/Medium/Find_K_Closest_Elements.java
@@ -2,28 +2,28 @@
 
 class Solution 
 {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) 
-    {
-       PriorityQueue<Integer> pq = new PriorityQueue<Integer>((Integer a , Integer b) -> {
-            if(Math.abs(a - x) == Math.abs(b-x)) return b - a;
-            return Math.abs(b - x) -Math.abs(a - x);
-        });
-
-        for(int i: arr)
+        public List<Integer> findClosestElements(int[] arr, int k, int x) 
         {
-            pq.add(i);
-            if(pq.size() > k)
-            {
-                    pq.remove();
-            }
-        }
+                PriorityQueue<Integer> pq = new PriorityQueue<Integer>((Integer a , Integer b) -> {
+                        if(Math.abs(a - x) == Math.abs(b-x)) return b - a;
+                        return Math.abs(b - x) -Math.abs(a - x);
+                });
 
-        List<Integer> ans = new ArrayList<>();
-        while (pq.size() > 0)
-        {
-            ans.add(pq.remove());
+                for(int i: arr)
+                {
+                        pq.add(i);
+                        if(pq.size() > k)
+                        {
+                                pq.remove();
+                        }
+                }
+
+                List<Integer> ans = new ArrayList<>();
+                while (pq.size() > 0)
+                {
+                        ans.add(pq.remove());
+                }
+                Collections.sort(ans);
+                return ans;
         }
-        Collections.sort(ans);
-        return ans;
-    }
 }
