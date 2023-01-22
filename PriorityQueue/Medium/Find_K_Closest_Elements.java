d58586c480c4d96a48cb8fbbb2cdf427d334251e
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-21 21:24:34 -0500

    Find K Closest Elements

--- PriorityQueue/Medium/Find_K_Closest_Elements.java
+++ PriorityQueue/Medium/Find_K_Closest_Elements.java
@@ -5,8 +5,10 @@ class Solution
         public List<Integer> findClosestElements(int[] arr, int k, int x) 
         {
                 PriorityQueue<Integer> pq = new PriorityQueue<Integer>((Integer a , Integer b) -> {
-                        if(Math.abs(a - x) == Math.abs(b-x)) return b - a;
-                        return Math.abs(b - x) -Math.abs(a - x);
+                        if(Math.abs(a - x) == Math.abs(b - x)) {
+                                return b - a;
+                        }
+                        return Math.abs(b - x) - Math.abs(a - x);
                 });
 
                 for(int i: arr)
