e4440d48d2d7973b35611bbea24834b54762ba52
Hardanish Singh <hardanishsingh1@gmail.com>
2023-01-23 06:28:14 -0500

    Alternating Digit Sum

--- Math/Easy/Alternating_Digit_Sum.java
+++ Math/Easy/Alternating_Digit_Sum.java
@@ -1,17 +1,18 @@
 // Leetcode: https://leetcode.com/problems/alternating-digit-sum/
 
-class Solution 
+class Solution
 {
         public int alternateDigitSum(int n)
         {
                 int sum = 0;
                 String num = Integer.toString( n );
-                for( int i = 0; i < num.length(); i++ ) 
+                for( int i = 0; i < num.length(); i++ )
                 {
-                        if( i % 2 == 0 ) 
+                        if( i % 2 == 0 )
                         {
                                 sum += Character.getNumericValue( num.charAt(i) );
-                        } else 
+                        } 
+                        else
                         {
                                 sum -= Character.getNumericValue( num.charAt(i) );
                         }
