839fef3dad10145cc507b84ef977d4a6707d5c04
Hardanish Singh <hardanishsingh1@gmail.com>
2023-02-02 08:13:35 -0500

    The Number of Passengers in Each Bus II

---
+++ SQL/Number_of_Passengers_in_Each_Bus/The_Number_of_Passengers_in_Each_Bus_II.sql
@@ -0,0 +1,27 @@
+WITH TEMP AS 
+(
+        SELECT 
+                bus_id, 
+                b.arrival_time, 
+                capacity, 
+                COUNT(passenger_id) AS num
+        FROM Buses b  
+        LEFT JOIN Passengers p  ON p.arrival_time <= b.arrival_time
+        WHERE bus_id is not NULL
+        GROUP BY bus_id
+        ORDER BY arrival_time
+)
+
+SELECT bus_id, passengers_cnt from
+(
+    SELECT 
+          bus_id, 
+          capacity, 
+          num,
+          @passengers_cnt := LEAST(capacity, num - @accum) AS passengers_cnt, 
+          @accum := @accum + @passengers_cnt
+    FROM TEMP, 
+    ( SELECT @accum := 0 ) AS accum,
+    ( SELECT @passengers_cnt := 0 ) AS passengers_cnt
+) temp
+ORDER BY bus_id
