d9b09e161185454f1a8b33fc675264c962a8a851
Hardanish Singh <hardanishsingh1@gmail.com>
2023-02-08 07:52:59 -0500

    Valid Anagrams

--- String/Anagrams/Valid_Anagrams.ts
+++ String/Anagrams/Valid_Anagrams.ts
@@ -1,13 +1,13 @@
 // Leetcode: https://leetcode.com/problems/valid-anagram/
 
-var isAnagram = function (s, t) {
+var isAnagram = function (s: string, t: string): boolean {
         if (s.length !== t.length) {
                 return false;
         }
-        var dictionaryOne = {};
-        var dictionaryTwo = {};
+        var dictionaryOne: { [key: string]: number } = {};
+        var dictionaryTwo: { [key: string]: number } = {};
 
-        for (let i = 0; i < s.length; i++) {
+        for (let i: number = 0; i < s.length; i++) {
                 dictionaryOne[s[i]] ? (dictionaryOne[s[i]] += 1) : (dictionaryOne[s[i]] = 1);
                 dictionaryTwo[t[i]] ? (dictionaryTwo[t[i]] += 1) : (dictionaryTwo[t[i]] = 1);
         }
