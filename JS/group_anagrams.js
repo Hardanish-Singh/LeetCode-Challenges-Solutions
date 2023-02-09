b9b3b606cbc0e058d290752a413d9e00723cc1ca
Hardanish Singh <hardanishsingh1@gmail.com>
2023-02-09 07:47:53 -0500

    Group Anagrams

--- JS/group_anagrams.js
+++ JS/group_anagrams.js
@@ -1,50 +1,4 @@
-/*
-        Given an array of strings strs, group the anagrams together. 
-        You can return the answer in any order.
-
-        An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-
-        Example 1:
-                Input: strs = ["eat","tea","tan","ate","nat","bat"]
-                Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
-        
-        Example 2:
-                Input: strs = [""]
-                Output: [[""]]
-        
-        Example 3:
-                Input: strs = ["a"]
-                Output: [["a"]]
-        
-        Constraints:
-                1) 1 <= strs.length <= 104
-                2) 0 <= strs[i].length <= 100
-                3) strs[i] consists of lower-case English letters.
-        
-        Companies Where this Question Was Asked Before:
-                1) Amazon
-                2) Microsoft
-                3) Goldman Sachs
-                4) Apple
-                5) Uber
-                6) eBay
-                7) VMware
-                8) Oracle
-                9) Facebook
-                10) Yandex
-                11) Twilio
-                12) BlackRock
-                13) Google
-                14) Expedia
-                15) Affirm
-                16) Docusign
-                17) Cisco
-                18) Two Sigma
-                19) ByteDance
-        
-        Difficulty Level: 
-                Medium
-*/
+// Leetcode: https://leetcode.com/problems/group-anagrams/
 
 /* 
         SOLUTION 1 
@@ -56,42 +10,40 @@
 */
 function sortAnagrams(value1, value2) {
         if (value1[0] === value2[0]) {
-            return 0;
-        }
-        else {
-            return ( value1[0] > value2[0] ) ? 1 : -1;
+                return 0;
+        } else {
+                return value1[0] > value2[0] ? 1 : -1;
         }
 }
 /*
         This function checks if two dictionaries are Anagrams or not
 */
 function isAnagram(word1, word2) {
-        for(key1 in word1) {
+        for (key1 in word1) {
                 let isFound = false;
-                for(key2 in word2) {
-                        if(key1 == key2 && word1[key1] == word2[key2]) {
+                for (key2 in word2) {
+                        if (key1 == key2 && word1[key1] == word2[key2]) {
                                 delete word2[key2];
                                 delete word1[key1];
                                 isFound = true;
                                 break;
                         }
                 }
-                if(isFound == false) return false;
+                if (isFound == false) return false;
         }
         return true;
 }
 /*
         This function counts character occurrences in a word, and returns dictionary
-*/ 
+*/
 function countCharacterOccurrences(word) {
-        
         let words = {};
         let count = 1;
 
-        for( let i=0; i<word.length; i++) {
-                if(word[i] in words) continue;
-                for( let j=i+1; j<word.length; j++ ) {
-                        if( word[i] == word[j] ) {
+        for (let i = 0; i < word.length; i++) {
+                if (word[i] in words) continue;
+                for (let j = i + 1; j < word.length; j++) {
+                        if (word[i] == word[j]) {
                                 count++;
                         }
                 }
@@ -102,13 +54,12 @@ function countCharacterOccurrences(word) {
         return words;
 }
 
-var groupAnagrams = function(strs) {
-
+var groupAnagrams = function (strs) {
         let sortedArray = [];
         let tempArray = [];
         let groupAnagrams = [];
 
-        for( let i=0; i<strs.length; i++ ) {
+        for (let i = 0; i < strs.length; i++) {
                 tempArray.push(strs[i].split("").sort().join(""));
                 tempArray.push(strs[i]);
                 sortedArray.push(tempArray);
@@ -117,37 +68,32 @@ var groupAnagrams = function(strs) {
 
         sortedArray.sort(sortAnagrams);
 
-        for( let i=0; i<sortedArray.length; i++ ) {
-                
+        for (let i = 0; i < sortedArray.length; i++) {
                 tempArray = [];
                 let checkAnagram = false;
 
                 let word1 = countCharacterOccurrences(sortedArray[i][0]);
-                
-                for( let j=i+1; j<sortedArray.length; j++ ) {
-                
-                        if( sortedArray[i][0] != sortedArray[j][0] ) {
+
+                for (let j = i + 1; j < sortedArray.length; j++) {
+                        if (sortedArray[i][0] != sortedArray[j][0]) {
                                 break;
                         }
-                        
+
                         let word2 = countCharacterOccurrences(sortedArray[j][0]);
-                        if( isAnagram( word1, word2 ) ) {
-                                tempArray.push( sortedArray[j][1] );
-                                sortedArray.splice(j,1);
+                        if (isAnagram(word1, word2)) {
+                                tempArray.push(sortedArray[j][1]);
+                                sortedArray.splice(j, 1);
                                 j--;
                                 checkAnagram = true;
-                        } 
-                
+                        }
                 }
-                
-                if( checkAnagram == true ) {
-                        tempArray.push( sortedArray[i][1] );
+
+                if (checkAnagram == true) {
+                        tempArray.push(sortedArray[i][1]);
                         groupAnagrams.push(tempArray);
-                } 
-                else {
+                } else {
                         groupAnagrams.push([sortedArray[i][1]]);
-                } 
-                
+                }
         }
 
         return groupAnagrams;
