0a6154b5130a568fef99b2b00ca2f26fb72b829a
Hardanish Singh <hardanishsingh1@gmail.com>
2023-02-12 09:23:05 -0500

    Find Resultant Array After Removing Anagrams

---
+++ String/Anagrams/Find_Resultant_Array_After_Removing_Anagrams.ts
@@ -0,0 +1,36 @@
+// Leetcode: https://leetcode.com/problems/find-resultant-array-after-removing-anagrams/
+
+var removeAnagrams = function (words: Array<string>): Array<string> {
+        for (let i: number = 1; i < words.length; i++) {
+                const firstWord = words[i - 1];
+                const secondWord = words[i];
+                const remove = isAnagram(firstWord, secondWord);
+                if (remove) {
+                        words.splice(i, 1);
+                        i--;
+                }
+        }
+
+        return words;
+};
+
+var isAnagram = function (s: string, t: string): boolean {
+        if (s.length !== t.length) {
+                return false;
+        }
+        var dictionaryOne: { [key: string]: number } = {};
+        var dictionaryTwo: { [key: string]: number } = {};
+
+        for (let i: number = 0; i < s.length; i++) {
+                dictionaryOne[s[i]] ? (dictionaryOne[s[i]] += 1) : (dictionaryOne[s[i]] = 1);
+                dictionaryTwo[t[i]] ? (dictionaryTwo[t[i]] += 1) : (dictionaryTwo[t[i]] = 1);
+        }
+
+        for (const [key, value] of Object.entries(dictionaryOne)) {
+                if (dictionaryTwo[key] !== value) {
+                        return false;
+                }
+        }
+
+        return true;
+};
