63476f5cac54622c8c8a709c64a805a1fea47c32
Hardanish Singh <hardanishsingh1@gmail.com>
2023-02-16 07:58:20 -0500

    Find All Anagrams In A String

--- String/Anagrams/Find_All_Anagrams_In_A_String.js
+++ String/Anagrams/Find_All_Anagrams_In_A_String.js
@@ -1,64 +1,33 @@
 // Leetcode: https://leetcode.com/problems/find-all-anagrams-in-a-string/
 
-function isAnagram(word1, { ...word2 }) {
-        for (key1 in word1) {
-                let isFound = false;
-                for (key2 in word2) {
-                        if (key1 == key2 && word1[key1] == word2[key2]) {
-                                delete word2[key2];
-                                isFound = true;
-                                break;
-                        }
-                }
-                if (isFound == false) {
+var isAnagram = function (s, t) {
+        if (s.length !== t.length) {
+                return false;
+        }
+        var dictionaryOne = {};
+        var dictionaryTwo = {};
+
+        for (let i = 0; i < s.length; i++) {
+                dictionaryOne[s[i]] ? (dictionaryOne[s[i]] += 1) : (dictionaryOne[s[i]] = 1);
+                dictionaryTwo[t[i]] ? (dictionaryTwo[t[i]] += 1) : (dictionaryTwo[t[i]] = 1);
+        }
+
+        for (const [key, value] of Object.entries(dictionaryOne)) {
+                if (dictionaryTwo[key] !== value) {
                         return false;
                 }
         }
+
         return true;
-}
-
-function adjustCharacterOccurrences(start, end, words) {
-        words[start] = words[start] - 1;
-        if (words[start] == 0) {
-                delete words[start];
-        }
-        end in words ? (words[end] = words[end] + 1) : (words[end] = 1);
-        return words;
-}
-
-function countCharacterOccurrences(word) {
-        let countOccurrences = {};
-        for (let i = 0; i < word.length; i++) {
-                word[i] in countOccurrences ? (countOccurrences[word[i]] += 1) : (countOccurrences[word[i]] = 1);
-        }
-        return countOccurrences;
-}
+};
 
 var findAnagrams = function (s, p) {
-        let substr = "";
-        let anagrams = [];
-        let words = countCharacterOccurrences(p);
-        let breakwords = {};
-
+        let res = [];
         for (let i = 0; i < s.length; i++) {
-                substr = s.slice(i, p.length + i);
-                if (substr.length != p.length) {
-                        break;
-                } else {
-                        if (i == 0) {
-                                breakwords = countCharacterOccurrences(substr);
-                        } else {
-                                breakwords = adjustCharacterOccurrences(
-                                        s[i - 1],
-                                        substr[substr.length - 1],
-                                        breakwords
-                                );
-                        }
-                        if (isAnagram(breakwords, words)) {
-                                anagrams.push(i);
-                        }
+                let substr = s.slice(i, p.length + i);
+                if (isAnagram(substr, p)) {
+                        res.push(i);
                 }
         }
-
-        return anagrams;
+        return res;
 };
