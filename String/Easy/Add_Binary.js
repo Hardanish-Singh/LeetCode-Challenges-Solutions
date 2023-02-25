39a91f1af96521ca3e9b919d01865c41bacb49ca
Hardanish Singh <hardanishsingh1@gmail.com>
2023-02-25 10:43:36 -0500

    Add Binary

--- String/Easy/Add_Binary.js
+++ String/Easy/Add_Binary.js
@@ -1,20 +1,6 @@
-/*
-        Given two binary strings a and b, return their sum as a binary string.  
+// Leetcode: https://leetcode.com/problems/add-binary/
 
-        Example 1:
-                Input: a = "11", b = "1"
-                Output: "100"
-        Example 2:
-                Input: a = "1010", b = "1011"
-                Output: "10101"
-        
-        Constraints:
-                1) 1 <= a.length, b.length <= 10^4
-                2) a and b consist only of '0' or '1' characters.
-                3) Each string does not contain leading zeros except for the zero itself.
-*/
-
-/*
+/**
  * @param { string } a
  * @param { string } b
  * @return { string }
