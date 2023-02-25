e8a7a724f4458418af350718bcbaa8d7e4128c0c
Hardanish Singh <hardanishsingh1@gmail.com>
2023-02-24 20:19:11 -0500

    Add Binary

--- String/Easy/Add_Binary.js
+++ String/Easy/Add_Binary.js
@@ -18,39 +18,35 @@
  * @param { string } a
  * @param { string } b
  * @return { string }
-*/
+ */
 
-var addBinary = function(a, b) {
-        if( b.length > a.length ) {
-                return addBinary( b, a );
+var addBinary = function (a, b) {
+        if (b.length > a.length) {
+                return addBinary(b, a);
         }
         let carry = 0;
-        let sum = '';
-        let answer = '';
-        let j = b.length-1;
+        let sum = "";
+        let answer = "";
+        let j = b.length - 1;
         let temp = b[j];
 
-        for( let i = a.length-1; i >= 0; i-- ) {
+        for (let i = a.length - 1; i >= 0; i--) {
                 temp = b[j--];
-                if( temp === undefined ) {
-                        temp = '0';
+                if (temp === undefined) {
+                        temp = "0";
                 }
-                if( a[i] === '1' && temp === '1' ) {
-                        carry === 1 ? sum = String( 1 ) : sum = String( 0 );
+                if (a[i] === "1" && temp === "1") {
+                        carry === 1 ? (sum = String(1)) : (sum = String(0));
                         carry = 1;
-                } else if(
-                                ( a[i] === '1' && temp === '0' ) 
-                                        ||
-                                ( a[i] === '0' && temp === '1' )
-                ) {
-                        carry === 1 ? ( sum = String( 0 ), carry = 1 ) : ( sum = String( 1 ), carry = 0 );
+                } else if ((a[i] === "1" && temp === "0") || (a[i] === "0" && temp === "1")) {
+                        carry === 1 ? ((sum = String(0)), (carry = 1)) : ((sum = String(1)), (carry = 0));
                 } else {
-                        carry === 1 ? sum = String( 1 ) : sum = String( 0 );
+                        carry === 1 ? (sum = String(1)) : (sum = String(0));
                         carry = 0;
                 }
-                answer = String( +sum[sum.length-1] ) + answer;
+                answer = String(+sum[sum.length - 1]) + answer;
         }
 
-        carry > 0 ? answer = carry + answer : null;
+        carry > 0 ? (answer = carry + answer) : null;
         return answer.length > 1 && answer.startsWith("0") ? answer.slice(1) : answer;
 };
