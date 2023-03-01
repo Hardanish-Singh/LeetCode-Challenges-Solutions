dd28cf33ca751f205785ea404af87b0aae0713da
Hardanish Singh <hardanishsingh1@gmail.com>
2023-03-01 07:09:02 -0500

    Longest Common Prefix

--- String/Easy/Longest_Common_Prefix.js
+++ String/Easy/Longest_Common_Prefix.js
@@ -3,22 +3,22 @@
 /**
  * @param { string[] } strs
  * @return { string }
-*/
+ */
 
 // SOLUTION 1
-var longestCommonPrefix = function( strs ) {
-        strs = strs.sort( ( a, b ) => a.length - b.length );
+var longestCommonPrefix = function (strs) {
+        strs = strs.sort((a, b) => a.length - b.length);
         let word = strs[0];
 
-        while( word.length !== 0 ) {
+        while (word.length !== 0) {
                 let isLongestPrefix = true;
-                for( let i = 1; i < strs.length; i++ ) {
-                        if( strs[i].indexOf(word) !== 0 ) {
+                for (let i = 1; i < strs.length; i++) {
+                        if (strs[i].indexOf(word) !== 0) {
                                 isLongestPrefix = false;
                                 break;
                         }
                 }
-                if( isLongestPrefix ) {
+                if (isLongestPrefix) {
                         return word;
                 }
                 word = strs[0].slice(0, word.length - 1);
@@ -32,32 +32,32 @@ var longestCommonPrefix = function( strs ) {
 /**
  * @param { string[] } strs
  * @return { string }
-*/
+ */
 
-var longestCommonPrefix = function( strs ) {
-        strs.sort( (a, b) => a.length - b.length );
-        if( strs.length === 1 ) {
+var longestCommonPrefix = function (strs) {
+        strs.sort((a, b) => a.length - b.length);
+        if (strs.length === 1) {
                 return strs[0];
         }
-        if( strs[0] === "" ){
+        if (strs[0] === "") {
                 return "";
         }
-        
+
         let flag = true;
         let index = 0;
         let word = strs[0][index];
-        
-        while( flag ) {
-                for( let i = 1; i < strs.length; i++ ) {
-                        if( strs[i].indexOf(word) !== 0 ) {
+
+        while (flag) {
+                for (let i = 1; i < strs.length; i++) {
+                        if (strs[i].indexOf(word) !== 0) {
                                 word = word.slice(0, word.length - 1);
                                 flag = false;
                                 break;
                         }
                 }
-                if( flag ) {
+                if (flag) {
                         let character = strs[0][++index];
-                        if( character == null ) {
+                        if (character == null) {
                                 break;
                         } else {
                                 word += character;
