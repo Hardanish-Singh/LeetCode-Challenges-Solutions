ca8d32c6f6b647a3779a7c3aa24d1c6fbab9bb92
Hardanish Singh <hardanishsingh1@gmail.com>
2023-03-03 08:49:01 -0500

    Reverse String II

---
+++ String/Easy/Reverse_String_II.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function (s, k) {
+        const arr = s.match(new RegExp(".{1," + k + "}", "g")); // split the string into an array of k parts
+
+        let result = "";
+
+        for (let i = 0; i < arr.length; i++) {
+                result += i % 2 === 0 ? arr[i].split("").reverse().join("") : arr[i];
+        }
+
+        return result; // return the result
+};
