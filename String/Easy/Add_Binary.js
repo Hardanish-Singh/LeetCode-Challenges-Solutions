af6a70aab57c3dc0b17ae607acd1a49a16a5bb27
Hardanish Singh <hardanishsingh1@gmail.com>
2023-03-04 16:15:41 -0500

    Add Binary

--- String/Easy/Add_Binary.js
+++ String/Easy/Add_Binary.js
@@ -6,29 +6,4 @@
  * @return { string }
  */
 
-var addBinary = function (a, b) {
-        if (b.length > a.length) {
-                return addBinary(b, a);
-        }
-        let carryOver = 0;
-        let sum = "";
-        let answer = "";
-        let j = b.length - 1;
-
-        for (let i = a.length - 1; i >= 0; i--) {
-                let temp = b[j] ? b[j--] : "0";
-                if (a[i] === "1" && temp === "1") {
-                        sum = carryOver === 1 ? "1" : "0";
-                        carryOver = 1;
-                } else if ((a[i] === "1" && temp === "0") || (a[i] === "0" && temp === "1")) {
-                        carryOver === 1 ? ((sum = "0"), (carryOver = 1)) : ((sum = "1"), (carryOver = 0));
-                } else {
-                        carryOver === 1 ? (sum = "1") : (sum = "0");
-                        carryOver = 0;
-                }
-                answer = String(+sum[sum.length - 1]) + answer;
-        }
-
-        carryOver > 0 ? (answer = carryOver + answer) : null;
-        return answer.length > 1 && answer.startsWith("0") ? answer.slice(1) : answer;
-};
+var addBinary = (a, b) => (BigInt("0b" + a) + BigInt("0b" + b)).toString(2);
