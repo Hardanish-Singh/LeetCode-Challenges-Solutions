4b7ff2f39ec21adc3ddda4da7c502970031f2e55
Hardanish Singh <hardanishsingh1@gmail.com>
2023-03-04 16:23:59 -0500

    Add Binary

--- String/Easy/Add_Binary.js
+++ String/Easy/Add_Binary.js
@@ -6,4 +6,4 @@
  * @return { string }
  */
 
-var addBinary = (a, b) => (BigInt("0b" + a) + BigInt("0b" + b)).toString(2);
+var addBinary = (a, b) => (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2);
