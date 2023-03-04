cd2f74b5ad659bd9795b3c25fb8c1e402349fc27
Hardanish Singh <hardanishsingh1@gmail.com>
2023-03-04 16:42:37 -0500

    Add String

--- String/Easy/Add_Strings.js
+++ String/Easy/Add_Strings.js
@@ -6,19 +6,4 @@
  * @return { string }
  */
 
-var addStrings = function (num1, num2) {
-        if (num2.length > num1.length) {
-                return addStrings(num2, num1);
-        }
-        let carry = 0;
-        let sum = 0;
-        let answer = "";
-        let j = num2.length - 1;
-        for (let i = num1.length - 1; i >= 0; i--) {
-                sum = String(+num1[i] + (num2[j] ? +num2[j--] : 0) + carry);
-                answer = String(+sum[sum.length - 1]) + answer;
-                sum.length > 1 ? (carry = +sum[0]) : (carry = 0);
-        }
-        carry > 0 ? (answer = carry + answer) : null;
-        return answer.length > 1 && answer.startsWith("0") ? answer.slice(1) : answer;
-};
+var addStrings = (num1, num2) => (BigInt(num1) + BigInt(num2)).toString();
