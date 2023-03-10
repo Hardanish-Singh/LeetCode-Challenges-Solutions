6998a49d9afe32e625050472d9cdd08367144fed
Hardanish Singh <hardanishsingh1@gmail.com>
2023-03-10 07:37:21 -0500

    Basic Calculator II

--- String/Basic_Calculator/Basic_Calculator_II.js
+++ String/Basic_Calculator/Basic_Calculator_II.js
@@ -1,5 +1,17 @@
 // Leetcode: https://leetcode.com/problems/basic-calculator-ii/
 
+/*
+        Our goal is to transform the input from a string into an array of numbers where each element is an operand with its sign affixed.
+        It's very obvious that we need to traverse the input string and somehow gathers information on both the numbers and the operators.
+        For the number part: a string is a correct number until we hit a operator.
+        For the operator part: The intuition here is to have a prevSign variable initialized to +, this way every time we see a new operator, 
+                               we actuall make the calculation using the prevSign and set the new operator as prevSign for next time use.
+                               The tricky part here is whenever we meet a * or /, we need to get the last number from our calculation stack in order to make the operation. 
+                               At the end, we just need to add all the numbers from our call stack.
+        Note: Substraction can be regarded as adding a negative number, that is, a - b = a + (-b)
+
+*/
+
 const isOperator = (c) => c === "+" || c === "-" || c === "*" || c === "/";
 
 const calculate = (s) => {
