fabffdc28140edced104185f83efb840422400e5
Hardanish Singh <hardanishsingh1@gmail.com>
2023-03-10 07:40:59 -0500

    Basic Calculator II

--- String/Basic_Calculator/Basic_Calculator_II.js
+++ String/Basic_Calculator/Basic_Calculator_II.js
@@ -4,9 +4,9 @@
         Our goal is to transform the input from a string into an array of numbers where each element is an operand with its sign affixed.
         It's very obvious that we need to traverse the input string and somehow gathers information on both the numbers and the operators.
         For the number part: a string is a correct number until we hit a operator.
-        For the operator part: The intuition here is to have a prevSign variable initialized to +, this way every time we see a new operator, 
+        For the operator part: The intuition here is to have a prevSign variable initialized to +, this way every time we see a new operator,
                                we actuall make the calculation using the prevSign and set the new operator as prevSign for next time use.
-                               The tricky part here is whenever we meet a * or /, we need to get the last number from our calculation stack in order to make the operation. 
+                               The tricky part here is whenever we meet a * or /, we need to get the last number from our calculation stack in order to make the operation.
                                At the end, we just need to add all the numbers from our call stack.
         Note: Substraction can be regarded as adding a negative number, that is, a - b = a + (-b)
 
@@ -22,6 +22,7 @@ const calculate = (s) => {
         let prevSign = "+";
 
         for (let i = 0; i < s.length; i++) {
+                // number
                 if (Number.isInteger(Number(s[i]))) {
                         num += s[i];
                 }
