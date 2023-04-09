7835455f8c72049c2c0b531ceafe657144ad0a0e
Hardanish Singh <hardanishsingh1@gmail.com>
2023-04-09 09:41:19 -0400

    Binary Search

---
+++ Array/Easy/Binary_Search.ts
@@ -0,0 +1,17 @@
+// Leetcode: https://leetcode.com/problems/binary-search/
+
+const search = (array: Array<number>, key: number): number => {
+        array.sort((a, b) => a - b);
+        let start: number = 0;
+        let end: number = array.length - 1;
+
+        while (start <= end) {
+                let midPoint: number = Math.floor((start + end) / 2);
+                if (key === array[midPoint]) {
+                        return midPoint;
+                }
+                key > array[midPoint] ? (start = midPoint + 1) : (end = midPoint - 1);
+        }
+
+        return -1;
+};
