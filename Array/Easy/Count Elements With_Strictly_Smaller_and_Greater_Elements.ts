07560787dc99f090438c074daae0753a1331b0d9
Hardanish Singh <hardanishsingh1@gmail.com>
2023-04-10 16:33:46 -0400

    Count Elements With Strictly Smaller and Greater Elements

---
+++ Array/Easy/Count Elements With_Strictly_Smaller_and_Greater_Elements.ts
@@ -0,0 +1,15 @@
+// Leetcode: https://leetcode.com/problems/count-elements-with-strictly-smaller-and-greater-elements/
+
+const countElements = (nums: number[]): number => {
+        const min = Math.min.apply(null, nums);
+        const max = Math.max.apply(null, nums);
+        let count = 0;
+
+        for (let i = 0; i < nums.length; i++) {
+                if (nums[i] > min && nums[i] < max) {
+                        count++;
+                }
+        }
+
+        return count;
+};
