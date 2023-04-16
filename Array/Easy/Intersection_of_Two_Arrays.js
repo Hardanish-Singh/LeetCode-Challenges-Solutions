28dd0b1d65de0b0052ea8054a22d6d313a9be405
Hardanish Singh <hardanishsingh1@gmail.com>
2023-04-16 19:58:26 -0400

    Intersection of Two Arrays

--- Array/Easy/Intersection_of_Two_Arrays.js
+++ Array/Easy/Intersection_of_Two_Arrays.js
@@ -1,31 +1,19 @@
-/*
-        Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
-        Example 1:
-                Input: nums1 = [1,2,2,1], nums2 = [2,2]
-                Output: [2]
-        Example 2:
-                Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
-                Output: [9,4]
-                Explanation: [4,9] is also accepted.
-        Constraints:
-                1) 1 <= nums1.length, nums2.length <= 1000
-                2) 0 <= nums1[i], nums2[i] <= 1000
-*/
+// Leetcode: https://leetcode.com/problems/intersection-of-two-arrays/
+
 /*
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number[]}
-*/
-var intersection = function( nums1, nums2 ) {
-        nums1.sort( (a, b) => a - b );
-        nums2.sort( (a, b) => a - b );
+ */
+var intersection = function (nums1, nums2) {
+        nums1.sort((a, b) => a - b);
+        nums2.sort((a, b) => a - b);
         let intersection_of_arrays = [];
-        for( let i=0; i<nums1.length; i++ ) {
-                for( let j=0; j<nums2.length; j++ ) {
-                        if( nums1[i] === nums2[j] && !( intersection_of_arrays.includes( nums1[i] ) ) ) {
+        for (let i = 0; i < nums1.length; i++) {
+                for (let j = 0; j < nums2.length; j++) {
+                        if (nums1[i] === nums2[j] && !intersection_of_arrays.includes(nums1[i])) {
                                 intersection_of_arrays.push(nums1[i]);
-                        }
-                        else if( nums1[i] < nums2[j] ) {
+                        } else if (nums1[i] < nums2[j]) {
                                 break;
                         }
                 }
