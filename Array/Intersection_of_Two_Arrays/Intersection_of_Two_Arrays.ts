7867ef6f68181d8fec1465df6d17c1f6f4b33ed3
Hardanish Singh <hardanishsingh1@gmail.com>
2023-04-22 07:51:37 -0400

    Intersection of Two Arrays

--- Array/Intersection_of_Two_Arrays/Intersection_of_Two_Arrays.ts
+++ Array/Intersection_of_Two_Arrays/Intersection_of_Two_Arrays.ts
@@ -1,17 +1,11 @@
 // Leetcode: https://leetcode.com/problems/intersection-of-two-arrays/
 
-/*
- * @param { number[] } nums1
- * @param { number[] } nums2
- * @return { number[] }
- */
-
-var intersection = function (nums1, nums2) {
+const intersection = (nums1: Array<number>, nums2: Array<number>): Array<number> => {
         nums1.sort((a, b) => a - b);
         nums2.sort((a, b) => a - b);
-        let intersection_of_arrays = [];
-        for (let i = 0; i < nums1.length; i++) {
-                for (let j = 0; j < nums2.length; j++) {
+        let intersection_of_arrays: Array<number> = [];
+        for (let i: number = 0; i < nums1.length; i++) {
+                for (let j: number = 0; j < nums2.length; j++) {
                         if (nums1[i] === nums2[j] && !intersection_of_arrays.includes(nums1[i])) {
                                 intersection_of_arrays.push(nums1[i]);
                         } else if (nums1[i] < nums2[j]) {
