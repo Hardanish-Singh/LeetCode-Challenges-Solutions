7f3299c6bab7a5140b2a3e1275d7a39cc4b1c836
Hardanish Singh <hardanishsingh1@gmail.com>
2023-04-28 07:28:50 -0400

    Last Stone Weight

---
+++ Array/Last_Stone_Weight/Last_Stone_Weight.ts
@@ -0,0 +1,15 @@
+// Leetcode: https://leetcode.com/problems/last-stone-weight/
+
+const lastStoneWeight = (stones: Array<number>): number => {
+        if (stones.length === 0) {
+                return 0;
+        }
+        while (stones.length !== 1) {
+                stones.sort((a, b) => b - a);
+                let x = stones.shift();
+                let y = stones.shift();
+                let diff = x !== y ? x - y : 0;
+                stones.push(diff);
+        }
+        return stones[0];
+};
