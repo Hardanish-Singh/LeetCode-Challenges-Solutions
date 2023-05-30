52ea3be6ce6f8d284d5b8e5b1a6c1ede8f8fdfb4
Hardanish Singh <hardanishsingh1@gmail.com>
2023-05-30 06:55:34 -0400

    Merge Sorted Array

--- Array/Easy/Merge_Sorted_Array.ts
+++ Array/Easy/Merge_Sorted_Array.ts
@@ -1,12 +1,12 @@
 // Leetcode: https://leetcode.com/problems/merge-sorted-array/
 
-const merge = ( nums1: Array<number>, m: number, nums2: Array<number>, n: number ): Array<number> => {
+const merge = (nums1: Array<number>, m: number, nums2: Array<number>, n: number): Array<number> => {
         var index1 = 0;
-        for( let i: number = 0; i < nums1.length; i++ ) {
-                if( index1 <= n - 1 && nums1[i] === 0 ) {
+        for (let i: number = 0; i < nums1.length; i++) {
+                if (index1 <= n - 1 && nums1[i] === 0) {
                         nums1[i] = nums2[index1];
                         index1++;
                 }
         }
-        return nums1.sort( ( a, b ) => a - b );
+        return nums1.sort((a, b) => a - b);
 };
