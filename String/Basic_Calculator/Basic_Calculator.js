e5a82998dbbc313a860c9909c7c51d9b62e9e3a9
Hardanish Singh <hardanishsingh1@gmail.com>
2023-08-07 08:17:12 -0400

    Basic Calculator

--- String/Basic_Calculator/Basic_Calculator.js
+++ String/Basic_Calculator/Basic_Calculator.js
@@ -1,14 +1,9 @@
 // Leetcode: https://leetcode.com/problems/basic-calculator/
 
 function parse(str) {
-        return Function(`'use strict'; return (${str})`)();
+    return Function(`'use strict'; return (${str})`)();
 }
 
-/**
- * @param { string } s
- * @return { number }
- */
-
 var calculate = function (s) {
-        return parse(s);
+    return parse(s);
 };
