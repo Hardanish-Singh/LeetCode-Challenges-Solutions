2cb42934e427af41364f1ec5770dcbd9e4494880
Hardanish Singh <hardanishsingh1@gmail.com>
2023-08-07 20:07:37 -0400

    Basic Calculator I

--- String/Basic_Calculator/Basic_Calculator.js
+++ String/Basic_Calculator/Basic_Calculator.js
@@ -1,9 +1,40 @@
 // Leetcode: https://leetcode.com/problems/basic-calculator/
 
-function parse(str) {
-    return Function(`'use strict'; return (${str})`)();
-}
+const isOperator = (c) => c === "+" || c === "-" || c === "(" || c === ")";
 
 var calculate = function (s) {
-    return parse(s);
+    // remove space from the string
+    s.replace(/\s/g, "");
+    let num = "";
+    let stack = [];
+    let prevSign = "+";
+
+    for (let i = 0; i < s.length; i++) {
+        // number
+        if (!isNaN(s[i])) {
+            num += s[i];
+        }
+        // isOperator or last character
+        if (isOperator(s[i]) || i === s.length - 1) {
+            stack.push(prevSign === "+" ? Number(num) : Number(-num));
+            if (s[i] === "+") {
+                prevSign = "+";
+            } else if (s[i] === "-") {
+                prevSign = "-";
+            } else if (s[i] === "(") {
+                stack.push(prevSign === "+" ? 1 : -1);
+                stack.push("X");
+                prevSign = "+"; // reset prevSign to +
+            } else if (s[i] === ")") {
+                let sum = 0;
+                while (stack[stack.length - 1] !== "X") {
+                    sum += stack.pop();
+                }
+                stack.pop(); // remove "X"
+                stack.push(Number(stack.pop()) * sum);
+            }
+            num = "";
+        }
+    }
+    return stack.reduce((a, b) => a + b, 0);
 };
