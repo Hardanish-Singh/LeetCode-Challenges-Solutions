c8b9ebc79157682e789e267ded6f7e27bc52979b
Hardanish Singh <hardanishsingh1@gmail.com>
2023-08-15 08:58:57 -0400

    Basic Calculator II

--- String/Basic_Calculator/Basic_Calculator_II.ts
+++ String/Basic_Calculator/Basic_Calculator_II.ts
@@ -5,21 +5,21 @@
         It's very obvious that we need to traverse the input string and somehow gathers information on both the numbers and the operators.
         
         For the number part: a string is a correct number until we hit a operator.
-        For the operator part: The intuition here is to have a prevSign variable initialized to +, this way every time we see a new operator,
-                               we actuall make the calculation using the prevSign and set the new operator as prevSign for next time use.
+        For the operator part: The intuition here is to have a sign variable initialized to +, this way every time we see a new operator,
+                               we actuall make the calculation using the sign and set the new operator as sign for next time use.
                                The tricky part here is whenever we meet a * or /, we need to get the last number from our calculation stack in order to make the operation.
                                At the end, we just need to add all the numbers from our call stack.
         
         NOTE: Substraction can be regarded as adding a negative number, that is, a - b = a + (-b)
 
         For example: "2+3*2" 
-                where we initialize stack = [] & prevSign = +
+                where we initialize stack = [] & sign = +
                 2 -> a number, stack = []
-                + -> calc w/ prevSign +, stack = [2], set prevSign +
+                + -> calc w/ sign +, stack = [2], set sign +
                 3 -> a number
-                * -> calc w/ prevSign + , stack = [2, 3], set prevSign *
+                * -> calc w/ sign + , stack = [2, 3], set sign *
                 2 -> a number
-                end -> calc w/ prevSign * using 3, calStack [2, 6]
+                end -> calc w/ sign * using 3, calStack [2, 6]
                 Sum of array is 8.
 */
 
@@ -30,7 +30,7 @@ const calculate = (s: string): number => {
     s.replace(/\s/g, "");
     let num: string = "";
     let stack: Array<number> = [];
-    let prevSign: string = "+";
+    let sign: string = "+";
 
     for (let i: number = 0; i < s.length; i++) {
         // number
@@ -39,16 +39,16 @@ const calculate = (s: string): number => {
         }
         // isOperator or last character
         if (isOperator(s[i]) || i === s.length - 1) {
-            if (prevSign == "+") {
+            if (sign == "+") {
                 stack.push(Number(num));
-            } else if (prevSign == "-") {
+            } else if (sign == "-") {
                 stack.push(Number(-num));
-            } else if (prevSign == "*") {
+            } else if (sign == "*") {
                 stack.push(Math.floor(stack.pop() * Number(num)));
             } else {
                 stack.push(Math.trunc(stack.pop() / Number(num)));
             }
-            prevSign = s[i];
+            sign = s[i];
             num = "";
         }
     }
