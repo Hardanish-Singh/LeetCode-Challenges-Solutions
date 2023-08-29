5a31248aa05e8e66531fe69501adbda78832310c
Hardanish Singh <hardanishsingh1@gmail.com>
2023-08-29 09:19:47 -0400

    Valid Anagrams

--- String/Anagrams/Valid_Anagrams.ts
+++ String/Anagrams/Valid_Anagrams.ts
@@ -1,22 +1,26 @@
 // Leetcode: https://leetcode.com/problems/valid-anagram/
 
-var isAnagram = function (s: string, t: string): boolean {
-        if (s.length !== t.length) {
-                return false;
-        }
-        var dictionaryOne: { [key: string]: number } = {};
-        var dictionaryTwo: { [key: string]: number } = {};
+type Anagram = {
+    [key: string]: number;
+};
 
-        for (let i: number = 0; i < s.length; i++) {
-                dictionaryOne[s[i]] ? (dictionaryOne[s[i]] += 1) : (dictionaryOne[s[i]] = 1);
-                dictionaryTwo[t[i]] ? (dictionaryTwo[t[i]] += 1) : (dictionaryTwo[t[i]] = 1);
-        }
+const isAnagram = (s: string, t: string): boolean => {
+    if (s.length !== t.length) {
+        return false;
+    }
+    let dictionaryOne: Anagram = {};
+    let dictionaryTwo: Anagram = {};
 
-        for (const [key, value] of Object.entries(dictionaryOne)) {
-                if (dictionaryTwo[key] !== value) {
-                        return false;
-                }
-        }
+    for (let i: number = 0; i < s.length; i++) {
+        dictionaryOne[s[i]] ? (dictionaryOne[s[i]] += 1) : (dictionaryOne[s[i]] = 1);
+        dictionaryTwo[t[i]] ? (dictionaryTwo[t[i]] += 1) : (dictionaryTwo[t[i]] = 1);
+    }
 
-        return true;
+    for (const [key, value] of Object.entries(dictionaryOne)) {
+        if (dictionaryTwo[key] !== value) {
+            return false;
+        }
+    }
+
+    return true;
 };
