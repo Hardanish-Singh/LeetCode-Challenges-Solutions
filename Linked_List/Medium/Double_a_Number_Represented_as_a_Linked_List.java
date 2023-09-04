3d034c3d5a54e6930e4f8213a151957ed60c924e
Hardanish Singh <hardanishsingh1@gmail.com>
2023-09-04 08:56:57 -0400

    Double a Number Represented as a Linked List

---
+++ Linked_List/Medium/Double_a_Number_Represented_as_a_Linked_List.java
@@ -0,0 +1,53 @@
+/*
+        // Definition for Singly-Linked List
+        public class ListNode
+	{
+                int val;
+                ListNode next;
+                public ListNode()
+                {
+                }
+                public ListNode( int val )
+                {
+                        this.val = val;
+                }
+                public ListNode( int val, ListNode next )
+                {
+                        this.val = val;
+                        this.next = next;
+                }
+        }
+*/
+
+import java.math.BigInteger;
+class Solution 
+{
+    public ListNode doubleIt(ListNode head) 
+    {
+        String number = "";
+        while( head != null )
+        {
+            number += head.val;
+            head = head.next;
+        }
+        BigInteger a = new BigInteger(number);
+        BigInteger b = new BigInteger("2");
+        number = String.valueOf(a.multiply(b));
+        head = null;
+		ListNode tail = null;
+        for(int i = 0; i < number.length(); i++) 
+        {
+            ListNode node = new ListNode( Character.getNumericValue( number.charAt(i) ) );
+            if( head == null && tail == null )
+			{
+			    head = tail = node;
+			}
+			else
+			{
+			    tail.next = node;
+			    tail = node;
+			}
+        }
+        return head;
+    }
+}
