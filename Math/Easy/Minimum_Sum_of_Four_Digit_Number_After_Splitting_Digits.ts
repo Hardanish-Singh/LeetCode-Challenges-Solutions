e4961d41182a4fa43ef81f18a1a1b014e448c94a
Hardanish Singh <hardanishsingh1@gmail.com>
2023-09-09 13:27:30 -0400

    Minimum Sum of Four Digit Number After Splitting Digits

---
+++ Math/Easy/Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits.ts
@@ -0,0 +1,8 @@
+// Leetcode: https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/
+
+const minimumSum = (num: number): number => {
+    const nums: Array<number> = String(num).split("").map(Number).sort((a, b) => a - b);
+    let num1: number = Number(nums[0] + nums[2]);
+    let num2: number = Number(nums[1] + nums[3]);
+    return num1 + num2;
+};
