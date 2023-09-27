7e61ca6fc1856a76c87fc04fafb1c93da3e7abcb
Hardanish Singh <hardanishsingh1@gmail.com>
2023-09-27 13:23:38 -0400

    N-ary Tree Postorder Traversal

--- Tree/Easy/N-ary_Tree_Postorder_Traversal.py
+++ Tree/Easy/N-ary_Tree_Postorder_Traversal.py
@@ -2,18 +2,34 @@
 
 """
         # Definition for a Node.
-        class Node(object):
-                def __init__( self, val = None, children = None ):
+        class Node:
+                def __init__(self, val = None, children = None):
                         self.val = val
                         self.children = children
 """
 
-class Solution(object):
-        def postorder(self, root):
-                """
-                :type root: Node
-                :rtype: List[int]
-                """
+"""
+        SOLUTION 1: RECURSIVE
+"""
+
+class Solution:
+        def postorderRecursiveTraversal(self, root: 'TreeNode', postOrderList: List[int]) -> List[int]:
+                if root is None:
+                        return None
+                for child in root.children:
+                        self.postorderRecursiveTraversal(child, postOrderList)
+                postOrderList.append(root.val)
+        def postorder(self, root: 'Node') -> List[int]:
+                postOrderList = []
+                self.postorderRecursiveTraversal( root, postOrderList )
+                return postOrderList
+
+"""
+        SOLUTION 2: ITERATIVE
+"""
+
+class Solution:
+        def postorder(self, root: 'Node') -> List[int]:
                 if root is None:
                         return []
         
