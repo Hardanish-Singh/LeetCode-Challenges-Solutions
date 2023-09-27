ed93154a9ebd3d529889069b9ee85600ae2b472c
Hardanish Singh <hardanishsingh1@gmail.com>
2023-09-27 13:15:43 -0400

    N-ary Tree Preorder Traversal

--- Tree/Easy/N-ary_Tree_Preorder_Traversal.py
+++ Tree/Easy/N-ary_Tree_Preorder_Traversal.py
@@ -28,7 +28,7 @@ class Solution:
         SOLUTION 2: ITERATIVE
 """
 
-class Solution( object ):
+class Solution:
         def preorder(self, root: 'Node') -> List[int]:
                 if root is None:
                         return []
