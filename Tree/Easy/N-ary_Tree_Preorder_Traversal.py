1b253f758ee67a775d1e2ebca0bb12fe360fb538
Hardanish Singh <hardanishsingh1@gmail.com>
2023-09-27 13:12:03 -0400

    N-ary Tree Preorder Traversal

--- Tree/Easy/N-ary_Tree_Preorder_Traversal.py
+++ Tree/Easy/N-ary_Tree_Preorder_Traversal.py
@@ -2,19 +2,34 @@
 
 """
         # Definition for a Node.
-        class Node(object):
-                def __init__( self, val = None, children = None ):
+        class Node:
+                def __init__(self, val = None, children = None):
                         self.val = val
                         self.children = children
 """
 
-class Solution( object ):
-        def preorder( self, root ):
-                """
-                :type root: TreeNode
-                :rtype: List[int]
-                """
+"""
+        SOLUTION 1: RECURSIVE
+"""
 
+class Solution:
+        def preorderRecursiveTraversal(self, root: 'Node', preOrderList: List[int]) -> List[int]:
+                if root is None:
+                        return None
+                preOrderList.append(root.val)
+                for child in root.children:
+                        self.preorderRecursiveTraversal(child, preOrderList)
+        def preorder(self, root: 'Node') -> List[int]:
+                preOrderList = []
+                self.preorderRecursiveTraversal(root, preOrderList)
+                return preOrderList
+
+"""
+        SOLUTION 2: ITERATIVE
+"""
+
+class Solution( object ):
+        def preorder(self, root: 'Node') -> List[int]:
                 if root is None:
                         return []
 
