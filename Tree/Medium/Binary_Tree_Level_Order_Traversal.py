a3de35bb84b7d438e1a8fad45fa229f153e36aba
Hardanish Singh <hardanishsingh1@gmail.com>
2023-09-30 09:58:34 -0400

    Binary Tree Level Order Traversal

--- Tree/Medium/Binary_Tree_Level_Order_Traversal.py
+++ Tree/Medium/Binary_Tree_Level_Order_Traversal.py
@@ -1,37 +1,32 @@
 # Leetcode: https://leetcode.com/problems/binary-tree-level-order-traversal/
 
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+"""
+        # Definition for a binary tree node.
+        class TreeNode:
+                def __init__( self, val = 0, left = None, right = None ):
+                        self.val = val
+                        self.left = left
+                        self.right = right
+"""
 
-class Solution(object):
-        def levelOrder(self, root):
-                """
-                :type root: TreeNode
-                :rtype: List[List[int]]
-                """
+class Solution:
+        def levelOrder(self, root: TreeNode) -> List[List[int]]:
                 if root is None:
                         return []
 
-                queue = [ root ]
-                result = [ ]
+                queue = [root]
+                result = []
 
                 while len( queue ) > 0:
-                        temp = []
-                        # GET ALL ELEMENTS FROM QUEUE
-                        for node in queue:
-                                temp.append( node.val )
-                        result.append( temp )
-                        
-                        # POP ALL ELEMENTS FROM QUEUE
+                        level = []
                         n = len( queue )
-                        for i in range( n ):
+                        for i in range(n):
                                 node = queue.pop(0)
+                                level.append(node.val)
                                 if node.left:
-                                        queue.append( node.left )
+                                        queue.append(node.left)
                                 if node.right:
-                                        queue.append( node.right )
+                                        queue.append(node.right)
+                        result.append(level)
+                
                 return result
