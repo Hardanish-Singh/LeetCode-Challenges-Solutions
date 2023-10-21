33f204df819e42e02841214ec52463c5b967adf6
Hardanish Singh <hardanishsingh1@gmail.com>
2023-10-21 11:02:01 -0400

    Find All Anagrams In A String

--- String/Anagrams/Find_All_Anagrams_In_A_String.ts
+++ String/Anagrams/Find_All_Anagrams_In_A_String.ts
@@ -1,54 +1,49 @@
 // Leetcode: https://leetcode.com/problems/find-all-anagrams-in-a-string/
 
-type HashMap = {
+type Anagram = {
         [key: string]: number;
 };
-
-const isAnagram = (hashmap1: HashMap, hashmap2: HashMap): boolean => {
-        for (const [key1, value1] of Object.entries(hashmap1)) {
-                let isFound = false;
-                for (const [key2, value2] of Object.entries(hashmap2)) {
-                        if (key1 === key2 && value1 === value2) {
-                                isFound = true;
+    
+const isAnagram = (dictionaryOne: Anagram, dictionaryTwo: Anagram): boolean => {
+        for (const [key1, value1] of Object.entries(dictionaryOne)) {
+                for (const [key2, value2] of Object.entries(dictionaryTwo)) {
+                        if( key1 === key2 && value1 === value2 ) {
+                                delete dictionaryTwo[key2];
+                                delete dictionaryOne[key1];
                                 break;
                         }
                 }
-                if (!isFound) {
-                        return false;
-                }
         }
-        return true;
+        return Object.keys(dictionaryOne).length === 0 && Object.keys(dictionaryTwo).length === 0;
 };
-
-const adjustCharacterOccurrences = (start: string, end: string, words: HashMap): HashMap => {
-        words[start] = words[start] - 1;
-        if (words[start] === 0) {
-                delete words[start];
-        }
-        end in words ? (words[end] = words[end] + 1) : (words[end] = 1);
-        return words;
+    
+const adjustCharacterOccurrences = (start: string, end: string, hashmap1: Anagram): Anagram => {
+        hashmap1[start] -= 1;
+        hashmap1[start] == 0 && delete hashmap1[start];
+        end in hashmap1 ? (hashmap1[end] += 1) : (hashmap1[end] = 1);
+        return hashmap1;
 };
-
-const countCharacterOccurrences = (word: string): HashMap => {
+    
+const countCharacterOccurrences = (word: string): Anagram => {
         let countOccurrences = {};
         for (let i = 0; i < word.length; i++) {
                 word[i] in countOccurrences ? (countOccurrences[word[i]] += 1) : (countOccurrences[word[i]] = 1);
         }
         return countOccurrences;
 };
+    
+const findAnagrams = (s: string, p: string): Array<number> => {
+        let leftPointer: number = 0;
+        let result: Array<number> = [];
+        let hashmap1: Anagram = {};
+        let hashmap2: Anagram = countCharacterOccurrences(p);
 
-var findAnagrams = function (s: string, p: string): Array<number> {
-        let anagrams: Array<number> = [];
-        let hashmap1: HashMap = {};
-        let hashmap2: HashMap = countCharacterOccurrences(p);
-        for (let i: number = 0; i <= s.length - p.length; i++) {
-                let substr: string = s.slice(i, p.length + i);
-                i === 0
-                        ? (hashmap1 = countCharacterOccurrences(substr))
-                        : (hashmap1 = adjustCharacterOccurrences(s[i - 1], substr[substr.length - 1], hashmap1));
-                if (isAnagram(hashmap1, hashmap2)) {
-                        anagrams.push(i);
-                }
+        while( (leftPointer + p.length) <= s.length ) {
+                let subString: string = s.slice(leftPointer, leftPointer + p.length);
+                leftPointer === 0 ? (hashmap1 = countCharacterOccurrences(subString)) : (hashmap1 = adjustCharacterOccurrences(s[leftPointer - 1], subString[subString.length - 1], hashmap1));
+                isAnagram( JSON.parse(JSON.stringify(hashmap1)), JSON.parse(JSON.stringify(hashmap2)) ) && result.push(leftPointer);
+                leftPointer++;
         }
-        return anagrams;
+
+        return result;
 };
