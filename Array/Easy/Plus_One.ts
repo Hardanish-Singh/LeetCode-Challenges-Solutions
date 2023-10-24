4b60292b0c2ad056e4bc828f1240416e0e6228a3
Hardanish Singh <hardanishsingh1@gmail.com>
2023-10-24 07:36:04 -0400

    Plus One

--- Array/Easy/Plus_One.ts
+++ Array/Easy/Plus_One.ts
@@ -1,6 +1,3 @@
 // Leetcode: https://leetcode.com/problems/plus-one/
 
-var plusOne = (digits: Array<number>): Array<number> => {
-        let data = (BigInt(digits.join("")) + 1n).toString();
-        return data.split("").map(Number);
-};
+var plusOne = (digits: Array<number>): Array<number> => String(BigInt(digits.join("")) + 1n).split("").map(Number);
