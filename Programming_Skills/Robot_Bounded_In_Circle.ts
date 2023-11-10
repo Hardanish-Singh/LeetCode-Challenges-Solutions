d8b85c05bec3030fca779630b6aad2279e7a18e8
Hardanish Singh <hardanishsingh1@gmail.com>
2023-11-10 07:07:25 -0500

    Robot Bounded In Circle

--- Programming_Skills/Robot_Bounded_In_Circle.ts
+++ Programming_Skills/Robot_Bounded_In_Circle.ts
@@ -1,8 +1,29 @@
+// Leetcode: https://leetcode.com/problems/robot-bounded-in-circle/description/
+
 const isRobotBounded = (directions: string): boolean => {
     let x = 0;
     let y = 0;
+    const turnDirections = {
+        N: {
+            L: "W",
+            R: "E",
+        },
+        E: {
+            L: "N",
+            R: "S",
+        },
+        S: {
+            L: "E",
+            R: "W",
+        },
+        W: {
+            L: "S",
+            R: "N",
+        },
+    };
     directions = directions.repeat(4); // repeat four times to make sure if a round exists
     let currentDirection = "N";
+
     for (const direction of directions) {
         if (direction === "G") {
             switch (currentDirection) {
@@ -22,23 +43,9 @@ const isRobotBounded = (directions: string): boolean => {
                     throw new Error("Direction not found");
             }
         } else {
-            currentDirection = turn(currentDirection, direction);
+            currentDirection = turnDirections[currentDirection][direction];
         }
     }
-    return x === 0 && y === 0;
-};
 
-const turn = (currentDirection: string, direction: string): string => {
-    switch (currentDirection) {
-        case "N":
-            return direction === "L" ? "W" : "E";
-        case "W":
-            return direction === "L" ? "S" : "N";
-        case "S":
-            return direction === "L" ? "E" : "W";
-        case "E":
-            return direction === "L" ? "N" : "S";
-        default:
-            throw new Error("Direction not found");
-    }
+    return x == 0 && y == 0;
 };
