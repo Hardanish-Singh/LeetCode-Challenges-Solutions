fc73a17383f95c0ac9df181aabef799c32037f87
Hardanish Singh <hardanishsingh1@gmail.com>
2023-11-11 11:36:23 -0500

    Sprial Matrix

--- Array/Medium/Spiral_Matrix/Spiral_Matrix.js
+++ Array/Medium/Spiral_Matrix/Spiral_Matrix.js
@@ -1,110 +1,74 @@
-/*
-        Given an m x n matrix, 
-                return all elements of the matrix in spiral order 
-                or 
-                return the array elements arranged from outermost elements to the middle element, traveling clockwise.
-        
-        Input: matrix = 
-                        [ 
-                                [1, 2, 3],
-                                [4, 5, 6],
-                                [7, 8, 9]
-                        ]
-        Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
+// Leetcode: https://leetcode.com/problems/spiral-matrix/
 
-        Input: matrix = 
-                        [
-                                [1,  2,  3,  4],
-                                [5,  6,  7,  8],
-                                [9, 10, 11, 12]
-                        ]
-        Output: [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
-
-        Constraints:
-                1) m == matrix.length
-                2) n == matrix[i].length
-                3) 1 <= m, n <= 10
-                4) -100 <= matrix[i][j] <= 100
-*/
-
-/*
+/**
  * @param {number[][]} matrix
  * @return {number[]}
-*/
+ */
 
-function moveRight( matrix, i, j, sprialOrder ) {
-        while( true ) {
-                j++;
-                if( j > matrix[i].length-1 || matrix[i][j] == '-' ) {
-                        j--;
-                        break;
-                }
-                sprialOrder.push( matrix[i][j] );
-                matrix[i][j] = '-';
+var spiralOrder = function (matrix) {
+    let resultArray = [];
+
+    const moveRight = () => {
+        if (matrix.length === 0) {
+            return;
         }
-        return [i, j];
-}
-
-function moveDown( matrix, i, j, sprialOrder ) {
-        while( true ) {
-                i++;
-                if( i > matrix.length-1 || matrix[i][j] == '-' ) {
-                        i--;
-                        break;
-                }
-                sprialOrder.push( matrix[i][j] );
-                matrix[i][j] = '-';
+        const row = 0;
+        for (let column = 0; column < matrix[row].length; column++) {
+            resultArray.push(matrix[row][column]);
         }
-        return [i, j];
-}
+        matrix.splice(row, 1);
+    };
 
-function moveLeft( matrix, i, j, sprialOrder ) {
-        while( true ) {
-                j--;
-                if( j < 0 || matrix[i][j] == '-' ) {
-                        j++;
-                        break;
-                }
-                sprialOrder.push( matrix[i][j] );
-                matrix[i][j] = '-';
+    const moveBottom = () => {
+        if (matrix.length === 0) {
+            return;
         }
-        return [i, j];
-}
-
-function moveUp( matrix, i, j, sprialOrder ) {
-        while( true ) {
-                i--;
-                if( i < 0 || matrix[i][j] == '-' ) {
-                        i++;
-                        break;
-                }
-                sprialOrder.push( matrix[i][j] );
-                matrix[i][j] = '-';
+        for (let row = 0; row < matrix.length; row++) {
+            let column = matrix[row].length - 1;
+            resultArray.push(matrix[row][column]);
         }
-        return [i, j];
-}
-
-var spiralOrder = function( matrix ) {
-        if( matrix[0].length == 0 ) {
-                return [];
+        for (let row = 0; row < matrix.length; row++) {
+            matrix[row].splice(-1, 1);
+            if (matrix[row].length === 0) {
+                matrix.splice(row, 1);
+                row--;
+            }
         }
-        let sprialOrder = [];
-        let i = 0;
-        let j = 0;
-        sprialOrder.push( matrix[i][j] );
-        matrix[i][j] = '-';
+    };
 
-        while( true ) {
-                [right_i, right_j] = moveRight( matrix, i, j, sprialOrder );
-                [down_i, down_j] = moveDown( matrix, right_i, right_j, sprialOrder );
-                [left_i, left_j] = moveLeft( matrix, down_i, down_j, sprialOrder );
-                [up_i, up_j] = moveUp( matrix, left_i, left_j, sprialOrder );
-                if(right_i === down_i && right_j === down_j ) {
-                        break;
-                }
-                i = up_i;
-                j = up_j;
+    const moveLeft = () => {
+        if (matrix.length === 0) {
+            return;
+        }
+        let last = matrix.length - 1;
+        for (let i = matrix[last].length - 1; i >= 0; i--) {
+            resultArray.push(matrix[last][i]);
+        }
+        matrix.splice(last, 1);
+    };
+
+    const moveUp = () => {
+        if (matrix.length === 0) {
+            return;
+        }
+        for (let i = matrix.length - 1; i >= 0; i--) {
+            resultArray.push(matrix[i][0]);
         }
 
-        return sprialOrder;
+        for (let i = matrix.length - 1; i >= 0; i--) {
+            matrix[i].splice(0, 1);
+            if (matrix[i].length === 0) {
+                matrix.splice(i, 1);
+            }
+        }
+    };
+
+    while (matrix.length > 0) {
+        moveRight();
+        moveBottom();
+        moveLeft();
+        moveUp();
+    }
+
+    return resultArray;
 };
