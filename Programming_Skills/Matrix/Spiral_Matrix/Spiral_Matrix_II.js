562309f2a9f75f75ef1b0dd3d5c87b4703e19306
Hardanish Singh <hardanishsingh1@gmail.com>
2023-11-11 17:12:46 -0500

    Sprial Matrix II

--- Programming_Skills/Matrix/Spiral_Matrix/Spiral_Matrix_II.js
+++ Programming_Skills/Matrix/Spiral_Matrix/Spiral_Matrix_II.js
@@ -1,8 +1,8 @@
 // Leetcode: https://leetcode.com/problems/spiral-matrix-ii/
 
 /**
- * @param { number } n
- * @return { number[][] }
+ * @param {number} n
+ * @return {number[][]}
  */
 
 var generateMatrix = function (n) {
