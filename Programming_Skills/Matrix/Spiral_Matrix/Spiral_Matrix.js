160d894e0ab8d7e8a49d232f0a3326854ae0f5e6
Hardanish Singh <hardanishsingh1@gmail.com>
2023-11-11 17:12:31 -0500

    Sprial Matrix I

--- Programming_Skills/Matrix/Spiral_Matrix/Spiral_Matrix.js
+++ Programming_Skills/Matrix/Spiral_Matrix/Spiral_Matrix.js
@@ -7,62 +7,38 @@
 
 var spiralOrder = function (matrix) {
     let result = [];
+    // row variables
+    let rowBegin = 0;
+    let rowEnd = matrix.length - 1;
+    // column variables
+    let colBegin = 0;
+    let colEnd = matrix[0].length - 1;
 
-    const moveRight = () => {
-        if (matrix.length === 0) {
-            return;
+    while (rowBegin <= rowEnd && colBegin <= colEnd) {
+        // Move Right
+        for (let i = colBegin; i <= colEnd; i++) {
+            result.push(matrix[rowBegin][i]);
         }
-        const row = 0;
-        for (let column = 0; column < matrix[row].length; column++) {
-            result.push(matrix[row][column]);
+        // Move Bottom
+        rowBegin++;
+        for (let i = rowBegin; i <= rowEnd; i++) {
+            result.push(matrix[i][colEnd]);
         }
-        matrix.splice(row, 1);
-    };
-
-    const moveBottom = () => {
-        if (matrix.length === 0) {
-            return;
-        }
-        for (let row = 0; row < matrix.length; row++) {
-            let element = matrix[row].pop();
-            result.push(element);
-            if (matrix[row].length === 0) {
-                matrix.splice(row, 1);
-                row--;
+        // Move Left
+        colEnd--;
+        if (rowBegin <= rowEnd) {
+            for (let i = colEnd; i >= colBegin; i--) {
+                result.push(matrix[rowEnd][i]);
             }
         }
-    };
-
-    const moveLeft = () => {
-        if (matrix.length === 0) {
-            return;
-        }
-        let lastIndex = matrix.length - 1;
-        for (let i = matrix[lastIndex].length - 1; i >= 0; i--) {
-            result.push(matrix[lastIndex][i]);
-        }
-        matrix.splice(lastIndex, 1);
-    };
-
-    const moveUp = () => {
-        if (matrix.length === 0) {
-            return;
-        }
-        for (let i = matrix.length - 1; i >= 0; i--) {
-            let element = matrix[i].shift();
-            result.push(element);
-            if (matrix[i].length === 0) {
-                matrix.splice(i, 1);
+        // Move Top
+        rowEnd--;
+        if (colBegin <= colEnd) {
+            for (let i = rowEnd; i >= rowBegin; i--) {
+                result.push(matrix[i][colBegin]);
             }
         }
-    };
-
-    while (matrix.length > 0) {
-        moveRight();
-        moveBottom();
-        moveLeft();
-        moveUp();
+        colBegin++;
     }
-
     return result;
 };
