068421cea6879c6da88fe7bfc480c0674af04b81
Hardanish Singh <hardanishsingh1@gmail.com>
2023-11-11 16:52:11 -0500

    Sprial Matrix II

--- Array/Medium/Spiral_Matrix/Spiral_Matrix_II.js
+++ Array/Medium/Spiral_Matrix/Spiral_Matrix_II.js
@@ -1,111 +1,45 @@
-/*
-        Given a positive integer n, generate an n x n matrix filled with elements from 1 to n^2 in spiral order.
-        
-        Example 1:
-                Input: n = 3
-                Output: 
-                        [
-                                [1, 2, 3],
-                                [8, 9, 4],
-                                [7, 6 ,5]
-                        ]
-        Example 2:
-                Input: n = 1
-                Output: 
-                        [
-                                [ 1 ]
-                        ]
+// Leetcode: https://leetcode.com/problems/spiral-matrix-ii/
 
-        Constraints:
-                1) 1 <= n <= 20
-*/
-
-/*
+/**
  * @param { number } n
  * @return { number[][] }
-*/
+ */
 
-function moveRight( matrix, i, j, obj ) {
-        while( true ) {
-                j++;
-                if( j > matrix[i].length-1 || Number( matrix[i][j] ) ) {
-                        j--;
-                        break;
-                }
-                obj.c++;
-                matrix[i][j] = obj.c;
-        }
-        return [i, j];
-}
+var generateMatrix = function (n) {
+    let result = new Array(n).fill().map(() => new Array(n).fill(0));
+    // row variables
+    let rowBegin = 0;
+    let rowEnd = n - 1;
+    // column variables
+    let colBegin = 0;
+    let colEnd = n - 1;
 
-function moveDown( matrix, i, j, obj ) {
-        while( true ) {
-                i++;
-                if( i > matrix.length-1 || Number( matrix[i][j] ) ) {
-                        i--;
-                        break;
-                }
-                obj.c++;
-                matrix[i][j] = obj.c;
+    let num = 1;
+    while (rowBegin <= rowEnd && colBegin <= colEnd) {
+        for (let i = colBegin; i <= colEnd; i++) {
+            result[rowBegin][i] = num;
+            num++;
         }
-        return [i, j];
-}
-
-function moveLeft( matrix, i, j, obj ) {
-        while( true ) {
-                j--;
-                if( j < 0 || Number( matrix[i][j] ) ) {
-                        j++;
-                        break;
-                }
-                obj.c++;
-                matrix[i][j] = obj.c;
+        rowBegin++;
+        for (let i = rowBegin; i <= rowEnd; i++) {
+            result[i][colEnd] = num;
+            num++;
         }
-        return [i, j];
-}
-
-function moveUp( matrix, i, j, obj ) {
-        while( true ) {
-                i--;
-                if( i < 0 || Number( matrix[i][j] ) ) {
-                        i++;
-                        break;
-                }
-                obj.c++;
-                matrix[i][j] = obj.c;
+        colEnd--;
+        if (rowBegin <= rowEnd) {
+            for (let i = colEnd; i >= colBegin; i--) {
+                result[rowEnd][i] = num;
+                num++;
+            }
         }
-        return [i, j];
-}
-
-
-var generateMatrix = function( n ) {
-        const matrix = Array( n );
-        for( let i = 0; i < n; i++ ) {
-                matrix[i] = Array( n ).fill('-');
+        rowEnd--;
+        if (colBegin <= colEnd) {
+            for (let i = rowEnd; i >= rowBegin; i--) {
+                result[i][colBegin] = num;
+                num++;
+            }
         }
-        if( matrix[0].length == 0 ) {
-                return [];
-        }
-        let obj = {
-                c : 1
-        }
-        let i = 0;
-        let j = 0;
-        matrix[i][j] = obj.c;
-
-        while( true ) {
-                [right_i, right_j] = moveRight( matrix, i, j, obj );
-                [down_i, down_j] = moveDown( matrix, right_i, right_j, obj );
-                [left_i, left_j] = moveLeft( matrix, down_i, down_j, obj );
-                [up_i, up_j] = moveUp( matrix, left_i, left_j, obj );
-                if(right_i === down_i && right_j === down_j ) {
-                        break;
-                }
-                i = up_i;
-                j = up_j;
-        }
-
-        return matrix;
+        colBegin++;
+    }
+    return result;
 };
-
-console.log( generateMatrix( 3 ) );
