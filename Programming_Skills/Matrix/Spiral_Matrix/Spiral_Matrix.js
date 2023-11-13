ae2c2bc470e5ea4626e3da9ddee24499285bb8bf
Hardanish Singh <hardanishsingh1@gmail.com>
2023-11-13 14:17:36 -0500

    Spiral Matrix I

--- Programming_Skills/Matrix/Spiral_Matrix/Spiral_Matrix.js
+++ Programming_Skills/Matrix/Spiral_Matrix/Spiral_Matrix.js
@@ -5,44 +5,44 @@
  * @return {number[]}
  */
 
-var spiralOrder = function (matrix) {
+const spiralOrder = (matrix) => {
     let result = [];
     // row variables
-    let rowBegin = 0;
-    let rowEnd = matrix.length - 1;
+    let rowStart = 0;
+    let rowEnd = matrix[0].length - 1;
     // column variables
-    let colBegin = 0;
-    let colEnd = matrix[0].length - 1;
+    let columnStart = 0;
+    let columnEnd = matrix.length - 1;
 
-    while (rowBegin <= rowEnd && colBegin <= colEnd) {
+    while (rowStart <= rowEnd && columnStart <= columnEnd) {
         // Move Right
-        if (rowBegin <= rowEnd) {
-            for (let i = colBegin; i <= colEnd; i++) {
-                result.push(matrix[rowBegin][i]);
+        if (columnStart <= columnEnd) {
+            for (let i = rowStart; i <= rowEnd; i++) {
+                result.push(matrix[columnStart][i]);
             }
         }
         // Move Bottom
-        rowBegin++;
-        if (colBegin <= colEnd) {
-            for (let i = rowBegin; i <= rowEnd; i++) {
-                result.push(matrix[i][colEnd]);
+        columnStart++;
+        if (rowStart <= rowEnd) {
+            for (let i = columnStart; i <= columnEnd; i++) {
+                result.push(matrix[i][rowEnd]);
             }
         }
         // Move Left
-        colEnd--;
-        if (rowBegin <= rowEnd) {
-            for (let i = colEnd; i >= colBegin; i--) {
-                result.push(matrix[rowEnd][i]);
+        rowEnd--;
+        if (columnStart <= columnEnd) {
+            for (let i = rowEnd; i >= rowStart; i--) {
+                result.push(matrix[columnEnd][i]);
             }
         }
         // Move Top
-        rowEnd--;
-        if (colBegin <= colEnd) {
-            for (let i = rowEnd; i >= rowBegin; i--) {
-                result.push(matrix[i][colBegin]);
+        columnEnd--;
+        if (rowStart <= rowEnd) {
+            for (let i = columnEnd; i >= columnStart; i--) {
+                result.push(matrix[i][rowStart]);
             }
         }
-        colBegin++;
+        rowStart++;
     }
     return result;
 };
