0d0594915732f809222fb0525f9b71a15a4c4aeb
Hardanish Singh <hardanishsingh1@gmail.com>
2023-11-17 13:24:43 -0500

    Spiral Matrix I

--- Programming_Skills/Matrix/Spiral_Matrix/Spiral_Matrix.js
+++ Programming_Skills/Matrix/Spiral_Matrix/Spiral_Matrix.js
@@ -6,7 +6,7 @@
  */
 
 const spiralOrder = (matrix) => {
-    let result = [];
+    let spiralOrderMatrix = [];
     // row variables
     let rowStart = 0;
     let rowEnd = matrix.length - 1;
@@ -18,32 +18,32 @@ const spiralOrder = (matrix) => {
         // Move Right
         if (rowStart <= rowEnd) {
             for (let column = columnStart; column <= columnEnd; column++) {
-                result.push(matrix[rowStart][column]);
+                spiralOrderMatrix.push(matrix[rowStart][column]);
             }
         }
         // Move Bottom
         rowStart++;
         if (columnStart <= columnEnd) {
             for (let row = rowStart; row <= rowEnd; row++) {
-                result.push(matrix[row][columnEnd]);
+                spiralOrderMatrix.push(matrix[row][columnEnd]);
             }
         }
         // Move Left
         columnEnd--;
         if (rowStart <= rowEnd) {
             for (let column = columnEnd; column >= columnStart; column--) {
-                result.push(matrix[rowEnd][column]);
+                spiralOrderMatrix.push(matrix[rowEnd][column]);
             }
         }
         // Move Top
         rowEnd--;
         if (columnStart <= columnEnd) {
             for (let row = rowEnd; row >= rowStart; row--) {
-                result.push(matrix[row][columnStart]);
+                spiralOrderMatrix.push(matrix[row][columnStart]);
             }
         }
         columnStart++;
     }
 
-    return result;
+    return spiralOrderMatrix;
 };
