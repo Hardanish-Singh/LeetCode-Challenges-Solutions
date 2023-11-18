e017feaa90b617394e1fb88ad7e2ac224a39e79a
Hardanish Singh <hardanishsingh1@gmail.com>
2023-11-18 09:26:29 -0500

    Find The Index Of The First Occurence In A String

---
+++ Programming_Skills/Basic_Implementation/Find_The_Index_Of_The_First_Occurrence_In_A_String.js
@@ -0,0 +1,9 @@
+// Leetcode: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
+/**
+ * @param { string } haystack
+ * @param { string } needle
+ * @return { number }
+ */
+
+const strStr = (haystack, needle) => haystack.indexOf(needle);
