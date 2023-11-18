18cad0ce34d9a0bdb893121479a099ada53736eb
Hardanish Singh <hardanishsingh1@gmail.com>
2023-11-18 07:30:15 -0500

    Merge Strings Alternatively

--- Programming_Skills/Basic_Implementation/Merge_Strings_Alternatively.js
+++ Programming_Skills/Basic_Implementation/Merge_Strings_Alternatively.js
@@ -9,7 +9,7 @@
 const mergeAlternately = (word1, word2) => {
     let result = "";
     for (let i = 0; i < word1.length; i++) {
-        result += word1[i] + (word2[i] ? word2[i] : "");
+        result += word1[i] + (word2[i] || "");
     }
     let maxLength = word2.length - word1.length;
     if (maxLength > 0) {
