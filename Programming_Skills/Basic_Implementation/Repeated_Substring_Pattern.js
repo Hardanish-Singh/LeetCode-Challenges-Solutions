7ac2910a46548511fa50f92e8719a2060de12616
Hardanish Singh <hardanishsingh1@gmail.com>
2023-11-19 08:10:39 -0500

    Repeated Substring Pattern

--- Programming_Skills/Basic_Implementation/Repeated_Substring_Pattern.js
+++ Programming_Skills/Basic_Implementation/Repeated_Substring_Pattern.js
@@ -1,9 +1,11 @@
+// Leetcode: https://leetcode.com/problems/repeated-substring-pattern/
+
 /**
  * @param {string} s
  * @return {boolean}
  */
 
-var repeatedSubstringPattern = function (s) {
+const repeatedSubstringPattern = (s) => {
     for (let i = 0; i < s.length - 1; i++) {
         let subString = s.slice(0, i + 1);
         if (s.split(subString).join("").length === 0) {
