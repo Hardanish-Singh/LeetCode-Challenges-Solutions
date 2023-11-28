15437789ae211dd5817da3f0027475bee0cab30e
Hardanish Singh <hardanishsingh1@gmail.com>
2023-11-27 19:22:47 -0500

    Set Matix Zeros

--- Array/Medium/Set_Matrix_Zeroes.js
+++ Array/Medium/Set_Matrix_Zeroes.js
@@ -1,7 +1,7 @@
 // Leetcode: https://leetcode.com/problems/set-matrix-zeroes/
 
 /**
- * @param {number[][] } matrix
+ * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 
