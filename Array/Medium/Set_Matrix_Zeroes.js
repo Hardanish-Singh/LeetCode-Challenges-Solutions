3fbad61c430669927aa83be0e9c50493161e215f
Hardanish Singh <hardanishsingh1@gmail.com>
2023-11-29 08:15:08 -0500

    Set Matix Zeros

--- Array/Medium/Set_Matrix_Zeroes.js
+++ Array/Medium/Set_Matrix_Zeroes.js
@@ -6,35 +6,35 @@
  */
 
 const setZeroes = (matrix) => {
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
-            if (matrix[i][j] == 0) {
-                matrix[i][j] = "X";
-                let row = 0;
-                let column = 0;
-                // Change the entire column to X if value != 0
-                while (column <= matrix[i].length - 1) {
-                    if (matrix[i][column] != 0) {
-                        matrix[i][column] = "X";
-                    }
-                    column++;
+        for (let i = 0; i < matrix.length; i++) {
+                for (let j = 0; j < matrix[i].length; j++) {
+                        if (matrix[i][j] == 0) {
+                                matrix[i][j] = "X";
+                                let row = 0;
+                                let column = 0;
+                                // Change the entire column to X if value != 0
+                                while (column <= matrix[i].length - 1) {
+                                        if (matrix[i][column] != 0) {
+                                                matrix[i][column] = "X";
+                                        }
+                                        column++;
+                                }
+                                // Change the entire row to X if value != 0
+                                while (row <= matrix.length - 1) {
+                                        if (matrix[row][j] != 0) {
+                                                matrix[row][j] = "X";
+                                        }
+                                        row++;
+                                }
+                        }
                 }
-                // Change the entire row to X if value != 0
-                while (row <= matrix.length - 1) {
-                    if (matrix[row][j] != 0) {
-                        matrix[row][j] = "X";
-                    }
-                    row++;
-                }
-            }
         }
-    }
 
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
-            if (matrix[i][j] === "X") {
-                matrix[i][j] = 0;
-            }
+        for (let i = 0; i < matrix.length; i++) {
+                for (let j = 0; j < matrix[i].length; j++) {
+                        if (matrix[i][j] === "X") {
+                                matrix[i][j] = 0;
+                        }
+                }
         }
-    }
 };
