cc17d892766a0564188b7b02f806993fdb62ae15
Hardanish Singh <hardanishsingh1@gmail.com>
2023-11-29 08:26:58 -0500

    Average Salary Excluding the Minimum and Maximum Salary

---
+++ Programming_Skills/Math/Average_Salary_Excluding_the_Minimum_and_Maximum_Salary.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} salary
+ * @return {number}
+ */
+
+const average = (salary) => {
+    const min = Math.min.apply(this, salary);
+    const max = Math.max.apply(this, salary);
+    const sum = salary.reduce((acc, val) => acc + val, 0);
+    return (sum - max - min) / (salary.length - 2);
+}
