7e24069835dddba479f7e6d8ad565cfedce0b988
Hardanish Singh <hardanishsingh1@gmail.com>
2023-12-01 13:04:58 -0500

    Multiply Strings

--- Programming_Skills/Math/Multiply_Strings.js
+++
@@ -1,9 +0,0 @@
-// Leetcode: https://leetcode.com/problems/multiply-strings/
-
-/**
- * @param { string } num1
- * @param { string } num2
- * @return { string }
- */
-
-var multiply = (num1, num2) => (BigInt(num1) * BigInt(num2)).toString();
---
+++ Programming_Skills/Math/Multiply_Strings.ts
@@ -0,0 +1,3 @@
+// Leetcode: https://leetcode.com/problems/multiply-strings/
+
+const multiply = (num1: string, num2: string) => (BigInt(num1) * BigInt(num2)).toString();
