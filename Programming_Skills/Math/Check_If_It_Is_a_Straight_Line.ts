551df516193e0a1e24cb83612b9ed3792db5d107
Hardanish Singh <hardanishsingh1@gmail.com>
2023-12-01 12:37:31 -0500

    Check If It is a Straight Line

---
+++ Programming_Skills/Math/Check_If_It_Is_a_Straight_Line.ts
@@ -0,0 +1,14 @@
+function checkStraightLine(coordinates: number[][]): boolean {
+    const [x1, y1] = coordinates[0];
+    const [x2, y2] = coordinates[1];
+
+    for (let i = 2; i < coordinates.length; i++) {
+        const [x, y] = coordinates[i];
+        // Calculate the cross product of vectors (x2-x1, y2-y1) and (x-x1, y-y1)
+        if ((x2 - x1) * (y - y1) !== (x - x1) * (y2 - y1)) {
+            return false; // Points are not collinear
+        }
+    }
+
+    return true; // All points are collinear
+}
