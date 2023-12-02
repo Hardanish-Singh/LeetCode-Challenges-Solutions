d8cfc9932b03543e184fcd576d5423af754b8c6e
Hardanish Singh <hardanishsingh1@gmail.com>
2023-12-02 09:27:10 -0500

    Merge Two Sorted Linked List

--- Programming_Skills/Linked_List/Merge_Two_Sorted_Linked_List.js
+++ Programming_Skills/Linked_List/Merge_Two_Sorted_Linked_List.js
@@ -23,6 +23,16 @@ const mergeTwoLists = (list1, list2) => {
 	let tail = null;
 	let node = null;
 
+	const parseList1 = () => {
+		node = new ListNode(list1.val);
+		list1 = list1.next;
+	}
+
+	const parseList2 = () => {
+		node = new ListNode(list2.val);
+		list2 = list2.next;
+	}
+
 	const addToLinkedList = () => {
 		if (head == null) {
 			head = tail = node;
@@ -34,29 +44,15 @@ const mergeTwoLists = (list1, list2) => {
 
 	// Loop until any of the list becomes null
 	while (list1 && list2) {
-		if (list1.val < list2.val) {
-			node = new ListNode(list1.val);
-			list1 = list1.next;
-		} else {
-			node = new ListNode(list2.val);
-			list2 = list2.next;
-		}
+		list1.val < list2.val ? parseList1() : parseList2();
 		addToLinkedList();
 	}
 
 	// Add all the nodes of list1, if remaining
-	while (list1) {
-		node = new ListNode(list1.val);
-		list1 = list1.next;
-		addToLinkedList();
-	}
+	while (list1) parseList1(), addToLinkedList();
 
 	// Add all the nodes of list2, if remaining
-	while (list2) {
-		node = new ListNode(list2.val);
-		list2 = list2.next;
-		addToLinkedList();
-	}
+	while (list2) parseList2(), addToLinkedList();
 
 	return head;
 };
