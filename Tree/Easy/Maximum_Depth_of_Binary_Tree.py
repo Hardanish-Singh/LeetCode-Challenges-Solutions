f5f3bf9dc0183b8410bec0127b1e98c566a28462
Hardanish Singh <hardanishsingh1@gmail.com>
2023-12-09 07:26:48 -0500

    Maximum Depth of Binary Tree

--- Tree/Easy/Maximum_Depth_of_Binary_Tree.py
+++ Tree/Easy/Maximum_Depth_of_Binary_Tree.py
@@ -1,7 +1,7 @@
 # Leetcode: https://leetcode.com/problems/maximum-depth-of-binary-tree/
 
 # Definition for a binary tree node.
-# class TreeNode( object ):
+# class TreeNode:
 #     def __init__( self, val = 0, left = None, right = None ):
 #         self.val = val
 #         self.left = left
@@ -16,9 +16,9 @@ class Solution:
         def maxDepth(self, root: TreeNode) -> int:                
                 if root is None:
                         return 0
-                left = self.maxDepth( root.left )
-                right = self.maxDepth( root.right )
-                return 1 + max( left, right )
+                left = self.maxDepth(root.left)
+                right = self.maxDepth(root.right)
+                return 1 + max(left, right)
 
 """
         SOLUTION 2: RECURSIVE DFS
@@ -28,9 +28,9 @@ class Solution:
         def maxDepth(self, root: TreeNode) -> int:
                 if root is None:
                         return 0
-                left = 1 + self.maxDepth( root.left )
-                right = 1 + self.maxDepth( root.right )
-                return max( left, right )
+                left = 1 + self.maxDepth(root.left)
+                right = 1 + self.maxDepth(root.right)
+                return max(left, right)
 
 """
         SOLUTION 3: ITERATIVE BFS / LEVEL ORDER TRAVERSAL
@@ -43,13 +43,8 @@ class Solution:
                 queue = [root]
                 result = []
 
-                while len(queue) > 0:
-                        temp = []
-                        # POP ALL ELEMENTS FROM QUEUE
-                        for item in queue:
-                                temp.append(item.val)
-                        result.append(temp)
-
+                while queue:
+                        result.append( item.val for item in queue ) # POP ALL ELEMENTS FROM QUEUE
                         n = len(queue)
                         for i in range(n):
                                 item = queue.pop(0)
