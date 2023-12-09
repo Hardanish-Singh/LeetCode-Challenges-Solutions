87783cfcbec62110d4b80206c5e23fae0adbf142
Hardanish Singh <hardanishsingh1@gmail.com>
2023-12-09 07:58:21 -0500

    Maximum Depth of N-ary Tree

--- Tree/Easy/Maximum_Depth_of_N-ary_Tree.py
+++ Tree/Easy/Maximum_Depth_of_N-ary_Tree.py
@@ -17,7 +17,6 @@ class Solution:
                         result.append( item.val for item in queue ) # POP ALL ELEMENTS FROM QUEUE
                         n = len(queue)
                         for i in range(n):
-                                item = queue.pop(0)
-                                queue.extend(child for child in item.children)
+                                queue.extend(child for child in queue.pop(0).children)
 
                 return len(result)
