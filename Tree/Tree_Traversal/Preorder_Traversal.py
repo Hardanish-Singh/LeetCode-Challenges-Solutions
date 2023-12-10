cc0e2e97c65416f568a5bbc748602e8dc42a9b27
Hardanish Singh <hardanishsingh1@gmail.com>
2023-12-10 13:35:53 -0500

    Preorder Traversal

--- Tree/Tree_Traversal/Preorder_Traversal.py
+++ Tree/Tree_Traversal/Preorder_Traversal.py
@@ -3,7 +3,7 @@
 """
         # Definition for a binary tree node.
         class TreeNode:
-                def __init__( self, val = 0, left = None, right = None ):
+                def __init__(self, val = 0, left = None, right = None):
                         self.val = val
                         self.left = left
                         self.right = right
@@ -33,14 +33,14 @@ class Solution:
 def preorderRecursiveTraversal(root: 'TreeNode', preOrderList: List[int]) -> List[int]:
                 if root is None:
                         return None
-                preOrderList.append( root.val )
-                preorderRecursiveTraversal( root.left, preOrderList )
-                preorderRecursiveTraversal( root.right, preOrderList )
+                preOrderList.append(root.val)
+                preorderRecursiveTraversal(root.left, preOrderList)
+                preorderRecursiveTraversal(root.right, preOrderList)
                 
 class Solution:
         def preorderTraversal(self, root: 'TreeNode') -> List[int]:
                 preOrderList = []
-                preorderRecursiveTraversal( root, preOrderList )
+                preorderRecursiveTraversal(root, preOrderList)
                 return preOrderList
 
 """
@@ -52,15 +52,14 @@ class Solution:
                 if root is None:
                         return []
                 preOrderList = []
-                stack = [ root ]
+                stack = [root]
                 
-                while len( stack ) > 0:
+                while len(stack) > 0:
                         currentNode = stack.pop()
-                        preOrderList.append( currentNode.val )
-                        
+                        preOrderList.append(currentNode.val)
                         if currentNode.right:
-                                stack.append( currentNode.right )
+                                stack.append(currentNode.right)
                         if currentNode.left:
-                                stack.append( currentNode.left )
+                                stack.append(currentNode.left)
                 
                 return preOrderList
