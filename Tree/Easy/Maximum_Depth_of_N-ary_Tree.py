eacb7520dda8009ceb080104779cc0a2ac53ff84
Hardanish Singh <hardanishsingh1@gmail.com>
2023-12-10 12:40:49 -0500

    Maximum Depth of N-ary Tree

--- Tree/Easy/Maximum_Depth_of_N-ary_Tree.py
+++ Tree/Easy/Maximum_Depth_of_N-ary_Tree.py
@@ -1,10 +1,12 @@
 # Leetcode: https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
 
-# Definition for a Node.
-# class Node:
-#         def __init__( self, val = None, children = None):
-#                 self.val = val
-#                 self.children = children
+"""
+        # Definition for a Node.
+        class Node:
+                def __init__(self, val = None, children = None):
+                        self.val = val
+                        self.children = children
+"""
 
 class Solution:
         def maxDepth( self, root: 'Node' ) -> int:
@@ -14,7 +16,7 @@ class Solution:
                 result = []
 
                 while queue:
-                        result.append( item.val for item in queue ) # POP ALL ELEMENTS FROM QUEUE
+                        result.append(item.val for item in queue) # POP ALL ELEMENTS FROM QUEUE
                         n = len(queue)
                         for i in range(n):
                                 queue.extend(child for child in queue.pop(0).children)
