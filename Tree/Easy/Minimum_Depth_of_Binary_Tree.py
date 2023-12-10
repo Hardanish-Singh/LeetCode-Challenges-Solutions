4d5e3cfa775d0308e8ca6ed9ba587246d63b0eba
Hardanish Singh <hardanishsingh1@gmail.com>
2023-12-10 12:39:49 -0500

    Minimum Depth of Binary Tree

--- Tree/Easy/Minimum_Depth_of_Binary_Tree.py
+++ Tree/Easy/Minimum_Depth_of_Binary_Tree.py
@@ -1,11 +1,13 @@
 # Leetcode: https://leetcode.com/problems/minimum-depth-of-binary-tree/
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__( self, val = 0, left = None, right = None ):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+"""
+        # Definition for a binary tree node.
+        class TreeNode:
+                def __init__(self, val = 0, left = None, right = None):
+                        self.val = val
+                        self.left = left
+                        self.right = right
+"""
 
 
 """
@@ -64,7 +66,7 @@ class Solution:
                 count = 1
 
                 while queue:
-                        result.append( item.val for item in queue ) # POP ALL ELEMENTS FROM QUEUE
+                        result.append(item.val for item in queue) # POP ALL ELEMENTS FROM QUEUE
                         n = len(queue)
                         for i in range(n):
                                 item = queue.pop(0)
