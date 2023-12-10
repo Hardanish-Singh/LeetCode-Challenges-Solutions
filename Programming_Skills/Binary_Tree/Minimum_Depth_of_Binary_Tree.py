9637c7a567d19f7c26bb607c9a3f4cb1cbefd797
Hardanish Singh <hardanishsingh1@gmail.com>
2023-12-10 12:38:44 -0500

    Minimum Depth of Binary Tree

--- Programming_Skills/Binary_Tree/Minimum_Depth_of_Binary_Tree.py
+++ Programming_Skills/Binary_Tree/Minimum_Depth_of_Binary_Tree.py
@@ -1,11 +1,13 @@
 # Leetcode: https://leetcode.com/problems/minimum-depth-of-binary-tree/
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__( self, val = 0, left = None, right = None ):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+"""
+        # Definition for a binary tree node.
+        class TreeNode:
+                def __init__(self, val = 0, left = None, right = None):
+                        self.val = val
+                        self.left = left
+                        self.right = right
+"""
 
 
 """
