7d65e7f6e6ff2d82a449816154d2e0ded22cdbf1
Hardanish Singh <hardanishsingh1@gmail.com>
2023-12-12 20:36:32 -0500

    Invert Binary Tree

--- Tree/Easy/Invert_Binary_Tree.py
+++ Tree/Easy/Invert_Binary_Tree.py
@@ -1,55 +1,46 @@
 # Leetcode: https://leetcode.com/problems/invert-binary-tree/
 
-# Definition for a binary tree node.
-# class TreeNode( object):
-#     def __init__( self, val = 0, left = None, right = None ):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+"""
+        # Definition for a binary tree node.
+        class TreeNode(object):
+                def __init__(self, val = 0, left = None, right = None):
+                        self.val = val
+                        self.left = left
+                        self.right = right
+"""
 
 """
         SOLUTION 1: RECURSIVE
 """
 
-class Solution( object ):
-        def invertTree( self, root ):
-                """
-                :type root: TreeNode
-                :rtype: TreeNode
-                """
+class Solution:
+        def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
                 if root is None:
                         return None
-                
                 root.right, root.left = root.left, root.right
-                
-                self.invertTree( root.left )
-                self.invertTree( root.right )
-                
+                self.invertTree(root.left)
+                self.invertTree(root.right)
                 return root
 
 """
         SOLUTION 2: ITERATIVE
 """
 
-class Solution( object ):
-        def invertTree( self, root ):
-                """
-                :type root: TreeNode
-                :rtype: TreeNode
-                """
+class Solution:
+        def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
                 if root is None:
                         return None
-                queue = [ root ]
+                queue = [root]
                 
                 # POP ALL ELEMENTS FROM QUEUE
-                while len( queue ) > 0:
-                        n = len( queue )
-                        for i in range( n ):
+                while queue:
+                        n = len(queue)
+                        for i in range(n):
                                 node = queue.pop(0)
                                 node.right, node.left = node.left, node.right
                                 if node.left:
-                                        queue.append( node.left )
+                                        queue.append(node.left)
                                 if node.right:
-                                        queue.append( node.right )
+                                        queue.append(node.right)
                 
                 return root
