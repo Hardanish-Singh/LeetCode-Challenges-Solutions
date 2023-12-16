761f7c41c294104d376db142cfefc248856e862d
Hardanish Singh <hardanishsingh1@gmail.com>
2023-12-16 08:57:39 -0500

    Binary Tree Vertical Order Traversal

--- Tree/Medium/Binary_Tree_Vertical_Order_Traversal.py
+++ Tree/Medium/Binary_Tree_Vertical_Order_Traversal.py
@@ -1,38 +1,27 @@
 # Leetcode: https://leetcode.com/problems/binary-tree-vertical-order-traversal/
 
-# Definition for a binary tree node.
-# class TreeNode( object ):
-#     def __init__( self, val = 0, left = None, right = None ):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+"""
+        # Definition for a binary tree node.
+        class TreeNode:
+                def __init__(self, val = 0, left = None, right = None):
+                        self.val = val
+                        self.left = left
+                        self.right = right
+"""
 
-class Solution( object ):
-        def verticalOrder( self, root ):
-                """
-                :type root: TreeNode
-                :rtype: List[List[int]]
-                """
-                dictionary = {
-                        
-                }
-                queue = [ [ root, 0 ] ]
+class Solution:
+        def verticalOrder(self, root: TreeNode) -> List[List[int]]:
+                dictionary = {}
+                queue = [[root, 0]]
 
                 while queue:
-                        node, order = queue.pop( 0 )
-                        if node is None:
-                                continue
-                        
-                        if order in dictionary:
-                                dictionary[ order ].append( node.val )
-                        else:
-                                dictionary[ order ] = [ node.val ]
-                        
-                        queue.append( [ node.left, order - 1 ] )
-                        queue.append( [ node.right, order + 1 ] )
+                        node, order = queue.pop(0)
+                        if node is not None:
+                                if order in dictionary:
+                                        dictionary[order].append(node.val)
+                                else:
+                                        dictionary[order] = [node.val]
+                                queue.append([node.left, order - 1])
+                                queue.append([node.right, order + 1])
                 
-                
-                verticalOrderTraversal = [ ]
-                for key in sorted( dictionary ):
-                        verticalOrderTraversal.append( dictionary [ key ] )
-                return verticalOrderTraversal
+                return [dictionary[key] for key in sorted(dictionary.keys())]
