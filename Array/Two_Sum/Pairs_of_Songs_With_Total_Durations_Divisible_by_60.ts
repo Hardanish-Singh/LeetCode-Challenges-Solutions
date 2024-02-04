919147694f692cfd4e56415c93572e7e6d65546f
Hardanish Singh <hardanishsingh1@gmail.com>
2024-02-04 08:53:45 -0500

    Pairs of Songs With Total Durations Divisible by 60

--- Array/Two_Sum/Pairs_of_Songs_With_Total_Durations_Divisible_by_60.ts
+++ Array/Two_Sum/Pairs_of_Songs_With_Total_Durations_Divisible_by_60.ts
@@ -1,18 +1,19 @@
 // Leetcode: https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
 
-/**
- * @param { number[] }time
- * @returns { number }
- */
+/*
+        SOLUTION 1: BRUTE FORCE
+                Time Complexity: O( n ^ 2 )
+                Space Complexity: O( 1 )
+*/
 
-function numPairsDivisibleBy60(time: number[]): number {
-        let count: number = 0;
-        for (let i: number = 0; i < time.length; i++) {
-                for (let j: number = i + 1; j < time.length; j++) {
-                        if ((time[i] + time[j]) % 60 === 0) {
-                                count++;
-                        }
-                }
+const numPairsDivisibleBy60 = (time: number[]): number => {
+    let count: number = 0;
+    for (let i: number = 0; i < time.length; i++) {
+        for (let j: number = i + 1; j < time.length; j++) {
+            if ((time[i] + time[j]) % 60 === 0) {
+                count++;
+            }
         }
-        return count;
-}
+    }
+    return count;
+};
