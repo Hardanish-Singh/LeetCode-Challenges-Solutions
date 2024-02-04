076bbd8740cfcb3350e81d2a69b6cbeb4122829c
Hardanish Singh <hardanishsingh1@gmail.com>
2024-02-04 08:08:28 -0500

    Maximum Number of K Sum Pairs

--- Array/Two_Sum/Max_Number_of_K_Sum_Pairs.ts
+++ Array/Two_Sum/Max_Number_of_K_Sum_Pairs.ts
@@ -20,3 +20,31 @@ var maxOperations = (nums: number[], target: number): number => {
     }
     return operations;
 };
+
+/*
+        SOLUTION 1: TWO POINTER APPROACH USING SORT
+                Time Complexity: O( nlogn )
+                Space Complexity: O( n ) or O( logn ) The space complexity of the sorting algorithm depends on the implementation of each program language
+*/
+
+var maxOperations = (nums: number[], target: number): number => {
+    nums.sort((a, b) => a - b);
+    let leftPointer = 0;
+    let rightPointer = nums.length - 1;
+    let operations = 0;
+
+    while (leftPointer < rightPointer) {
+        const sum = nums[leftPointer] + nums[rightPointer];
+        if (sum < target) {
+            leftPointer++;
+        } else if (sum > target) {
+            rightPointer--;
+        } else {
+            leftPointer++;
+            rightPointer--;
+            operations++;
+        }
+    }
+
+    return operations;
+};
