299a6825fb35b3bb7e0fef20ddb1536052abedc0
Hardanish Singh <hardanishsingh1@gmail.com>
2024-02-06 20:39:56 -0500

    Pairs of Songs With Total Durations Divisible by 60

--- Array/Two_Sum/Pairs_of_Songs_With_Total_Durations_Divisible_by_60.ts
+++ Array/Two_Sum/Pairs_of_Songs_With_Total_Durations_Divisible_by_60.ts
@@ -6,7 +6,7 @@
                 Space Complexity: O( 1 )
 */
 
-const numPairsDivisibleBy60 = (time: number[]): number => {
+var numPairsDivisibleBy60 = (time: number[]): number => {
     let count: number = 0;
     for (let i: number = 0; i < time.length; i++) {
         for (let j: number = i + 1; j < time.length; j++) {
@@ -15,5 +15,26 @@ const numPairsDivisibleBy60 = (time: number[]): number => {
             }
         }
     }
+    return count;
+};
+
+var numPairsDivisibleBy60 = (times: number[]): number => {
+    const numbers = [
+        60, 120, 180, 240, 300, 360, 420, 480, 540, 600, 660, 720, 780, 840,
+        900, 960,
+    ];
+    let count = 0;
+    let map = new Map<number, number>();
+
+    for (const time of times) {
+        for (const number of numbers) {
+            let compliment = number - time;
+            if (map.get(compliment)) {
+                count += map.get(compliment) as number;
+            }
+        }
+        map.set(time, (map.get(time) ?? 0) + 1);
+    }
+
     return count;
 };
