26d916133e2d3359d8274df988cfccb895b891c4
Hardanish Singh <hardanish.singh@rangle.io>
2024-02-14 17:17:02 -0500

    Three Sum Smaller

--- Array/Three_Sum/Three_Sum_Smaller.ts
+++ Array/Three_Sum/Three_Sum_Smaller.ts
@@ -1,6 +1,6 @@
 // Leetcode: https://leetcode.com/problems/3sum-smaller/
 
-var threeSumSmaller = function (nums: number[], target: number): number {
+var threeSumSmaller = (nums: number[], target: number): number => {
     nums.sort((a, b) => a - b);
     let count: number = 0;
 
@@ -25,6 +25,29 @@ var threeSumSmaller = function (nums: number[], target: number): number {
                     }
                     end--;
                 }
+                leftPointer++;
+            } else {
+                rightPointer--;
+            }
+        }
+    }
+
+    return count;
+};
+
+var threeSumSmaller = (nums: number[], target: number): number => {
+    nums.sort((a, b) => a - b);
+    let count: number = 0;
+
+    for (let i: number = 0; i < nums.length - 1; i++) {
+        let leftPointer: number = i + 1;
+        let rightPointer: number = nums.length - 1;
+
+        while (leftPointer < rightPointer) {
+            const sum = nums[leftPointer] + nums[rightPointer] + nums[i];
+            if (sum < target) {
+                // rightPointer - leftPointer: represents number of possible pairs, that satisfy the condition sum < target
+                count += rightPointer - leftPointer;
                 leftPointer++;
             } else {
                 rightPointer--;
