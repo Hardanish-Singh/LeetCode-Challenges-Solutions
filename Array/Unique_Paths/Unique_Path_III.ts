ed47129e48b259e1a180b94cc4108417fa676845
Hardanish Singh <hardanish.singh@rangle.io>
2024-02-19 09:43:45 -0500

    Unique Paths III

--- Array/Unique_Paths/Unique_Path_III.ts
+++ Array/Unique_Paths/Unique_Path_III.ts
@@ -1,135 +1,44 @@
-type Path = [number, number, string[], number][];
+// Leetcode: https://leetcode.com/problems/unique-paths-iii/
 
-/**
- * @param { number } i
- * @param { number } j
- * @return { string }
- */
+const uniquePathsIII = (grid: number[][]): number => {
+    let nonObstacleSquares = 0;
+    let startRow = 0;
+    let startCol = 0;
+    let result = 0;
 
-function move_left(i: number, j: number): string {
-        j = j - 1;
-        return j >= 0 ? i + "," + j : "";
-}
-
-/**
- * @param { number } i
- * @param { number } j
- * @return { string }
- */
-
-function move_top(i: number, j: number): string {
-        i = i - 1;
-        return i >= 0 ? i + "," + j : "";
-}
-
-/**
- * @param { number[][] } grid
- * @param { number } i
- * @param { number } j
- * @return { string }
- */
-
-function move_right(grid: number[][], i: number, j: number): string {
-        j = j + 1;
-        return j <= grid[i].length - 1 ? i + "," + j : "";
-}
-
-/**
- * @param { number[][] } grid
- * @param { number } i
- * @param { number } j
- * @return { string }
- */
-
-function move_down(grid: number[][], i: number, j: number): string {
-        i = i + 1;
-        return i <= grid.length - 1 ? i + "," + j : "";
-}
-
-/**
- * @param { number[][] } grid
- * @param { string[] } path
- * @param { number } count
- * @param { string } position
- * @param { number } zeros
- * @param { Path } stack
- * @param { number } result
- * @return { number }
- */
-
-function findPaths(
-        grid: number[][],
-        path: string[],
-        count: number,
-        position: string,
-        zeros: number,
-        stack: Path,
-        result: number
-): number {
-        let [ni, nj] = position.split(",");
-        if (grid[ni][nj] !== -1) {
-                if (grid[ni][nj] === 2) {
-                        if (count === zeros) {
-                                result++;
-                        }
-                } else {
-                        if (!path.includes(`${ni},${nj}`)) {
-                                let temp = JSON.parse(JSON.stringify(path));
-                                temp.push(`${ni},${nj}`);
-                                stack.push([+ni, +nj, temp, count + 1]);
-                        }
-                }
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[0].length; c++) {
+            if (grid[r][c] === 1) {
+                startRow = r;
+                startCol = c;
+            }
+            if (grid[r][c] !== -1) {
+                nonObstacleSquares++;
+            }
         }
-        return result;
-}
+    }
 
-/**
- * @param { number[][] } grid
- * @return { number }
- */
-
-var uniquePathsIII = function (grid: number[][]): number {
-        let zeros: number = 0;
-        let starting_row: number;
-        let staring_column: number;
-
-        for (let row: number = 0; row < grid.length; row++) {
-                for (let column: number = 0; column < grid[row].length; column++) {
-                        if (grid[row][column] === 1) {
-                                starting_row = row;
-                                staring_column = column;
-                        } else if (grid[row][column] === 0) {
-                                zeros++;
-                        }
-                }
+    const DFSWithBacktrack = (row: number, column: number, count: number) => {
+        if (row < 0 || column < 0 || row >= grid.length || column >= grid[0].length || grid[row][column] === -1) {
+            return;
+        }
+        if (grid[row][column] === 2) {
+            if (count === nonObstacleSquares) {
+                result++;
+            }
+            return;
         }
 
-        let stack: Path = [[starting_row, staring_column, [`${starting_row},${staring_column}`], 0]];
-        let result: number = 0;
+        const original = grid[row][column]; // Save visited cell
+        grid[row][column] = -1; // Simply replace "original cell" with a "-1" when visiting an empty cell and put back the "original cell" after returning. This will avoid revisiting the same cell again.
+        DFSWithBacktrack(row + 1, column, count + 1); // MoveDown
+        DFSWithBacktrack(row, column + 1, count + 1); // MoveRight
+        DFSWithBacktrack(row - 1, column, count + 1); // MoveTop
+        DFSWithBacktrack(row, column - 1, count + 1); // MoveBottom
+        // After backtrack free visited cell to Original Position for other possible paths
+        grid[row][column] = original;
+    };
 
-        while (stack.length > 0) {
-                let [row, column, path, count]: [number, number, string[], number] = stack.pop();
-
-                let leftPosition: string = move_left(row, column);
-                if (leftPosition) {
-                        result = findPaths(grid, path, count, leftPosition, zeros, stack, result);
-                }
-
-                let topPosition: string = move_top(row, column);
-                if (topPosition) {
-                        result = findPaths(grid, path, count, topPosition, zeros, stack, result);
-                }
-
-                let bottomPosition: string = move_down(grid, row, column);
-                if (bottomPosition) {
-                        result = findPaths(grid, path, count, bottomPosition, zeros, stack, result);
-                }
-
-                let rightPosition: string = move_right(grid, row, column);
-                if (rightPosition) {
-                        result = findPaths(grid, path, count, rightPosition, zeros, stack, result);
-                }
-        }
-
-        return result;
+    DFSWithBacktrack(startRow, startCol, 1);
+    return result;
 };
