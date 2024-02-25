43269b7ede0e57a1ba48eb66abdfe5578c452213
Hardanish Singh <hardanishsingh1@gmail.com>
2024-02-25 14:52:25 -0500

    Unique Path II

--- Array/Unique_Paths/Unique_Path_II.ts
+++ Array/Unique_Paths/Unique_Path_II.ts
@@ -6,7 +6,6 @@ const uniquePathsWithObstacles1 = (obstacleGrid: number[][], row: number = 0, co
     if (row >= obstacleGrid.length || column >= obstacleGrid[0].length || obstacleGrid[row][column] === 1) {
         return 0;
     }
-
     // Base Case / Reach Destination
     if (row === obstacleGrid.length - 1 && column === obstacleGrid[0].length - 1) {
         return 1;
@@ -22,25 +21,24 @@ const uniquePathsWithObstacles2 = (
     obstacleGrid: number[][],
     row: number = 0,
     column: number = 0,
-    cache: Record<string, number> = {}
+    map: Map<string, number> = new Map<string, number>()
 ): number => {
     // Out of Bounds/Grid Check or Obstacle Check
     if (row >= obstacleGrid.length || column >= obstacleGrid[0].length || obstacleGrid[row][column] === 1) {
         return 0;
     }
-
     // Base Case / Reach Destination
     if (row === obstacleGrid.length - 1 && column === obstacleGrid[0].length - 1) {
         return 1;
     }
-
+    // Memoization/Caching
     const key = `${row},${column}`;
-    if (key in cache) {
-        return cache[key];
+    if (map.has(key)) {
+        return map.get(key) as number;
     }
 
-    const moveDown = uniquePathsWithObstacles2(obstacleGrid, row + 1, column, cache);
-    const moveRight = uniquePathsWithObstacles2(obstacleGrid, row, column + 1, cache);
-    cache[key] = moveDown + moveRight;
-    return cache[key];
+    const moveDown = uniquePathsWithObstacles2(obstacleGrid, row + 1, column, map);
+    const moveRight = uniquePathsWithObstacles2(obstacleGrid, row, column + 1, map);
+    map.set(key, moveDown + moveRight);
+    return moveDown + moveRight;
 };
