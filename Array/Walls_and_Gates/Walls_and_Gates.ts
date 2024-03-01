11615a6c1d251fdc36423eb77248537a319d485f
Hardanish Singh <hardanishsingh1@gmail.com>
2024-03-01 16:40:22 -0500

    Walls and Gates

--- Array/Walls_and_Gates/Walls_and_Gates.ts
+++ Array/Walls_and_Gates/Walls_and_Gates.ts
@@ -9,13 +9,11 @@ const wallsAndGates = (rooms: number[][]): void => {
         if (row < 0 || row >= m || column < 0 || column >= n) {
             return;
         }
-
         // encountered an obstacle
         if (rooms[row][column] === -1) {
             return;
         }
-
-        // encountered another gate and count > 0 ( which means we have made atleast 1 move down, up, left and right )
+        // encountered another gate and count > 0 ( which means we have made atleast 1 move down, up, right and left )
         if (rooms[row][column] === 0 && count > 0) {
             return;
         }
@@ -24,16 +22,16 @@ const wallsAndGates = (rooms: number[][]): void => {
         rooms[row][column] = min;
 
         if (row + 1 < m && count + 1 < rooms[row + 1][column]) {
-            dfs(row + 1, column, count + 1);
+            dfs(row + 1, column, count + 1); // move down
         }
         if (row > 0 && count + 1 < rooms[row - 1][column]) {
-            dfs(row - 1, column, count + 1);
+            dfs(row - 1, column, count + 1); // move up
         }
         if (column + 1 < n && count + 1 < rooms[row][column + 1]) {
-            dfs(row, column + 1, count + 1);
+            dfs(row, column + 1, count + 1); // move right
         }
         if (column > 0 && rooms[row][column - 1]) {
-            dfs(row, column - 1, count + 1);
+            dfs(row, column - 1, count + 1); // move left
         }
     };
 
