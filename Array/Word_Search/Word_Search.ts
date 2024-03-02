275ec79023aadc965f86cc57626a76c99572287a
Hardanish Singh <hardanishsingh1@gmail.com>
2024-03-02 08:41:07 -0500

    Word Search I

--- Array/Word_Search/Word_Search.ts
+++ Array/Word_Search/Word_Search.ts
@@ -4,7 +4,7 @@ const exist = (board: string[][], word: string): boolean => {
     const m = board.length;
     const n = board[0].length;
 
-    const dfs = (row: number, column: number, index: number = 0): boolean => {
+    const DFSWithBacktrack = (row: number, column: number, index: number = 0): boolean => {
         // word matched
         if (index === word.length) {
             return true;
@@ -24,17 +24,17 @@ const exist = (board: string[][], word: string): boolean => {
 
         const original = board[row][column]; // Save visited cell
         board[row][column] = "-1"; // // Simply replace "original cell" with a "-1" when visiting an empty cell and put back the "original cell" after returning. This will avoid revisiting the same cell again.
-        const moveDown = dfs(row + 1, column, index + 1);
-        const moveUp = dfs(row - 1, column, index + 1);
-        const moveRight = dfs(row, column + 1, index + 1);
-        const moveLeft = dfs(row, column - 1, index + 1);
+        const moveDown = DFSWithBacktrack(row + 1, column, index + 1);
+        const moveUp = DFSWithBacktrack(row - 1, column, index + 1);
+        const moveRight = DFSWithBacktrack(row, column + 1, index + 1);
+        const moveLeft = DFSWithBacktrack(row, column - 1, index + 1);
         board[row][column] = original; // // After backtrack free visited cell to Original Position for other possible paths
         return moveDown || moveUp || moveRight || moveLeft;
     };
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (dfs(i, j)) {
+            if (DFSWithBacktrack(i, j)) {
                 return true;
             }
         }
