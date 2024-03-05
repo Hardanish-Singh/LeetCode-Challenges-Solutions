7ea50c2d15f598b83e8d15ec7c6131e22ea21a0c
Hardanish Singh <hardanishsingh1@gmail.com>
2024-03-04 19:23:47 -0500

    Remove Duplicates From Sorted Array I

--- Array/Remove_Duplicates_From_Sorted_Array/Remove_Duplicates_From_Sorted_Array.ts
+++ Array/Remove_Duplicates_From_Sorted_Array/Remove_Duplicates_From_Sorted_Array.ts
@@ -1,10 +1,19 @@
 // Leetcode: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
-const removeDuplicates = (nums: number[]): number => {
+// Solution 1
+var removeDuplicates = (nums: number[]): number => {
     for (let i = nums.length - 1; i > 0; i--) {
         if (nums[i] === nums[i - 1]) {
             nums.splice(i, 1);
         }
     }
+    return nums.length;
+};
+
+// Solution 2
+var removeDuplicates = (nums: number[]): number => {
+    const set = new Set(nums);
+    nums.length = 0;
+    nums.push(...set);
     return nums.length;
 };
