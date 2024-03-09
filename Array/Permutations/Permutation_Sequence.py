f0412d7401a2bed63e864e0fa48f50d33710936f
Hardanish Singh <hardanishsingh1@gmail.com>
2024-03-09 18:55:29 -0500

    Permutation Sequence

---
+++ Array/Permutations/Permutation_Sequence.py
@@ -0,0 +1,9 @@
+# Leetcode: https://leetcode.com/problems/permutation-sequence/
+
+from itertools import permutations
+
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        ans = list(permutations(range(1, n + 1)))
+        kthPermutation = ans[k - 1]
+        return "".join(str(item) for item in kthPermutation)
