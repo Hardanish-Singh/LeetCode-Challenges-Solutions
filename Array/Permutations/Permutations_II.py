cd68592beb0dea3fe95e35a92ea82d1615831ef1
Hardanish Singh <hardanishsingh1@gmail.com>
2024-03-09 18:52:22 -0500

    Permutations II

--- Array/Permutations/Permutations_II.py
+++ Array/Permutations/Permutations_II.py
@@ -5,15 +5,13 @@ class Solution:
                 nums.sort()
                 stack, result = [], []
                 
-                for i in range(len(nums)):
-                        stack.append( [ [ nums[i] ], [i] ] )
+                [stack.append( [ [nums[i]], [i] ] ) for i in range(len(nums))]
 
                 while stack:
                         item, indexes = stack.pop()
                         if len(item) == len(nums) and item not in result:
                                 result.append(item)
                         else:
-                                for i in range(len(nums)):
-                                        if i not in indexes:
-                                                stack.append( [ item + [ nums[i] ], indexes + [i] ] )
+                                [ stack.append( [ item + [nums[i]], indexes + [i] ] ) for i in range(len(nums)) if i not in indexes]
+
                 return result
