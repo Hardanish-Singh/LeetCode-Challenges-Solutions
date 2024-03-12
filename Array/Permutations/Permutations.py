9030fe3e2a94907c71d08b16fe69f84ba216f9d4
Hardanish Singh <hardanishsingh1@gmail.com>
2024-03-12 15:49:04 -0400

    Permutations I

--- Array/Permutations/Permutations.py
+++ Array/Permutations/Permutations.py
@@ -1,5 +1,6 @@
 # Leetcode: https://leetcode.com/problems/permutations/
 
+# Solution 1
 class Solution:
         def permute(self, nums: List[int]) -> List[List[int]]:
                 nums.sort()
@@ -15,3 +16,10 @@ class Solution:
                                 [stack.append( item + [num] ) for num in nums if num not in item]
 
                 return result
+
+# Solution 2
+from itertools import permutations
+
+class Solution:
+        def permute(self, nums: List[int]) -> List[List[int]]:
+                return list(permutations(nums))
