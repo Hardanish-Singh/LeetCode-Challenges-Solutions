a4424d362d9a41936c198dbfa8773f5a74c712fc
Hardanish Singh <hardanishsingh1@gmail.com>
2024-03-14 17:14:35 -0400

    Valid Parenthesis

--- Array/Parenthesis/Valid_Parenthesis.ts
+++ Array/Parenthesis/Valid_Parenthesis.ts
@@ -1,43 +1,38 @@
 // Leetcode: https://leetcode.com/problems/valid-parentheses/
 
 type openingBrackets = {
-        [key: string]: boolean;
+    [key: string]: boolean;
 };
 
 type validBrackets = {
-        [key: string]: string;
+    [key: string]: string;
 };
 
-/**
- * @param { string } str
- * @return { boolean }
- */
+const isValid = (str: string): boolean => {
+    let stack: Array<string> = [];
+    let openingBrackets: openingBrackets = {
+        "(": true,
+        "[": true,
+        "{": true,
+    };
+    let validBrackets: validBrackets = {
+        ")": "(",
+        "]": "[",
+        "}": "{",
+    };
 
-var isValid = function (str: string): boolean {
-        let stack: Array<string> = [];
-        let openingBrackets: openingBrackets = {
-                "(": true,
-                "[": true,
-                "{": true,
-        };
-        let validBrackets: validBrackets = {
-                ")": "(",
-                "]": "[",
-                "}": "{",
-        };
-
-        for (let i: number = 0; i < str.length; i++) {
-                let bracket: string = str[i];
-                if (bracket in openingBrackets) {
-                        stack.push(bracket);
-                } else {
-                        if (stack.length > 0 && stack[stack.length - 1] === validBrackets[bracket]) {
-                                stack.pop();
-                        } else {
-                                return false;
-                        }
-                }
+    for (let i: number = 0; i < str.length; i++) {
+        let bracket: string = str[i];
+        if (bracket in openingBrackets) {
+            stack.push(bracket);
+        } else {
+            if (stack.length > 0 && stack[stack.length - 1] === validBrackets[bracket]) {
+                stack.pop();
+            } else {
+                return false;
+            }
         }
+    }
 
-        return stack.length > 0 ? false : true;
+    return stack.length > 0 ? false : true;
 };
