413d0afa513445e33ab20384a1f9c80b42a62ddc
Hardanish Singh <hardanishsingh1@gmail.com>
2024-03-16 11:24:36 -0400

    Valid Parenthesis String

--- Array/Parenthesis/Valid_Parenthesis_String.ts
+++ Array/Parenthesis/Valid_Parenthesis_String.ts
@@ -1,34 +1,29 @@
-// Leetcode: https://leetcode.com/problems/valid-parenthesis-string/submissions/
-
-/**
- * @param { string } str
- * @returns { boolean }
- */
+// Leetcode: https://leetcode.com/problems/valid-parenthesis-string/
 
 function checkValidString(str: string): boolean {
-        let openCount: number = 0;
-        let closedCount: number = 0;
+    let openCount: number = 0;
+    let closedCount: number = 0;
 
-        for (let i: number = 0; i < str.length; i++) {
-                if (str[i] == "*" || str[i] == "(") {
-                        openCount++;
-                } else {
-                        openCount--;
-                }
-                if (openCount < 0) {
-                        return false;
-                }
+    for (let i: number = 0; i < str.length; i++) {
+        if (str[i] == "*" || str[i] == "(") {
+            openCount++;
+        } else {
+            openCount--;
         }
+        if (openCount < 0) {
+            return false;
+        }
+    }
 
-        for (let i: number = str.length - 1; i >= 0; i--) {
-                if (str[i] == "*" || str[i] == ")") {
-                        closedCount++;
-                } else {
-                        closedCount--;
-                }
-                if (closedCount < 0) {
-                        return false;
-                }
+    for (let i: number = str.length - 1; i >= 0; i--) {
+        if (str[i] == "*" || str[i] == ")") {
+            closedCount++;
+        } else {
+            closedCount--;
         }
-        return true;
+        if (closedCount < 0) {
+            return false;
+        }
+    }
+    return true;
 }
