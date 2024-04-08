066391b4cde200242fd92338a0c5f5c87e5ddaa2
Hardanish Singh <hardanishsingh1@gmail.com>
2024-04-08 16:08:54 -0400

    Remove Invalid Parenthesis

--- Array/Parenthesis/Remove_Invalid_Parentheses.ts
+++ Array/Parenthesis/Remove_Invalid_Parentheses.ts
@@ -1,56 +1,45 @@
 // Leetcode: https://leetcode.com/problems/remove-invalid-parentheses/
 
-type Paranthesis = {
-    [key: string]: boolean;
-};
-
-const generateValidParenthesis = (
-    str: string,
-    minimumNumberOfBracketsToBeRemoved: number,
-    hash_set: Set<string>,
-    isVisited: Paranthesis
-): void => {
-    let queue: Array<[string, number]> = [[str, minimumNumberOfBracketsToBeRemoved]];
-    while (queue.length > 0) {
-        let [string, count] = queue.shift() as [string, number];
-        if (count === 0) {
-            // CHECK IF THE STRING IS VALID
-            if (getMinimumNumberOfBracketsToBeRemoved(string) === 0) {
-                hash_set.add(string);
-            }
-        }
-        for (let i: number = 0; i < string.length; i++) {
-            let left: string = string.slice(0, i);
-            let right: string = string.slice(i + 1);
-            if (!isVisited[left + right] && count - 1 >= 0) {
-                isVisited[left + right] = true;
-                queue.push([left + right, count - 1]);
-            }
-        }
-    }
-};
-
-const getMinimumNumberOfBracketsToBeRemoved = (str: string): number => {
-    let stack: Array<string> = [];
-    for (let i: number = 0; i < str.length; i++) {
-        let bracket: string = str[i];
-        if (bracket === "(") {
-            stack.push(bracket);
-        } else if (bracket === ")") {
-            if (stack[stack.length - 1] === "(") {
+const isValid = (s: string): boolean => {
+    const stack: string[] = [];
+    for (const c of s) {
+        if (c === "(") {
+            stack.push("(");
+        } else if (c === ")") {
+            if (stack.length > 0) {
                 stack.pop();
             } else {
-                stack.push(bracket);
+                return false;
             }
         }
     }
-    return stack.length;
+    return stack.length === 0;
 };
 
-var removeInvalidParentheses = function (s: string): Array<string> {
-    let minimumNumberOfBracketsToBeRemoved: number = getMinimumNumberOfBracketsToBeRemoved(s);
-    let hash_set: Set<string> = new Set<string>();
-    let isVisited: Paranthesis = {};
-    generateValidParenthesis(s, minimumNumberOfBracketsToBeRemoved, hash_set, isVisited);
-    return Array.from(hash_set);
+const removeInvalidParentheses = (s: string): string[] => {
+    let result: string[] = [];
+    let visit: Set<string> = new Set();
+    let queue = [s];
+    let found = false;
+
+    while (queue.length) {
+        let node = queue.shift() as string;
+        if (isValid(node)) {
+            result.push(node);
+            found = true;
+        }
+        if (!found) {
+            for (let i = 0; i < node.length; i++) {
+                if ("()".includes(node[i])) {
+                    let new_node = node.slice(0, i) + node.slice(i + 1);
+                    if (!visit.has(new_node)) {
+                        queue.push(new_node);
+                        visit.add(new_node);
+                    }
+                }
+            }
+        }
+    }
+
+    return result;
 };
