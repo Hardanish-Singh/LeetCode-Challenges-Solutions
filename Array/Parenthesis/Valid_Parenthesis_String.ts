25f1dce19bc6e59643c1579a39f03ca8199aa7c2
Hardanish Singh <hardanishsingh1@gmail.com>
2024-04-08 15:12:58 -0400

    Valid Parenthesis String

--- Array/Parenthesis/Valid_Parenthesis_String.ts
+++ Array/Parenthesis/Valid_Parenthesis_String.ts
@@ -1,9 +1,8 @@
 // Leetcode: https://leetcode.com/problems/valid-parenthesis-string/
 
 const checkValidString = (str: string): boolean => {
+    // The idea is to keep track of the number of opening parenthesis
     let openingParenthesis: number = 0;
-    let closingParenthesis: number = 0;
-
     for (let i: number = 0; i < str.length; i++) {
         if (str[i] == "*" || str[i] == "(") {
             openingParenthesis++;
@@ -14,7 +13,8 @@ const checkValidString = (str: string): boolean => {
             return false;
         }
     }
-
+    // The idea is to keep track of the number of closing parenthesis
+    let closingParenthesis: number = 0;
     for (let i: number = str.length - 1; i >= 0; i--) {
         if (str[i] == "*" || str[i] == ")") {
             closingParenthesis++;
