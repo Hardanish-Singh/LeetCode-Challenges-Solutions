86573d38e4739675bf45e0ca9c4d2d35630a387c
Hardanish Singh <hardanishsingh1@gmail.com>
2024-04-08 16:46:51 -0400

    Remove Invalid Parenthesis

--- Array/Parenthesis/Remove_Invalid_Parentheses.py
+++ Array/Parenthesis/Remove_Invalid_Parentheses.py
@@ -1,8 +1,8 @@
 #  Leetcode: https://leetcode.com/problems/remove-invalid-parentheses/
 
-# Solution 1: DFS
+# Solution 1: BFS
 class Solution:
-    def isValid(s: str) -> bool:
+    def is_valid(s: str) -> bool:
         stack = []
         for c in s:
             if c == "(":
@@ -22,7 +22,7 @@ class Solution:
 
         while queue:
             node = queue.pop(0)
-            if Solution.isValid(node):
+            if Solution.is_valid(node):
                 result.append(node)
                 found = True
             if not found:
@@ -37,31 +37,31 @@ class Solution:
 
 # Solution 2: Recursion
 class Solution:
+    def get_invalid_paranthesis_count(s: str) -> int:
+        stack = []
+        for c in s:
+            if c=='(':
+                stack.append(c)
+            elif c==')':
+                if stack and stack[-1]=='(':
+                    stack.pop()
+                else:
+                    stack.append(c)
+        return len(stack)
+
     def removeInvalidParentheses(self, s: str) -> List[str]:
-        ans = set()
-        def get_invalid_paranthesis_count(s):
-            st = []
-            for c in s:
-                if c=='(':
-                    st.append(c)
-                elif c==')':
-                    if st and st[-1]=='(':
-                        st.pop()
-                    else:
-                        st.append(c)
-            return len(st)
-        
-        inv_count = get_invalid_paranthesis_count(s)
+        visit = set()    
+        count = Solution.get_invalid_paranthesis_count(s)
         
         @cache
-        def recursion(st, count):
+        def recursion(st: str, count: int) -> None:
             if count == 0:
-                if get_invalid_paranthesis_count(st) == 0:
-                    ans.add(st)
+                if Solution.get_invalid_paranthesis_count(st) == 0:
+                    visit.add(st)
             else:
                 for i in range(len(st)):
                     new_st = st[0:i] + st[i+1:]
                     recursion(new_st, count - 1)
 
-        recursion(s, inv_count)
-        return ans
+        recursion(s, count)
+        return visit
