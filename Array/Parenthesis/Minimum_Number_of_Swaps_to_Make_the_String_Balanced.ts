49b67f3c05e359e9a465af4ce038a1e53a61d897
Hardanish Singh <hardanishsingh1@gmail.com>
2024-04-10 18:07:44 -0400

    Minimum Number of Swaps to Make the String Balanced

--- Array/Parenthesis/Minimum_Number_of_Swaps_to_Make_the_String_Balanced.ts
+++ Array/Parenthesis/Minimum_Number_of_Swaps_to_Make_the_String_Balanced.ts
@@ -1,30 +1,26 @@
 // Leetcode: https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/
 
-/**
- * @param { string } str
- * @returns { number }
- */
-
-function minSwaps(str: string): number {
-        let stack: Array<string> = [];
-        for (let i: number = 0; i < str.length; i++) {
-                let bracket: string = str[i];
-                if (bracket === "[") {
-                        stack.push(bracket);
-                } else {
-                        if (stack[stack.length - 1] === "[") {
-                                stack.pop();
-                        } else {
-                                stack.push(bracket);
-                        }
-                }
+const minSwaps = (str: string): number => {
+    let stack: Array<string> = [];
+    for (let i: number = 0; i < str.length; i++) {
+        let bracket: string = str[i];
+        if (bracket === "[") {
+            stack.push(bracket);
+        } else {
+            if (stack[stack.length - 1] === "[") {
+                stack.pop();
+            } else {
+                stack.push(bracket);
+            }
         }
-        let length = stack.length / 2; // Because the string consists of exactly n / 2 opening brackets '[' and n / 2 closing brackets ']'.
-        /*
-                If even number of pairs: Simplying using integer division by 2 returns the the number of switches needed to matched the unmatched pair.
-                If odd numbers of pairs: Even after switching the half of the unmatched pairs, you will be left with one pair which needs an additional swapping. 
-                                         Hence, you either add + 1 or use a ceiling function to round the number up.
-        */
-        return length % 2 === 0 ? length / 2 : Math.floor(length / 2) + 1;
-        // return length % 2 === 0 ? length / 2 : Math.ceil(length / 2);
-}
+    }
+
+    let length = stack.length / 2; // Because the string consists of exactly n / 2 opening brackets '[' and n / 2 closing brackets ']'.
+    /*
+        If even number of pairs: Simplying using integer division by 2 returns the the number of switches needed to matched the unmatched pair.
+        If odd numbers of pairs: Even after switching the half of the unmatched pairs, you will be left with one pair which needs an additional swapping. 
+        Hence, you either add + 1 or use a ceiling function to round the number up.
+    */
+    return length % 2 === 0 ? length / 2 : Math.ceil(length / 2);
+    // return length % 2 === 0 ? length / 2 : Math.floor(length / 2) + 1;
+};
