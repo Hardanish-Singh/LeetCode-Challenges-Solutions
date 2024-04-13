7e38246b56fb9b33efb380a217050b52b0a08819
Hardanish Singh <hardanishsingh1@gmail.com>
2024-04-13 15:28:09 -0400

    Generate Parentheses

--- Array/Parenthesis/Generate_Parentheses.ts
+++ Array/Parenthesis/Generate_Parentheses.ts
@@ -10,28 +10,23 @@
                 3) We get a valid combination IIF,  open == close && close == n && open == n
 */
 
-/**
- * @param { number } n
- * @return { string[] }
- */
-
-var generateParenthesis = function (n: number): Array<string> {
-        const parenthesis: Array<[string, number, number]> = [["(", 1, 0]];
-        while (true) {
-                const [bracket, open, close]: [string, number, number] = parenthesis[0];
-                // RULE 3
-                if (open == close && close == n && open == n) {
-                        break;
-                }
-                parenthesis.shift();
-                // RULE 1
-                if (open < n) {
-                        parenthesis.push([bracket + "(", open + 1, close]);
-                }
-                // RULE 2
-                if (open > close) {
-                        parenthesis.push([bracket + ")", open, close + 1]);
-                }
+const generateParenthesis = (n: number): Array<string> => {
+    const parenthesis: Array<[string, number, number]> = [["(", 1, 0]];
+    while (true) {
+        const [bracket, open, close]: [string, number, number] = parenthesis[0];
+        // RULE 3
+        if (open == close && close == n && open == n) {
+            break;
         }
-        return parenthesis.map((result) => result[0]);
+        parenthesis.shift();
+        // RULE 1
+        if (open < n) {
+            parenthesis.push([bracket + "(", open + 1, close]);
+        }
+        // RULE 2
+        if (open > close) {
+            parenthesis.push([bracket + ")", open, close + 1]);
+        }
+    }
+    return parenthesis.map((result) => result[0]);
 };
