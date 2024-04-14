a4c298c76ad5f186001eccfd8ac214c0e8c0679d
Hardanish Singh <hardanishsingh1@gmail.com>
2024-04-14 14:14:58 -0400

    Check if a Parentheses String Can Be Valid

--- Array/Parenthesis/Check_if_a_Parentheses_String_Can_Be_Valid.ts
+++ Array/Parenthesis/Check_if_a_Parentheses_String_Can_Be_Valid.ts
@@ -1,40 +1,34 @@
 // Leetcode: https://leetcode.com/problems/check-if-a-parentheses-string-can-be-valid/
 
-/**
- * @param { string } str
- * @param { string } locked
- * @returns { boolean }
- */
+const canBeValid = (str: string, locked: string): boolean => {
+    if (str.length % 2 !== 0) {
+        return false;
+    }
 
-function canBeValid(str: string, locked: string): boolean {
-        if (str.length % 2 !== 0) {
-                return false;
+    let open: number = 0;
+    let close: number = 0;
+
+    for (let i: number = 0; i < str.length; i++) {
+        if (locked[i] === "0" || str[i] === "(") {
+            open++;
+        } else {
+            open--;
         }
-
-        let open: number = 0;
-        let close: number = 0;
-
-        for (let i: number = 0; i < str.length; i++) {
-                if (locked[i] === "0" || str[i] === "(") {
-                        open++;
-                } else {
-                        open--;
-                }
-                if (open < 0) {
-                        return false;
-                }
+        if (open < 0) {
+            return false;
         }
+    }
 
-        for (let i: number = str.length - 1; i >= 0; i--) {
-                if (locked[i] === "0" || str[i] === ")") {
-                        close++;
-                } else {
-                        close--;
-                }
-                if (close < 0) {
-                        return false;
-                }
+    for (let i: number = str.length - 1; i >= 0; i--) {
+        if (locked[i] === "0" || str[i] === ")") {
+            close++;
+        } else {
+            close--;
         }
+        if (close < 0) {
+            return false;
+        }
+    }
 
-        return true;
-}
+    return true;
+};
