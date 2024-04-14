c9e80b6810d2bb8cd092350651ebf6c63344e3de
Hardanish Singh <hardanishsingh1@gmail.com>
2024-04-14 14:45:32 -0400

    Combination Sum IV

--- Array/Combinations/Combination_Sum_IV.ts
+++ Array/Combinations/Combination_Sum_IV.ts
@@ -2,58 +2,58 @@
 
 // SOLUTION 1: RECURSION
 var combinationSum4 = function (nums: Array<number>, target: number): number {
-        let count: number = 0;
-        nums.sort((a, b) => a - b);
+    let count: number = 0;
+    nums.sort((a, b) => a - b);
 
-        const permutationSum = (sum: number = 0) => {
-                if (sum < 0 || sum > target) {
-                        return count;
-                }
+    const permutationSum = (sum: number = 0) => {
+        if (sum < 0 || sum > target) {
+            return count;
+        }
 
-                if (sum === target) {
-                        count = count + 1;
-                        return count;
-                }
+        if (sum === target) {
+            count = count + 1;
+            return count;
+        }
 
-                for (let num of nums) {
-                        permutationSum(sum + num);
-                }
-        };
+        for (let num of nums) {
+            permutationSum(sum + num);
+        }
+    };
 
-        permutationSum();
-        return count;
+    permutationSum();
+    return count;
 };
 
 // SOLUTION 2: RECURSION WITH MEMOIZATION
 var combinationSum4 = function (nums: Array<number>, target: number): number {
-        const memo: {
-                [key: number]: number;
-        } = {};
-        nums.sort((a, b) => a - b);
+    const memo: {
+        [key: number]: number;
+    } = {};
+    nums.sort((a, b) => a - b);
 
-        const permutationSum = (sum: number = 0) => {
-                let count: number = 0;
-                if (sum < 0 || sum > target) {
-                        return count;
-                }
+    const permutationSum = (sum: number = 0) => {
+        let count: number = 0;
+        if (sum < 0 || sum > target) {
+            return count;
+        }
 
-                if (sum in memo) {
-                        count += memo[sum];
-                        return count;
-                }
+        if (sum in memo) {
+            count += memo[sum];
+            return count;
+        }
 
-                if (sum === target) {
-                        count = count + 1;
-                        return count;
-                }
+        if (sum === target) {
+            count = count + 1;
+            return count;
+        }
 
-                for (const num of nums) {
-                        count += permutationSum(sum + num);
-                }
+        for (const num of nums) {
+            count += permutationSum(sum + num);
+        }
 
-                memo[sum] = count;
-                return count;
-        };
+        memo[sum] = count;
+        return count;
+    };
 
-        return permutationSum();
+    return permutationSum();
 };
