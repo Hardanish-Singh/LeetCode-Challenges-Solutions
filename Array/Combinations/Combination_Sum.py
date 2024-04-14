3c4b5737a1882ee5026d788bfb0aa42f85f1f5ec
Hardanish Singh <hardanishsingh1@gmail.com>
2024-04-14 14:38:35 -0400

    Combination Sum I

--- Array/Combinations/Combination_Sum.py
+++ Array/Combinations/Combination_Sum.py
@@ -4,9 +4,8 @@ class Solution(object):
         def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
                 candidates.sort()
                 queue, result = [], []
-                
-                for candidate in candidates:
-                        queue.append( [candidate] )
+
+                [queue.append( [candidate] ) for candidate in candidates]
 
                 while queue: 
                         item = queue.pop(0)
@@ -14,8 +13,6 @@ class Solution(object):
                         if remainder == 0 and item not in result:
                                 result.append(item)
                         else:
-                                for candidate in candidates:
-                                        if remainder >= candidate:
-                                                queue.append(sorted(item + [candidate]))
+                                [queue.append( sorted(item + [candidate]) ) for candidate in candidates if remainder >= candidate]
 
                 return result
