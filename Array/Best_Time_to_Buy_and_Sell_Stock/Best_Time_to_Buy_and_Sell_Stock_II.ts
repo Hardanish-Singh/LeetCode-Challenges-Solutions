b068c1af4acde997e1f258af7c69ddd438100084
Hardanish Singh <hardanishsingh1@gmail.com>
2024-04-16 17:35:27 -0400

    Best Time to Buy and Sell Stock II

--- Array/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock_II.ts
+++ Array/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock_II.ts
@@ -1,11 +1,11 @@
 // Leetcode: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 
 const maxProfit = (prices: Array<number>): number => {
-        let profit: number = 0;
-        for (let i: number = 1; i < prices.length; i++) {
-                if (prices[i] > prices[i - 1]) {
-                        profit += prices[i] - prices[i - 1];
-                }
+    let profit: number = 0;
+    for (let i: number = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1];
         }
-        return profit;
+    }
+    return profit;
 };
