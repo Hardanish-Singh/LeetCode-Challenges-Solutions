d2e5dd7714e3f8f6b7402a970251fab0a06dd2e8
Hardanish Singh <hardanishsingh1@gmail.com>
2024-04-16 17:45:37 -0400

    Min Cost Climbing Stairs

--- Array/Climbing_Stairs/Min_Cost_Climbing_Stairs.py
+++ Array/Climbing_Stairs/Min_Cost_Climbing_Stairs.py
@@ -1,5 +1,6 @@
 # Leetcode: https://leetcode.com/problems/min-cost-climbing-stairs/
 
+# Solution 1: Recursion with memoization
 from typing import Dict, List
 
 class Solution:
@@ -12,5 +13,19 @@ class Solution:
                         minCost = cost[stepIndex] + min( minSteps( stepIndex + 1, cache ), minSteps( stepIndex + 2, cache ) )
                         cache[stepIndex] = minCost
                         return minCost
+
+                return min( minSteps( 0 ), minSteps( 1 ) )
+
+# Solution 2: Recursion with inbuild Python LRU cache
+from functools import lru_cache
+from typing import List
+
+class Solution:
+        def minCostClimbingStairs(self, cost: List[int]) -> int:
+                @lru_cache(None)
+                def minSteps( stepIndex: int ) -> int:
+                        if stepIndex >= len(cost):
+                                return 0
+                        return cost[stepIndex] + min( minSteps( stepIndex + 1 ), minSteps( stepIndex + 2 ) )
 
                 return min( minSteps( 0 ), minSteps( 1 ) )
