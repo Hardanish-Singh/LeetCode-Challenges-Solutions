35d50a53415f1086e3cefaa4f0f5093bf609d813
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-02 09:09:09 -0400

    Duplicate Zeros

--- Array/Easy/Duplicate_Zeros.ts
+++ Array/Easy/Duplicate_Zeros.ts
@@ -1,17 +1,11 @@
 // Leetcode: https://leetcode.com/problems/duplicate-zeros/
 
-const insertZero = (arr: Array<number>, position: number): void => {
-        for (let i: number = arr.length - 2; i >= position; i--) {
-                arr[i + 1] = arr[i];
+const duplicateZeros = function (arr: number[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            arr.splice(i, 0, 0); // inserts element at index i
+            arr.pop();
+            i++;
         }
-        arr[position] = 0;
-};
-
-const duplicateZeros = (arr: Array<number>): void => {
-        for (let i: number = 0; i < arr.length - 1; i++) {
-                if (arr[i] === 0) {
-                        insertZero(arr, i + 1);
-                        i++;
-                }
-        }
+    }
 };
