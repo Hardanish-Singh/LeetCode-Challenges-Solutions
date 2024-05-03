6e9ea4a613740337790be6cae408d8f5e70d5b3d
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-03 09:18:52 -0400

    Find the Difference of Two Arrays

--- Array/Easy/Find_the_Difference_of_Two_Arrays.ts
+++ Array/Easy/Find_the_Difference_of_Two_Arrays.ts
@@ -1,17 +1,12 @@
 // Leetcode: https://leetcode.com/problems/find-the-difference-of-two-arrays/
 
-const findDifference = (nums1: Array<number>, nums2: Array<number>): Array<Array<number>> => {
-        let nums1Set: Set<number> = new Set(nums1);
-        let nums2Set: Set<number> = new Set(nums2);
+const findDifference = (nums1: number[], nums2: number[]): number[][] => {
+    let set1: Set<number> = new Set(nums1);
+    let set2: Set<number> = new Set(nums2);
+    let result: number[][] = [[], []];
 
-        let result: Array<Array<number>> = [[], []];
-        for (const num of nums1Set) {
-                if (!nums2Set.has(num)) result[0].push(num);
-        }
+    for (const num of set1) if (!set2.has(num)) result[0].push(num);
+    for (const num of set2) if (!set1.has(num)) result[1].push(num);
 
-        for (const num of nums2Set) {
-                if (!nums1Set.has(num)) result[1].push(num);
-        }
-
-        return result;
+    return result;
 };
