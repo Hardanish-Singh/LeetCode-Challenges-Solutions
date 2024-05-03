064d11a6e8c8eaa5d7cc9a29ec183ecce370cc2a
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-03 09:12:20 -0400

    Find All Numbers Disappeared in an Array

--- Array/Easy/Find_All_Numbers_Disappeared_in_an_Array.ts
+++ Array/Easy/Find_All_Numbers_Disappeared_in_an_Array.ts
@@ -1,23 +1,12 @@
 // Leetcode: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 
-var findDisappearedNumbers = function (nums: Array<number>): Array<number> {
-        var result: Array<number> = [];
-        var hash_table: {
-                [key: number]: boolean;
-        } = {};
-
-        for (let i: number = 0; i < nums.length; i++) {
-                if (nums[i] in hash_table) {
-                        continue;
-                } else {
-                        hash_table[nums[i]] = true;
-                }
+const findDisappearedNumbers = (nums: number[]): number[] => {
+    let uniques = new Set(nums);
+    let result: number[] = [];
+    for (let i = 1; i <= nums.length; i++) {
+        if (!uniques.has(i)) {
+            result.push(i);
         }
-        for (let i: number = 1; i <= nums.length; i++) {
-                if (!hash_table[i]) {
-                        result.push(i);
-                }
-        }
-
-        return result;
+    }
+    return result;
 };
