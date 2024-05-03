ddffc4b9d3a5ec9e291abcd880efa1465387b682
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-03 09:14:16 -0400

    Find Numbers With Even Number of Digits

--- Array/Easy/Find_Numbers_with_Even_Number_of_Digits.ts
+++ Array/Easy/Find_Numbers_with_Even_Number_of_Digits.ts
@@ -1,11 +1,3 @@
 // Leetcode: https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
 
-var findNumbers = function (nums: Array<number>): number {
-        var count = 0;
-        for (let i: number = 0; i < nums.length; i++) {
-                if (String(nums[i]).length % 2 === 0) {
-                        count++;
-                }
-        }
-        return count;
-};
+const findNumbers = (nums: number[]): number => nums.filter((num) => num.toString().length % 2 === 0).length;
