f44192bdcecdc49d7bb69edec66740cb373cb449
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-04 08:23:54 -0400

    Longest Palindrome

---
+++ Array/Palindrome/Longest_Palindrome.ts
@@ -0,0 +1,9 @@
+// Leetcode: https://leetcode.com/problems/longest-palindrome/
+
+const longestPalindrome = (s: string): number => {
+    let set = new Set();
+    for (const char of s) {
+        set.has(char) ? set.delete(char) : set.add(char);
+    }
+    return set.size == 0 ? s.length : s.length - set.size + 1;
+};
