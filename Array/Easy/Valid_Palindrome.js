232ba1e8e89773b4f225a6404ac8ffc5aa3b6734
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-05 09:37:11 -0400

    Valid Palindrome

--- Array/Easy/Valid_Palindrome.js
+++ Array/Easy/Valid_Palindrome.js
@@ -1,36 +1,32 @@
-/*
-        Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+// SOLUTION 1
+/**
+ * @param {string} str
+ * @return {boolean}
+ */
+var isPalindrome = (str) =>
+    str.toLowerCase().replace(/[^0-9a-z]/gi, "") ===
+    str
+        .toLowerCase()
+        .replace(/[^0-9a-z]/gi, "")
+        .split("")
+        .reverse()
+        .join("");
 
-        Example 1:
-                Input: s = "A man, a plan, a canal: Panama"
-                Output: true
-                Explanation: "amanaplanacanalpanama" is a palindrome.
-        
-        Example 2:
-                Input: s = "race a car"
-                Output: false
-                Explanation: "raceacar" is not a palindrome.
-
-        Constraints:
-                1) 1 <= s.length <= 2 * 105
-                2) s consists only of printable ASCII characters.
-*/
-
-/*
- * @param { string } str
- * @return { boolean }
-*/
- var isPalindrome = function( str ) {
-        str = str.toLowerCase().replace(/[^0-9a-z]/gi, '');
-        //str = str.toLowerCase().replace(/[^a-z0-9+]+/gi, '');
-        let leftPointer = 0;
-        let rightPointer = str.length - 1;
-        while( leftPointer <= rightPointer ) {
-                if( str[leftPointer] !== str[rightPointer] ) {
-                        return false;
-                }
-                leftPointer++;
-                rightPointer--;
+// SOLUTION 2
+/**
+ * @param {string} str
+ * @return {boolean}
+ */
+var isPalindrome = function (str) {
+    str = str.toLowerCase().replace(/[^0-9a-z]/gi, "");
+    let leftPointer = 0;
+    let rightPointer = str.length - 1;
+    while (leftPointer <= rightPointer) {
+        if (str[leftPointer] !== str[rightPointer]) {
+            return false;
         }
-        return true;
+        leftPointer++;
+        rightPointer--;
+    }
+    return true;
 };
