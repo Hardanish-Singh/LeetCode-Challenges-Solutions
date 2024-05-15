a9d65beff221c48440661ba4a143cba92276d06a
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-15 19:04:49 -0400

    Check If N and Its Double Exist

--- Array/Easy/Check_If_N_and_Its_Double_Exist.ts
+++ Array/Easy/Check_If_N_and_Its_Double_Exist.ts
@@ -1,7 +1,7 @@
 // Leetcode: https://leetcode.com/problems/check-if-n-and-its-double-exist/
 
 const checkIfExist = (arr: number[]): boolean => {
-    let map = new Map<number, boolean>();
+    const map = new Map<number, boolean>();
     for (const element of arr) {
         if (map.has(element * 2) || map.has(element / 2)) {
             return true;
