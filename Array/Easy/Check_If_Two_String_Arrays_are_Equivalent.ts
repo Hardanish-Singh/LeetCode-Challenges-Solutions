046ef28e700e20b934a9bf3f26c5438f3498b7ce
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-15 19:05:38 -0400

    Check if two string arrays are equal

--- Array/Easy/Check_If_Two_String_Arrays_are_Equivalent.ts
+++ Array/Easy/Check_If_Two_String_Arrays_are_Equivalent.ts
@@ -1,3 +1,3 @@
 // Leetcode: https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
 
-var arrayStringsAreEqual = (word1: string[], word2: string[]): boolean => word1.join("") === word2.join("");
+const arrayStringsAreEqual = (word1: string[], word2: string[]): boolean => word1.join("") === word2.join("");
