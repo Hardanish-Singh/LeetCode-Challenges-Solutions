29bb9b09aeeb3cbfd7bb6678c76ad3aa57d20e45
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-15 19:01:12 -0400

    Array Partition

--- Array/Easy/Array_Partition.ts
+++ Array/Easy/Array_Partition.ts
@@ -1,9 +1,9 @@
 // Leetcode: https://leetcode.com/problems/array-partition/
 
-const arrayPairSum = (nums: number[]): number => {
-    nums.sort((a, b) => a - b);
-    return nums.reduce(
-        (accumulator: number, num: number, index: number) => (index % 2 === 0 ? accumulator + num : accumulator),
-        0
-    );
-};
+const arrayPairSum = (nums: Array<number>): number =>
+    nums
+        .sort((a, b) => a - b)
+        .reduce(
+            (accumulator: number, num: number, index: number) => (index % 2 === 0 ? accumulator + num : accumulator),
+            0
+        );
