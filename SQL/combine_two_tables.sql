325f94607a92af480ae218c4e9fc8b021a576597
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-23 10:53:52 -0400

    Combine Two Tables

--- SQL/combine_two_tables.sql
+++ SQL/combine_two_tables.sql
@@ -1,38 +1,6 @@
-/*
+-- Leetcode: https://leetcode.com/problems/combine-two-tables/
 
-    Table: Person
-
-                    +-------------+---------+
-                    | Column Name | Type    |
-                    +-------------+---------+
-                    | PersonId    | int     |
-                    | FirstName   | varchar |
-                    | LastName    | varchar |
-                    +-------------+---------+
-                    
-                    PersonId is the primary key column for this table.
-    
-    Table: Address
-
-                    +-------------+---------+
-                    | Column Name | Type    |
-                    +-------------+---------+
-                    | AddressId   | int     |
-                    | PersonId    | int     |
-                    | City        | varchar |
-                    | State       | varchar |
-                    +-------------+---------+
-    
-                    AddressId is the primary key column for this table.
-    
-
-    Write a SQL query for a report that provides the following information 
-    for each person in the Person table, 
-    regardless if there is an address for each of those people:
-
-    FirstName, LastName, City, State
-
-*/
+-- Solution 1
 SELECT
         Person.FirstName,
         Person.LastName,
@@ -42,3 +10,14 @@ FROM
         Person
 
 LEFT JOIN Address ON Address.PersonId = Person.PersonId
+
+-- Solution 2
+SELECT
+        Person.FirstName,
+        Person.LastName,
+        Address.City,
+        Address.State
+FROM
+        Person
+
+LEFT JOIN Address USING(PersonId)
