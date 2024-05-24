1b95face4ff5580e9434488a0b86e0157408ddc4
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-24 06:42:31 -0400

    Find the Duplicate Number

--- Array/Easy/Find_the_Duplicate_Number.ts
+++ Array/Easy/Find_the_Duplicate_Number.ts
@@ -1,6 +1,7 @@
 // Leetcode: https://leetcode.com/problems/find-the-duplicate-number/
 
-const findDuplicate = (nums: number[]): number => {
+// Solution 1
+var findDuplicate = (nums: number[]): number => {
     const map = new Map<number, number>();
     for (const num of nums) {
         if (map.has(num)) {
@@ -9,4 +10,17 @@ const findDuplicate = (nums: number[]): number => {
         map.set(num, 1);
     }
     return -1;
+};
+
+// Solution 2
+var findDuplicate = (nums: number[]): number => {
+    nums.sort((a, b) => a - b);
+    return nums.reduce((accumulator: any, num: number, _, nums: number[]) => {
+        if (accumulator.has(num)) {
+            nums.splice(1); // eject early by mutating iterated copy
+            return num;
+        }
+        accumulator.set(num, 1);
+        return accumulator;
+    }, new Map<number, number>());
 };
