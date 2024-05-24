73c51c501ffb227643a4538a37d6def522af5b81
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-24 09:20:24 -0400

    Keep Multiplying Found Values by Two

--- Array/Easy/Keep_Multiplying_Found_Values_by_Two.ts
+++ Array/Easy/Keep_Multiplying_Found_Values_by_Two.ts
@@ -1,8 +1,13 @@
 // Leetcode: https://leetcode.com/problems/keep-multiplying-found-values-by-two/
 
-const findFinalValue = (nums: number[], original: number): number => {
+// Solution 1
+var findFinalValue = (nums: number[], original: number): number => {
     while (nums.includes(original)) {
         original *= 2;
     }
     return original;
 };
+
+// Solution 2
+var findFinalValue = (nums: number[], original: number): number =>
+    nums.indexOf(original) > -1 ? findFinalValue(nums, original * 2) : original;
