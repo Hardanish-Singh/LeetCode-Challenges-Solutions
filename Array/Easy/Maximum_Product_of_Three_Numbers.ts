0732368b97e10d22bcd8148692f8e69edfcb0522
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-25 12:00:30 -0400

    Maximum Product of Three Numbers

--- Array/Easy/Maximum_Product_of_Three_Numbers.ts
+++ Array/Easy/Maximum_Product_of_Three_Numbers.ts
@@ -1,16 +1,16 @@
 // Leetcode: https://leetcode.com/problems/maximum-product-of-three-numbers/
 
-const maximumProduct = (nums: Array<number>): number => {
-        nums.sort((a, b) => a - b);
-        /*
-                It could also be possible that two negative numbers lying at the left extreme end 
-                could also contribute to lead to a larger product if the third number in the triplet 
-                being considered is the largest positive number in the nums array.
-        */
-        let firstHalf = nums[0] * nums[1] * nums[nums.length - 1];
-        /*
-                Another solution could be to sort the given numsnums array(in ascending order) and find out the product of the last three numbers.
-        */
-        let lastHalf = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
-        return Math.max(firstHalf, lastHalf);
+const maximumProduct = (nums: number[]): number => {
+    //sort the array in ascending order
+    nums.sort((a, b) => a - b);
+
+    // Calculate the product of the three largest elements
+    const n = nums.length;
+    const product1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+
+    // Calculate the product of the two smallest elements and the largest element
+    const product2 = nums[0] * nums[1] * nums[n - 1];
+
+    // Return the maximum of the two products
+    return Math.max(product1, product2);
 };
