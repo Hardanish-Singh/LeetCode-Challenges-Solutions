83b5656d7d66847c64731c72be3d200369276bee
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-26 18:11:51 -0400

    Swap Salary

--- SQL/Easy/Swap_Salary.sql
+++ SQL/Easy/Swap_Salary.sql
@@ -1,3 +1,7 @@
 -- Leetcode: https://leetcode.com/problems/swap-salary/
 
+-- Solution 1
 UPDATE salary SET sex = CASE WHEN sex = 'm' THEN 'f' ELSE 'm' END;
+
+-- Solution 2
+UPDATE salary SET sex = CASE sex WHEN 'm' THEN 'f' ELSE 'm' END;
