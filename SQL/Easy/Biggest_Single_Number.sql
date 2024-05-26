411c1569d31ba665c392b06bcb9bb8cb7938cb4a
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-26 12:24:36 -0400

    Biggest Single Number

--- SQL/Easy/Biggest_Single_Number.sql
+++ SQL/Easy/Biggest_Single_Number.sql
@@ -29,3 +29,19 @@ SELECT
                 ),
                 NULL
               ) AS num;
+
+-- SOLUTION 3
+SELECT
+        COALESCE(
+                (
+                        SELECT
+                                num
+                        FROM
+                                mynumbers
+                        GROUP BY num
+                        HAVING COUNT(num) = 1
+                        ORDER BY num DESC
+                        LIMIT 1
+                ),
+                NULL
+              ) AS num;
