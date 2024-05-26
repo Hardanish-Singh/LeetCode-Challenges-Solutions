1f34854182c817e120c5258d42f931abb04937a5
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-26 17:28:02 -0400

    Combine Two Tables

--- SQL/Easy/Combine_Two_Tables.sql
+++ SQL/Easy/Combine_Two_Tables.sql
@@ -2,22 +2,22 @@
 
 -- Solution 1
 SELECT
-        Person.FirstName,
-        Person.LastName,
-        Address.City,
-        Address.State
+    Person.FirstName,
+    Person.LastName,
+    Address.City,
+    Address.State
 FROM
-        Person
+    Person
 
-LEFT JOIN Address ON Address.PersonId = Person.PersonId
+LEFT JOIN Address ON Address.PersonId = Person.PersonId;
 
 -- Solution 2
 SELECT
-        Person.FirstName,
-        Person.LastName,
-        Address.City,
-        Address.State
+    Person.FirstName,
+    Person.LastName,
+    Address.City,
+    Address.State
 FROM
-        Person
+    Person
 
 LEFT JOIN Address USING(PersonId)
