4ba28197afc7c1fb2f61308a9af0b43da5bb71ee
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-26 17:57:49 -0400

    Recyclable and Low Fat Products

--- SQL/Easy/Recyclable_and_Low_Fat_Products.sql
+++ SQL/Easy/Recyclable_and_Low_Fat_Products.sql
@@ -1,7 +1,15 @@
 -- Leetcode: https://leetcode.com/problems/recyclable-and-low-fat-products/
 
+-- Solution 1
 SELECT
-        product_id
+    product_id
 FROM
-        products
+    products
 WHERE low_fats = TRUE AND recyclable = TRUE;
+
+-- Solution 2
+SELECT
+    product_id
+FROM
+    products
+WHERE low_fats = 'Y' AND recyclable = 'Y';
