99a9fcb7b8cdb35dd3f8c8858ba367e62453a379
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-26 18:08:53 -0400

    Reported Posts

--- SQL/Easy/Reported_Posts.sql
+++ SQL/Easy/Reported_Posts.sql
@@ -1,11 +1,12 @@
 -- Leetcode: https://leetcode.com/problems/reported-posts/
 
+-- Solution 1
 SELECT
-        extra AS report_reason,
-        COUNT( DISTINCT post_id ) as report_count
+    extra AS report_reason,
+    COUNT( DISTINCT post_id ) AS report_count
 FROM
-        Actions
-WHERE action_date BETWEEN DATE( '2019-07-05' - INTERVAL 1 DAY ) AND DATE('2019-07-05')
+    Actions
+WHERE action_date BETWEEN DATE('2019-07-05' - INTERVAL 1 DAY) AND DATE('2019-07-05')
 AND action = 'report'
 AND extra IS NOT NULL
 GROUP BY extra;
