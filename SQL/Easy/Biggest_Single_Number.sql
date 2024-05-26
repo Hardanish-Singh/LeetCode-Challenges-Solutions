d6ab398005fb20f4661d8963b2df05d8d50473ad
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-26 17:24:40 -0400

    Biggest Single Number

--- SQL/Easy/Biggest_Single_Number.sql
+++ SQL/Easy/Biggest_Single_Number.sql
@@ -2,46 +2,48 @@
 
 -- SOLUTION 1
 SELECT
-        MAX(num) AS num
+    MAX(num) AS num
 FROM
 (
-        SELECT
-                COUNT(num) AS count_num,
-                num
-        FROM
-                mynumbers
-        GROUP BY num
-)AS SubQuery
+    SELECT
+        COUNT(num) AS count_num,
+        num
+    FROM
+        mynumbers
+    GROUP BY num
+) AS SubQuery
 WHERE SubQuery.count_num = 1;
 
 -- SOLUTION 2
 SELECT
-        IFNULL(
-                (
-                        SELECT
-                                num
-                        FROM
-                                mynumbers
-                        GROUP BY num
-                        HAVING COUNT(num) = 1
-                        ORDER BY num DESC
-                        LIMIT 1
-                ),
-                NULL
-              ) AS num;
+    IFNULL
+        (
+            (
+                SELECT
+                    num
+                FROM
+                    mynumbers
+                GROUP BY num
+                HAVING COUNT(num) = 1
+                ORDER BY num DESC
+                LIMIT 1
+            ),
+            NULL
+        ) AS num;
 
 -- SOLUTION 3
 SELECT
-        COALESCE(
-                (
-                        SELECT
-                                num
-                        FROM
-                                mynumbers
-                        GROUP BY num
-                        HAVING COUNT(num) = 1
-                        ORDER BY num DESC
-                        LIMIT 1
-                ),
-                NULL
-              ) AS num;
+    COALESCE
+        (
+            (
+                SELECT
+                    num
+                FROM
+                    mynumbers
+                GROUP BY num
+                HAVING COUNT(num) = 1
+                ORDER BY num DESC
+                LIMIT 1
+            ),
+            NULL
+        ) AS num;
