c05e04c8e454963a193a59bfc4e6483758cc298d
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-28 14:06:58 -0400

    Add Two Promises

---
+++ Array/Easy/Add_Two_Promises.ts
@@ -0,0 +1,18 @@
+type P = Promise<number>;
+
+// Solution 1
+var addTwoPromises = async (promise1: P, promise2: P): P => {
+    // Wait for both promises to resolve and retrieve their values
+    const [value1, value2] = await Promise.all([promise1, promise2]);
+    // Return a new promise that resolves with the sum of the values
+    return value1 + value2;
+};
+
+// Solution 2
+var addTwoPromises = async (promise1: P, promise2: P): P =>
+    Promise.all([promise1, promise2]).then(([num1, num2]) => num1 + num2);
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
