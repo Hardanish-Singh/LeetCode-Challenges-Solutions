b55e0da9c20f74fbf10f407ef4365c8ad86c99fd
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-29 09:02:24 -0400

    Maximum SubArray

--- Array/Easy/Maximum_SubArray.ts
+++ Array/Easy/Maximum_SubArray.ts
@@ -1,59 +1,24 @@
 // Leetcode: https://leetcode.com/problems/maximum-subarray/
 
-/*
-        SOLUTION 1: OPTIMIZED BRUTE FORCE APPROACH
-                        Time Complexity: O(N^2), where N is the length of nums.
-                        Space Complexity: O(1)
-*/
-
+// Solution 1 (OPTIMIZED BRUTE FORCE APPROACH)
 var maxSubArray = (nums: number[]): number => {
-    let sum = Number.NEGATIVE_INFINITY;
+    let maxSubArraySum = Number.NEGATIVE_INFINITY; // or -Infinity;
     for (let i = 0; i < nums.length; i++) {
-        let temp = 0;
+        let cumulativeSum = 0;
         for (let j = i; j < nums.length; j++) {
-            temp += nums[j];
-            sum = Math.max(sum, temp);
-            if (temp < 0) {
-                break;
-            }
+            cumulativeSum += nums[j];
+            maxSubArraySum = Math.max(maxSubArraySum, cumulativeSum);
         }
     }
-    return sum;
+    return maxSubArraySum;
 };
 
-/*
-        SOLUTION 2: OPTIMIZED BRUTE FORCE APPROACH
-                        Time Complexity: O(N^2), where N is the length of nums.
-                        Space Complexity: O(1)
-*/
-
+// Solution 2 (DYNAMIC PROGRAMMING, KADANE'S ALGORITHM)
 var maxSubArray = (nums: number[]): number => {
-    let sum = Number.NEGATIVE_INFINITY;
-    for (let i = 0; i < nums.length; i++) {
-        let temp = nums[i];
-        sum = Math.max(sum, temp);
-        for (let j = i + 1; j < nums.length; j++) {
-            temp += nums[j];
-            sum = Math.max(sum, temp);
-            if (temp < 0) {
-                break;
-            }
-        }
-    }
-    return sum;
-};
-
-/*
-        SOLUTION 3: DYNAMIC PROGRAMMING, KADANE'S ALGORITHM
-                        Time Complexity: O(N), where N is the length of nums.
-                        Space Complexity: O(1)
-*/
-
-var maxSubArray = (nums: number[]): number => {
-    let max = nums[0];
+    let maxSubArraySum = nums[0];
     for (let i = 1; i < nums.length; i++) {
         nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
-        max = Math.max(max, nums[i]);
+        maxSubArraySum = Math.max(maxSubArraySum, nums[i]);
     }
-    return max;
+    return maxSubArraySum;
 };
