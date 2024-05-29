64e3091b13e5f703f73e551504633b269f6ca431
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-29 09:05:24 -0400

    Maximum SubArray

--- Array/Easy/Maximum_SubArray.ts
+++ Array/Easy/Maximum_SubArray.ts
@@ -15,6 +15,17 @@ var maxSubArray = (nums: number[]): number => {
 
 // Solution 2 (DYNAMIC PROGRAMMING, KADANE'S ALGORITHM)
 var maxSubArray = (nums: number[]): number => {
+    let maxSubArraySum = Number.NEGATIVE_INFINITY; // or -Infinity;
+    let localSum = 0;
+    for (const num of nums) {
+        localSum = Math.max(num, localSum + num);
+        maxSubArraySum = Math.max(maxSubArraySum, localSum);
+    }
+    return maxSubArraySum;
+};
+
+// Solution 3 (DYNAMIC PROGRAMMING, KADANE'S ALGORITHM)
+var maxSubArray = (nums: number[]): number => {
     let maxSubArraySum = nums[0];
     for (let i = 1; i < nums.length; i++) {
         nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
