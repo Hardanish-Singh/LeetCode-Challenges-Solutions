6169cf0be34998e6433332145994b8020aad4d28
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-29 13:29:55 -0400

    Maximum Units on a Truck

--- Array/Easy/Maximum_Units_on_a_Truck.ts
+++ Array/Easy/Maximum_Units_on_a_Truck.ts
@@ -1,6 +1,6 @@
 // Leetcode: https://leetcode.com/problems/maximum-units-on-a-truck/
 
-// SOLUTION 1
+// Solution 1
 var maximumUnits = (boxTypes: number[][], truckSize: number): number => {
     // SORT BOXES IN DESCENDING ORDER
     boxTypes.sort((a, b) => b[1] - a[1]);
@@ -14,7 +14,19 @@ var maximumUnits = (boxTypes: number[][], truckSize: number): number => {
     return totalUnits;
 };
 
-// SOLUTION 2
+// Solution 2
+var maximumUnits = (boxTypes: number[][], truckSize: number): number =>
+    boxTypes
+        .sort((a, b) => b[1] - a[1])
+        .reduce((totalUnits, boxType) => {
+            const [numberOfBoxes, numberOfUnitsPerBox] = boxType;
+            let boxCount = Math.min(truckSize, numberOfBoxes);
+            truckSize -= boxCount;
+            totalUnits += boxCount * numberOfUnitsPerBox;
+            return totalUnits;
+        }, 0);
+
+// Solution 3
 var maximumUnits = (boxTypes: number[][], truckSize: number): number => {
     // SORT BOXES IN DESCENDING ORDER
     boxTypes.sort((a, b) => b[1] - a[1]);
@@ -27,6 +39,7 @@ var maximumUnits = (boxTypes: number[][], truckSize: number): number => {
             totalUnits += truckSize * numberOfUnitsPerBox;
         }
         truckSize -= numberOfBoxes;
+        if (truckSize <= 0) break;
     }
     return totalUnits;
 };
