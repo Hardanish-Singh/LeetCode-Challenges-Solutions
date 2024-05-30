a883f811787ca34e7174b823e60211f793961e32
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-30 10:08:16 -0400

    Minimum Operations to Make the Array Increasing

--- Array/Easy/Minimum_Operations_to_Make_the_Array_Increasing.ts
+++ Array/Easy/Minimum_Operations_to_Make_the_Array_Increasing.ts
@@ -1,18 +1,10 @@
 // Leetcode: https://leetcode.com/problems/minimum-operations-to-make-the-array-increasing/
 
-var minOperations = function (nums: Array<number>): number {
-    let moves: number = 0;
-    for (let i: number = 1; i < nums.length; i++) {
+const minOperations = (nums: number[]): number =>
+    nums.reduce((accumulator: number, _, i: number) => {
         if (nums[i - 1] >= nums[i]) {
-            let diff = Math.abs(nums[i] - nums[i - 1]);
-            if (diff === 0) {
-                diff = 1;
-            } else if (diff + nums[i] === nums[i - 1]) {
-                ++diff;
-            }
-            nums[i] += diff;
-            moves += diff;
+            accumulator += nums[i - 1] - nums[i] + 1;
+            nums[i] = nums[i - 1] + 1;
         }
-    }
-    return moves;
-};
+        return accumulator;
+    }, 0);
