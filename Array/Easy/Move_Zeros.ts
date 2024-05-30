1f8a2ae54189c79012e1832e9b35dfa259c9e861
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-30 11:58:28 -0400

    Move Zeros

--- Array/Easy/Move_Zeros.ts
+++ Array/Easy/Move_Zeros.ts
@@ -1,15 +1,14 @@
 // Leetcode: https://leetcode.com/problems/move-zeroes/
 
-const moveZeroes = (nums: number[]): number[] => {
+// Solution 1
+var moveZeroes = (nums: number[]): number[] => {
     let zeros = 0;
     for (let i = nums.length - 1; i >= 0; i--) {
         if (nums[i] === 0) {
             zeros++;
             nums.splice(i, 1);
+            nums.push(0);
         }
-    }
-    for (let i = 0; i < zeros; i++) {
-        nums.push(0);
     }
     return nums;
 };
