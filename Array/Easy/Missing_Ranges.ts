98a86b7d25c7810fa5abd85374ac8e926e81949c
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-30 11:40:47 -0400

    Missing Ranges

--- Array/Easy/Missing_Ranges.ts
+++ Array/Easy/Missing_Ranges.ts
@@ -1,14 +1,14 @@
 // Leetcode: https://leetcode.com/problems/missing-ranges/
 
 const findMissingRanges = (nums: number[], lower: number, upper: number): number[][] => {
-    const missings: number[][] = [];
-
     // Check for empty nums
     if (nums.length === 0) return [[lower, upper]];
 
+    const missingRange: number[][] = [];
+
     // Check missing ranges before nums
     if (lower < nums[0]) {
-        missings.push([lower, nums[0] - 1]);
+        missingRange.push([lower, nums[0] - 1]);
     }
 
     // Check missing ranges in nums
@@ -19,13 +19,13 @@ const findMissingRanges = (nums: number[], lower: number, upper: number): number
         // Skip if elements smaller than lower or Skip if elements larger than upper
         if (nums[i] < lower || nums[i] > upper) continue;
 
-        missings.push([nums[i - 1] + 1, nums[i] - 1]);
+        missingRange.push([nums[i - 1] + 1, nums[i] - 1]);
     }
 
     // Check missing ranges after nums
     if (upper > nums[nums.length - 1]) {
-        missings.push([nums[nums.length - 1] + 1, upper]);
+        missingRange.push([nums[nums.length - 1] + 1, upper]);
     }
 
-    return missings;
+    return missingRange;
 };
