b54a9d241fee162467403ea5d2232d8e7a80cd67
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-30 20:24:52 -0400

    Next Greater Element I

--- Array/Easy/Next_Greater_Element_I.js
+++ Array/Easy/Next_Greater_Element_I.js
@@ -1,58 +1,9 @@
-/*
-        The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
-        You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+// Leetcode: https://leetcode.com/problems/next-greater-element-i/
 
-        For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. 
-        If there is no next greater element, then the answer for this query is -1.
-
-        Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
-
-        Example 1:
-                Input: 
-                        nums1 = [4, 1, 2], nums2 = [1, 3, 4, 2]
-                Output: 
-                        [-1, 3, -1]
-                Explanation: The next greater element for each value of nums1 is as follows:
-                        - 4 is underlined in nums2 = [1, 3, 4, 2]. There is no next greater element, so the answer is -1.
-                        - 1 is underlined in nums2 = [1, 3, 4, 2]. The next greater element is 3.
-                        - 2 is underlined in nums2 = [1, 3, 4, 2]. There is no next greater element, so the answer is -1.
-        
-        Example 2:
-                Input: 
-                        nums1 = [2, 4], nums2 = [1, 2, 3, 4]
-                Output: 
-                        [3, -1]
-                Explanation: The next greater element for each value of nums1 is as follows:
-                        - 2 is underlined in nums2 = [1, 2, 3, 4]. The next greater element is 3.
-                        - 4 is underlined in nums2 = [1, 2, 3, 4]. There is no next greater element, so the answer is -1.
-        
-        Constraints:
-                1) 1 <= nums1.length <= nums2.length <= 1000
-                2) 0 <= nums1[i], nums2[i] <= 10^4
-                3) All integers in nums1 and nums2 are unique.
-                4) All the integers of nums1 also appear in nums2
-*/
-
-/*
- * @param { number[] } nums1
- * @param { number[] } nums2
- * @return { number[] }
-*/
-
-var nextGreaterElement = function( nums1, nums2 ) {
-        var result = [];
-        for( let i = 0; i < nums1.length; i++ ) {
-                let isFound = false;
-                for( let j = nums2.indexOf( nums1[ i ] ); j < nums2.length; j++ ) {
-                        if( nums1[ i ] < nums2[ j ] ) {
-                                isFound = true;
-                                result.push( nums2[ j ] );
-                                break;
-                        }
-                }
-                if( !isFound ) {
-                        result.push( -1 );
-                }
-        }
-        return result;
-};
+const nextGreaterElement = (nums1, nums2) =>
+    nums1.reduce((accumulator, curr, index) => {
+        const nums2Index = nums2.findIndex((x) => x === curr);
+        const result = nums2.find((x, index) => x > curr && index > nums2Index) || -1;
+        accumulator.push(result);
+        return accumulator;
+    }, new Array());
