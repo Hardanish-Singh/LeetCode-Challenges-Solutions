681dda45b9c19723fc99b50c91937d5808d4b113
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-31 13:54:10 -0400

    Prime in Diagonal

--- Array/Easy/Prime_In_Diagonal.ts
+++ Array/Easy/Prime_In_Diagonal.ts
@@ -1,27 +1,28 @@
 // Leetcode: https://leetcode.com/problems/prime-in-diagonal/
 
-const isPrime = (num: number): boolean => {
-    if (num < 2) {
-        return false;
+const isPrime = (num) => {
+    if (num < 2) return false;
+
+    if (num % 2 == 0) return num == 2;
+    let root = Math.sqrt(num);
+
+    for (let i = 3; i <= root; ++i) {
+        if (num % i == 0) return false;
     }
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            return false;
-        }
-    }
+
     return true;
 };
 
-const primeInDiagonal = (nums: number[][]): number => {
-    let total = 0;
-    for (let row = 0; row < nums.length; row++) {
-        for (let column = 0; column < nums[row].length; column++) {
-            if (row === column || row + column === nums.length - 1) {
-                if (isPrime(nums[row][column])) {
-                    total += nums[row][column];
-                }
-            }
-        }
+var diagonalPrime = function (nums: number[][]): number {
+    let primes: number[] = [];
+
+    for (let i = 0; i < nums.length; ++i) {
+        let diag1 = nums[i][i];
+        let diag2 = nums[i][nums.length - 1 - i];
+
+        if (isPrime(diag1)) primes.push(diag1);
+        if (isPrime(diag2)) primes.push(diag2);
     }
-    return total;
+
+    return primes.length ? Math.max.apply(null, primes) : 0;
 };
