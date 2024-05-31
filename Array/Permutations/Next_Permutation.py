2e0d379cd9872b1d18bba200539291af1c5f423f
Hardanish Singh <hardanishsingh1@gmail.com>
2024-05-31 11:22:15 -0400

    Next Permutation

--- Array/Permutations/Next_Permutation.py
+++ Array/Permutations/Next_Permutation.py
@@ -8,9 +8,10 @@ class Solution:
         Step-1) Find The element and its index (say i) which is JUST SMALLER THAN ITS LATER (from n-2 to 0)
         Step-2) Find the element which is JUST GREATER THAN that element (from n-1 to 0)
         Step-3) Swap the elements.
-        Step-4) Now sort the element from (i+1th to end index)
+        Step-4) Now reverse the element from (i+1th to end index)
         """
         index = -1
+        # this loop finds the index of the element which is just smaller than its later element
         for i in range(len(nums)-2, -1, -1):
             if (nums[i] < nums[i + 1]):
                 index = i
@@ -18,9 +19,10 @@ class Solution:
         else:
             nums.sort()
             return
+        # this loop finds the element which is just greater than that element and swaps them
         for j in range(len(nums)-1, index, -1):
             if (nums[j] > nums[index]):
                 nums[index], nums[j] = nums[j], nums[index]
                 break
-        
+        # this line reverses the element from (i+1th to end index) and converts it to list
         nums[index+1:] = list(reversed(nums[index+1:]))
