91551b1302b8035f48cad891ca4968e547b2a1cc
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-01 17:00:41 -0400

    Find the XOR of Numbers Which Appear Twice

---
+++ Array/Easy/Find_the_XOR_of_Numbers_Which_Appear_Twice.ts
@@ -0,0 +1,31 @@
+// Leetcode: https://leetcode.com/problems/find-the-xor-of-numbers-which-appear-twice/
+
+// Solution 1
+var duplicateNumbersXOR = (nums: number[]): number => {
+    let map = new Map();
+    for (let num of nums) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
+
+    let result = 0;
+    for (const [key, value] of map.entries()) {
+        if (value === 2) {
+            result ^= key;
+        }
+    }
+    return result;
+};
+
+// Solution 2
+var duplicateNumbersXOR = (nums: number[]): number => {
+    const map = new Map<number, number>();
+    for (const num of nums) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
+    return Array.from(map.entries()).reduce((accumulator, [key, value]) => {
+        if (accumulator === 2) {
+            accumulator ^= key;
+        }
+        return accumulator;
+    }, 0);
+};
