4ca92ce85996fa8eab76df9c843269b8c46e38f8
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-01 15:34:08 -0400

    Single Number II

--- Array/Single_Number/Single_Number_II.ts
+++ Array/Single_Number/Single_Number_II.ts
@@ -1,7 +1,7 @@
 // Leetcode: https://leetcode.com/problems/single-number-ii/
 
 // Solution 1 (using XOR operator)
-const singleNumber = (nums: number[]): number => {
+var singleNumber = (nums: number[]): number => {
     let ones: number = 0;
     let twos: number = 0;
     for (let i = 0; i < nums.length; i++) {
@@ -9,4 +9,18 @@ const singleNumber = (nums: number[]): number => {
         twos = (twos ^ nums[i]) & ~ones;
     }
     return ones;
+};
+
+// Solution 2(using MAP)
+var singleNumber = (nums: number[]): number => {
+    const map: Map<number, number> = new Map();
+    for (const num of nums) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
+    for (let [key, value] of map) {
+        if (value === 1) {
+            return key;
+        }
+    }
+    return -1;
 };
