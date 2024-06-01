49a9b3989bbc4dd8d6025e8fb6ec1b7fb9f15305
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-01 09:15:41 -0400

    Replace Elements with Greatest Element on Right Side

--- Array/Easy/Replace_Elements_with_Greatest_Element_on_Right_Side.ts
+++ Array/Easy/Replace_Elements_with_Greatest_Element_on_Right_Side.ts
@@ -1,7 +1,29 @@
 // Leetcode: https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
 
-const replaceElements = (arr: number[]): number[] =>
+// Solution 1 (Brute Force (TLE))
+var replaceElements = (arr: number[]): number[] =>
     arr.reduce((accumulator: number[], _, currentIndex: number, arr: number[]) => {
         accumulator[currentIndex] = Math.max(...arr.slice(currentIndex + 1), -1);
         return accumulator;
     }, []);
+
+// Solution 2 (Brute Force (TLE))
+var replaceElements = (arr: number[]): number[] =>
+    arr.reduceRight((accumulator: number[], currentValue: number, currentIndex: number, arr: number[]) => {
+        accumulator[currentIndex] = Math.max(...arr.slice(currentIndex + 1), -1);
+        return accumulator;
+    }, []);
+
+// Solution 3 (Optimized)
+var replaceElements = (arr: number[]): number[] => {
+    const n = arr.length;
+    const result = new Array(n).fill(-1);
+    let maxRight = -1;
+
+    for (let i = n - 1; i >= 0; i--) {
+        result[i] = maxRight;
+        maxRight = Math.max(maxRight, arr[i]);
+    }
+
+    return result;
+};
