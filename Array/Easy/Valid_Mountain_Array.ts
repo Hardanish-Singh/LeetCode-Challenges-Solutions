26f25dc59c2b9e1ab4039fe6535d098495d635bd
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-03 16:05:44 -0400

    Valid Mountain Array

---
+++ Array/Easy/Valid_Mountain_Array.ts
@@ -0,0 +1,21 @@
+// Leetcode: https://leetcode.com/problems/valid-mountain-array/
+
+var validMountainArray = (arr: number[]): boolean => {
+    let peak = arr.indexOf(Math.max(...arr));
+    if (peak == 0 || peak == arr.length - 1) {
+        return false;
+    }
+    // Check for strickly increasing elements
+    for (let i = 0; i < peak; i++) {
+        if (arr[i] >= arr[i + 1]) {
+            return false;
+        }
+    }
+    // Check for strickly decresing elements
+    for (let i = peak; i < arr.length - 1; i++) {
+        if (arr[i] <= arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+};
