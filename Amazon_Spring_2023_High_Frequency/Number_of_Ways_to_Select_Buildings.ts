6cf096e61fa2185f111bcd027680e934456d050d
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-04 17:11:25 -0400

    Number of Ways to Select Buildings

--- Amazon_Spring_2023_High_Frequency/Number_of_Ways_to_Select_Buildings.ts
+++ Amazon_Spring_2023_High_Frequency/Number_of_Ways_to_Select_Buildings.ts
@@ -1,16 +1,22 @@
 // Leetcode: https://leetcode.com/problems/number-of-ways-to-select-buildings/
 
-// We are looking for 010 or 101 combinations.
+/* 
+    We are only looking for 010 or 101 combinations.
+    cnt0: Respresents the number of 0s in the string
+    cnt1: Respresents the number of 1s in the string
+    cnt01: Represents the number of 01 combinations
+    cnt10: Represents the number of 10 combinations
+*/
 const numberOfWays = (s: string, cnt0: number = 0, cnt1: number = 0, cnt01: number = 0, cnt10: number = 0): number =>
-    s.split("").reduce((acc, curr) => {
+    s.split("").reduce((accumulator, curr) => {
         if (curr === "0") {
             cnt0++;
             cnt01 += cnt1;
-            acc += cnt10;
+            accumulator += cnt10;
         } else {
             cnt1++;
             cnt10 += cnt0;
-            acc += cnt01;
+            accumulator += cnt01;
         }
-        return acc;
+        return accumulator;
     }, 0);
