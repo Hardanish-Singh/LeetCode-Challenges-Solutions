df46c492610ee189e5cc752046a23e9af3a7da72
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-04 17:20:35 -0400

    Page Recommendations I

--- SQL/Medium/Page_Recommendations.sql
+++ SQL/Medium/Page_Recommendations.sql
@@ -1,86 +1,11 @@
-/*
-        Table: Friendship
+-- Leetcode: https://leetcode.com/problems/page-recommendations/
 
-        +---------------+---------+
-        | Column Name   | Type    |
-        +---------------+---------+
-        | user1_id      | int     |
-        | user2_id      | int     |
-        +---------------+---------+
-        (user1_id, user2_id) is the primary key for this table.
-        Each row of this table indicates that there is a friendship relation between user1_id and user2_id.
-        
-        Table: Likes
-
-        +-------------+---------+
-        | Column Name | Type    |
-        +-------------+---------+
-        | user_id     | int     |
-        | page_id     | int     |
-        +-------------+---------+
-        (user_id, page_id) is the primary key for this table.
-        Each row of this table indicates that user_id likes page_id.
-
-        Write an SQL query to recommend pages to the user with user_id = 1 using the pages that your friends liked. 
-        It should not recommend pages you already liked.
-        Return result table in any order without duplicates.
-        The query result format is in the following example.
-
-        Example 1:
-
-                Input: 
-                
-                Friendship table:
-                +----------+----------+
-                | user1_id | user2_id |
-                +----------+----------+
-                | 1        | 2        |
-                | 1        | 3        |
-                | 1        | 4        |
-                | 2        | 3        |
-                | 2        | 4        |
-                | 2        | 5        |
-                | 6        | 1        |
-                +----------+----------+
-
-                Likes table:
-                +---------+---------+
-                | user_id | page_id |
-                +---------+---------+
-                | 1       | 88      |
-                | 2       | 23      |
-                | 3       | 24      |
-                | 4       | 56      |
-                | 5       | 11      |
-                | 6       | 33      |
-                | 2       | 77      |
-                | 3       | 77      |
-                | 6       | 88      |
-                +---------+---------+
-
-                Output: 
-                +------------------+
-                | recommended_page |
-                +------------------+
-                | 23               |
-                | 24               |
-                | 56               |
-                | 33               |
-                | 77               |
-                +------------------+
-
-                Explanation: 
-                        User one is friend with users 2, 3, 4 and 6.
-                        Suggested pages are 23 from user 2, 24 from user 3, 56 from user 3 and 33 from user 6.
-                        Page 77 is suggested from both user 2 and user 3.
-                        Page 88 is not suggested because user 1 already likes it.
-*/
 SELECT
-    DISTINCT  Likes.page_id  AS recommended_page 
+    DISTINCT Likes.page_id  AS recommended_page 
 FROM
 (
                 SELECT
-                        user2_id AS combined_data
+                        user2_id AS user_id
                 FROM
                         Friendship
                 WHERE user1_id = 1
@@ -88,15 +13,15 @@ FROM
                 UNION
 
                 SELECT
-                        user1_id AS combined_data
+                        user1_id AS user_id
                 FROM
                         Friendship
                 WHERE user2_id = 1
 
-)AS xx
-JOIN Likes ON Likes.user_id = xx.combined_data
+)AS SubQuery
+JOIN Likes ON Likes.user_id = SubQuery.user_id
 WHERE  
-        CASE WHEN page_id IN ( SELECT page_id FROM Likes WHERE user_id = 1 ) 
+        CASE WHEN Likes.page_id IN ( SELECT page_id FROM Likes WHERE user_id = 1 ) 
                 THEN FALSE
                 ELSE TRUE
         END
