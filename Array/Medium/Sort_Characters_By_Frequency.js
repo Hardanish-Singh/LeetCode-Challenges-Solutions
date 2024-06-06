4a489ed076eb9398437ee20cff618019c23591fb
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-05 21:08:26 -0400

    Sort Characters By Frequency

---
+++ Array/Medium/Sort_Characters_By_Frequency.js
@@ -0,0 +1,65 @@
+// Leetcode: https://leetcode.com/problems/sort-characters-by-frequency/
+
+// Solution 1
+var frequencySort = (str) => {
+    let frequencyMap = {};
+    for (let char of str) {
+        frequencyMap[char] = (frequencyMap[char] || 0) + 1;
+    }
+
+    let sortedChars = Object.keys(frequencyMap).sort((a, b) => {
+        if (frequencyMap[a] === frequencyMap[b]) {
+            return a.localeCompare(b);
+        }
+        return frequencyMap[b] - frequencyMap[a];
+    });
+
+    let result = "";
+    for (let char of sortedChars) {
+        result += char.repeat(frequencyMap[char]);
+    }
+
+    return result;
+};
+
+// Solution 2
+const frequencySort = (str) => {
+    let frequencyMap = {};
+    for (let char of str) {
+        frequencyMap[char] = (frequencyMap[char] || 0) + 1;
+    }
+
+    let sortedChars = Object.entries(frequencyMap).sort((a, b) => {
+        if (a[1] === b[1]) {
+            return a[0].localeCompare(b[0]);
+        }
+        return b[1] - a[1];
+    });
+
+    let result = "";
+    for (let char of sortedChars) {
+        result += char[0].repeat(char[1]);
+    }
+    return result;
+};
+
+// Solution 3
+const frequencySort = (str) => {
+    let map = new Map();
+    for (let char of str) {
+        map.set(char, (map.get(char) || 0) + 1);
+    }
+
+    let sortedChars = Array.from(map).sort((a, b) => {
+        if (a[1] === b[1]) {
+            return a[0].localeCompare(b[0]);
+        }
+        return b[1] - a[1];
+    });
+
+    let result = "";
+    for (let char of sortedChars) {
+        result += char[0].repeat(char[1]);
+    }
+    return result;
+};
