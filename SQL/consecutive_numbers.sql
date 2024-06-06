0257fa12a52e5e0179e77d22b4b1ca08b5f9a6be
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-06 07:29:45 -0400

    Consecutive Numbers

--- SQL/consecutive_numbers.sql
+++ SQL/consecutive_numbers.sql
@@ -1,25 +1,15 @@
-/*
-     Write a SQL query to find all numbers that appear at least three times consecutively.
+-- Leetcode: 
 
-               +----+-----+
-               | Id | Num |
-               +----+-----+
-               | 1  |  1  |
-               | 2  |  1  |
-               | 3  |  1  |
-               | 4  |  2  |
-               | 5  |  1  |
-               | 6  |  2  |
-               | 7  |  2  |
-               +----+-----+
-     For example, given the above Logs table, 1 is the only number that appears consecutively for at least three times.
+-- Solution 1
+SELECT 
+     DISTINCT num AS ConsecutiveNums
+FROM logs l1
+WHERE
+     EXISTS(SELECT * FROM logs l2 WHERE l2.id = l1.id + 1 AND l1.num = l2.num) 
+AND
+     EXISTS(SELECT * FROM logs l3 WHERE l3.id = l1.id + 2 AND l3.num = l1.num) 
 
-               +-----------------+
-               | ConsecutiveNums |
-               +-----------------+
-               | 1               |
-               +-----------------+
-*/
+-- Solution 2
 SELECT 
      DISTINCT Num AS ConsecutiveNums
 FROM 
