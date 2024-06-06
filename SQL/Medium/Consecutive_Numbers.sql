4fba3b0f966a796e38251ec2d56a8067e4b31a42
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-06 07:33:06 -0400

    Consecutive Numbers

---
+++ SQL/Medium/Consecutive_Numbers.sql
@@ -0,0 +1,34 @@
+-- Leetcode: 
+
+-- Solution 1
+SELECT 
+     DISTINCT num AS ConsecutiveNums
+FROM logs l1
+WHERE
+     EXISTS(SELECT * FROM logs l2 WHERE l2.id = l1.id + 1 AND l1.num = l2.num) 
+AND
+     EXISTS(SELECT * FROM logs l3 WHERE l3.id = l1.id + 2 AND l3.num = l1.num) 
+
+-- Solution 2
+WITH cte AS 
+(
+     SELECT
+          *,
+          LEAD(num, 1) OVER(ORDER BY id) AS num1,
+          LEAD(num, 2) OVER(ORDER BY id) AS num2,
+          LEAD(id, 1)  OVER(ORDER BY id) AS id1,
+          LEAD(id, 2)  OVER(ORDER BY id) AS id2
+     FROM Logs
+)
+
+SELECT
+     DISTINCT(num) AS ConsecutiveNums
+FROM cte
+WHERE
+     num = num1 
+AND 
+     num = num2 
+AND 
+     id1 = id + 1 
+AND 
+     id2 = id + 2;
