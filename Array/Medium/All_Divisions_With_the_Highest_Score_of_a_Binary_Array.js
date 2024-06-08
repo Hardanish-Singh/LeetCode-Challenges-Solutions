bcc0cc1aad3d711ffcbf30409836e6ccb2716aee
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-08 10:32:09 -0400

    All Divisions With The Highest Score of a Binary Array

--- Array/Medium/All_Divisions_With_the_Highest_Score_of_a_Binary_Array.js
+++ Array/Medium/All_Divisions_With_the_Highest_Score_of_a_Binary_Array.js
@@ -1,14 +1,40 @@
 // Leetcode: https://leetcode.com/problems/all-divisions-with-the-highest-score-of-a-binary-array/
 
-// Solution 1
-const maxScoreIndices = (nums) => {
-    let map = new Map();
+// Solution 1 (TLE)
+var maxScoreIndices = (nums) => {
+    const map = new Map();
 
     for (let i = 0; i <= nums.length; i++) {
         let left = nums.slice(0, i).filter((n) => n === 0).length;
         let right = nums.slice(i).filter((n) => n === 1).length;
 
         const sum = left + right;
+        if (map.has(sum)) {
+            let temp = map.get(sum);
+            temp.push(i);
+            map.set(sum, temp);
+        } else {
+            map.set(sum, [i]);
+        }
+    }
+
+    const max = Math.max.apply(null, Array.from(map.keys()));
+    return map.get(max);
+};
+
+// Solution 2 (Optimized)
+var maxScoreIndices = (nums) => {
+    const map = new Map();
+    let ones = nums.filter((n) => n === 1).length;
+    let zeros = 0;
+
+    for (let i = 0; i <= nums.length; i++) {
+        if (nums[i - 1] === 0) {
+            zeros++;
+        } else {
+            ones--;
+        }
+        const sum = ones + zeros;
         if (map.has(sum)) {
             let temp = map.get(sum);
             temp.push(i);
