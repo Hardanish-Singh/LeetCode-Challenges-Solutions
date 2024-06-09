5fd77295c5c7d461d3da81c65e1ef3defe4f6f69
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-09 18:44:50 -0400

    Circular Array Loop

--- Array/Medium/Circular_Array_Loop.ts
+++ Array/Medium/Circular_Array_Loop.ts
@@ -3,7 +3,7 @@
 /*
     This question is fun, but the explanation is not so great. Basically, each spot in nums represents the number of steps you should take forward or backward in the array. A negative represents going left in nums, a positive represents going right. If we go outside the indexing range of the array, loop back to the other side. So if we go too far right (index == nums.Length) we loop back to index == 0. If we go too far left (index == -1) we loop back to index == nums.Length - 1.
 
-    We are searching for a starting index in nums where, starting from that point, we follow the steps and it takes us in a full loop back to where we started. However the direction of the steps in the cycle must either be going ALL RIGHT (positive) or ALL LEFT (negative). If we start with a negative number and then encounter a positive, the cycle is considered invalidated.
+    We are searching for a starting index in nums where, starting from that point, we follow the steps and it takes us in a FULL LOOP back to where we started. However the direction of the steps in the cycle must either be going ALL RIGHT (positive) or ALL LEFT (negative). If we start with a negative number and then encounter a positive, the cycle is considered invalidated.
 
     Finally, the cycle must land at more than 1 index. In other words [0, 0, 0, 0, 5] is not considered a valid cycle because 5 loops back to itself, and every other index doesn't go anywhere but itself. So even though 5 does cycle back to 5, it only ever "touches" 1 index in that cycle.
 
@@ -25,7 +25,7 @@ const circularArrayLoop = (nums: number[]): boolean => {
             if (currentIndex < 0) {
                 currentIndex = currentIndex + n;
             }
-            // If current value is positive and jumpedIndexValue value is negative or current value is negative and jumpedIndexValue value is negative, then we need to break the loop
+            // If current value is positive and jumpedIndexValue value is negative or current value is negative and jumpedIndexValue value is positive, then we need to break the loop
             if ((nums[i] > 0 && jumpedIndexValue < 0) || (nums[i] < 0 && jumpedIndexValue > 0)) {
                 break;
             }
