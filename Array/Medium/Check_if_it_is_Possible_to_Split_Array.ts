718e9eecca6367987ab8b4af4437a5492c61318b
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-09 09:07:32 -0400

    Check if it is Possible to Split the Array

--- Array/Medium/Check_if_it_is_Possible_to_Split_Array.ts
+++ Array/Medium/Check_if_it_is_Possible_to_Split_Array.ts
@@ -1,6 +1,7 @@
 // Leetcode: https://leetcode.com/problems/check-if-it-is-possible-to-split-array/
 
-const canSplitArray = (nums: number[], m: number): boolean => {
+// Solution 1
+var canSplitArray = (nums: number[], m: number): boolean => {
     if (nums.length <= 2) {
         return true;
     }
@@ -11,3 +12,7 @@ const canSplitArray = (nums: number[], m: number): boolean => {
     }
     return false;
 };
+
+// Solution 2
+var canSplitArray = (nums: number[], m: number): boolean =>
+    nums.length <= 2 || nums.some((num, i, nums) => num + nums[i + 1] >= m);
