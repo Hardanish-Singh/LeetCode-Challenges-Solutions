9c335bc7a4129cb46d06f6cbb56e9bcc062186ec
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-12 08:10:02 -0400

    All Divisions With The Highest Score of a Binary Array

--- Array/Medium/All_Divisions_With_the_Highest_Score_of_a_Binary_Array.js
+++ Array/Medium/All_Divisions_With_the_Highest_Score_of_a_Binary_Array.js
@@ -48,3 +48,27 @@ var maxScoreIndices = (nums) => {
     const max = Math.max.apply(null, Array.from(map.keys()));
     return map.get(max);
 };
+
+// Solution 3 (Optimized)
+var maxScoreIndices = (nums) => {
+    const map = new Map();
+    let ones = nums.filter((n) => n === 1).length;
+    let zeros = 0;
+
+    for (let i = 0; i <= nums.length; i++) {
+        if (nums[i - 1] === 0) {
+            zeros++;
+        } else {
+            ones--;
+        }
+        const sum = ones + zeros;
+        if (map.has(sum)) {
+            map.set(sum, [...map.get(sum), i]);
+        } else {
+            map.set(sum, [i]);
+        }
+    }
+
+    const max = Math.max.apply(null, Array.from(map.keys()));
+    return map.get(max);
+};
