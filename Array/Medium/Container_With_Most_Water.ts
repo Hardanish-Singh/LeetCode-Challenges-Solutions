0c7500c70407b87d0b36672998225cbbb95f8fd6
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-12 09:04:40 -0400

    Container With Most Water

---
+++ Array/Medium/Container_With_Most_Water.ts
@@ -0,0 +1,30 @@
+// Leetcode: https://leetcode.com/problems/container-with-most-water/description/
+
+const maxArea = (height: number[]): number => {
+    let left = 0; // Left pointer starting from the leftmost edge
+    let right = height.length - 1; // Right pointer starting from the rightmost edge
+    let containerWithMaxWater = 0; // Initialize the maximum water capacity
+
+    while (left < right) {
+        // Calculate the width of the container
+        let width = right - left;
+
+        // Calculate the height of the container (the minimum height between the two lines)
+        let containerHeight = Math.min(height[left], height[right]);
+
+        // Calculate the water capacity of the current container
+        let waterCapacity = width * containerHeight;
+
+        // Update the maximum water capacity if the current container holds more water
+        containerWithMaxWater = Math.max(containerWithMaxWater, waterCapacity);
+
+        // Move the pointers towards each other
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return containerWithMaxWater;
+};
