a3022bc473b2c01ed05987672004804263808e98
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-13 16:40:02 -0400

    Is Subsequence

---
+++ String/Easy/Is_Subsequence.ts
@@ -0,0 +1,21 @@
+// Leetcode: https://leetcode.com/problems/is-subsequence/
+
+const isSubsequence = (source: string, target: string): boolean => {
+    let leftPointer1 = 0;
+    let leftPointer2 = 0;
+
+    //The iteration would terminate, when either of the pointers exceeds its boundary.
+    while (leftPointer1 < source.length && leftPointer2 < target.length) {
+        // we found a match. Hence, we move both pointers one step forward.
+        if (source[leftPointer1] === target[leftPointer2]) {
+            leftPointer1++;
+            leftPointer2++;
+        }
+        // no match is found. We then move only the right pointer on the target string.
+        else {
+            leftPointer2++;
+        }
+    }
+    // At the end of the iteration, the result solely depends on the fact that whether we have consumed all the characters in the source string.
+    return leftPointer1 === source.length;
+};
