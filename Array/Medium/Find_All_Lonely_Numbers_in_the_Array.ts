ff23324a95f280beae1c2841d2221b3849b3be30
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-18 06:09:49 -0400

    Find All Lonely Numbers in the Array

--- Array/Medium/Find_All_Lonely_Numbers_in_the_Array.ts
+++ Array/Medium/Find_All_Lonely_Numbers_in_the_Array.ts
@@ -5,7 +5,6 @@ const findLonely = (nums: Array<number>): Array<number> => {
     for (let i: number = 0; i < nums.length; i++) {
         map.set(nums[i], (map.get(nums[i]) || 0) + 1);
     }
-
     return Array.from(map.entries()).reduce((lonelyNumbers: number[], [key, value]) => {
         if (!map.has(Number(key) + 1) && !map.has(Number(key) - 1) && value === 1) {
             lonelyNumbers.push(Number(key));
