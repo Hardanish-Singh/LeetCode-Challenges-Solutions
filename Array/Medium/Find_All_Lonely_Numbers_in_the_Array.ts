1f50ada55d9fb25f37a95f633fce904bc4b4abbc
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-18 06:07:41 -0400

    Find All Lonely Numbers in the Array

--- Array/Medium/Find_All_Lonely_Numbers_in_the_Array.ts
+++ Array/Medium/Find_All_Lonely_Numbers_in_the_Array.ts
@@ -1,29 +1,15 @@
 // Leetcode: https://leetcode.com/problems/find-all-lonely-numbers-in-the-array/
 
-type map = {
-    [key: number]: number;
-};
-
 const findLonely = (nums: Array<number>): Array<number> => {
-    let lonelyNumbers: Array<number> = [];
-    let map: map = {};
-
+    const map = new Map();
     for (let i: number = 0; i < nums.length; i++) {
-        map[nums[i]] ? (map[nums[i]] += 1) : (map[nums[i]] = 1);
+        map.set(nums[i], (map.get(nums[i]) || 0) + 1);
     }
 
-    /*
-        LOOP THROUGH MAP AND CHECK FOLLOWING CONDITIONS
-            1) IF NUMBER HAS NO ADJACENT NUMBERS PRESENT IN THE MAP
-            2) IF NUMBER APPREARS EXACTLY ONCE IN THE MAP
-    */
-    for (let [key, value] of Object.entries(map)) {
-        const next = Number(key) + 1;
-        const previous = Number(key) - 1;
-        if (!map.hasOwnProperty(next) && !map.hasOwnProperty(previous) && value === 1) {
+    return Array.from(map.entries()).reduce((lonelyNumbers: number[], [key, value]) => {
+        if (!map.has(Number(key) + 1) && !map.has(Number(key) - 1) && value === 1) {
             lonelyNumbers.push(Number(key));
         }
-    }
-
-    return lonelyNumbers;
+        return lonelyNumbers;
+    }, []);
 };
