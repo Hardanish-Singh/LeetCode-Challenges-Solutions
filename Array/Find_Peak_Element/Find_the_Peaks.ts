23e062040e1eb562d847615c066c315caad02ee6
Hardanish Singh <hardanishsingh1@gmail.com>
2024-06-22 11:55:30 -0400

    Find the Peaks

---
+++ Array/Find_Peak_Element/Find_the_Peaks.ts
@@ -0,0 +1,24 @@
+// Leetcode: https://leetcode.com/problems/find-the-peaks/
+
+// Solution 1
+var findPeaks = (mountain: number[]): number[] =>
+    mountain.reduce((accumulator: number[], currentValue, index, mountain) => {
+        if (
+            index !== 0 &&
+            index !== mountain.length - 1 &&
+            currentValue > mountain[index - 1] &&
+            currentValue > mountain[index + 1]
+        ) {
+            accumulator.push(index);
+        }
+        return accumulator;
+    }, []);
+
+// Solution 2
+var findPeaks = (mountain: number[]): number[] =>
+    mountain.reduce((accumulator: number[], currentValue, index, mountain) => {
+        if (currentValue > mountain[index - 1] && currentValue > mountain[index + 1]) {
+            accumulator.push(index);
+        }
+        return accumulator;
+    }, []);
