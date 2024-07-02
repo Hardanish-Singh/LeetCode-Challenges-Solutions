9f1eec82c99f558122bef5ebf3496c130328c351
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-02 09:23:39 -0400

    Largest Number

--- Array/Medium/Largest_Number.ts
+++ Array/Medium/Largest_Number.ts
@@ -4,9 +4,10 @@ const largestNumber = (nums: number[]): string =>
     nums.every((num) => num === 0)
         ? "0"
         : nums
+              .map(String)
               .sort((a, b) => {
-                  const num1 = parseInt(String(a) + String(b));
-                  const num2 = parseInt(String(b) + String(a));
+                  const num1 = Number(a + b);
+                  const num2 = Number(b + a);
                   return num1 > num2 ? -1 : 1;
               })
               .join("");
