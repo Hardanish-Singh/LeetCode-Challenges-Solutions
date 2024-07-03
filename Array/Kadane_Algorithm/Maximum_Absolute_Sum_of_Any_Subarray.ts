2e3aa6b895fd57a60b5976853127d1579f4e515f
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-03 09:16:35 -0400

    Maximum Absolute Sum of Any Subarray

--- Array/Kadane_Algorithm/Maximum_Absolute_Sum_of_Any_Subarray.ts
+++ Array/Kadane_Algorithm/Maximum_Absolute_Sum_of_Any_Subarray.ts
@@ -2,10 +2,10 @@
 
 // Solution 1 (KADANE'S ALGORITHM)
 // Here the idea is to run Kadane Algorithm twice, Once calculating the max sum & Once calculating the min sum
-const maxAbsoluteSum = (nums: number[]): number => {
+var maxAbsoluteSum = (nums: number[]): number => {
     let clonedArray = [...nums];
 
-    let max = Number.MIN_VALUE;
+    let max = Number.NEGATIVE_INFINITY; // or -Infinity;
     for (let i = 0; i < clonedArray.length; i++) {
         clonedArray[i] = Math.max(clonedArray[i], clonedArray[i] + (clonedArray[i - 1] || 0));
         max = Math.max(max, clonedArray[i]);
@@ -13,10 +13,28 @@ const maxAbsoluteSum = (nums: number[]): number => {
 
     clonedArray = [...nums];
 
-    let min = Number.MAX_VALUE;
+    let min = Number.POSITIVE_INFINITY; // or Infinity;
     for (let i = 0; i < clonedArray.length; i++) {
         clonedArray[i] = Math.min(clonedArray[i], clonedArray[i] + (clonedArray[i - 1] || 0));
         min = Math.min(min, clonedArray[i]);
+    }
+
+    return Math.max(max, Math.abs(min));
+};
+
+// Solution 2 (KADANE'S ALGORITHM)
+// Here the idea is to run Kadane Algorithm twice, Once calculating the max sum & Once calculating the min sum
+var maxAbsoluteSum = (nums: number[]): number => {
+    let max = Number.NEGATIVE_INFINITY; // or -Infinity;
+    let min = Number.POSITIVE_INFINITY; // or Infinity;
+    let currentMax = 0;
+    let currentMin = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        currentMax = Math.max(nums[i], currentMax + nums[i]);
+        currentMin = Math.min(nums[i], currentMin + nums[i]);
+        max = Math.max(max, currentMax);
+        min = Math.min(min, currentMin);
     }
 
     return Math.max(max, Math.abs(min));
