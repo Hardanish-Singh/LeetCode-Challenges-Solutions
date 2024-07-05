03b843af95b0d71cb7515f304a1696d3253bb6e2
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-05 14:18:25 -0400

    Minimum Deletions to Make Array Beautiful

---
+++ Array/Medium/Minimum_Deletions_to_Make_Array_Beautiful.ts
@@ -0,0 +1,14 @@
+// Leetcode: https://leetcode.com/problems/minimum-deletions-to-make-array-beautiful/
+
+var minDeletion = (nums: number[]): number => {
+    var deletions = 0;
+
+    for (let i = 0; i < nums.length; i += 2) {
+        if (nums[i] === nums[i + 1]) {
+            i--;
+            deletions++;
+        }
+    }
+
+    return (nums.length - deletions) % 2 !== 0 ? deletions + 1 : deletions;
+};
