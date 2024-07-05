0b4e31aa441555507e0c2d7ba3c49c50478b3f7b
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-05 12:24:38 -0400

    Neighboring Bitwise XOR

---
+++ Array/Medium/Neighboring_Bitwise_XOR.ts
@@ -0,0 +1,24 @@
+// Leetcode: https://leetcode.com/problems/neighboring-bitwise-xor/
+
+// Solution 1
+var doesValidArrayExist = (derived: number[]): boolean => {
+    let n = derived.length;
+
+    let ans = 0;
+    for (let i = 0; i < n; i++) {
+        ans = ans ^ derived[i];
+    }
+
+    return ans == 0;
+};
+
+// Solution 2
+var doesValidArrayExist = (derived: number[]): boolean => {
+    return derived.reduce((accumulator: any, _, index: number, nums: number[]) => {
+        accumulator = accumulator ^ derived[index];
+        if (index === nums.length - 1) {
+            return accumulator == 0;
+        }
+        return accumulator;
+    }, 0);
+};
