d015677eafc5f77c52962622c5f859122e67f68e
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-06 10:16:18 -0400

    Partition Array According to Given Pivot

--- Array/Medium/Partition_Array_According_to_Given_Pivot.ts
+++ Array/Medium/Partition_Array_According_to_Given_Pivot.ts
@@ -1,8 +1,16 @@
 // Leetcode: https://leetcode.com/problems/partition-array-according-to-given-pivot/
 
+// Solution 1
 var pivotArray = function (nums: number[], pivot: number): number[] {
     let left = nums.filter((i) => i < pivot);
     let mid = nums.filter((i) => i === pivot);
     let right = nums.filter((i) => i > pivot);
     return [...left, ...mid, ...right];
 };
+
+// Solution 2
+var pivotArray = (nums: number[], pivot: number): number[] => [
+    ...nums.filter((i) => i < pivot),
+    ...nums.filter((i) => i === pivot),
+    ...nums.filter((i) => i > pivot),
+];
