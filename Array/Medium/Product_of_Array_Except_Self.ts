a5f51f75bd26f0cddde3ff3732ad0a67043cbeb7
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-06 13:43:02 -0400

    Product of Array Except Self

--- Array/Medium/Product_of_Array_Except_Self.ts
+++ Array/Medium/Product_of_Array_Except_Self.ts
@@ -1,8 +1,8 @@
 // Leetcode: https://leetcode.com/problems/product-of-array-except-self/
 
 const productExceptSelf = (nums: number[]): number[] => {
-    let sum = nums.reduce((a, b) => a * b, 1);
+    let product = nums.reduce((a, b) => a * b, 1);
     return nums.map((num, index) =>
-        num != 0 ? sum / num : [...nums.slice(0, index), ...nums.slice(index + 1)].reduce((a, b) => a * b, 1)
+        num != 0 ? product / num : [...nums.slice(0, index), ...nums.slice(index + 1)].reduce((a, b) => a * b, 1)
     );
 };
