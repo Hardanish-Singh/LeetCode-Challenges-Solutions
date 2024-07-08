94b644daad5fe0aef8e38b27d6b2eab9746ff575
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-08 16:23:00 -0400

    Rotate Array

--- Array/Medium/Rotate_Array.ts
+++ Array/Medium/Rotate_Array.ts
@@ -1,8 +1,8 @@
 // Leetcode: https://leetcode.com/problems/rotate-array/
 
-var rotate = (nums: number[], k: number): void => {
+const rotate = (nums: number[], k: number): void => {
     k %= nums.length;
-    let rotatedArray = [...nums.slice(nums.length - k), ...nums.slice(0, nums.length - k)];
+    const rotatedArray = [...nums.slice(nums.length - k), ...nums.slice(0, nums.length - k)];
     for (let i = 0; i < rotatedArray.length; i++) {
         nums[i] = rotatedArray[i];
     }
