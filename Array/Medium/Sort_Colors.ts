856ee21ae303a0f6fbc2807d9e654290b5366582
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-09 16:15:46 -0400

    Sort Colors

--- Array/Medium/Sort_Colors.ts
+++ Array/Medium/Sort_Colors.ts
@@ -1,32 +1,23 @@
 // Leetcode: https://leetcode.com/problems/sort-colors/
 
-/**
- * @param { number[] } nums
- * @return { void }
- */
+const sortColors = (nums: number[]): void => {
+    let red = 0;
+    let white = 0;
+    let blue = 0;
 
-function sortColors(nums: number[]): void {
-        let red = 0;
-        let white = 0;
-        let blue = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 0) {
+            red++;
+        } else if (nums[i] == 1) {
+            white++;
+        } else if (nums[i] == 2) {
+            blue++;
+        }
+    }
 
-        for (let i = 0; i < nums.length; i++) {
-                if (nums[i] == 0) {
-                        red++;
-                } else if (nums[i] == 1) {
-                        white++;
-                } else if (nums[i] == 2) {
-                        blue++;
-                }
-        }
-
-        for (let i = 0; i < red; i++) {
-                nums[i] = 0;
-        }
-        for (let i = 0; i < white; i++) {
-                nums[red + i] = 1;
-        }
-        for (let i = 0; i < blue; i++) {
-                nums[red + white + i] = 2;
-        }
-}
+    for (let i = 0; i < nums.length; i++) {
+        if (i < red) nums[i] = 0;
+        else if (i < red + white) nums[i] = 1;
+        else if (i < red + white + blue) nums[i] = 2;
+    }
+};
