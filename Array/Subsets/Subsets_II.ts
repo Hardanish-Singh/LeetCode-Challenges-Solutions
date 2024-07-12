f36a91120a5672d7c380b1318f0f5591aa446676
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-12 12:07:33 -0400

    Subsets II

--- Array/Subsets/Subsets_II.ts
+++ Array/Subsets/Subsets_II.ts
@@ -1,6 +1,7 @@
 // Leetcode: https://leetcode.com/problems/subsets-ii/
 
-function subsetsWithDup(nums: number[]): number[][] {
+// Solution 1
+var subsetsWithDup = (nums: number[]): number[][] => {
     nums.sort((a, b) => a - b);
     let subsets: number[][] = [[]];
 
@@ -14,4 +15,20 @@ function subsetsWithDup(nums: number[]): number[][] {
     }
     // @ts-ignore
     return [...new Set(subsets.map(JSON.stringify))].map(JSON.parse);
-}
+};
+
+// Solution 2
+var subsetsWithDup = (nums: number[]): number[][] => {
+    nums.sort((a, b) => a - b);
+    let subsets: number[][] = [[]];
+
+    for (const num of nums) {
+        let n = subsets.length;
+        for (let i = 0; i < n; i++) {
+            let currentCombo = subsets[i].concat(num); // The concat() method is used to merge arrays. Concat does not change the existing arrays, but instead returns a new array.
+            subsets.push(currentCombo);
+        }
+    }
+    // @ts-ignore
+    return [...new Set(subsets.map(JSON.stringify))].map(JSON.parse);
+};
