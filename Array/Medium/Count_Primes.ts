c70d6307abb837931255e59b18b65181d5e47e99
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-14 12:17:43 -0400

    Count Primes

--- Array/Medium/Count_Primes.ts
+++ Array/Medium/Count_Primes.ts
@@ -1,6 +1,7 @@
 // Leetcode: https://leetcode.com/problems/count-primes/
 
-const countPrimes = (n: number): number => {
+// Solution 1
+var countPrimes = (n: number): number => {
     if (n <= 2) {
         return 0;
     }
@@ -10,6 +11,27 @@ const countPrimes = (n: number): number => {
         if (isNotPrime[i]) {
             continue;
         } else {
+            count++;
+            isNotPrime[i] = true;
+            for (let j: number = i + i; j <= n; j += i) {
+                if (isNotPrime[j] === false) {
+                    isNotPrime[j] = true;
+                }
+            }
+        }
+    }
+    return count;
+};
+
+// Solution 2
+var countPrimes = (n: number): number => {
+    if (n <= 2) {
+        return 0;
+    }
+    let count: number = 0;
+    const isNotPrime = new Array(n).fill(false);
+    for (let i: number = 2; i < n; i++) {
+        if (!isNotPrime[i]) {
             count++;
             isNotPrime[i] = true;
             for (let j: number = i + i; j <= n; j += i) {
