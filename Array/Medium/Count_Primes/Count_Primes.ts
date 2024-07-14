dabf76a133812bc31b289f248d07b5e2ca657811
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-14 09:40:04 -0400

    Count Primes

--- Array/Medium/Count_Primes/Count_Primes.ts
+++ Array/Medium/Count_Primes/Count_Primes.ts
@@ -1,35 +1,23 @@
-/*
-        Given an integer n, return the number of prime numbers that are strictly less than n.
+// Leetcode: https://leetcode.com/problems/count-primes/
 
-        Example 1:
-                Input: n = 10
-                Output: 4
-                Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
-        Example 2:
-                Input: n = 0
-                Output: 0
-        Example 3:
-                Input: n = 1
-                Output: 0
-        Constraints:
-                0 <= n <= 5 * 10^6
-*/
-function countPrimes( n: number ): number {
-        let count: number = 0;
-        let isChecked: boolean[] = new Array(n).fill(false);
-        for ( let i: number = 2; i < n; i++ ) {
-                if( isChecked[i] ) { 
-                        continue;
+const countPrimes = (n: number): number => {
+    if (n <= 2) {
+        return 0;
+    }
+    let count: number = 0;
+    const isNotPrime = new Array(n).fill(true);
+    for (let i: number = 2; i < n; i++) {
+        if (isNotPrime[i]) {
+            continue;
+        } else {
+            count++;
+            isNotPrime[i] = true;
+            for (let j: number = i + i; j <= n; j = j + i) {
+                if (isNotPrime[j] === false) {
+                    isNotPrime[j] = true;
                 }
-                else{
-                        count++;
-                        isChecked[i] = true;
-                        for( let j: number = i+i; j <= n; j = j+i ) {
-                                if( isChecked[j] === false ) {
-                                        isChecked[j] = true;
-                                }
-                        }
-                }   
+            }
         }
-        return count;
+    }
+    return count;
 };
