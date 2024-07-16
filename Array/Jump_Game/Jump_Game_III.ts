404debb0440c453c65f81b765c9d1f30960686dd
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-16 13:43:13 -0400

    Jump Game III

--- Array/Jump_Game/Jump_Game_III.ts
+++ Array/Jump_Game/Jump_Game_III.ts
@@ -1,29 +1,29 @@
 // Leetcode: https://leetcode.com/problems/jump-game-iii/
 
-function canReach(arr: number[], start: number): boolean {
+const canReach = (arr: number[], start: number): boolean => {
     const seen: Set<number> = new Set();
     const stack: Array<number> = [start];
-    
+
     while (stack.length) {
         const curr = stack.pop() as number;
-        
+
         if (arr[curr] === 0) {
             return true;
         }
-        
+
         seen.add(curr);
-        
+
         const next: number = curr + arr[curr];
         const prev: number = curr - arr[curr];
-        
+
         if (next < arr.length && !seen.has(next)) {
             stack.push(next);
         }
-        
+
         if (prev >= 0 && !seen.has(prev)) {
             stack.push(prev);
         }
     }
-    
+
     return false;
 };
