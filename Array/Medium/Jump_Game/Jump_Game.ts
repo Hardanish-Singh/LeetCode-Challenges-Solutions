bc766b53d831a81dc4a44aab5ba0807ba0f9bef4
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-16 12:10:42 -0400

    Jump Game I

--- Array/Medium/Jump_Game/Jump_Game.ts
+++ Array/Medium/Jump_Game/Jump_Game.ts
@@ -1,6 +1,7 @@
 // Leetcode: https://leetcode.com/problems/jump-game/
 
-const canJump = (nums: number[]): boolean => {
+// Solution 1
+var canJump = (nums: number[]): boolean => {
     let reachable: number = 0;
     for (let i: number = 0; i < nums.length; i++) {
         if (i > reachable) {
@@ -10,3 +11,15 @@ const canJump = (nums: number[]): boolean => {
     }
     return true;
 };
+
+// Solution 2
+var canJump = (nums: number[]): boolean =>
+    nums.length > 1
+        ? nums.reduce((accumulator: any, num: number, index: number) => {
+              if (index > accumulator) {
+                  return false;
+              }
+              accumulator = Math.max(accumulator, index + nums[index]);
+              return accumulator;
+          }, 0)
+        : true;
