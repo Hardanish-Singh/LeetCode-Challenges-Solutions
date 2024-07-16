c09170e3a95156c2f07529c5d0f6886b0760edd4
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-16 12:40:53 -0400

    Jump Game II

--- Array/Jump_Game/Jump_Game_II.ts
+++ Array/Jump_Game/Jump_Game_II.ts
@@ -1,4 +1,6 @@
-function jump(nums: number[]): number {
+// Leetcode: https://leetcode.com/problems/jump-game-ii/
+
+const jump = (nums: number[]): number => {
     let reachable: number = 0;
     let curPosition: number = -1;
     let jumps: number = 0;
@@ -7,4 +9,4 @@ function jump(nums: number[]): number {
         reachable = Math.max(reachable, i + nums[i]);
     }
     return jumps;
-}
+};
