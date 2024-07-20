185acf2879863d25686e327adb19e75cad93b9f3
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-20 09:02:43 -0400

    JSON Deep Equal

---
+++ Array/Medium/JSON_Deep_Equal.js
@@ -0,0 +1,13 @@
+// Leetcode: https://leetcode.com/problems/json-deep-equal/
+/**
+ * @param {null|boolean|number|string|Array|Object} o1
+ * @param {null|boolean|number|string|Array|Object} o2
+ * @return {boolean}
+ */
+
+const replacer = (key, value) =>
+    value && typeof value === "object" && !Array.isArray(value)
+        ? Object.fromEntries(Object.entries(value).sort((a, b) => a[0].localeCompare(b[0])))
+        : value;
+
+const areDeeplyEqual = (o1, o2) => JSON.stringify(o1, replacer) === JSON.stringify(o2, replacer);
