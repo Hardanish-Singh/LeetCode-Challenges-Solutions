352696d8187c2ee848d1fd2ecca2a94c9084bd9b
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-21 12:26:51 -0400

    Search a 2D Matrix I

--- Array/Medium/Search_2D_Matrix/Search_a_2D_Matrix.ts
+++
@@ -1,4 +0,0 @@
-// Leetcode: https://leetcode.com/problems/search-a-2d-matrix/
-
-var searchMatrix = (matrix: number[], target: number): boolean =>
-    matrix.flat(Infinity).findIndex((element) => element === target) != -1;
