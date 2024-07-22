0c3d85786b7418bd06f2d8b5b368213fb26c98d0
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-22 07:12:47 -0400

    Largest Rectangle in Histogram

--- Array/Hard/Largest_Rectangle_in_Histogram.js
+++ Array/Hard/Largest_Rectangle_in_Histogram.js
@@ -1,40 +1,18 @@
-/**
- * @param { number[] } heights
- * @return { number }
-*/
+// Leetcode: https://leetcode.com/problems/largest-rectangle-in-histogram/
 
-var largestRectangleArea = function(buildings) {
-        //         var maxArea = 0;
-        //         for( let i = 0; i < buildings.length; i++ ) {
-        //                 let height = buildings[i];
-        //                 let width = 1;
-        //                 let left = i - 1;
-        //                 let right = i + 1;
-                        
-        //                 while( buildings[left--] >= buildings[i] ) {
-        //                         width++;  
-        //                 }
-        //                 while( buildings[right++] >= buildings[i] ){
-        //                         width++;
-        //                 }
-                        
-        //                 let area = width * height;
-        //                 maxArea = Math.max( area, maxArea);
-        //         }
-        //         return maxArea;
-                
-        var maxArea = 0;
-        
-        for( let i = 0; i < buildings.length; i++ ) {
-                let width = 1;
-                let height = Number.MAX_VALUE;
-                for( let j = i; j < buildings.length; j++ ) {
-                        height = Math.min(height, buildings[j])
-                        let area = (width) * height;
-                        maxArea = Math.max( area, maxArea);
-                        width++;
-                }
+const largestRectangleArea = (buildings) => {
+    let maxArea = 0;
+
+    for (let i = 0; i < buildings.length; i++) {
+        let width = 1;
+        let height = Number.MAX_VALUE;
+        for (let j = i; j < buildings.length; j++) {
+            height = Math.min(height, buildings[j]);
+            let area = width * height;
+            maxArea = Math.max(area, maxArea);
+            width++;
         }
-        
-        return maxArea;
+    }
+
+    return maxArea;
 };
