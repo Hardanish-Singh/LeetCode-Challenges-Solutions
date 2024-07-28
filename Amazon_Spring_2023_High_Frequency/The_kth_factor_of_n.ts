468fe1c1b0c1a340747ca33616836f083acd5959
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-28 09:52:14 -0400

    The kth Factor of n

--- Amazon_Spring_2023_High_Frequency/The_kth_factor_of_n.ts
+++ Amazon_Spring_2023_High_Frequency/The_kth_factor_of_n.ts
@@ -4,6 +4,10 @@
 var kthFactor = (n: number, k: number): number => [...Array(n + 1).keys()].filter((i) => n % i === 0)[k - 1] ?? -1;
 
 // Solution 2
+var kthFactor = (n: number, k: number): number =>
+    Array.from({ length: n + 1 }, (_, i) => i).filter((i) => n % i === 0)[k - 1] ?? -1;
+
+// Solution 3
 var kthFactor = (n: number, k: number): number => {
     let count = 0;
     for (let i = 1; i <= n; i++) {
