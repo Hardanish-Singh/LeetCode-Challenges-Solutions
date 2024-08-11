4844457969af2f42287a4bc801ddcbcf5abe920c
Hardanish Singh <hardanishsingh1@gmail.com>
2024-08-11 08:44:21 -0400

    Remove One Element to Make the Array Strictly Increasing

--- Array/Easy/Remove_One_Element_to_Make_the_Array_Strictly_Increasing.ts
+++ Array/Easy/Remove_One_Element_to_Make_the_Array_Strictly_Increasing.ts
@@ -9,7 +9,8 @@ const isStrictlyIncreasing = (sequence: number[]): boolean =>
         return accumulator;
     }, true);
 
-const canBeIncreasing = (sequence: number[]): boolean =>
+// Solution 1
+var canBeIncreasing = (sequence: number[]): boolean =>
     sequence.reduce((accumulator, _, i, array) => {
         let clonedArray = [...sequence];
         clonedArray.splice(i, 1); // remove the current index element from the array
@@ -19,3 +20,17 @@ const canBeIncreasing = (sequence: number[]): boolean =>
         }
         return accumulator;
     }, false);
+
+// Solution 2
+var canBeIncreasing = (sequence: number[]): boolean => {
+    for (let i = 1; i < sequence.length; i++) {
+        if (sequence[i - 1] >= sequence[i]) {
+            // let copy1 = nums.slice(0, i-1).concat(nums.slice(i));
+            // let copy2 = nums.slice(0, i).concat(nums.slice(i+1));
+            let copy1 = [...sequence.slice(0, i - 1), ...sequence.slice(i)];
+            let copy2 = [...sequence.slice(0, i), ...sequence.slice(i + 1)];
+            return isStrictlyIncreasing(copy1) || isStrictlyIncreasing(copy2);
+        }
+    }
+    return true;
+};
