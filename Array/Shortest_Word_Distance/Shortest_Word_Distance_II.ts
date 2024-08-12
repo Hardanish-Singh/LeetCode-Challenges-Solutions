62686381eede6938e8d40f12c49c6849f6f8ed6d
Hardanish Singh <hardanishsingh1@gmail.com>
2024-08-12 06:56:25 -0400

    Shortest Word Distance II

---
+++ Array/Shortest_Word_Distance/Shortest_Word_Distance_II.ts
@@ -0,0 +1,25 @@
+// Leetcode: https://leetcode.com/problems/shortest-word-distance-ii/
+
+class WordDistance {
+    #map: Record<string, number[]>;
+    constructor(wordsDict: string[]) {
+        this.#map = {};
+        wordsDict.forEach((word, index) =>
+            this.#map[word] ? this.#map[word].push(index) : (this.#map[word] = [index])
+        );
+    }
+
+    shortest(word1: string, word2: string): number {
+        let indexes1 = this.#map[word1];
+        let indexes2 = this.#map[word2];
+        let min = Number.POSITIVE_INFINITY;
+
+        for (let index1 of indexes1) {
+            for (let index2 of indexes2) {
+                min = Math.min(min, Math.abs(index1 - index2));
+            }
+        }
+
+        return min;
+    }
+}
