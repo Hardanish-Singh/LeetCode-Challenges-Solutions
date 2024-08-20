189b00d0a43a24b5abb190d84dc11e8d788086a1
Hardanish Singh <hardanishsingh1@gmail.com>
2024-08-20 18:02:53 -0400

    Reverse Substrings Between Each Pair of Parentheses

--- String/Medium/Reverse_Substrings_Between_Each_Pair_of_Parentheses.js
+++ String/Medium/Reverse_Substrings_Between_Each_Pair_of_Parentheses.js
@@ -1,55 +1,25 @@
-/*
-        You are given a string s that consists of lower case English letters and brackets.
-        Reverse the strings in each pair of matching parentheses, starting from the innermost one.
-        Your result should not contain any brackets.
+// Leetcode: https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/
 
-        Example 1:
-                Input: s = "(abcd)"
-                Output: "dcba"
-        Example 2:
-                Input: s = "(u(love)i)"
-                Output: "iloveu"
-                Explanation: The substring "love" is reversed first, then the whole string is reversed.
-        Example 3:
-                Input: s = "(ed(et(oc))el)"
-                Output: "leetcode"
-                Explanation: First, we reverse the substring "oc", then "etco", and finally, the whole string.
+/**
+ * @param {string} inputString
+ * @return {string}
+ */
 
-        Constraints:
-                1) 1 <= s.length <= 2000
-                2) s only contains lower case English characters and parentheses.
-                3) It is guaranteed that all parentheses are balanced.
-*/
-
-/*
- * @param { string } s
- * @return { string }
-*/
-
-const reverseString = ( s, leftIndex, rightIndex ) => {
-        while( leftIndex < rightIndex ) {
-                [ s[leftIndex], s[rightIndex] ] = [ s[rightIndex], s[leftIndex] ];
-                leftIndex++;
-                rightIndex--;
-        }
-}
-
-var reverseParentheses = function( s ) {
-        s = s.split("");
-        let indexes = [];
-        for( let i = 0; i < s.length; i++ ) {
-                if( s[i] === "(" ) {
-                      indexes.push( i + 1 );  
-                }
-                else if( s[i] === ")" ) {
-                        reverseString( s, indexes.pop(), i - 1 );
-                }
-        }
-        let reversedSubString = "";
-        for( let i = 0; i < s.length; i++ ) {
-                if( s[i] !== "(" && s[i] !== ")") {
-                        reversedSubString += s[i];
-                }
-        }
-        return reversedSubString;
-}
+var reverseParentheses = function (inputString) {
+    let lastIndex = inputString.indexOf(")");
+    let firstIndex = inputString.lastIndexOf("(", lastIndex);
+    while (firstIndex != -1) {
+        let before = inputString.slice(0, firstIndex);
+        // prettier-ignore
+        let reversed = inputString
+                                    .slice(firstIndex + 1, lastIndex)
+                                    .split("")
+                                    .reverse()
+                                    .join("");
+        let after = inputString.slice(lastIndex + 1);
+        inputString = before + reversed + after;
+        lastIndex = inputString.indexOf(")");
+        firstIndex = inputString.lastIndexOf("(", lastIndex);
+    }
+    return inputString;
+};
