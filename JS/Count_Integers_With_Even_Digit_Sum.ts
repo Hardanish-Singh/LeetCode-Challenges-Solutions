eccbb3e35a3628e3effaab9235c2ff3a188c8645
Hardanish Singh <hardanishsingh1@gmail.com>
2024-08-21 08:11:23 -0400

    Count Integers With Even Digit Sum

--- JS/Count_Integers_With_Even_Digit_Sum.ts
+++ JS/Count_Integers_With_Even_Digit_Sum.ts
@@ -1,6 +1,6 @@
 // Leetcode: https://leetcode.com/problems/count-integers-with-even-digit-sum/
 
-var countEven = (num: number): number => {
+const countEven = (num: number): number => {
     // prettier-ignore
     return Array
                 .from({ length: num }, (_, i) => i + 1)
