7ad57bf81839053102e5fae6216e21552fb78144
Hardanish Singh <hardanishsingh1@gmail.com>
2024-08-24 09:21:51 -0400

    Divide Two Integers

--- JS/divide_two_integers.js
+++ JS/divide_two_integers.js
@@ -1,4 +1,4 @@
-// Leetcode:https://leetcode.com/problems/divide-two-integers/
+// Leetcode: https://leetcode.com/problems/divide-two-integers/
 
 var divide = (dividend, divisor) =>
     parseInt(dividend / divisor) > Math.pow(2, 31) - 1 ? Math.pow(2, 31) - 1 : parseInt(dividend / divisor);
