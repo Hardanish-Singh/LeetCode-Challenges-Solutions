e567fc2ecbf4075a1df3595d9222de26ae8c0307
Hardanish Singh <hardanishsingh1@gmail.com>
2024-08-25 14:19:20 -0400

    Fizz Buzz

--- JS/Fizz_Buzz.js
+++ JS/Fizz_Buzz.js
@@ -21,3 +21,15 @@ var fizzBuzz = (n) => {
     }
     return result;
 };
+
+// Solution 2
+var fizzBuzz = (n) =>
+    Array.from({ length: n }, (_, i) =>
+        (i + 1) % 3 === 0 && (i + 1) % 5 === 0
+            ? "FizzBuzz"
+            : (i + 1) % 3 === 0
+            ? "Fizz"
+            : (i + 1) % 5 === 0
+            ? "Buzz"
+            : String(i + 1)
+    );
