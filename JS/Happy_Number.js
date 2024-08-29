8e87abe39a163c82a37afd859a29804a61be90e9
Hardanish Singh <hardanishsingh1@gmail.com>
2024-08-29 07:47:47 -0400

    Happy Number

--- JS/Happy_Number.js
+++ JS/Happy_Number.js
@@ -6,9 +6,10 @@
  */
 const isHappy = (n) => {
     while (true) {
-        n = String(n)
-            .split("")
-            .reduce((acc, digit) => acc + Math.pow(Number(digit), 2), 0);
+        // prettier-ignore
+        n = n.toString()
+             .split("")
+             .reduce((acc, digit) => acc + Math.pow(Number(digit), 2), 0);
         if (n < 10) {
             return n === 1 || n === 7;
         }
