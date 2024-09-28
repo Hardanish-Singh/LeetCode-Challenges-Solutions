ccd4c069351b433334eb85ca4239a3a1f98b2aa9
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-09-28 17:50:24 -0400

    Count Student Number in Department

--- SQL/count_student_number_in_departments.sql
+++ SQL/count_student_number_in_departments.sql
@@ -1,70 +1,12 @@
-/*
-    A university uses 2 data tables, student and department, to store data about its students and the departments associated with each major.
+-- Leetcode: https://leetcode.com/problems/count-student-number-in-departments/
 
-    Write a query to print the respective department name and number of students majoring in each department for all departments in the department table (even ones with no current students).
-
-    Sort your results by descending number of students; if two or more departments have the same number of students, then sort those departments alphabetically by department name.
-
-    The student is described as follow:
-
-    | Column Name  | Type      |
-    |--------------|-----------|
-    | student_id   | Integer   |
-    | student_name | String    |
-    | gender       | Character |
-    | dept_id      | Integer   |
-    
-    where student_id is the student's ID number, student_name is the student's name, gender is their gender, and dept_id is the department ID associated with their declared major.
-
-    And the department table is described as below:
-
-    | Column Name | Type    |
-    |-------------|---------|
-    | dept_id     | Integer |
-    | dept_name   | String  |
-    
-    where dept_id is the department's ID number and dept_name is the department name.
-
-    Here is an example input:
-    
-    student table:
-
-    | student_id | student_name | gender | dept_id |
-    |------------|--------------|--------|---------|
-    | 1          | Jack         | M      | 1       |
-    | 2          | Jane         | F      | 1       |
-    | 3          | Mark         | M      | 2       |
-    
-    department table:
-
-    | dept_id | dept_name   |
-    |---------|-------------|
-    | 1       | Engineering |
-    | 2       | Science     |
-    | 3       | Law         |
-    
-    The Output should be:
-
-    | dept_name   | student_number |
-    |-------------|----------------|
-    | Engineering | 2              |
-    | Science     | 1              |
-    | Law         | 0              |
-*/
+-- Solution 1
 SELECT
-    department.dept_name AS dept_name,
-    COALESCE(student_number, 0) AS student_number
+    dept_name, 
+    COUNT(student_id) AS student_number
 FROM
     department
-LEFT JOIN
-(
-    SELECT
-        COUNT(dept_id) AS student_number,
-        dept_id
-    FROM
-        student
-    GROUP BY
-        dept_id
-)AS student ON department.dept_id = student.dept_id
-ORDER BY COALESCE(student_number, 0) DESC
+LEFT JOIN student USING(dept_id)
+GROUP BY department.dept_name
+ORDER BY student_number DESC, department.dept_name;
 
