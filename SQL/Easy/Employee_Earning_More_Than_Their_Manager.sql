77b1d00a220f2ef7af081f9410efaf8a1d6591a6
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-09-28 14:39:55 -0400

    Employee Earning More Than Their Manager

--- SQL/Easy/Employee_Earning_More_Than_Their_Manager.sql
+++ SQL/Easy/Employee_Earning_More_Than_Their_Manager.sql
@@ -6,10 +6,10 @@ SELECT
 FROM
     Employee AS emp
 JOIN Employee AS mgr ON mgr.id = emp.ManagerId
-WHERE emp.Salary > mgr.Salary
+WHERE emp.Salary > mgr.Salary;
 
 -- Solution 2
 SELECT
     emp.Name AS Employee
 FROM Employee emp, Employee mgr
-WHERE mgr.id = emp.ManagerId AND emp.Salary > mgr.Salary
+WHERE mgr.id = emp.ManagerId AND emp.Salary > mgr.Salary;
