98082eb40271d1cf43503819994a6b3ee802b7cf
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-09-29 14:26:45 -0400

    Consecutive Available Seats

--- SQL/consecutive_available_seats.sql
+++ SQL/consecutive_available_seats.sql
@@ -1,82 +1,36 @@
-/*
-        SQL Schema
-        
-        Several friends at a cinema ticket office would like to reserve consecutive available seats.
-        Can you help to query all the consecutive available seats order by the seat_id using the following cinema table?
-        | seat_id | free |
-        |---------|------|
-        | 1       | 1    |
-        | 2       | 0    |
-        | 3       | 1    |
-        | 4       | 1    |
-        | 5       | 1    |
-        
+-- Leetcode: https://leetcode.com/problems/consecutive-available-seats/
 
-        Your query should return the following result for the sample case above.
-        
-
-        | seat_id |
-        |---------|
-        | 3       |
-        | 4       |
-        | 5       |
-        
-        Note:
-                The seat_id is an auto increment int, and free is bool ('1' means free, and '0' means occupied.).
-                Consecutive available seats are more than 2(inclusive) seats consecutively available.
-
-*/
-SELECT
-    *
+-- Solution 1
+SELECT 
+  DISTINCT a.seat_id
 FROM
-(
-    SELECT  
-            CASE WHEN is_consecutive = 1 THEN seat_id-1
-            END AS seat_id
-    FROM
-    (
-            SELECT  
-                    *,
-                    CASE WHEN free = previous_seat AND free != 0 THEN @rn := @rn + 1
-                    ELSE @rn := 0
-                    END AS is_consecutive
-            FROM 
-            (
-                    SELECT 
-                            *,
-                            LAG(free) OVER (
-                                    ORDER BY seat_id
-                            ) AS previous_seat,
-                            (SELECT @rn := 0) AS rn
-                    FROM 
-                            cinema
-            )AS SubQuery1
-    )AS SubQuery2
-    WHERE SubQuery2.is_consecutive = 1
+    Cinema AS a, Cinema AS b
+WHERE ABS(b.seat_id - a.seat_id) = 1 AND a.free AND b.free
+ORDER BY 1;
 
-    UNION ALL 
+-- Solution 2
+WITH CinemaNeighbors AS (
+        SELECT
+                *,
+                LAG(free) OVER( ORDER BY seat_id ) AS prev_free,
+                LEAD(free) OVER( ORDER BY seat_id ) AS next_free
+        FROM Cinema
+)
+SELECT 
+        seat_id
+FROM CinemaNeighbors
+WHERE free = 1 AND (prev_free = 1 OR next_free = 1)
+ORDER BY 1;
 
-    SELECT  
-            seat_id
-    FROM
-    (
-            SELECT  
-                    *,
-                    CASE WHEN free = previous_seat AND free != 0 THEN @rn := @rn + 1
-                    ELSE @rn := 0
-                    END AS is_consecutive
-            FROM 
-            (
-                    SELECT 
-                            *,
-                            LAG(free) OVER (
-                                    ORDER BY seat_id
-                            ) AS previous_seat,
-                            (SELECT @rn := 0) AS rn
-                    FROM 
-                            cinema
-            )AS SubQuery1
-    )AS SubQuery2
-    WHERE SubQuery2.is_consecutive >= 1
-)AS MainQuery
-ORDER BY MainQuery.seat_id;
+-- Solution 3
+WITH CinemaNeighbors AS (
+        SELECT
+                *,
+                SUM(free) OVER ( ORDER BY seat_id ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING ) AS count
+        FROM Cinema
+)
+SELECT 
+        seat_id
+FROM CinemaNeighbors
+WHERE free = 1 AND count > 1
+ORDER BY 1;
