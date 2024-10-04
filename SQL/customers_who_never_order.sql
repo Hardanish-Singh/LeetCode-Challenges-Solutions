52e954e75c373475807ca9ddb07dbd640b65face
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-10-04 12:19:38 -0400

    Customers Who Never Order

--- SQL/customers_who_never_order.sql
+++ SQL/customers_who_never_order.sql
@@ -1,33 +1,5 @@
-/*
-    Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+-- Leetcode: https://leetcode.com/problems/customers-who-never-order/
 
-    Table: Customers.
-
-            +----+-------+
-            | Id | Name  |
-            +----+-------+
-            | 1  | Joe   |
-            | 2  | Henry |
-            | 3  | Sam   |
-            | 4  | Max   |
-            +----+-------+
-    Table: Orders.
-
-            +----+------------+
-            | Id | CustomerId |
-            +----+------------+
-            | 1  | 3          |
-            | 2  | 1          |
-            +----+------------+
-    Using the above tables as example, return the following:
-
-            +-----------+
-            | Customers |
-            +-----------+
-            | Henry     |
-            | Max       |
-            +-----------+
-*/
 SELECT
         Customers.Name AS Customers
 FROM
