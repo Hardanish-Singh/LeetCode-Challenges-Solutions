3907751cfbe507453f28d3ecfc74ccbbda13021b
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-10-05 17:31:52 -0400

    Employee Bonus

--- SQL/employee_bonus.sql
+++ SQL/employee_bonus.sql
@@ -1,38 +1,5 @@
-/*
-    Select all employee's name and bonus whose bonus is < 1000.
+-- Leetcode: https://leetcode.com/problems/employee-bonus/
 
-    Table:Employee
-
-    +-------+--------+-----------+--------+
-    | empId |  name  | supervisor| salary |
-    +-------+--------+-----------+--------+
-    |   1   | John   |  3        | 1000   |
-    |   2   | Dan    |  3        | 2000   |
-    |   3   | Brad   |  null     | 4000   |
-    |   4   | Thomas |  3        | 4000   |
-    +-------+--------+-----------+--------+
-    empId is the primary key column for this table.
-   
-    Table: Bonus
-
-    +-------+-------+
-    | empId | bonus |
-    +-------+-------+
-    | 2     | 500   |
-    | 4     | 2000  |
-    +-------+-------+
-    empId is the primary key column for this table.
-    
-    Example ouput:
-
-    +-------+-------+
-    | name  | bonus |
-    +-------+-------+
-    | John  | null  |
-    | Dan   | 500   |
-    | Brad  | null  |
-    +-------+-------+
-*/
 SELECT
     employee.name,
     bonus.bonus
