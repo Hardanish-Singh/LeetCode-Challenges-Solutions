9c5f53e75d7eac1204d96ba51f6aad0f07cd4180
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-10-06 14:48:42 -0400

    Friend Requests II Who Has the Most Friends

--- SQL/friend_requests_II_who_has_the_most_friends.sql
+++ SQL/friend_requests_II_who_has_the_most_friends.sql
@@ -1,56 +1,33 @@
-/*
-        SQL Schema
-        In social network like Facebook or Twitter, people send friend requests and accept others' requests as well.
+-- Leetcode: https://leetcode.com/problems/friend-requests-ii-who-has-the-most-friends/
 
-        
-
-        Table request_accepted
-
-        +--------------+-------------+------------+
-        | requester_id | accepter_id | accept_date|
-        |--------------|-------------|------------|
-        | 1            | 2           | 2016_06-03 |
-        | 1            | 3           | 2016-06-08 |
-        | 2            | 3           | 2016-06-08 |
-        | 3            | 4           | 2016-06-09 |
-        +--------------+-------------+------------+
-        This table holds the data of friend acceptance, while requester_id and accepter_id both are the id of a person.
-        
-
-        Write a query to find the the people who has most friends and the most friends number under the following rules:
-
-        It is guaranteed there is only 1 people having the most friends.
-        The friend request could only been accepted once, which mean there is no multiple records with the same requester_id and accepter_id value.
-        For the sample data above, the result is:
-
-        Result table:
-        +------+------+
-        | id   | num  |
-        |------|------|
-        | 3    | 3    |
-        +------+------+
-        The person with id '3' is a friend of people '1', '2' and '4', so he has 3 friends in total, which is the most number than any others.
-*/
+-- Solution 1
 SELECT 
-        *
+    id, 
+    COUNT(id) AS num
 FROM
 (
-    SELECT 
-        id, 
-        COUNT(id) AS num
+    SELECT
+        requester_id as id
     FROM
-    (
-        SELECT
-            requester_id as id
-        FROM
-            request_accepted
-        UNION ALL
-        SELECT
-            accepter_id as id
-        FROM
-            request_accepted
-    )AS x
-    GROUP BY x.id
-) AS xx
+        RequestAccepted
+    UNION ALL
+    SELECT
+        accepter_id as id
+    FROM
+        RequestAccepted
+)AS x
+GROUP BY x.id
 ORDER BY num DESC
 LIMIT 1
+
+-- Solution 2
+WITH CTE AS (
+        SELECT requester_id, accepter_id FROM RequestAccepted
+        UNION
+        SELECT accepter_id, requester_id FROM RequestAccepted
+    )
+SELECT requester_id AS id, COUNT(accepter_id) AS num
+FROM CTE
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
