958ec268d74a91035a50600cc8e28d8a637e1bcf
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-10-06 10:37:40 -0400

    Exchange Seats

--- SQL/exchange_seats.sql
+++ SQL/exchange_seats.sql
@@ -1,56 +1,9 @@
-/*
-        SQL Schema
-        
-        Mary is a teacher in a middle school and she has a table seat storing students' names and their corresponding seat ids.
+-- Leetcode: https://leetcode.com/problems/exchange-seats/
 
-        The column id is continuous increment.
-
-        Mary wants to change seats for the adjacent students.
-
-        Can you write a SQL query to output the result for Mary?
-
-                +---------+---------+
-                |    id   | student |
-                +---------+---------+
-                |    1    | Abbot   |
-                |    2    | Doris   |
-                |    3    | Emerson |
-                |    4    | Green   |
-                |    5    | Jeames  |
-                +---------+---------+
-        
-        For the sample input, the output is:
-
-                +---------+---------+
-                |    id   | student |
-                +---------+---------+
-                |    1    | Doris   |
-                |    2    | Abbot   |
-                |    3    | Green   |
-                |    4    | Emerson |
-                |    5    | Jeames  |
-                +---------+---------+
-        
-        Note:If the number of students is odd, there is no need to change the last one's seat.
-*/
-SELECT
+SELECT 
     id,
-    CASE WHEN MOD(id,2) = 0 THEN COALESCE(next_seat, student)
-         ELSE COALESCE(previous_seat, student)
+    CASE
+        WHEN MOD(id, 2) = 0 THEN LAG(student) OVER(ORDER BY id)
+        ELSE COALESCE(LEAD(student) OVER(ORDER BY id), student)
     END AS student
-FROM
-(
-    SELECT
-        id,
-        student,
-        LAG(student) OVER 
-        (
-                ORDER BY id
-        ) AS next_seat,
-        LEAD(student) OVER 
-        (
-                ORDER BY id
-        ) AS previous_seat
-    FROM
-        seat
-) AS SubQuery
+FROM Seat
