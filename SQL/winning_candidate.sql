20c9d2b29ba2642fd87d3465dafdfa37929c997a
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-10-07 06:52:03 -0400

    Winning Candidate

--- SQL/winning_candidate.sql
+++ SQL/winning_candidate.sql
@@ -1,40 +1,6 @@
-/*
-    Table: Candidate
+-- Leetcode: https://leetcode.com/problems/winning-candidate/
 
-    +-----+---------+
-    | id  | Name    |
-    +-----+---------+
-    | 1   | A       |
-    | 2   | B       |
-    | 3   | C       |
-    | 4   | D       |
-    | 5   | E       |
-    +-----+---------+  
-    
-    Table: Vote
-
-    +-----+--------------+
-    | id  | CandidateId  |
-    +-----+--------------+
-    | 1   |     2        |
-    | 2   |     4        |
-    | 3   |     3        |
-    | 4   |     2        |
-    | 5   |     5        |
-    +-----+--------------+
-    id is the auto-increment primary key,
-    CandidateId is the id appeared in Candidate table.
-    
-    Write a sql to find the name of the winning candidate, the above example will return the winner B.
-
-    +------+
-    | Name |
-    +------+
-    | B    |
-    +------+
-    
-    Notes: You may assume there is no tie, in other words there will be only one winning candidate.
-*/
+-- Solution 1
 SELECT
     name
 FROM
@@ -44,4 +10,14 @@ JOIN
     SELECT CandidateId, COUNT(*) AS c FROM vote GROUP BY CandidateId  
 )AS vote ON vote.CandidateId   = candidate.id
 ORDER BY c DESC
-LIMIT 1
+LIMIT 1;
+
+-- Solution 2
+SELECT 
+    name
+FROM
+    Candidate AS c
+LEFT JOIN Vote AS v ON c.id = v.candidateId
+GROUP BY c.id
+ORDER BY COUNT(name) DESC
+LIMIT 1;
