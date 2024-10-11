de2a0b91a3227b5cd0927609319f4c7b333bc0b6
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-10-11 16:26:48 -0400

    Get Highest Answer Rate Question

--- SQL/get_highest_answer_rate_question.sql
+++ SQL/get_highest_answer_rate_question.sql
@@ -1,32 +1,12 @@
 -- Leetcode: https://leetcode.com/problems/get-highest-answer-rate-question/
 
-SELECT  
-        question_id AS survey_log 
-FROM 
-( 
-    SELECT 
-       a / (
-                SELECT 
-                    COUNT(s2.action) 
-                FROM 
-                    survey_log s2 
-                WHERE s2.action = 'show' 
-                AND s2.question_id = x.question_id 
-                GROUP BY 
-                    s2.question_id 
-            ) AS ans,
-       question_id
-    FROM 
-    (
-        SELECT
-            COUNT(s1.action) AS a,
-            question_id
-        FROM
-            survey_log s1
-        WHERE s1.action = 'answer'
-        GROUP BY s1.question_id
-    ) AS x
-    GROUP BY question_id
-) AS xx 
-ORDER BY ans DESC 
-LIMIT 1
+WITH CTE AS (
+    SELECT
+        question_id AS survey_log,
+        ( SUM(action = 'answer') OVER (PARTITION BY question_id) ) / ( SUM(action = 'show') OVER (PARTITION BY question_id) ) AS ratio
+    FROM SurveyLog
+)
+SELECT survey_log
+FROM CTE
+ORDER BY ratio DESC, survey_log ASC
+LIMIT 1;
