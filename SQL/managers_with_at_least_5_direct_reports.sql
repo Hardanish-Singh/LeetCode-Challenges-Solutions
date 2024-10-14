86746eece3d6133d3f2274700eab0ee5f5515672
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-10-14 10:41:10 -0400

    Managers with at Least 5 Direct Reports

--- SQL/managers_with_at_least_5_direct_reports.sql
+++ SQL/managers_with_at_least_5_direct_reports.sql
@@ -1,25 +1,18 @@
-/*
-        The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+-- Leetcode: https://leetcode.com/problems/managers-with-at-least-5-direct-reports/
 
-        +------+----------+-----------+----------+
-        |Id    |Name 	  |Department |ManagerId |
-        +------+----------+-----------+----------+
-        |101   |John 	  |A 	      |null      |
-        |102   |Dan 	  |A 	      |101       |
-        |103   |James 	  |A 	      |101       |
-        |104   |Amy 	  |A 	      |101       |
-        |105   |Anne 	  |A 	      |101       |
-        |106   |Ron 	  |B 	      |101       |
-        +------+----------+-----------+----------+
-        
-        Given the Employee table, write a SQL query that finds out managers with at least 5 direct report. For the above table, your SQL query should return:
+-- Solution 1
+SELECT 
+        name 
+FROM 
+        Employee 
+WHERE id IN (
+        SELECT managerId 
+        FROM Employee 
+        GROUP BY managerId 
+        HAVING COUNT(managerId) >= 5
+);
 
-        +-------+
-        | Name  |
-        +-------+
-        | John  |
-        +-------+
-*/
+-- Solution 2
 SELECT
         e1.name
 FROM
