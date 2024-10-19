d43182348123e2c02f3851d7b7847fe043c5e92e
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-10-19 07:32:32 -0400

    Rising Temperature

--- SQL/rising_temperature.sql
+++ SQL/rising_temperature.sql
@@ -1,24 +1,4 @@
-/*
-    SQL Schema
-        Given a Weather table, write a SQL query to find all dates' Ids with higher temperature compared to its previous (yesterday's) dates.
-
-        +---------+------------------+------------------+
-        | Id(INT) | RecordDate(DATE) | Temperature(INT) |
-        +---------+------------------+------------------+
-        |       1 |       2015-01-01 |               10 |
-        |       2 |       2015-01-02 |               25 |
-        |       3 |       2015-01-03 |               20 |
-        |       4 |       2015-01-04 |               30 |
-        +---------+------------------+------------------+
-        For example, return the following Ids for the above Weather table:
-
-        +----+
-        | Id |
-        +----+
-        |  2 |
-        |  4 |
-        +----+
-*/
+-- Leetcode: https://leetcode.com/problems/rising-temperature/
 
 SELECT w2.id 
 FROM WEATHER AS w1
