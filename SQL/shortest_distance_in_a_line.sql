4e8892c0f13c66ac87fa9fca19b90b4cb40c4be2
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-10-19 09:18:58 -0400

    Shortest Distance in a Line

--- SQL/shortest_distance_in_a_line.sql
+++ SQL/shortest_distance_in_a_line.sql
@@ -1,38 +1,27 @@
-/*
-        SQL Schema
-                Table point holds the x coordinate of some points on x-axis in a plane, which are all integers.
-        
-        Write a query to find the shortest distance between two points in these points.
+-- Leetcode: https://leetcode.com/problems/shortest-distance-in-a-line/
 
-        |  x  |
-        |-----|
-        | -1  |
-        |  0  |
-        |  2  |
-        
-        The shortest distance is '1' obviously, which is from point '-1' to '0'. So the output is as below:
+-- Solution 1
+SELECT MIN(p2.x - p1.x) AS shortest
+FROM Point AS p1
+JOIN Point AS p2 ON p1.x < p2.x;
 
-        | shortest|
-        |---------|
-        |    1    |
-        
+-- Solution 2
+SELECT x - LAG(x) OVER (ORDER BY x) AS shortest
+FROM Point
+ORDER BY 1
+LIMIT 1, 1;
 
-        Note: Every point is unique, which means there is no duplicates in table point.
-        
-        Follow-up: What if all these points have an id and are arranged from the left most to the right most of x axis?
-*/
-SELECT 
-    MIN( distance ) AS shortest
+-- Solution 3
+SELECT MIN( distance ) AS shortest
 FROM 
 (
-    SELECT
-        ABS( x1-x2 ) AS distance
+    SELECT ABS( x1-x2 ) AS distance
     FROM
     (
         SELECT
             x AS x1,
             LAG( x, 1 ) OVER ( ) AS x2
         FROM
-            point
+            Point
     )AS SubQuery1
 )AS SubQuery2
