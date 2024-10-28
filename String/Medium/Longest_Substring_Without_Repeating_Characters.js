6d3c0e53d19c09118688f7458846d8c06f1378e7
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-10-28 06:45:47 -0400

    Longest Substring Without Repeating Characters

---
+++ String/Medium/Longest_Substring_Without_Repeating_Characters.js
@@ -0,0 +1,19 @@
+// Leetcode: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+const lengthOfLongestSubstring = (string) => {
+    let set = new Set();
+    let left = 0;
+    let right = 0;
+    let max = 0;
+    while (right < s.length) {
+        if (set.has(s[right])) {
+            set.delete(s[left]);
+            left++;
+        } else {
+            set.add(s[right]);
+            right++;
+        }
+        max = Math.max(max, set.size);
+    }
+    return max;
+};
