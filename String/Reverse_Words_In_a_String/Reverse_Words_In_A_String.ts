ca06b86e0ce9cbc47eef079eaa56a1c7fb435ed6
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-11-09 08:08:48 -0500

    Reverse Words In A String I

--- String/Reverse_Words_In_a_String/Reverse_Words_In_A_String.ts
+++ String/Reverse_Words_In_a_String/Reverse_Words_In_A_String.ts
@@ -1,13 +1,8 @@
 // Leetcode: https://leetcode.com/problems/reverse-words-in-a-string/
 
-/**
- * @param { string } str
- * @return { string }
-*/
-
-function reverseWords( str: string ): string {
-        return str.trim()
-                  .split(/\s+/) // REGEX USED TO SPIT STRING AT SINGLE OR MULTIPLE SPACES
-                  .reverse()
-                  .join(" ");
-};
+const reverseWords = (str: string): string =>
+    str
+        .trim()
+        .split(/\s+/) // REGEX USED TO SPIT STRING AT SINGLE OR MULTIPLE SPACES
+        .reverse()
+        .join(" ");
