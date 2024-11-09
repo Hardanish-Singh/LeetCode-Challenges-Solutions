0898d8da12a8329207182f0eeb35d94db0f28953
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-11-09 08:27:29 -0500

    Reverse Words In A String III

--- String/Reverse_Words_In_a_String/Reverse_Words_In_A_String_III.py
+++ String/Reverse_Words_In_a_String/Reverse_Words_In_A_String_III.py
@@ -1,22 +1,19 @@
 # Leetcode: https://leetcode.com/problems/reverse-words-in-a-string-iii/
 
-class Solution( object ):
-        def reverse( self, str, left, right ):
-                while left < right:
-                        str[left], str[right] = str[right], str[left]
-                        left, right = left + 1, right - 1
-        
+class Solution( object ):        
         def reverseWords( self, str ):
                 """
                 :type s: List[str]
                 :rtype: None Do not return anything, modify s in-place instead.
                 """
+                str = str + " "
                 str = list( str )
                 left = 0
                 for i in range( len( str ) ):
                         if str[i] == " ":
-                                self.reverse( str, left, i - 1 )
+                                part_before = str[:left]
+                                part_to_reverse = str[left:i]
+                                part_after = str[i:]
+                                str = part_before + part_to_reverse[::-1] + part_after
                                 left = i + 1
-                        elif i == len( str ) - 1:
-                                self.reverse( str, left, i )
-                return ''.join( str )
+                return ''.join( str ).strip()
