4ba169a157acc14bb98cda7c3febc496269a8dc6
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-11-10 10:38:13 -0500

    Add Two Promises

--- Array/Easy/Add_Two_Promises.ts
+++ Array/Easy/Add_Two_Promises.ts
@@ -1,3 +1,5 @@
+// Leetcode: https://leetcode.com/problems/add-two-promises/
+
 type P = Promise<number>;
 
 // Solution 1
@@ -11,6 +13,12 @@ var addTwoPromises = async (promise1: P, promise2: P): P => {
 // Solution 2
 var addTwoPromises = (promise1: P, promise2: P): P =>
     Promise.all([promise1, promise2]).then(([num1, num2]) => num1 + num2);
+
+// Solution 3
+var addTwoPromises = async (promise1: P, promise2: P): P =>
+    Promise.allSettled([promise1, promise2]).then((values) =>
+        values.reduce((accumulator, value) => accumulator + (value.status === "fulfilled" ? value.value : 0), 0)
+    );
 
 /**
  * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
