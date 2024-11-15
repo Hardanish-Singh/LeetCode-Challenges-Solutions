3c9917518483fbb77c6ec8bd2ed6c7600f3dbce3
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-11-15 17:34:19 -0500

    Duplicate Zeros

--- Array/Easy/Duplicate_Zeros.ts
+++ Array/Easy/Duplicate_Zeros.ts
@@ -3,7 +3,7 @@
 const duplicateZeros = (arr: number[]): void => {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === 0) {
-            arr.splice(i, 0, 0); // inserts element at index i
+            arr.splice(i, 0, 0); // arr.splice(index, 0, item); will insert item into arr at the specified index (deleting 0 items first, that is, it's just an insert).
             arr.pop();
             i++;
         }
