fd9c82b7c149281e39e5a24c28137faa759318f0
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-11-17 08:59:01 -0500

    Arranging Coins

---
+++ Array/Easy/Arranging_Coins.cpp
@@ -0,0 +1,11 @@
+// Leetcode: https://leetcode.com/problems/arranging-coins/
+
+class Solution {
+    public:
+        int arrangeCoins(int n, int k = 1) {
+            if( n < k ) {
+                return k - 1;
+            }
+            return arrangeCoins(n - k, k + 1);
+        }
+};
