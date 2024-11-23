0fb41acaf0b2a00ea46772bfa100b5d668fde75b
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-11-23 05:53:20 -0500

    Merge Sorted Array

--- Array/Easy/Merge_Sorted_Array.ts
+++ Array/Easy/Merge_Sorted_Array.ts
@@ -1,5 +1,6 @@
 // Leetcode: https://leetcode.com/problems/merge-sorted-array/
 
+// Solution 1
 const merge = (nums1: number[], m: number, nums2: number[], n: number): void => {
     nums1.length = m;
     nums1.push(...nums2);
