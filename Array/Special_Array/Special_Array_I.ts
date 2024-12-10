c4ba6fe3324f61aff1e9e4aa7c5db096482a615e
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-12-10 09:40:58 -0500

    Special Array I

--- Array/Special_Array/Special_Array_I.ts
+++ Array/Special_Array/Special_Array_I.ts
@@ -11,4 +11,15 @@ var isArraySpecial = (nums: number[]): boolean => {
     return true;
 };
 
+var isArraySpecial = (nums: number[]): boolean =>
+    nums.reduce((acc, currentElement, currentIndex) => {
+        const parity_1 = currentElement % 2;
+        const parity_2 = nums[currentIndex - 1] % 2;
+        if (parity_1 === parity_2) {
+            nums.length = 0;
+            return false;
+        }
+        return acc;
+    }, true);
+
 var isArraySpecial = (nums: number[]): boolean => nums.every((num, i) => i === 0 || num % 2 !== nums[i - 1] % 2);
