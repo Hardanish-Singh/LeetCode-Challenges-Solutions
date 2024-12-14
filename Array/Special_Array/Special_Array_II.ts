016e890fc7775fdc427bd890acd16866305245de
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-12-14 14:28:36 -0500

    Special Array II

--- Array/Special_Array/Special_Array_II.ts
+++ Array/Special_Array/Special_Array_II.ts
@@ -1,1 +1,14 @@
 // Leetcode: https://leetcode.com/problems/special-array-ii/
+
+// Solution 2
+var isArraySpecial = (nums: number[], queries: number[][]): boolean[] => {
+    const prefixSum = [0];
+    for (let i = 1; i < nums.length; ++i) {
+        if (nums[i] % 2 !== nums[i - 1] % 2) {
+            prefixSum.push(prefixSum[prefixSum.length - 1]);
+        } else {
+            prefixSum.push(prefixSum[prefixSum.length - 1] + 1);
+        }
+    }
+    return queries.map(([from, to]) => prefixSum[from] === prefixSum[to]);
+};
