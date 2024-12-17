4506fcdff0d563cd2b726c6d27f2613c47676c3e
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-12-17 06:52:50 -0500

    Preorder Traversal

--- Tree/Tree_Traversal/Preorder_Traversal.py
+++ Tree/Tree_Traversal/Preorder_Traversal.py
@@ -1,15 +1,8 @@
 # Leetcode: https://leetcode.com/problems/binary-tree-preorder-traversal/
 
-"""
-        # Definition for a binary tree node.
-        class TreeNode:
-                def __init__(self, val = 0, left = None, right = None):
-                        self.val = val
-                        self.left = left
-                        self.right = right
-"""
-
 from typing import List
+
+# Definition for a binary tree node.
 class TreeNode:
         def __init__(self, val = 0, left = None, right = None):
                 self.val = val
@@ -19,7 +12,6 @@ class TreeNode:
 """
         SOLUTION 1: RECURSIVE
 """
-
 class Solution:
         def preorderRecursiveTraversal(self, root: 'TreeNode', preOrderList: List[int]) -> List[int]:
                 if root is None:
@@ -36,7 +28,6 @@ class Solution:
 """
         SOLUTION 2: RECURSIVE
 """
-
 def preorderRecursiveTraversal(root: 'TreeNode', preOrderList: List[int]) -> List[int]:
                 if root is None:
                         return None
@@ -53,7 +44,6 @@ class Solution:
 """
         SOLUTION 3: ITERATIVE
 """
-
 class Solution:
         def preorderTraversal(self, root: 'TreeNode') -> List[int]:
                 if root is None:
