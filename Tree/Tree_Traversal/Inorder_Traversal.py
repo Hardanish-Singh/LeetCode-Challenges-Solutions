0c53f7f0d655434ff00eb233ae8a06f1f3e864d0
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-12-18 15:06:07 -0500

    Inorder Traversal

--- Tree/Tree_Traversal/Inorder_Traversal.py
+++ Tree/Tree_Traversal/Inorder_Traversal.py
@@ -1,16 +1,7 @@
 # Leetcode: https://leetcode.com/problems/binary-tree-inorder-traversal/
 
-
-"""
-        # Definition for a binary tree node.
-        class TreeNode:
-                def __init__(self, val = 0, left = None, right = None):
-                        self.val = val
-                        self.left = left
-                        self.right = right
-"""
-
 from typing import List
+
 class TreeNode:
         def __init__(self, val = 0, left = None, right = None):
                 self.val = val
