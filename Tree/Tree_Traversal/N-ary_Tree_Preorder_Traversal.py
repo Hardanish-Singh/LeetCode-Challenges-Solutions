ca9d60b25f0db9eb190c3c2155b6300b32138e4b
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-12-22 09:43:41 -0500

    N-ary Tree Preorder Traversal

--- Tree/Tree_Traversal/N-ary_Tree_Preorder_Traversal.py
+++ Tree/Tree_Traversal/N-ary_Tree_Preorder_Traversal.py
@@ -1,21 +1,26 @@
 # Leetcode: https://leetcode.com/problems/n-ary-tree-preorder-traversal/
 
-"""
-        # Definition for a Node.
-        class Node:
-                def __init__(self, val = None, children = None):
-                        self.val = val
-                        self.children = children
-"""
+from typing import List, Optional
 
-"""
-        SOLUTION 1: RECURSIVE
-"""
+# Definition for a binary tree node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
 
+
+# SOLUTION 1: RECURSIVE
 class Solution:
-        def preorderRecursiveTraversal(self, root: 'Node', preOrderList: List[int]) -> List[int]:
+        def preorder(self, root: Node) -> List[int]:
                 if root is None:
-                        return None
+                        return []
+                return [root.val] + sum([self.preorder(child) for child in root.children], []) #  sum(..., []) is used to concatenate lists or flattens the list of lists.
+
+# SOLUTION 2: RECURSIVE
+class Solution:
+        def preorderRecursiveTraversal(self, root: Node, preOrderList: List[int]) -> List[int]:
+                if root is None:
+                        return []
                 preOrderList.append(root.val)
                 for child in root.children:
                         self.preorderRecursiveTraversal(child, preOrderList)
@@ -24,21 +29,18 @@ class Solution:
                 self.preorderRecursiveTraversal(root, preOrderList)
                 return preOrderList
 
-"""
-        SOLUTION 2: ITERATIVE
-"""
-
+# SOLUTION 3: ITERATIVE
 class Solution:
-        def preorder(self, root: 'Node') -> List[int]:
+        def preorder(self, root: Node) -> List[int]:
                 if root is None:
                         return []
-                preOrderList = []
-                stack = [root]
+                preOrderList, stack = [], [root]
                 
                 while stack:
-                        currentNode = stack.pop()
-                        preOrderList.append(currentNode.val)
-                        stack.extend(child for child in currentNode.children[::-1])
+                        node = stack.pop()
+                        preOrderList.append(node.val)
+                        # stack.extend(child for child in node.children[::-1])
+                        stack.extend(child for child in reversed(node.children)) # Reversing the list of children to get the preorder traversal in the correct order.
                 
                 return preOrderList
         
