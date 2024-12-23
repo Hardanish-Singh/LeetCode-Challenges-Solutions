3f902a45cee6f5c5360e9f464f6dc27196c5deac
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-12-23 08:56:20 -0500

    N-ary Tree Postorder Traversal

--- Tree/Tree_Traversal/N-ary_Tree_Postorder_Traversal.py
+++ Tree/Tree_Traversal/N-ary_Tree_Postorder_Traversal.py
@@ -1,11 +1,21 @@
 # Leetcode: https://leetcode.com/problems/n-ary-tree-postorder-traversal/
 
 from typing import List, Optional
+# from itertools import chain
 
 class Node:
     def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
         self.val = val
         self.children = children
+
+# Solution 1: RECURSIVE
+class Solution:
+        def postorder(self, root: 'Node') -> List[int]:
+                if root is None:
+                        return []
+                # return [val for child in root.children for val in self.postorder(child)] + [root.val]
+                # return list(chain.from_iterable(self.postorder(child) for child in root.children)) + [root.val]
+                return sum([self.postorder(child) for child in root.children], []) + [root.val]
 
 # SOLUTION 2: RECURSIVE
 class Solution:
