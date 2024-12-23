adae5702d148584b0a4f2de3c3b850ee24346f4e
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-12-23 12:49:11 -0500

    Binary Tree Vertical Order Traversal

--- Tree/Tree_Traversal/Binary_Tree_Vertical_Order_Traversal.py
+++ Tree/Tree_Traversal/Binary_Tree_Vertical_Order_Traversal.py
@@ -1,14 +1,22 @@
 # Leetcode: https://leetcode.com/problems/binary-tree-vertical-order-traversal/
 
-"""
-        # Definition for a binary tree node.
-        class TreeNode:
-                def __init__(self, val = 0, left = None, right = None):
-                        self.val = val
-                        self.left = left
-                        self.right = right
-"""
+from typing import List
 
+# Definition for a binary tree node.
+class TreeNode:
+        def __init__(self, val = 0, left = None, right = None):
+                self.val = val
+                self.left = left
+                self.right = right
+
+
+"""
+        Here are the steps we might intuitively follow to come up with the solution:
+        1. Create a dictionary to store the nodes in each vertical order.
+        2. We use a queue to perform the level-order traversal, we record its value in our dictionary under its associated horizontal distance.
+        3. For every node we process, we enqueue its left child (if not null) with the horizontal distance decremented by 1, and its right child (if not null) with the horizontal distance incremented by 1.
+        4. After we have traversed the entire tree, we will have a dictionary filled with vertical levels as keys and lists of node values as values. The only thing left would be to sort these keys and output the associated lists in the sorted order.
+"""
 class Solution:
         def verticalOrder(self, root: TreeNode) -> List[List[int]]:
                 dictionary = {}
