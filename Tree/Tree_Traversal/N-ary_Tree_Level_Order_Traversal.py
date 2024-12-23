06e4efea90ef24133c1473595481df221a175ef8
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-12-23 09:10:33 -0500

    N-ary Tree Level Order Traversal

--- Tree/Tree_Traversal/N-ary_Tree_Level_Order_Traversal.py
+++ Tree/Tree_Traversal/N-ary_Tree_Level_Order_Traversal.py
@@ -32,10 +32,30 @@ class Solution:
 
                 while queue:
                         level, n = [], len(queue)
-                        for i in range(n):
+                        for _ in range(n):
                                 node = queue.pop(0)
                                 level.append(node.val)
                                 # queue.extend(child for child in node.children)
+                                queue.extend(node.children)
+                        result.append(level)
+                
+                return result
+
+# Solution 3: ITERATIVE USING DEQUE
+from collections import deque
+
+class Solution:
+        def levelOrder(self, root: Node) -> List[List[int]]:
+                if root is None:
+                        return []
+                queue, result = deque([root]), []
+
+                while queue:
+                        level_size = len(queue)
+                        level = []
+                        for _ in range(level_size):
+                                node = queue.popleft()
+                                level.append(node.val)
                                 queue.extend(node.children)
                         result.append(level)
                 
