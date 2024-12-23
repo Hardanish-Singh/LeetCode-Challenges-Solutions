96b54322fbb7a7c7226dd19398556e5458fc7586
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-12-23 09:09:19 -0500

    N-ary Tree Level Order Traversal

--- Tree/Tree_Traversal/N-ary_Tree_Level_Order_Traversal.py
+++ Tree/Tree_Traversal/N-ary_Tree_Level_Order_Traversal.py
@@ -1,27 +1,42 @@
 # Leetcode: https://leetcode.com/problems/n-ary-tree-level-order-traversal/
 
-"""
-        # Definition for a Node.
-        class Node:
-                def __init__(self, val = None, children = None):
-                        self.val = val
-                        self.children = children
-"""
+from typing import List, Optional
 
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val = val
+        self.children = children
+
+# Solution 1: RECURSIVE
 class Solution:
-        def levelOrder(self, root: 'Node') -> List[List[int]]:
+    def levelOrder(self, root: Node) -> List[List[int]]:
+        def traverse(node, level):
+            if node is None:
+                return []
+            if len(result) == level:
+                result.append([])
+            result[level].append(node.val)
+            for child in node.children:
+                traverse(child, level + 1)
+        
+        result = []
+        traverse(root, 0)
+        return result
+
+# Solution 2: ITERATIVE
+class Solution:
+        def levelOrder(self, root: Node) -> List[List[int]]:
                 if root is None:
                         return []
-                queue = [root]
-                result = []
+                queue, result = [root], []
 
                 while queue:
-                        level = []
-                        n = len(queue)
+                        level, n = [], len(queue)
                         for i in range(n):
                                 node = queue.pop(0)
                                 level.append(node.val)
-                                queue.extend(child for child in node.children)
+                                # queue.extend(child for child in node.children)
+                                queue.extend(node.children)
                         result.append(level)
                 
                 return result
