4d058b0750e3e730983a85422c679149f205e7ac
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-12-26 07:40:56 -0500

    Implement Stack Using Queues

---
+++ Array/Medium/Implement_Stack_using_Queues.ts
@@ -0,0 +1,64 @@
+// @ts-nocheck
+
+// Solution 1 (using Typescript classes)
+class MyStack {
+    #myStack: number[];
+    constructor() {
+        this.#myStack = [];
+    }
+
+    push(x: number): void {
+        this.#myStack.push(x);
+    }
+
+    pop(): number {
+        return this.#myStack.pop();
+    }
+
+    top(): number {
+        return this.#myStack[this.#myStack.length - 1];
+    }
+
+    empty(): boolean {
+        return this.#myStack.length === 0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
+
+// Solution 2 (using Javascript prototypes)
+var MyStack = function () {
+    this.myStack = [];
+};
+
+MyStack.prototype.push = function (x: number) {
+    this.myStack.push(x);
+};
+
+MyStack.prototype.pop = function (): number {
+    return this.myStack.pop();
+};
+
+MyStack.prototype.top = function (): number {
+    return this.myStack[this.myStack.length - 1];
+};
+
+MyStack.prototype.empty = function (): boolean {
+    return this.myStack.length === 0;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
