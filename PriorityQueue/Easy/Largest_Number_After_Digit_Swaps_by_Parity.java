a6fbe1885c4f5a74f0a16ffcc42c0dd69b884524
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-12-26 09:42:10 -0500

    Largest Number After Digit Swaps by Parity

--- PriorityQueue/Easy/Largest_Number_After_Digit_Swaps_by_Parity.java
+++ PriorityQueue/Easy/Largest_Number_After_Digit_Swaps_by_Parity.java
@@ -1,38 +1,24 @@
 // Leetcode: https://leetcode.com/problems/largest-number-after-digit-swaps-by-parity/
 
-class Solution 
+class Solution
 {
-        public int largestInteger(int num) 
+        public int largestInteger(int num)
         {
                 PriorityQueue<Integer> oddQueue = new PriorityQueue<Integer>((a,b) -> b - a);
                 PriorityQueue<Integer> evenQueue = new PriorityQueue<Integer>((a,b) -> b - a);
                 String s = Integer.toString(num);
 
-                for(int i = 0; i < s.length(); i++)
+                for (char c : s.toCharArray())
                 {
-                        int n = Character.getNumericValue( s.charAt(i) );
-                        if(n % 2 == 0)
-                        {
-                                evenQueue.add(n);
-                        }
-                        else
-                        {
-                                oddQueue.add(n);
-                        }
+                        int n = Character.getNumericValue(c);
+                        (n % 2 == 0 ? evenQueue : oddQueue).add(n);
                 }
 
                 StringBuilder sb = new StringBuilder();
-                for(int i = 0; i < s.length(); i++)
+                for (char c : s.toCharArray())
                 {
-                        int n = Character.getNumericValue( s.charAt(i) );
-                        if(n % 2 == 0)
-                        {
-                                sb.append(evenQueue.poll());
-                        }
-                        else
-                        {
-                                sb.append(oddQueue.poll());
-                        }
+                        int n = Character.getNumericValue(c);
+                        sb.append(n % 2 == 0 ? evenQueue.poll() : oddQueue.poll());
                 }
 
                 return Integer.parseInt(sb.toString());
