85025d5dd931a6e048af488b22478d43f55089b1
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-12-27 07:51:11 -0500

    Maximum Depth of a Binary Tree

--- Tree/Easy/Maximum_Depth_of_Binary_Tree.py
+++ Tree/Easy/Maximum_Depth_of_Binary_Tree.py
@@ -1,19 +1,14 @@
 # Leetcode: https://leetcode.com/problems/maximum-depth-of-binary-tree/
 
-"""
-        # Definition for a binary tree node.
-        class TreeNode:
-                def __init__(self, val = 0, left = None, right = None):
-                        self.val = val
-                        self.left = left
-                        self.right = right
-"""
+# Definition for a binary tree node.
+class TreeNode:
+        def __init__(self, val = 0, left = None, right = None):
+                self.val = val
+                self.left = left
+                self.right = right
 
 
-"""
-        SOLUTION 1: RECURSIVE DFS
-"""
-
+# SOLUTION 1: RECURSIVE DFS
 class Solution:
         def maxDepth(self, root: TreeNode) -> int:                
                 if root is None:
@@ -22,10 +17,7 @@ class Solution:
                 right = self.maxDepth(root.right)
                 return 1 + max(left, right)
 
-"""
-        SOLUTION 2: RECURSIVE DFS
-"""
-
+# SOLUTION 2: RECURSIVE DFS
 class Solution:
         def maxDepth(self, root: TreeNode) -> int:
                 if root is None:
@@ -34,21 +26,17 @@ class Solution:
                 right = 1 + self.maxDepth(root.right)
                 return max(left, right)
 
-"""
-        SOLUTION 3: ITERATIVE BFS / LEVEL ORDER TRAVERSAL
-"""
-
+# SOLUTION 3: ITERATIVE BFS / LEVEL ORDER TRAVERSAL
 class Solution:
         def maxDepth(self, root: TreeNode) -> int:
                 if root is None:
                         return 0
-                queue = [root]
-                result = []
+                queue, result = [root], []
 
                 while queue:
                         result.append(item.val for item in queue) # POP ALL ELEMENTS FROM QUEUE
                         n = len(queue)
-                        for i in range(n):
+                        for _ in range(n):
                                 item = queue.pop(0)
                                 if item.left:
                                         queue.append(item.left)
