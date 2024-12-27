0d20589ce3bc5de146a12bbb3e0f4a955f2e9255
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-12-27 08:26:16 -0500

    Delete Greatest Value in Each Row

--- PriorityQueue/Medium/Delete_Greatest_Value_in_Each_Row.java
+++ PriorityQueue/Medium/Delete_Greatest_Value_in_Each_Row.java
@@ -1,6 +1,8 @@
+// Leetcode: https://leetcode.com/problems/delete-greatest-value-in-each-row/
+
 class Solution {
     public int deleteGreatestValue(int[][] grid) {
-        int ans = 0;
+        int result = 0;
         int row = grid.length;
         int col = grid[0].length;
         PriorityQueue<Integer>[] pq = new PriorityQueue[row];
@@ -17,9 +19,9 @@ class Solution {
             for (int j = 0; j < row; j++) {
                 max = Math.max(max, pq[j].poll());
             }
-            ans += max;
+            result += max;
         }
 
-        return ans;
+        return result;
     }
 }
