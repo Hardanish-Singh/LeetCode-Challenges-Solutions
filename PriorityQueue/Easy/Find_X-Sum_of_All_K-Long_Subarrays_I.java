df7ee8eace21d88ea9a809cdb5906928785bafd7
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-01-01 10:58:59 -0500

    Find X-Sum of All K-Long Subarrays I

--- PriorityQueue/Easy/Find_X-Sum_of_All_K-Long_Subarrays_I.java
+++ PriorityQueue/Easy/Find_X-Sum_of_All_K-Long_Subarrays_I.java
@@ -1,2 +1,37 @@
-// Leetcode: https://leetcode.com/problems/find-x-sum-of-all-k-long-subarrays-i
+// Leetcode: https://leetcode.com/problems/find-x-sum-of-all-k-long-subarrays-i 
 
+class Solution {
+        public int[] findXSum(int[] nums, int k, int x) {
+                int n = nums.length;
+                int[] res = new int[n - k + 1];
+                for (int i = 0; i < n - k + 1; i++) {
+                        HashMap<Integer, Integer> map = new HashMap<>();
+
+                        for (int j = i; j < i + k; j++) {
+                                map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+                        }
+
+                        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> {
+                            // If the values is not same, then sort by value in descending order
+                            if (a.getValue() != b.getValue())
+                            {
+                                return b.getValue() - a.getValue();
+                            }
+                            // Else sort by key in descending order if values are same
+                            else
+                            {
+                                return b.getKey() - a.getKey();
+                            }
+                        });
+                        pq.addAll(map.entrySet());
+
+                        int sum = 0;
+                        for (int j = 0; j < x && !pq.isEmpty(); j++) {
+                                Map.Entry<Integer, Integer> e = pq.poll();
+                                sum += e.getKey() * e.getValue();
+                        }
+                        res[i] = sum;
+                }
+                return res;
+        }
+}
