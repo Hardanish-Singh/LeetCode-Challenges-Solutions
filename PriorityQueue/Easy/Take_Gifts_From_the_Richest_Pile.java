37e7dbd37bf1e6a4dc6812a4d8c5b9476e859626
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-01-02 07:07:28 -0500

    Take Gifts From the Richest Pile

--- PriorityQueue/Easy/Take_Gifts_From_the_Richest_Pile.java
+++ PriorityQueue/Easy/Take_Gifts_From_the_Richest_Pile.java
@@ -2,7 +2,8 @@
 
 class Solution
 {
-        public long pickGifts(int[] gifts, int k) {
+        public long pickGifts(int[] gifts, int k)
+        {
                 PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
                 for (int i : gifts) {
                         pq.add(i);
