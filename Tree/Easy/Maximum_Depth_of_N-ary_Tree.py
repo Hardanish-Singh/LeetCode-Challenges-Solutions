32e2a22875fe4efc12511c0385f7207c6957e192
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-01-03 10:08:19 -0500

    Maximum Depth of N-ary Tree

--- Tree/Easy/Maximum_Depth_of_N-ary_Tree.py
+++ Tree/Easy/Maximum_Depth_of_N-ary_Tree.py
@@ -8,6 +8,7 @@ class Node:
         self.val = val
         self.children = children
 
+# Solution 1
 class Solution:
         def maxDepth( self, root: 'Node' ) -> int:
                 if root is None:
@@ -19,5 +20,22 @@ class Solution:
                         n = len(queue)
                         for i in range(n):
                                queue.extend(queue.pop(0).children) # POP ALL ELEMENTS FROM QUEUE AND ADD THEIR CHILDREN TO QUEUE
+
+                return len(result)
+        
+# Solution 2
+from collections import deque # deque is a double-ended queue
+
+class Solution:
+        def maxDepth( self, root: 'Node' ) -> int:
+                if root is None:
+                        return 0
+                queue, result = deque([root]), []
+
+                while queue:
+                        result.append(item.val for item in queue) # POP ALL ELEMENTS FROM QUEUE
+                        n = len(queue)
+                        for i in range(n):
+                               queue.extend(queue.popleft().children) # POP ALL ELEMENTS FROM QUEUE AND ADD THEIR CHILDREN TO QUEUE
 
                 return len(result)
