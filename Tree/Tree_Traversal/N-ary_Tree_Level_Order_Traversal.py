57d008d2e247c7653dd5633ecae3009ac497e77e
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-01-03 09:59:00 -0500

    N-ary Tree Level Order Traversal

--- Tree/Tree_Traversal/N-ary_Tree_Level_Order_Traversal.py
+++ Tree/Tree_Traversal/N-ary_Tree_Level_Order_Traversal.py
@@ -2,6 +2,7 @@
 
 from typing import List, Optional
 
+# Definition for a Node.
 class Node:
     def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
         self.val = val = val
