cb5c3745e6e28c621f98639748ba4db03fab7405
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-01-03 09:59:52 -0500

    Maximum Depth of N-ary Tree

--- Tree/Easy/Maximum_Depth_of_N-ary_Tree.py
+++ Tree/Easy/Maximum_Depth_of_N-ary_Tree.py
@@ -1,12 +1,12 @@
 # Leetcode: https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
 
-"""
-        # Definition for a Node.
-        class Node:
-                def __init__(self, val = None, children = None):
-                        self.val = val
-                        self.children = children
-"""
+from typing import List, Optional
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
 
 class Solution:
         def maxDepth( self, root: 'Node' ) -> int:
