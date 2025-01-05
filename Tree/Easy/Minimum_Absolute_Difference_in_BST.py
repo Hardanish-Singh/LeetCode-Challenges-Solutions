a4a89ad1b3493faad698b132767245b39f005c27
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-01-05 07:31:04 -0500

    Minimum Absolute Difference in BST

--- Tree/Easy/Minimum_Absolute_Difference_in_BST.py
+++ Tree/Easy/Minimum_Absolute_Difference_in_BST.py
@@ -1,28 +1,25 @@
 # Leetcode: https://leetcode.com/problems/minimum-absolute-difference-in-bst/
 
-# Definition for a binary tree node.
-# class TreeNode( object ):
-#     def __init__( self, val = 0, left = None, right = None ):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import List
 
-def preorderRecursiveTraversal( root, preOrderList ):
+# Definition for a binary tree node.
+class TreeNode:
+        def __init__(self, val = 0, left = None, right = None):
+                self.val = val
+                self.left = left
+                self.right = right
+
+def preorderRecursiveTraversal(root: TreeNode, preOrderList: List[int]) -> None:
         if root is None:
                 return None
-        preOrderList.append( root.val )
-        preorderRecursiveTraversal( root.left, preOrderList )
-        preorderRecursiveTraversal( root.right, preOrderList )
+        preOrderList.append(root.val)
+        preorderRecursiveTraversal(root.left, preOrderList)
+        preorderRecursiveTraversal(root.right, preOrderList)
         
         
-class Solution( object ):
-        def getMinimumDifference( self, root ):
-                """
-                :type root: TreeNode
-                :rtype: int
-                """
+class Solution:
+        def getMinimumDifference(self, root: TreeNode) -> int:
                 preOrderList = []
-                preorderRecursiveTraversal( root, preOrderList )
-                
+                preorderRecursiveTraversal(root, preOrderList)
                 preOrderList.sort()
                 return min(preOrderList[i + 1] - preOrderList[i] for i in range(len(preOrderList) - 1))
