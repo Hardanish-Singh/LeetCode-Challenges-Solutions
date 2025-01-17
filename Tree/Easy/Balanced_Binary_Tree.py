cf143a3fc2664bf8aa5536f40c735d7a0eb9281c
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-01-17 09:11:46 -0500

    Balanced Binary Tree

--- Tree/Easy/Balanced_Binary_Tree.py
+++ Tree/Easy/Balanced_Binary_Tree.py
@@ -1,41 +1,30 @@
 # Leetcode: https://leetcode.com/problems/balanced-binary-tree/
 
 # Definition for a binary tree node.
-# class TreeNode( object ):
-#     def __init__( self, val = 0, left = None, right = None ):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+        def __init__(self, val = 0, left = None, right = None):
+                self.val = val
+                self.left = left
+                self.right = right
 
 """
         SOLUTION 1: RECURSIVE
 """
 
-class Solution( object ):
-        def isBalanced( self, root ):
-                """
-                :type root: TreeNode
-                :rtype: bool
-                """
-                if root is None:
-                        return True
-
-                self.result = True
-                self.isTreeBalanced( root )
-                return self.result
-
-        def isTreeBalanced( self, root ):
-                if root is None:
-                        return 0
-                
-                left = self.isTreeBalanced( root.left )
-                right = self.isTreeBalanced( root.right )
-
-                if abs( left - right ) > 1:
-                        self.result = False
-                        return self.result
-                
-                return 1 + max( left, right )
+class Solution:
+        def isBalanced(self, root: TreeNode) -> bool:
+                isBalanced = True
+                def dfs(node: TreeNode) -> int:
+                        nonlocal isBalanced
+                        if not node:
+                                return 0
+                        left = dfs(node.left)
+                        right = dfs(node.right)
+                        if abs(left - right) > 1:
+                                isBalanced = False
+                        return 1 + max(left, right)
+                dfs(root)
+                return isBalanced
 
 """
         SOLUTION 2: ITERATIVE
