39bacb3d4242ed2c148f9af5cde64a61b1957ad7
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-03-02 06:43:47 -0500

    Binary Tree Paths

--- Tree/Easy/Binary_Tree_Paths.py
+++ Tree/Easy/Binary_Tree_Paths.py
@@ -3,8 +3,8 @@
 from typing import List
 
 # Definition for a binary tree node.
-class TreeNode( object ):
-    def __init__( self, val = 0, left = None, right = None ):
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
         self.val = val
         self.left = left
         self.right = right
