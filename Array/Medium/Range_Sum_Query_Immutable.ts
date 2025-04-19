091095c6036896a328cf403e539c644a60343e68
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-19 08:23:53 -0400

    Range Sum Query Immutable

--- Array/Medium/Range_Sum_Query_Immutable.ts
+++ Array/Medium/Range_Sum_Query_Immutable.ts
@@ -21,13 +21,25 @@ class NumArray {
 
 // Solution 2 ( using Typescript classes )
 class NumArray {
-    // private nums: number[];
-
-    // constructor(nums: number[]) {
-    //     this.nums = nums;
-    // }
-
     constructor(private nums: number[]) {}
+
+    sumRange(left: number, right: number): number {
+        let sum = 0;
+        // Iterate over the range [left, right] and sum the elements
+        for (let i = left; i <= right; i++) {
+            sum += this.nums[i];
+        }
+        return sum;
+    }
+}
+
+// Solution 3 ( using Typescript classes )
+class NumArray {
+    private nums: number[];
+
+    constructor(nums: number[]) {
+        this.nums = nums;
+    }
 
     sumRange(left: number, right: number): number {
         let sum = 0;
