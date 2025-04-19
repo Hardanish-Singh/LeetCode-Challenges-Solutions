1e50232fc42968cbf78620dd388fc361486f84b5
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-19 08:26:01 -0400

    Range Sum Query Immutable

--- Array/Medium/Range_Sum_Query_Immutable.ts
+++ Array/Medium/Range_Sum_Query_Immutable.ts
@@ -52,3 +52,20 @@ class NumArray {
 }
 
 // Solution 4 (using Javascript prototypes)
+var NumArray = function (nums: number[]) {
+    this.nums = nums;
+};
+
+NumArray.prototype.sumRange = function (left: number, right: number): number {
+    let sum = 0;
+    for (let i = left; i <= right; i++) {
+        sum += this.nums[i];
+    }
+    return sum;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
