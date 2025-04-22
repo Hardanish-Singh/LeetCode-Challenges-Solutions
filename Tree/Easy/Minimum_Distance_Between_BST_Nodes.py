2cf1c66d37b0bc6ccb61a0fb76b7225041d6e448
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-22 08:06:17 -0400

    Minimum Distance Between BST Nodes

--- Tree/Easy/Minimum_Distance_Between_BST_Nodes.py
+++ Tree/Easy/Minimum_Distance_Between_BST_Nodes.py
@@ -1,31 +1,27 @@
+# Leetcode: https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+
 # Definition for a binary tree node.
-# class TreeNode( object ):
-#     def __init__( self, val = 0, left = None, right = None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+        def __init__(self, val = 0, left = None, right = None):
+                self.val = val
+                self.left = left
+                self.right = right
 
 
-def preorderRecursiveTraversal( root, preOrderList ):
+def preorderRecursiveTraversal(root, preOrderList: list[int]) -> None:
         if root is None:
                 return None
-        preOrderList.append( root.val )
-        preorderRecursiveTraversal( root.left, preOrderList )
-        preorderRecursiveTraversal( root.right, preOrderList )
+        preOrderList.append(root.val)
+        preorderRecursiveTraversal(root.left, preOrderList)
+        preorderRecursiveTraversal(root.right, preOrderList)
         
         
-class Solution(object):
-        def minDiffInBST(self, root):
-                """
-                :type root: TreeNode
-                :rtype: int
-                """
+class Solution:
+        def minDiffInBST(self, root: TreeNode) -> int:
                 preOrderList = []
-                preorderRecursiveTraversal( root, preOrderList )
-                
+                preorderRecursiveTraversal(root, preOrderList)
                 preOrderList.sort()
-                difference = [ ]
-                for i in range( 0, len( preOrderList ) - 1 ):
-                        difference.append( preOrderList[ i + 1 ] - preOrderList[ i ] )
-
-                return min( difference )
+                difference = []
+                for i in range(0, len(preOrderList) - 1):
+                        difference.append(preOrderList[i+1] - preOrderList[i])
+                return min(difference)
