4d45f5539640a1bf15abeb8d1a144fcb15e5df6f
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-23 19:48:01 -0400

    Path Sum

--- Tree/Easy/Path_Sum.py
+++ Tree/Easy/Path_Sum.py
@@ -7,9 +7,7 @@ class TreeNode:
                 self.left = left
                 self.right = right
 
-"""
-        SOLUTION 1
-"""
+# SOLUTION 1
 class Solution:
         def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
                 if root is None:
@@ -32,24 +30,39 @@ class Solution:
                                 return True
                 
                 return False
-
-"""
-        SOLUTION 2
-"""
+        
+# SOLUTION 2
 class Solution:
-        def hasPathSum( self, root: TreeNode, targetSum: int ) -> bool:
+        def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
                 if root is None:
                         return False
-                
-                stack = [ [ root, targetSum - root.val ]  ]
-                
-                while len( stack ) > 0:
-                        currentNode, pathSum = stack.pop()
-                        if currentNode.left is None and currentNode.right is None and pathSum == 0:
+                stack = [[root, root.val]]
+
+                while stack:
+                        currentNode, currentSum = stack.pop()
+                        if currentNode.left is None and currentNode.right is None and currentSum == targetSum:
                                 return True
                         if currentNode.right:
-                                stack.append( [ currentNode.right, pathSum - currentNode.right.val ] )
+                                stack.append([currentNode.right, currentSum + currentNode.right.val])
                         if currentNode.left:
-                                stack.append( [ currentNode.left, pathSum - currentNode.left.val ] )
+                                stack.append([currentNode.left, currentSum + currentNode.left.val])
+                
+                return False
+
+# SOLUTION 3
+class Solution:
+        def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+                if not root:
+                        return False
+                stack = [[root, targetSum - root.val]]
+                
+                while stack:
+                        currentNode, remainingSum = stack.pop()
+                        if not currentNode.left and not currentNode.right and remainingSum == 0:
+                                return True
+                        if currentNode.right:
+                                stack.append([currentNode.right, remainingSum - currentNode.right.val])
+                        if currentNode.left:
+                                stack.append([currentNode.left, remainingSum - currentNode.left.val])
                 
                 return False
