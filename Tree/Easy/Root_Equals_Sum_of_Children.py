6d1579852542134430ab6ddd6e2d6a1c79cd5e26
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-25 15:50:48 -0400

    Root Equals Sum of Children

--- Tree/Easy/Root_Equals_Sum_of_Children.py
+++ Tree/Easy/Root_Equals_Sum_of_Children.py
@@ -1,5 +1,7 @@
 # Leetcode: https://leetcode.com/problems/root-equals-sum-of-children/
 
+from typing import Optional
+
 # Definition for a binary tree node.
 class TreeNode:
         def __init__(self, val = 0, left = None, right = None):
@@ -7,15 +9,6 @@ class TreeNode:
                 self.left = left
                 self.right = right
 
-class Solution( object ):
-        def checkTree( self, root ):
-                """
-                :type root: Optional[TreeNode]
-                :rtype: bool
-                """
-
-                if( root.val == root.left.val + root.right.val ):
-                        return True
-                else:
-                        return False
-        
+class Solution:
+        def checkTree(self, root: Optional[TreeNode]) -> bool:
+                return root.val == root.left.val + root.right.val
