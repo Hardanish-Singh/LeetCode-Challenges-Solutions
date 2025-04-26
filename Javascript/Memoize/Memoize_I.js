593082d9b91f64851d9c803ec9d55c5dd6fef18e
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-26 09:24:55 -0400

    Memoize I

--- Javascript/Memoize/Memoize_I.js
+++ Javascript/Memoize/Memoize_I.js
@@ -1,5 +1,6 @@
 // Leetcode: https://leetcode.com/problems/memoize/
 
+// Solution 1: Memoize a function with a cache object
 /**
  * @param {Function} fn
  * @return {Function}
@@ -18,6 +19,35 @@ function memoize(fn) {
     };
 }
 
+/**
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1
+ */
+
+// Solution 2: Memoize a function with a Map object
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = new Map();
+    return function (...args) {
+        const key = JSON.stringify(args);
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+        // const result = fn.apply(this, args);
+        const result = fn(...args);
+        cache.set(key, result);
+        return result;
+    };
+}
 /**
  * let callCount = 0;
  * const memoizedFn = memoize(function (a, b) {
