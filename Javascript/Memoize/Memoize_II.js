b4d0db93d104ca0cd8631b79ae3a2dd29d165c41
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-26 09:30:17 -0400

    Memoize II

--- Javascript/Memoize/Memoize_II.js
+++ Javascript/Memoize/Memoize_II.js
@@ -1,12 +1,19 @@
 // Leetcode: https://leetcode.com/problems/memoize-ii/
 
+/* 
+    This version is a harder one because it relaxes the constraints:
+        1) Any type of input is allowed (object, array, number, etc).
+        2) Inputs are identical if they are === to each other (strict equality).
+        3) So you can't just JSON.stringify to create a key like before. Instead, you need to cache based on references properly.
+*/
+
+// Solution 1: Memoize a function with a Map object
 /**
  * @param {Function} fn
  * @return {Function}
  */
 function memoize(fn) {
     const cache = new Map();
-
     return function (...args) {
         let curr = cache;
         for (const arg of args) {
@@ -24,6 +31,46 @@ function memoize(fn) {
     };
 }
 
+/**
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1
+ */
+
+/* 
+    Solution 2:
+        Since objects does not prevent garbage collection, we can use:
+            1) WeakMap for object keys (because WeakMap doesn't prevent GC)
+            2) Map for primitive keys (number, string, etc.)
+*/
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = new Map();
+    return function (...args) {
+        let curr = cache;
+        for (const arg of args) {
+            const isObject = arg !== null && (typeof arg === "object" || typeof arg === "function");
+            if (!curr.has(arg)) {
+                curr.set(arg, isObject ? new WeakMap() : new Map());
+            }
+            curr = curr.get(arg);
+        }
+        if (curr.has("result")) {
+            return curr.get("result");
+        }
+        const result = fn(...args);
+        curr.set("result", result);
+        return result;
+    };
+}
 /**
  * let callCount = 0;
  * const memoizedFn = memoize(function (a, b) {
