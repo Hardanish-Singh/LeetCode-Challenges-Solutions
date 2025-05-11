c8d43ae97afaa644d2cc11f550549978515701c9
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-05-11 07:48:21 -0400

    Smallest Value of the Rearranged Number

---
+++ Javascript/Smallest_Value_of_the_Rearranged_Number.js
@@ -0,0 +1,27 @@
+// Leetcode: https://leetcode.com/problems/smallest-value-of-the-rearranged-number/
+
+/*
+ * @param { number } num
+ * @return { number }
+ */
+
+var smallestNumber = function (num) {
+    var n = num;
+    num = Array.from(num.toString());
+    if (n > 0) {
+        num = num.map(Number).sort((a, b) => a - b);
+        let index = -1;
+        if (num[0] === 0) {
+            index = num.lastIndexOf(0) + 1;
+            [num[0], num[index]] = [num[index], num[0]];
+        }
+        return num.join("");
+    } else if (n < 0) {
+        num = num
+            .slice(1)
+            .map(Number)
+            .sort((a, b) => b - a);
+        return -num.join("");
+    }
+    return n;
+};
