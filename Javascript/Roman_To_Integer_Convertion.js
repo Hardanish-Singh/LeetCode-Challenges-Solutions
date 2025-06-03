bf6134692e0e2eea85bd4efea9f4a3d8beee0b55
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-06-03 09:53:12 -0400

    Roman To Integer Convertion

--- Javascript/Roman_To_Integer_Convertion.js
+++ Javascript/Roman_To_Integer_Convertion.js
@@ -1,6 +1,10 @@
 // Leetcode: https://leetcode.com/problems/roman-to-integer/
 
-var romanToInt = function (string) {
+/**
+ * @param {s} string
+ * @return {number}
+ */
+var romanToInt = function (s) {
     let romanNumerals = {
         I: 1,
         V: 5,
@@ -10,19 +14,19 @@ var romanToInt = function (string) {
         D: 500,
         M: 1000,
     };
-    if (string.length === 1) return romanNumerals[string];
+    if (s.length === 1) return romanNumerals[s];
 
     let romansum = 0;
 
-    for (let i = 0; i < string.length; i++) {
-        if (i !== string.length - 1) {
-            if (romanNumerals[string[i]] >= romanNumerals[string[i + 1]]) romansum += romanNumerals[string[i]];
+    for (let i = 0; i < s.length; i++) {
+        if (i !== s.length - 1) {
+            if (romanNumerals[s[i]] >= romanNumerals[s[i + 1]]) romansum += romanNumerals[s[i]];
             else {
-                romansum += romanNumerals[string[i]] + romanNumerals[string[i + 1]] - 2 * romanNumerals[string[i]];
+                romansum += romanNumerals[s[i]] + romanNumerals[s[i + 1]] - 2 * romanNumerals[s[i]];
                 i++;
             }
-        } else if (i === string.length - 1 && romanNumerals[string[i]] <= romanNumerals[string[i - 1]])
-            romansum += romanNumerals[string[i]];
+        } else if (i === s.length - 1 && romanNumerals[s[i]] <= romanNumerals[s[i - 1]])
+            romansum += romanNumerals[s[i]];
     }
 
     return romansum;
