8aeb4431a53e1245cf0bd5c140d2d3756287d968
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-06-07 09:55:37 -0400

    Symmetric Tree

--- Tree/Easy/Symmetric_Tree.py
+++ Tree/Easy/Symmetric_Tree.py
@@ -14,18 +14,18 @@ class Solution:
         def isSymmetric(self, root: TreeNode) -> bool:
                 if root is None:
                         return False
-                return self.isTreeSymmetric( root.left, root.right )
+                return self.isTreeSymmetric(root.left, root.right)
         
-        def isTreeSymmetric( self, left, right ):
+        def isTreeSymmetric(self, left, right):
                 if left is None and right is None:
                         return True
                 if left is None and right is not None or right is None and left is not None:
                         return False
                 if left.val != right.val:
                         return False
-                
-                outterPair = self.isTreeSymmetric( left.left, right.right )
-                innerPair = self.isTreeSymmetric( left.right, right.left )
+
+                outterPair = self.isTreeSymmetric(left.left, right.right)
+                innerPair = self.isTreeSymmetric(left.right, right.left)
                 
                 if outterPair and innerPair:
                         return True
