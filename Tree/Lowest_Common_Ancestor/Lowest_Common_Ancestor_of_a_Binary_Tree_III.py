e184486b88611006e34b9baf12c9953bb59de7bb
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-06-09 14:47:11 -0400

    Lowest Common Ancestor of a Binary Tree III

--- Tree/Lowest_Common_Ancestor/Lowest_Common_Ancestor_of_a_Binary_Tree_III.py
+++ Tree/Lowest_Common_Ancestor/Lowest_Common_Ancestor_of_a_Binary_Tree_III.py
@@ -1,17 +1,15 @@
 # Leetcode: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/
 
-"""
-    # Definition for a Node.
-    class Node:
-        def __init__(self, val):
-            self.val = val
-            self.left = None
-            self.right = None
-            self.parent = None
-"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int) -> None:
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
 
 class Solution:
-    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+    def lowestCommonAncestor(self, p: Node, q: Node) -> Node:
         path = []
         while p:
             path.append(p)
