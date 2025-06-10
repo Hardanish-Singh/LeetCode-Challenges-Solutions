ce07de565f8f77a97bc202e7b4086aeb2391ae6e
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-06-10 11:27:15 -0400

    Lowest Common Ancestor of a Binary Tree IV

--- Tree/Lowest_Common_Ancestor/Lowest_Common_Ancestor_of_a_Binary_Tree_IV.py
+++ Tree/Lowest_Common_Ancestor/Lowest_Common_Ancestor_of_a_Binary_Tree_IV.py
@@ -1,13 +1,13 @@
 # Leetcode: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iv/
 
-"""
-        # Definition for a binary tree node.
-        class TreeNode:
-                def __init__( self, x ):
-                        self.val = x
-                        self.left = None
-                        self.right = None
-"""
+from typing import List
+
+# Definition for a binary tree node.
+class TreeNode:
+        def __init__(self, x: int ) -> None:
+                self.val = x
+                self.left = None
+                self.right = None
 
 class Solution:
         def findLowestCommonAncestor(self, root: TreeNode, s: List[TreeNode]) -> TreeNode:
@@ -16,13 +16,13 @@ class Solution:
                 if root.val in s:
                         return root
 
-                left = self.findLowestCommonAncestor( root.left, s )
-                right = self.findLowestCommonAncestor( root.right, s )
+                left = self.findLowestCommonAncestor(root.left, s)
+                right = self.findLowestCommonAncestor(root.right, s)
                 
                 if left and right:
                         return root
                 else:
                         return left or right
         def lowestCommonAncestor(self, root: TreeNode, nodes: List[TreeNode]) -> TreeNode:
-                s = [ node.val for node in nodes ]
+                s = [node.val for node in nodes]
                 return self.findLowestCommonAncestor(root, s)
