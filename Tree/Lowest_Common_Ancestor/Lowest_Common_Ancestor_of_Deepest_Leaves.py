3a570afc12f36ec79b3f62294affdce7dacdf312
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-06-11 11:53:36 -0400

    Lowest Common Ancestor of Deepest Leaves

--- Tree/Lowest_Common_Ancestor/Lowest_Common_Ancestor_of_Deepest_Leaves.py
+++ Tree/Lowest_Common_Ancestor/Lowest_Common_Ancestor_of_Deepest_Leaves.py
@@ -15,14 +15,14 @@ class TreeNode:
 """
 
 class Solution:
-    def lowestCommonAncestor( self, root: TreeNode, p: TreeNode, q: TreeNode ) -> TreeNode:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
         if root is None:
             return None
         if root == p or root == q:
             return root
 
-        left = self.lowestCommonAncestor( root.left, p, q )
-        right = self.lowestCommonAncestor( root.right, p, q )
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
         
         if left and right:
             return root
