56cca01a2e2b3308bfcbd6cf096ca90dc447a529
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-06-16 06:45:18 -0400

    Insert into Binary Search Tree

--- Tree/Medium/Insert_Into_Binary_Search_Tree.py
+++ Tree/Medium/Insert_Into_Binary_Search_Tree.py
@@ -1,11 +1,11 @@
 # Leetcode: https://leetcode.com/problems/insert-into-a-binary-search-tree/
 
 # Definition for a binary tree node.
-# class TreeNode( object ):
-#     def __init__( self, val = 0, left = None, right = None ):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+        def __init__(self, val = 0, left = None, right = None):
+                self.val = val
+                self.left = left
+                self.right = right
 
 """
         SOLUTION 1: RECURSIVE
