7f686acc54f978fddd2ea946958d17c2954e4946
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-06-17 10:23:45 -0400

    Maximum Width of a Binary Tree

--- Tree/Medium/Maximum_Width_of_a_Binary_Tree.py
+++ Tree/Medium/Maximum_Width_of_a_Binary_Tree.py
@@ -22,7 +22,6 @@ class Solution:
                                         temp.append([node.left, 2 * position])
                                 if node.right:
                                         temp.append([node.right, (2 * position) + 1])
-
                         if temp:
                                 width = max(width, ((temp[-1][1] - temp[0][1]) + 1))
                         queue = temp
