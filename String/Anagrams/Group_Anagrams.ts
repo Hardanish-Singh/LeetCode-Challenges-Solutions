9bbb1997d15ef528a033146699169c30c176c544
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-06-19 12:12:54 -0400

    Group Anagrams

--- String/Anagrams/Group_Anagrams.ts
+++ String/Anagrams/Group_Anagrams.ts
@@ -1,16 +1,18 @@
 // Leetcode: https://leetcode.com/problems/group-anagrams/
 
 type GroupedAnagrams = {
-        [key: string]: Array<string>;
+    [key: string]: Array<string>;
 };
 
 const groupAnagrams = (strs: Array<string>): Array<Array<string>> => {
-        let groupedAnagrams: GroupedAnagrams = {};
+    let groupedAnagrams: GroupedAnagrams = {};
 
-        for (let i: number = 0; i < strs.length; i++) {
-                let sortedAnagram: string = strs[i].split("").sort().join("");
-                sortedAnagram in groupedAnagrams ? groupedAnagrams[sortedAnagram].push(strs[i]) : groupedAnagrams[sortedAnagram] = [strs[i]];
-        }
+    for (let i: number = 0; i < strs.length; i++) {
+        let sortedAnagram: string = strs[i].split("").sort().join("");
+        sortedAnagram in groupedAnagrams
+            ? groupedAnagrams[sortedAnagram].push(strs[i])
+            : (groupedAnagrams[sortedAnagram] = [strs[i]]);
+    }
 
-        return Object.values(groupedAnagrams);
+    return Object.values(groupedAnagrams);
 };
