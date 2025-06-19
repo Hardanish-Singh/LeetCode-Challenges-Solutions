b33641859a0b15a7671482103ed3ce589f16590b
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-06-19 12:12:33 -0400

    Find Resultant Array After Removing Anagrams

--- String/Anagrams/Find_Resultant_Array_After_Removing_Anagrams.ts
+++ String/Anagrams/Find_Resultant_Array_After_Removing_Anagrams.ts
@@ -1,42 +1,42 @@
 // Leetcode: https://leetcode.com/problems/find-resultant-array-after-removing-anagrams/
 
 type Anagram = {
-        [key: string]: number;
+    [key: string]: number;
 };
-    
+
 const isAnagram = (s: string, t: string): boolean => {
-        if (s.length !== t.length) {
-            return false;
-        }
-        let dictionaryOne: Anagram = {};
-        let dictionaryTwo: Anagram = {};
-    
-        for (let i: number = 0; i < s.length; i++) {
-            dictionaryOne[s[i]] ? (dictionaryOne[s[i]] += 1) : (dictionaryOne[s[i]] = 1);
-            dictionaryTwo[t[i]] ? (dictionaryTwo[t[i]] += 1) : (dictionaryTwo[t[i]] = 1);
-        }
-    
-        for (const [key1, value1] of Object.entries(dictionaryOne)) {
-            for (const [key2, value2] of Object.entries(dictionaryTwo)) {
-                if( key1 === key2 && value1 === value2 ) {
-                    delete dictionaryTwo[key2];
-                    delete dictionaryOne[key1];
-                    break;
-                }
+    if (s.length !== t.length) {
+        return false;
+    }
+    let dictionaryOne: Anagram = {};
+    let dictionaryTwo: Anagram = {};
+
+    for (let i: number = 0; i < s.length; i++) {
+        dictionaryOne[s[i]] ? (dictionaryOne[s[i]] += 1) : (dictionaryOne[s[i]] = 1);
+        dictionaryTwo[t[i]] ? (dictionaryTwo[t[i]] += 1) : (dictionaryTwo[t[i]] = 1);
+    }
+
+    for (const [key1, value1] of Object.entries(dictionaryOne)) {
+        for (const [key2, value2] of Object.entries(dictionaryTwo)) {
+            if (key1 === key2 && value1 === value2) {
+                delete dictionaryTwo[key2];
+                delete dictionaryOne[key1];
+                break;
             }
         }
-    
-        return Object.keys(dictionaryOne).length === 0 && Object.keys(dictionaryTwo).length === 0;
+    }
+
+    return Object.keys(dictionaryOne).length === 0 && Object.keys(dictionaryTwo).length === 0;
 };
 
 const removeAnagrams = (words: Array<string>): Array<string> => {
-        for (let i: number = 1; i < words.length; i++) {
-                const firstWord: string = words[i - 1];
-                const secondWord: string = words[i];
-                if (isAnagram(firstWord, secondWord)) {
-                        words.splice(i, 1);
-                        i--;
-                }
+    for (let i: number = 1; i < words.length; i++) {
+        const firstWord: string = words[i - 1];
+        const secondWord: string = words[i];
+        if (isAnagram(firstWord, secondWord)) {
+            words.splice(i, 1);
+            i--;
         }
-        return words;
+    }
+    return words;
 };
