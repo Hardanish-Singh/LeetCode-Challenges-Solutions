ab7c0142f599cef92c064e9e0538c7a3001224c8
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-06-21 19:31:29 -0400

    Count Vowel Substrings of a String

--- String/Easy/Count_Vowel_Substrings_of_a_String.ts
+++ String/Easy/Count_Vowel_Substrings_of_a_String.ts
@@ -1,12 +1,12 @@
 // Leetcode: https://leetcode.com/problems/count-vowel-substrings-of-a-string/
 
 const countVowelSubstrings = (word: string): number => {
-    let vowels: Set<string> = new Set(['a', 'e', 'i', 'o', 'u']);
+    let vowels: Set<string> = new Set(["a", "e", "i", "o", "u"]);
     let count: number = 0;
 
-    for(let i: number = 0; i < word.length; i++) {
+    for (let i: number = 0; i < word.length; i++) {
         let set: Set<string> = new Set();
-        for(let j: number = i; j < word.length; j++) {
+        for (let j: number = i; j < word.length; j++) {
             let letter: string = word[j];
             if (!vowels.has(word[j])) {
                 break;
