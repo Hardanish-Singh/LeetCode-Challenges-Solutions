ab28c83cd83c0ea9a137846edff15355bdcbf6a0
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-06-21 19:31:59 -0400

    First Unique Character in a String

--- String/Easy/First_Unique_Character_in_a_String.js
+++ String/Easy/First_Unique_Character_in_a_String.js
@@ -7,28 +7,28 @@
 
 // SOLUTION 1
 var firstUniqChar = function (string) {
-        for (let i = 0; i < string.length; i++) {
-                let isUnique = true;
-                for (let b = 0; b < i; b++) {
-                        if (string[b] === string[i]) {
-                                isUnique = false;
-                                break;
-                        }
-                }
-                if (!isUnique) {
-                        continue;
-                }
-                for (let j = i + 1; j < string.length; j++) {
-                        if (string[i] === string[j]) {
-                                isUnique = false;
-                                break;
-                        }
-                }
-                if (isUnique) {
-                        return i;
-                }
+    for (let i = 0; i < string.length; i++) {
+        let isUnique = true;
+        for (let b = 0; b < i; b++) {
+            if (string[b] === string[i]) {
+                isUnique = false;
+                break;
+            }
         }
-        return -1;
+        if (!isUnique) {
+            continue;
+        }
+        for (let j = i + 1; j < string.length; j++) {
+            if (string[i] === string[j]) {
+                isUnique = false;
+                break;
+            }
+        }
+        if (isUnique) {
+            return i;
+        }
+    }
+    return -1;
 };
 
 /**
@@ -38,22 +38,22 @@ var firstUniqChar = function (string) {
 
 // SOLUTION 2
 var firstUniqChar = function (string) {
-        let duplicates = {};
-        for (let i = 0; i < string.length; i++) {
-                let isUnique = true;
-                if (string[i] in duplicates) {
-                        continue;
-                }
-                for (let j = i + 1; j < string.length; j++) {
-                        if (string[i] === string[j]) {
-                                duplicates[string[i]] = true;
-                                isUnique = false;
-                                break;
-                        }
-                }
-                if (isUnique) {
-                        return i;
-                }
+    let duplicates = {};
+    for (let i = 0; i < string.length; i++) {
+        let isUnique = true;
+        if (string[i] in duplicates) {
+            continue;
         }
-        return -1;
+        for (let j = i + 1; j < string.length; j++) {
+            if (string[i] === string[j]) {
+                duplicates[string[i]] = true;
+                isUnique = false;
+                break;
+            }
+        }
+        if (isUnique) {
+            return i;
+        }
+    }
+    return -1;
 };
