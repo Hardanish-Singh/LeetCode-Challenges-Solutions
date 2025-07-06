00c989ded1ca19ae695e93e37169351194854f7d
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-07-06 17:36:14 -0400

    Reverse Bits

--- String/Easy/Reverse_Bits.js
+++ String/Easy/Reverse_Bits.js
@@ -6,9 +6,9 @@
  */
 
 var reverseBits = function (n) {
-        let str = (n >>> 0).toString(2);
-        str = str.padStart(32, "0");
-        str = str.split("").reverse().join("");
-        str = parseInt(str, 2);
-        return str;
+    let str = (n >>> 0).toString(2);
+    str = str.padStart(32, "0");
+    str = str.split("").reverse().join("");
+    str = parseInt(str, 2);
+    return str;
 };
