5d72502ff5352afabc1cc7d99c99c44600b308a4
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-07-06 17:46:48 -0400

    String Matching In An Array

--- String/Easy/String_Matching_in_an_Array.js
+++ String/Easy/String_Matching_in_an_Array.js
@@ -6,15 +6,15 @@
  */
 
 var stringMatching = function (words) {
-        let rs = [];
-        for (let i = 0; i < words.length; i++) {
-                for (let j = i + 1; j < words.length; j++) {
-                        if (words[i].indexOf(words[j]) !== -1) {
-                                rs.push(words[j]);
-                        } else if (words[j].indexOf(words[i]) !== -1) {
-                                rs.push(words[i]);
-                        }
-                }
+    let rs = [];
+    for (let i = 0; i < words.length; i++) {
+        for (let j = i + 1; j < words.length; j++) {
+            if (words[i].indexOf(words[j]) !== -1) {
+                rs.push(words[j]);
+            } else if (words[j].indexOf(words[i]) !== -1) {
+                rs.push(words[i]);
+            }
         }
-        return Array.from(new Set(rs));
+    }
+    return Array.from(new Set(rs));
 };
