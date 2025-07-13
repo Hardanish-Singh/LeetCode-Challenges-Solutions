6533a8ff6738b87cd44cd163301d4e2fddfcb0d0
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-07-13 10:34:23 -0400

    Find Substring With Given Hash Value

---
+++ String/Hard/Find_Substring_With_Given_Hash_Value.js
@@ -0,0 +1,26 @@
+// Leetcode: https://leetcode.com/problems/find-substring-with-given-hash-value/
+
+/**
+ * @param {string} s
+ * @param {number} power
+ * @param {number} modulo
+ * @param {number} k
+ * @param {number} hashValue
+ * @return {string}
+ */
+var subStrHash = function (s, p, m, k, hashValue) {
+    p = BigInt(p);
+    m = BigInt(m);
+    hashValue = BigInt(hashValue);
+    let tmp = BigInt(0);
+    let res = 0;
+    let pk = BigInt(1);
+    let n = s.length;
+    for (let i = n - 1; i >= 0; i--) {
+        tmp = (tmp * p + BigInt(s[i].charCodeAt() - 96)) % m;
+        if (i + k >= n) pk = (pk * p) % m;
+        else tmp = (tmp - ((BigInt(s[i + k].charCodeAt() - 96) * pk) % m) + m) % m;
+        if (tmp === hashValue) res = i;
+    }
+    return s.slice(res, res + k);
+};
