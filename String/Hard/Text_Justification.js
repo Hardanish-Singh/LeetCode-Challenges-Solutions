c80c1f48df0529a1b874f9a3bbd02d9340e127d6
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-07-20 09:25:01 -0400

    Text Justification

--- String/Hard/Text_Justification.js
+++ String/Hard/Text_Justification.js
@@ -1,84 +1,145 @@
 // Leetcode: https://leetcode.com/problems/text-justification/
 
-function makeGreedyApproachSpaces(positions, c) {
-    let greedySpaces = new Array(positions).fill(0);
-    let I = 0;
-    while (c-- > 0) {
-        greedySpaces[I] = greedySpaces[I++] + 1;
-        I === positions ? (I = 0) : null;
+/**
+ * Distributes extra spaces evenly among gaps between words
+ * @param {number} gaps - Number of gaps between words
+ * @param {number} extraSpaces - Number of extra spaces to distribute
+ * @returns {number[]} Array containing number of spaces for each gap
+ */
+function distributeSpaces(gaps, extraSpaces) {
+    const spacesPerGap = new Array(gaps).fill(0);
+    let currentGap = 0;
+
+    while (extraSpaces > 0) {
+        spacesPerGap[currentGap]++;
+        currentGap = (currentGap + 1) % gaps;
+        extraSpaces--;
     }
-    return greedySpaces;
+
+    return spacesPerGap;
 }
 
-function makeGreedyString(initial, g, arr, str) {
-    let CC = 0;
-    let temp = "";
-    for (let k = initial; k < g; k++) {
-        temp += str[k];
-        if (arr[CC]) for (let p = 0; p < arr[CC]; p++) temp += " ";
-        CC++;
+/**
+ * Creates a justified line by adding appropriate spaces between words
+ * @param {string[]} words - Array of words
+ * @param {number} startIndex - Starting index in words array
+ * @param {number} endIndex - Ending index in words array
+ * @param {number[]} spacesArray - Array of spaces to add between words
+ * @returns {string} Justified line
+ */
+function createJustifiedLine(words, startIndex, endIndex, spacesArray) {
+    let line = "";
+    let spaceIndex = 0;
+
+    for (let i = startIndex; i < endIndex; i++) {
+        line += words[i];
+
+        // Add spaces after word (except for last word)
+        if (i < endIndex - 1) {
+            const spacesToAdd = spacesArray[spaceIndex] || 0;
+            line += " ".repeat(spacesToAdd + 1); // +1 for minimum space
+            spaceIndex++;
+        }
     }
-    return temp;
+
+    return line;
 }
 
-var fullJustify = function (str, len) {
-    let sc = (positions = word_count = initial = 0);
-    let temp = "";
-    let output = [];
-    for (let i = 0; i < str.length; i++) {
-        for (let j = 0; j < str[i].length; j++) {
-            word_count++;
-            if (word_count >= len) {
-                let g;
+/**
+ * Creates the last line (left-justified) with padding
+ * @param {string[]} words - Array of words
+ * @param {number} startIndex - Starting index in words array
+ * @param {number} maxWidth - Maximum width of the line
+ * @returns {string} Left-justified line with padding
+ */
+function createLastLine(words, startIndex, maxWidth) {
+    let line = "";
 
-                j < str[i].length - 1 ? (g = i--) : (g = i + 1);
+    // Join remaining words with single spaces
+    for (let i = startIndex; i < words.length; i++) {
+        if (i > startIndex) line += " ";
+        line += words[i];
+    }
 
-                for (let k = initial; k < g; k++) {
-                    sc += str[k].length;
-                    positions++;
-                }
+    // Pad with spaces to reach maxWidth
+    while (line.length < maxWidth) {
+        line += " ";
+    }
 
-                positions > 1 ? positions-- : null;
+    return line;
+}
 
-                let arr = makeGreedyApproachSpaces(positions, len - sc);
+/**
+ * Calculates total length of words in a range
+ * @param {string[]} words - Array of words
+ * @param {number} startIndex - Starting index
+ * @param {number} endIndex - Ending index
+ * @returns {number} Total character count
+ */
+function getTotalWordLength(words, startIndex, endIndex) {
+    let totalLength = 0;
+    for (let i = startIndex; i < endIndex; i++) {
+        totalLength += words[i].length;
+    }
+    return totalLength;
+}
 
-                let temp = makeGreedyString(initial, g, arr, str);
+/**
+ * Main function to justify text
+ * @param {string[]} words - Array of words to justify
+ * @param {number} maxWidth - Maximum width of each line
+ * @returns {string[]} Array of justified lines
+ */
+var fullJustify = function (words, maxWidth) {
+    const result = [];
+    let currentLineStart = 0;
 
-                initial = g;
-                sc = positions = 0;
-                output.push(temp);
-                temp = "";
-                word_count = -1;
+    while (currentLineStart < words.length) {
+        // Find words that fit in current line
+        let currentLineEnd = currentLineStart;
+        let currentLineLength = 0;
+
+        // Add words while they fit (including minimum spaces)
+        while (currentLineEnd < words.length) {
+            const wordLength = words[currentLineEnd].length;
+            const spacesNeeded = currentLineEnd > currentLineStart ? 1 : 0;
+
+            if (currentLineLength + spacesNeeded + wordLength > maxWidth) {
                 break;
             }
+
+            currentLineLength += spacesNeeded + wordLength;
+            currentLineEnd++;
         }
-        word_count++;
-        if (i === str.length - 1 && word_count > 0) {
-            sc = 0;
-            for (let k = initial; k <= str.length - 1; k++) {
-                if (k != str.length) temp += str[k] + " ";
-            }
-            sc = temp.length;
-            for (let k = 0; k < len - sc; k++) temp += " ";
-            output.push(temp);
-        } else if (word_count === len) {
-            for (let k = initial; k <= i; k++) {
-                sc += str[k].length;
-                positions++;
-            }
 
-            positions > 1 ? positions-- : null;
-
-            let arr = makeGreedyApproachSpaces(positions, len - sc);
-
-            let temp = makeGreedyString(initial, i + 1, arr, str);
-
-            initial = i + 1;
-            sc = positions = 0;
-            output.push(temp);
-            temp = "";
-            word_count = 0;
+        // Handle last line (left-justify)
+        if (currentLineEnd === words.length) {
+            result.push(createLastLine(words, currentLineStart, maxWidth));
+            break;
         }
+
+        // Handle single word in line
+        if (currentLineEnd - currentLineStart === 1) {
+            let line = words[currentLineStart];
+            while (line.length < maxWidth) {
+                line += " ";
+            }
+            result.push(line);
+        } else {
+            // Justify line with multiple words
+            const wordsInLine = currentLineEnd - currentLineStart;
+            const gaps = wordsInLine - 1;
+            const totalWordLength = getTotalWordLength(words, currentLineStart, currentLineEnd);
+            const totalSpaces = maxWidth - totalWordLength;
+            const extraSpaces = totalSpaces - gaps; // Subtract minimum spaces
+
+            const spacesDistribution = distributeSpaces(gaps, extraSpaces);
+            const justifiedLine = createJustifiedLine(words, currentLineStart, currentLineEnd, spacesDistribution);
+            result.push(justifiedLine);
+        }
+
+        currentLineStart = currentLineEnd;
     }
-    return output;
+
+    return result;
 };
