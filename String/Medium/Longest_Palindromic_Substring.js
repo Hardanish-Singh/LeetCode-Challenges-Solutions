68608ce1b007d9ef2c1fec5a181eca9c8d065210
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-07-21 10:59:56 -0400

    Longest Palindromic Substring

--- String/Medium/Longest_Palindromic_Substring.js
+++ String/Medium/Longest_Palindromic_Substring.js
@@ -1,19 +1,4 @@
-/*
-        Given a string s, return the longest palindromic substring in s.
-
-        Example 1:
-                Input: s = "babad"
-                Output: "bab"
-                Explanation: "aba" is also a valid answer.
-        
-        Example 2:
-                Input: s = "cbbd"
-                Output: "bb"
-
-        Constraints:
-                1) 1 <= s.length <= 1000
-                2) s consist of only digits and English letters.
-*/
+// Leetcode: https://leetcode.com/problems/longest-palindromic-substring/
 
 /*
  * @param { string } s
