0cb29c2bfe4fa301d080e5d507c48ced24834f1e
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-08-08 10:16:02 -0400

    Subtract Strings

--- String/Medium/Subtract_Strings.js
+++ String/Medium/Subtract_Strings.js
@@ -1,7 +1,7 @@
 /**
  * @param {string} num1
- * @param { string } num2
- * @return { string }
+ * @param {string} num2
+ * @return {string}
  */
 
 const subtract = (num1, num2) => (BigInt(num1) - BigInt(num2)).toString();
