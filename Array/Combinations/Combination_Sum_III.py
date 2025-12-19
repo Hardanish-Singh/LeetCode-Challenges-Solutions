a677f9bd9a2447ef616dca7263d1727608e487e1
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-19 10:19:20 -0500

    Combination Sum III

--- Array/Combinations/Combination_Sum_III.py
+++ Array/Combinations/Combination_Sum_III.py
@@ -1,5 +1,7 @@
 # Leetcode: https://leetcode.com/problems/combination-sum-iii
 
+from typing import List
+
 class Solution:
         def combinationSum3(self, k: int, n: int) -> List[List[int]]:
                 combinations = []
