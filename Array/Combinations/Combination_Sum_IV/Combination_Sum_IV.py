5d53a9719ee6bad0660f1e99ecd871243880b3ed
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-19 10:20:06 -0500

    Combination Sum IV

--- Array/Combinations/Combination_Sum_IV/Combination_Sum_IV.py
+++ Array/Combinations/Combination_Sum_IV/Combination_Sum_IV.py
@@ -1,5 +1,7 @@
 # Leetcode: https://leetcode.com/problems/combination-sum-iv
 
+from typing import List
+
 class Solution:
         def combinationSum4(self, nums: List[int], target: int) -> int:
                 nums.sort()
