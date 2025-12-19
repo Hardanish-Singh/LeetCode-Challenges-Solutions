aaea9d3381a8a8c33cf3f896be87324ef1ca399b
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-19 10:19:07 -0500

    Combination Sum I

--- Array/Combinations/Combination_Sum.py
+++ Array/Combinations/Combination_Sum.py
@@ -1,5 +1,7 @@
 # Leetcode: https://leetcode.com/problems/combination-sum/
 
+from typing import List
+
 class Solution(object):
         def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
                 candidates.sort()
