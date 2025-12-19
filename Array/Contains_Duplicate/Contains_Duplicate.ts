c6e8f0e81920d81d67950d80ba0d2e08023f756a
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-19 10:23:15 -0500

    Contains Duplicate I

--- Array/Contains_Duplicate/Contains_Duplicate.ts
+++ Array/Contains_Duplicate/Contains_Duplicate.ts
@@ -1,9 +1,11 @@
 // Leetcode: https://leetcode.com/problems/contains-duplicate/
 
+type CountOccurrences = {
+    [key: number]: boolean;
+};
+
 const containsDuplicate = (nums: Array<number>): boolean => {
-    let countOccurrences: {
-        [key: number]: boolean;
-    } = {};
+    let countOccurrences: CountOccurrences = {};
     for (const num of nums) {
         if (num in countOccurrences) {
             return true;
