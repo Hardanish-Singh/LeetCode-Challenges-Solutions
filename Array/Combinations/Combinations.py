ff27f8e6133e768dae78d4ec1a9be1bc7f3bbdf4
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-19 10:13:06 -0500

    Combinations I

--- Array/Combinations/Combinations.py
+++ Array/Combinations/Combinations.py
@@ -1,5 +1,7 @@
 # Leetcode: https://leetcode.com/problems/combinations/
 
+from typing import List
+
 class Solution:
         def combine(self, n: int, k: int) -> List[List[int]]:
                 stack, result = [], []
