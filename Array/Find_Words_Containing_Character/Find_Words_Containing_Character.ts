1105574fa0e3526e7b3b185320d0accdfe7e7f8c
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-23 09:14:14 -0500

    Find Words Containing Character

--- Array/Find_Words_Containing_Character/Find_Words_Containing_Character.ts
+++ Array/Find_Words_Containing_Character/Find_Words_Containing_Character.ts
@@ -1,4 +1,5 @@
 // Leetcode: https://leetcode.com/problems/find-words-containing-character/
+
 // Solution 1
 var findWordsContaining = (words: string[], x: string): number[] =>
     words.flatMap((word: string, index: number) => (word.includes(x) ? index : []));
