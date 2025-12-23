c48e23f3951e902ed18f396d4109c308930ae451
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-23 09:17:04 -0500

    First Missing Positive

--- Array/Hard/First_Missing_Positive.ts
+++ Array/Hard/First_Missing_Positive.ts
@@ -3,8 +3,6 @@
 const firstMissingPositive = (nums: number[]): number => {
     const set = new Set(nums);
     let i = 1;
-    while (set.has(i)) {
-        i++;
-    }
+    while (set.has(i)) i++;
     return i;
 };
