8ea46c2b3e6e9895dc72a39201181509f5b84b9a
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-23 09:15:42 -0500

    Four Sum II

--- Array/Four_Sum/Four_Sum_II.ts
+++ Array/Four_Sum/Four_Sum_II.ts
@@ -1,5 +1,6 @@
 // Leetcode: https://leetcode.com/problems/4sum-ii/
 
+// Solution 1: Using HashMap
 const fourSumCount = (nums1: number[], nums2: number[], nums3: number[], nums4: number[]): number => {
     let map = new Map<number, number>();
     let count = 0;
@@ -16,6 +17,25 @@ const fourSumCount = (nums1: number[], nums2: number[], nums3: number[], nums4: 
             let compliment = -(nums3[i] + nums4[j]);
             if (map.has(compliment)) {
                 count += map.get(compliment) as number;
+            }
+        }
+    }
+
+    return count;
+};
+
+// Solution 2: Using Brute Force
+const fourSumCountBruteForce = (nums1: number[], nums2: number[], nums3: number[], nums4: number[]): number => {
+    let count = 0;
+
+    for (let i = 0; i < nums1.length; i++) {
+        for (let j = 0; j < nums2.length; j++) {
+            for (let k = 0; k < nums3.length; k++) {
+                for (let l = 0; l < nums4.length; l++) {
+                    if (nums1[i] + nums2[j] + nums3[k] + nums4[l] === 0) {
+                        count++;
+                    }
+                }
             }
         }
     }
