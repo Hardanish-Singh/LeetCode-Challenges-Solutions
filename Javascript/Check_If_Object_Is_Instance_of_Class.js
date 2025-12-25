8958607772ce43e15cea672c757c0cc00a170e12
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-25 11:00:06 -0500

    Check If Object Is Instance of a Class

--- Javascript/Check_If_Object_Is_Instance_of_Class.js
+++ Javascript/Check_If_Object_Is_Instance_of_Class.js
@@ -43,3 +43,7 @@ var checkIfInstanceOf = function (obj, classFunction) {
  */
 
 // Solution 3
+var checkIfInstanceOf = function (obj, classFunction) {
+    if (obj == null || typeof classFunction !== "function") return false;
+    return Object(obj) instanceof classFunction;
+};
