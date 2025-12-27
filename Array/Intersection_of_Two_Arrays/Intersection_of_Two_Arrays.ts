f864e9b05fb12f66032b4e7e9dcbdf977120e96d
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-27 11:40:08 -0500

    Intersection of Two Arrays I

--- Array/Intersection_of_Two_Arrays/Intersection_of_Two_Arrays.ts
+++ Array/Intersection_of_Two_Arrays/Intersection_of_Two_Arrays.ts
@@ -1,17 +1,17 @@
 // Leetcode: https://leetcode.com/problems/intersection-of-two-arrays/
 
 const intersection = (nums1: Array<number>, nums2: Array<number>): Array<number> => {
-        nums1.sort((a, b) => a - b);
-        nums2.sort((a, b) => a - b);
-        let intersection_of_arrays: Array<number> = [];
-        for (let i: number = 0; i < nums1.length; i++) {
-                for (let j: number = 0; j < nums2.length; j++) {
-                        if (nums1[i] === nums2[j] && !intersection_of_arrays.includes(nums1[i])) {
-                                intersection_of_arrays.push(nums1[i]);
-                        } else if (nums1[i] < nums2[j]) {
-                                break;
-                        }
-                }
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
+    let intersection_of_arrays: Array<number> = [];
+    for (let i: number = 0; i < nums1.length; i++) {
+        for (let j: number = 0; j < nums2.length; j++) {
+            if (nums1[i] === nums2[j] && !intersection_of_arrays.includes(nums1[i])) {
+                intersection_of_arrays.push(nums1[i]);
+            } else if (nums1[i] < nums2[j]) {
+                break;
+            }
         }
-        return intersection_of_arrays;
+    }
+    return intersection_of_arrays;
 };
