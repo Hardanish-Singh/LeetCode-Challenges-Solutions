76a15123b6abc502418b617f1e4b1600b8a3ab43
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-30 08:41:41 -0500

    Intersection of Two Arrays I

--- Array/Intersection_of_Two_Arrays/Intersection_of_Two_Arrays.ts
+++ Array/Intersection_of_Two_Arrays/Intersection_of_Two_Arrays.ts
@@ -4,6 +4,7 @@ const intersection = (nums1: number[], nums2: number[]): number[] => {
     nums1.sort((a, b) => a - b);
     nums2.sort((a, b) => a - b);
     const intersection_of_arrays: number[] = [];
+
     for (let i: number = 0; i < nums1.length; i++) {
         for (let j: number = 0; j < nums2.length; j++) {
             if (nums1[i] === nums2[j] && !intersection_of_arrays.includes(nums1[i])) {
