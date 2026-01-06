4d6a4ba10850f39c9c03f4b34c203684278b8d3a
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-01-06 10:19:50 -0500

    Insert Intervals

--- Array/Intervals/Insert_Interval.ts
+++ Array/Intervals/Insert_Interval.ts
@@ -5,8 +5,9 @@
  * @return { number[][] }
  */
 
-var merge = function (intervals: number[][]): Array<Array<number>> {
+var merge = function (intervals: number[][]): number[][] {
     intervals.sort((a, b) => a[0] - b[0]);
+
     for (let i: number = 0; i < intervals.length - 1; i++) {
         let j: number = i + 1;
         if (intervals[j][0] >= intervals[i][0] && intervals[j][0] <= intervals[i][1]) {
@@ -15,6 +16,7 @@ var merge = function (intervals: number[][]): Array<Array<number>> {
             i--;
         }
     }
+
     return intervals;
 };
 
