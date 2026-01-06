ee721d1e0f3c3671528429858cd13b1c4eaa13b7
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-01-06 10:19:11 -0500

    Merge Intervals

--- Array/Intervals/Merge_Intervals.ts
+++ Array/Intervals/Merge_Intervals.ts
@@ -6,14 +6,14 @@
  */
 
 var merge = function (intervals: Array<Array<number>>): Array<Array<number>> {
-        intervals.sort((a, b) => a[0] - b[0]);
-        for (let i: number = 0; i < intervals.length - 1; i++) {
-                let j: number = i + 1;
-                if (intervals[j][0] >= intervals[i][0] && intervals[j][0] <= intervals[i][1]) {
-                        intervals[i] = [intervals[i][0], Math.max(intervals[i][1], intervals[j][1])];
-                        intervals.splice(j, 1);
-                        i--;
-                }
+    intervals.sort((a, b) => a[0] - b[0]);
+    for (let i: number = 0; i < intervals.length - 1; i++) {
+        let j: number = i + 1;
+        if (intervals[j][0] >= intervals[i][0] && intervals[j][0] <= intervals[i][1]) {
+            intervals[i] = [intervals[i][0], Math.max(intervals[i][1], intervals[j][1])];
+            intervals.splice(j, 1);
+            i--;
         }
-        return intervals;
+    }
+    return intervals;
 };
