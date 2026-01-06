abcee46d19274ff204f7d4d68e2fd087b23eb3a1
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-01-06 10:18:46 -0500

    Insert Intervals

--- Array/Intervals/Insert_Interval.ts
+++ Array/Intervals/Insert_Interval.ts
@@ -6,16 +6,16 @@
  */
 
 var merge = function (intervals: Array<Array<number>>): Array<Array<number>> {
-        intervals.sort((a, b) => a[0] - b[0]);
-        for (let i: number = 0; i < intervals.length - 1; i++) {
-                let j: number = i + 1;
-                if (intervals[j][0] >= intervals[i][0] && intervals[j][0] <= intervals[i][1]) {
-                        intervals[i] = [intervals[i][0], Math.max(intervals[i][1], intervals[j][1])];
-                        intervals.splice(j, 1);
-                        i--;
-                }
+    intervals.sort((a, b) => a[0] - b[0]);
+    for (let i: number = 0; i < intervals.length - 1; i++) {
+        let j: number = i + 1;
+        if (intervals[j][0] >= intervals[i][0] && intervals[j][0] <= intervals[i][1]) {
+            intervals[i] = [intervals[i][0], Math.max(intervals[i][1], intervals[j][1])];
+            intervals.splice(j, 1);
+            i--;
         }
-        return intervals;
+    }
+    return intervals;
 };
 
 /**
@@ -24,6 +24,6 @@ var merge = function (intervals: Array<Array<number>>): Array<Array<number>> {
  * @return { number[][] }
  */
 var insert = function (intervals: Array<Array<number>>, newInterval: Array<number>): Array<Array<number>> {
-        intervals = [...intervals, newInterval];
-        return merge(intervals);
+    intervals = [...intervals, newInterval];
+    return merge(intervals);
 };
