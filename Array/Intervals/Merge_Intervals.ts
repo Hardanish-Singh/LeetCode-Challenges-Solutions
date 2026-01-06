308d6482af3dac021d5310833f556716b69b4c0f
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-01-06 10:20:50 -0500

    Merge Intervals

--- Array/Intervals/Merge_Intervals.ts
+++ Array/Intervals/Merge_Intervals.ts
@@ -7,6 +7,7 @@
 
 var merge = function (intervals: number[][]): number[][] {
     intervals.sort((a, b) => a[0] - b[0]);
+
     for (let i: number = 0; i < intervals.length - 1; i++) {
         let j: number = i + 1;
         if (intervals[j][0] >= intervals[i][0] && intervals[j][0] <= intervals[i][1]) {
@@ -15,5 +16,6 @@ var merge = function (intervals: number[][]): number[][] {
             i--;
         }
     }
+
     return intervals;
 };
