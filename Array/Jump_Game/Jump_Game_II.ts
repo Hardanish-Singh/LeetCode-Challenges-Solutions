21f2abfdb4919601bc42441d293e759d6b6c140f
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-01-07 09:05:38 -0500

    Jump Game II

--- Array/Jump_Game/Jump_Game_II.ts
+++ Array/Jump_Game/Jump_Game_II.ts
@@ -4,6 +4,7 @@ const jump = (nums: number[]): number => {
     let reachable: number = 0;
     let curPosition: number = -1;
     let jumps: number = 0;
+
     for (let i: number = 0; reachable < nums.length - 1; i++) {
         if (i > curPosition) (curPosition = reachable), jumps++;
         reachable = Math.max(reachable, i + nums[i]);
