6dd2d60c242c0cab3f54f3f9979bfde72c6a9687
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-01-08 11:59:00 -0500

    Jump Game

--- Array/Jump_Game/Jump_Game.ts
+++ Array/Jump_Game/Jump_Game.ts
@@ -3,12 +3,14 @@
 // Solution 1
 var canJump = (nums: number[]): boolean => {
     let reachable: number = 0;
+
     for (let i: number = 0; i < nums.length; i++) {
         if (i > reachable) {
             return false;
         }
         reachable = Math.max(reachable, i + nums[i]);
     }
+
     return true;
 };
 
