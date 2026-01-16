d856c994973c505ce7d9ec15568059458d4f4685
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-01-16 09:41:14 -0500

    Confusing Number II

--- Math/Confusing_Number/Confusing_Number_II.js
+++ Math/Confusing_Number/Confusing_Number_II.js
@@ -1,38 +1,37 @@
 // Leetcode: https://leetcode.com/problems/confusing-number-ii/
 
 /**
- * @param { number } n
- * @return { boolean }
+ * @param {number} n
+ * @return {boolean}
  */
-
 var isConfusingNumber = function (n) {
-        var rotation = {
-                0: "0",
-                1: "1",
-                6: "9",
-                8: "8",
-                9: "6",
-        };
-        var invalids = {
-                2: true,
-                3: true,
-                4: true,
-                5: true,
-                7: true,
-        };
+    var rotation = {
+        0: "0",
+        1: "1",
+        6: "9",
+        8: "8",
+        9: "6",
+    };
+    var invalids = {
+        2: true,
+        3: true,
+        4: true,
+        5: true,
+        7: true,
+    };
 
-        var s1 = String(n);
-        var s2 = "";
+    var s1 = String(n);
+    var s2 = "";
 
-        for (let i = s1.length - 1; i >= 0; i--) {
-                if (invalids[s1[i]]) {
-                        return false;
-                } else {
-                        s2 += rotation[s1[i]];
-                }
+    for (let i = s1.length - 1; i >= 0; i--) {
+        if (invalids[s1[i]]) {
+            return false;
+        } else {
+            s2 += rotation[s1[i]];
         }
+    }
 
-        return s1 !== s2 ? true : false;
+    return s1 !== s2 ? true : false;
 };
 
 /**
@@ -41,33 +40,33 @@ var isConfusingNumber = function (n) {
  */
 
 var confusingNumberII = function (n) {
-        // SOLUTION 1 (TLE)
-        // var count = 0;
+    // SOLUTION 1 (TLE)
+    // var count = 0;
 
-        // for (let i = n; i >= 0; i--) {
-        //         if (isConfusingNumber(i)) {
-        //                 count++;
-        //         }
-        // }
+    // for (let i = n; i >= 0; i--) {
+    //         if (isConfusingNumber(i)) {
+    //                 count++;
+    //         }
+    // }
 
-        // return count;
+    // return count;
 
-        // SOLUTION 2
-        let canRotateNumbers = ["0", "1", "6", "8", "9"];
-        let stack = ["1", "6", "8", "9"];
-        let count = 0;
+    // SOLUTION 2
+    let canRotateNumbers = ["0", "1", "6", "8", "9"];
+    let stack = ["1", "6", "8", "9"];
+    let count = 0;
 
-        while (stack.length > 0) {
-                let number = stack.pop();
-                if (isConfusingNumber(number)) {
-                        count++;
-                }
-                for (const item of canRotateNumbers) {
-                        if (Number(number + item) <= n) {
-                                stack.push(number + item);
-                        }
-                }
+    while (stack.length > 0) {
+        let number = stack.pop();
+        if (isConfusingNumber(number)) {
+            count++;
         }
+        for (const item of canRotateNumbers) {
+            if (Number(number + item) <= n) {
+                stack.push(number + item);
+            }
+        }
+    }
 
-        return count;
+    return count;
 };
