7f3b1428d49321952c2ee25a04344bcc257f3c22
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-01-28 07:26:35 -0500

    Count Odd Numbers in an Interval Range

--- Programming_Skills/Math/Count_Odd_Numbers_in_an_Interval_Range.ts
+++ Programming_Skills/Math/Count_Odd_Numbers_in_an_Interval_Range.ts
@@ -2,10 +2,12 @@
 
 const countOdds = (low: number, high: number): number => {
     let count = 0;
+
     for (let i = low; i <= high; i++) {
         if (i % 2 !== 0) {
             count++;
         }
     }
+
     return count;
 };
