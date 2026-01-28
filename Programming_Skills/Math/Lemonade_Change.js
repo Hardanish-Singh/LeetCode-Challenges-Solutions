d6c99e6387e5687d020a1a7f375c5a354fe7416f
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-01-28 07:32:19 -0500

    Lemonade Change

--- Programming_Skills/Math/Lemonade_Change.js
+++ Programming_Skills/Math/Lemonade_Change.js
@@ -6,16 +6,26 @@
  */
 
 const lemonadeChange = (bills) => {
-    let fives = 0, tens = 0;
+    let fives = 0;
+    let tens = 0;
     for (const bill of bills) {
         if (bill === 5) fives++;
         else if (bill === 10) {
-            if (fives <= 0) return false;
-            else fives--, tens++;
+            if (fives <= 0) {
+                return false;
+            } else {
+                fives--;
+                tens++;
+            }
         } else if (bill === 20) {
-            if (fives > 0 && tens > 0) fives--, tens--;
-            else if (fives >= 3) fives -= 3;
-            else return false;
+            if (fives > 0 && tens > 0) {
+                fives--;
+                tens--;
+            } else if (fives >= 3) {
+                fives -= 3;
+            } else {
+                return false;
+            }
         }
     }
     return true;
