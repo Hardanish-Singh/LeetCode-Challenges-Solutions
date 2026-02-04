8d218ef36496856674821d04aabe864c29f44537
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-02-04 08:22:37 -0500

    String Compression II

--- String/String_Compression/String_Compression_II.js
+++ String/String_Compression/String_Compression_II.js
@@ -7,33 +7,33 @@
  */
 
 var getLengthOfOptimalCompression = function (s, k) {
-        const memo = new Map();
+    const memo = new Map();
 
-        const findMinimumLength = (i, lastChar, lastCharCount, k) => {
-                if (k < 0) {
-                        return Number.POSITIVE_INFINITY;
-                }
-                if (i >= s.length) {
-                        return 0;
-                }
+    const findMinimumLength = (i, lastChar, lastCharCount, k) => {
+        if (k < 0) {
+            return Number.POSITIVE_INFINITY;
+        }
+        if (i >= s.length) {
+            return 0;
+        }
 
-                const key = `${i},${lastChar},${lastCharCount},${k}`;
-                if (key in memo) {
-                        return memo[key];
-                }
+        const key = `${i},${lastChar},${lastCharCount},${k}`;
+        if (key in memo) {
+            return memo[key];
+        }
 
-                let count = 0;
-                if (s[i] === lastChar) {
-                        const incrementor = [1, 9, 99].includes(lastCharCount) ? 1 : 0;
-                        count = incrementor + findMinimumLength(i + 1, lastChar, lastCharCount + 1, k);
-                } else {
-                        count = Math.min(
-                                1 + findMinimumLength(i + 1, s[i], 1, k), //keep char
-                                findMinimumLength(i + 1, lastChar, lastCharCount, k - 1) //delete char
-                        );
-                }
-                memo[key] = count;
-                return count;
-        };
-        return findMinimumLength(0, "", 0, k);
+        let count = 0;
+        if (s[i] === lastChar) {
+            const incrementor = [1, 9, 99].includes(lastCharCount) ? 1 : 0;
+            count = incrementor + findMinimumLength(i + 1, lastChar, lastCharCount + 1, k);
+        } else {
+            count = Math.min(
+                1 + findMinimumLength(i + 1, s[i], 1, k), //keep char
+                findMinimumLength(i + 1, lastChar, lastCharCount, k - 1), //delete char
+            );
+        }
+        memo[key] = count;
+        return count;
+    };
+    return findMinimumLength(0, "", 0, k);
 };
