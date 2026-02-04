5b5c5712396a713fb8aa2f58bb13277137e4aeba
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-02-04 08:21:25 -0500

    String Compression I

--- String/String_Compression/String_Compression_I.ts
+++ String/String_Compression/String_Compression_I.ts
@@ -7,23 +7,23 @@
  */
 
 function compress(chars: Array<string>): number {
-        chars.push("π");
-        let i = 0;
+    chars.push("π");
+    let i = 0;
 
-        while (chars[i] != "π") {
-                let j = i;
-                let count = 1;
-                while (chars[j] == chars[j + 1]) {
-                        count++;
-                        j++;
-                }
-                chars.push(chars[i]);
-                count > 1 && chars.push(...String(count).split(""));
-                i = j + 1;
+    while (chars[i] != "π") {
+        let j = i;
+        let count = 1;
+        while (chars[j] == chars[j + 1]) {
+            count++;
+            j++;
         }
-        let index = chars.indexOf("π");
-        for (let i = index; i >= 0; i--) {
-                chars.splice(i, 1);
-        }
-        return chars.length;
+        chars.push(chars[i]);
+        count > 1 && chars.push(...String(count).split(""));
+        i = j + 1;
+    }
+    let index = chars.indexOf("π");
+    for (let i = index; i >= 0; i--) {
+        chars.splice(i, 1);
+    }
+    return chars.length;
 }
