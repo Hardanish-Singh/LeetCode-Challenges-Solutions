ce88c44ff64bdf0bfa3c36c3fa208d6a67583dc0
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-02-17 12:06:09 -0500

    Max Consecutive Ones I

--- Array/Max_Consecutive_Ones/Max_Consecutive_Ones.ts
+++ Array/Max_Consecutive_Ones/Max_Consecutive_Ones.ts
@@ -1,19 +1,19 @@
 // Leetcode: https://leetcode.com/problems/max-consecutive-ones/
 
-const findMaxConsecutiveOnes = (nums: Array<number>): number => {
-        var count = 0;
-        var maxConsecutiveOnes = 0;
+const findMaxConsecutiveOnes = (nums: number[]): number => {
+    var count = 0;
+    var maxConsecutiveOnes = 0;
 
-        for (let i = 0; i < nums.length; i++) {
-                if (nums[i]) {
-                        count++;
-                } else {
-                        maxConsecutiveOnes = Math.max(count, maxConsecutiveOnes);
-                        count = 0;
-                }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i]) {
+            count++;
+        } else {
+            maxConsecutiveOnes = Math.max(count, maxConsecutiveOnes);
+            count = 0;
         }
-        if (count > 0) {
-                maxConsecutiveOnes = Math.max(count, maxConsecutiveOnes);
-        }
-        return maxConsecutiveOnes;
+    }
+    if (count > 0) {
+        maxConsecutiveOnes = Math.max(count, maxConsecutiveOnes);
+    }
+    return maxConsecutiveOnes;
 };
