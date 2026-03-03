991123d9ee64f94759b15ceab0b93e22757176ae
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-03-03 13:51:22 -0500

    Meeting Rooms II

--- Array/Meeting_Rooms/Meeting_Rooms_II.ts
+++ Array/Meeting_Rooms/Meeting_Rooms_II.ts
@@ -1,24 +1,19 @@
 // Leetcode: https://leetcode.com/problems/meeting-rooms-ii/
 
-/**
- * @param { number[][] } meetings
- * @return { number }
- */
+function minMeetingRooms(meetings: number[][]): number {
+    let startTime: number[] = meetings.map((interval) => interval[0]).sort((a, b) => a - b);
+    let endTime: number[] = meetings.map((interval) => interval[1]).sort((a, b) => a - b);
+    let rooms = 0;
 
-function minMeetingRooms(meetings: Array<Array<number>>): number {
-        let startTime: Array<number> = meetings.map((interval) => interval[0]).sort((a, b) => a - b);
-        let endTime: Array<number> = meetings.map((interval) => interval[1]).sort((a, b) => a - b);
-        let rooms = 0;
-
-        let endTimeIndex: number = 0;
-        for (let startTimeIndex: number = 0; startTimeIndex < startTime.length; startTimeIndex++) {
-                rooms++;
-                // When the start is bigger than end, it means at this time one of the previous meeting ends, and it can take and reuse that room.
-                if (startTime[startTimeIndex] >= endTime[endTimeIndex]) {
-                        rooms--;
-                        endTimeIndex++;
-                }
+    let endTimeIndex: number = 0;
+    for (let startTimeIndex: number = 0; startTimeIndex < startTime.length; startTimeIndex++) {
+        rooms++;
+        // When the start is bigger than end, it means at this time one of the previous meeting ends, and it can take and reuse that room.
+        if (startTime[startTimeIndex] >= endTime[endTimeIndex]) {
+            rooms--;
+            endTimeIndex++;
         }
+    }
 
-        return rooms;
+    return rooms;
 }
