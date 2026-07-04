68d0a503af0f3e2355eab2e90f6b4dfd8224d555
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-07-04 11:44:56 -0400

    Merge Strings Alternatively

--- Programming_Skills/Basic_Implementation/Merge_Strings_Alternatively.js
+++ Programming_Skills/Basic_Implementation/Merge_Strings_Alternatively.js
@@ -8,9 +8,7 @@
 
 const mergeAlternately = (word1, word2) => {
     let result = "";
-    for (let i in word1) {
-        result += word1[i] + (word2[i] || "");
-    }
+    for (let i in word1) result += word1[i] + (word2[i] || "");
     result += word2.slice(word1.length);
     return result;
 };
