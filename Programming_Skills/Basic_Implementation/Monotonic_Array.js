aacc38ed2d6d07baeeb68e9f69249d0e9db90a72
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-07-04 11:45:46 -0400

    Monotonic Array

--- Programming_Skills/Basic_Implementation/Monotonic_Array.js
+++ Programming_Skills/Basic_Implementation/Monotonic_Array.js
@@ -4,11 +4,6 @@
  */
 
 const isMonotonic = (nums) => {
-    /* 
-        The problem can be solved by checking if the array is in increasing order or in decreasing order.
-        If array is either in increasing or decreasing order, then it is monotonic array
-        else it is NOT monotonic
-    */
     const isIncreasing = nums.every((num, index) => index === 0 || num >= nums[index - 1]);
     const isDecreasing = nums.every((num, index) => index === 0 || num <= nums[index - 1]);
     return isIncreasing || isDecreasing;
