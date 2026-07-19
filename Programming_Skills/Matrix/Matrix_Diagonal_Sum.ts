c2f7228ccdb9f5913d71c7617bd8cf2337c08142
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-07-19 11:33:22 -0400

    Matrix Diagonal Sum

--- Programming_Skills/Matrix/Matrix_Diagonal_Sum.ts
+++ Programming_Skills/Matrix/Matrix_Diagonal_Sum.ts
@@ -2,7 +2,6 @@
 
 const diagonalSum = (matrix: number[][]): number => {
     let total = 0;
-
     for (let row = 0; row < matrix.length; row++) {
         for (let column = 0; column < matrix[row].length; column++) {
             if (row === column || row + column === matrix.length - 1) {
