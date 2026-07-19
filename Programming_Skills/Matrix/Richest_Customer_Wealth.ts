02a0d9024bde4f4275b73027507ca7524f4eefc2
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-07-19 11:31:40 -0400

    Richest Customer Wealth

--- Programming_Skills/Matrix/Richest_Customer_Wealth.ts
+++ Programming_Skills/Matrix/Richest_Customer_Wealth.ts
@@ -2,11 +2,13 @@
 
 const maximumWealth = (accounts: number[][]): number => {
     let maxWealth = Number.NEGATIVE_INFINITY;
+
     for (let i = 0; i < accounts.length; i++) {
         let sum = accounts[i].reduce((a, b) => a + b, 0);
         if (sum > maxWealth) {
             maxWealth = sum;
         }
     }
+
     return maxWealth;
 };
