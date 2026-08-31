9370c17f4910d33061f2486c4a3f2ccdc0c9aea2
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-08-31 08:53:08 -0400

    Binary Tree Inorder Traversal

--- Programming_Skills/Binary_Tree/Binary_Tree_Inorder_Traversal.py
+++ Programming_Skills/Binary_Tree/Binary_Tree_Inorder_Traversal.py
@@ -1,13 +1,13 @@
 # Leetcode: https://leetcode.com/problems/binary-tree-inorder-traversal/
 
-"""
-        # Definition for a binary tree node.
-        class TreeNode:
-                def __init__(self, val = 0, left = None, right = None):
-                        self.val = val
-                        self.left = left
-                        self.right = right
-"""
+from typing import List
+
+ # Definition for a binary tree node.
+class TreeNode:
+        def __init__(self, val = 0, left = None, right = None):
+                self.val = val
+                self.left = left
+                self.right = right
 
 """
         SOLUTION 1: RECURSIVE
